e7e35a9350b3e852f0b07b4924dcd931b0acd029
psm9718 <psm9718@ajou.ac.kr>
2023-01-09 12:52:50 +0900

    feat: request url에서 접근 경로와 queryString 분리 ## how String의 split 메서드 활용하여 "?"split BASE_URL 필드 설정해주어 회원 가입 로직 후에는 다시 index.html로 redirect되도록 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,18 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.nio.Buffer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    private final String BASE_URL = "/index.html";
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -29,15 +29,19 @@ public class RequestHandler implements Runnable {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
             String line = br.readLine();
-            logger.debug(">> line : {}", line);
+            logger.debug("> input line : {}", line);
+            String url = getUrl(line);
+            logger.debug("> request url : {}", url);
 
-            //문자열 분리 후 -> 문자열 배열에 삽입
-            String[] tokens = line.split(" ");
-            String url = tokens[1];
-            if (url.equals("/")) {
-                url = "/index.html";
+            // url : /user/create?userId=psm9718&password=11123&name=qq&email=124%401
+            String[] queryStrings = url.split("\\?");
+            if (queryStrings.length != 1) {
+                logger.debug(">> {}", queryStrings[1]);
+                /**
+                 * 회원가입 로직
+                 */
+                url = BASE_URL;
             }
-            logger.debug("request url : {}", url);
 
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("src/main/resources/templates/" + url).toPath());
@@ -49,7 +53,17 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    private static void printHeaders(InputStream in) throws IOException {
+    private String getUrl(String line) {
+        //문자열 분리 후 -> 문자열 배열에 삽입
+        String[] tokens = line.split(" ");
+        String url = tokens[1];
+        if (url.equals("/")) {
+            url = BASE_URL;
+        }
+        return url;
+    }
+
+    private void printHeaders(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
         String line = br.readLine();
         logger.debug("request line : {}", line);
