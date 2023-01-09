973191d76192232c9a4657469a17f4b53c2caaa3
rentalhub <rentalhub@rentalhubui-MacBookPro.local>
2023-01-09 11:39:25 +0900

    절대경로를 이용한 static page 제공 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,6 +2,7 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -25,20 +26,23 @@ public class RequestHandler implements Runnable {
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader br = new BufferedReader(reader);
             String httpHeader = br.readLine();
-            String httpMethod = httpHeader.substring(0,httpHeader.indexOf(' '));
-            String reqURL = httpHeader.substring(httpHeader.indexOf('/')+1,httpHeader.indexOf(' ',httpHeader.indexOf('/')+1));
-            //reqURL = (reqURL==null)?"index.html":reqURL;  기본 url index.html으로 설정해주기 (추후 작업 예정)
+            String[] headers = httpHeader.split(" ");
+            String httpMethod = headers[0];
+            String reqURL = headers[1];
+            if (reqURL.equals("/"))
+                reqURL = "/index.html";
+            String httpVersion = headers[2];
             System.out.println("Http Method: "+httpMethod);
             System.out.println("Http reqURL: "+reqURL);
-            System.out.println("reqHeader: "+httpHeader);
+            System.out.println("Http Version: "+httpVersion);
             String strBr = "";
             while(!(strBr = br.readLine()).equals("")){
                 System.out.println(strBr);
             }
-
+            byte[] body = Files.readAllBytes(new File("/Users/rentalhub/HMG/be-java-web-server/src/main/resources/templates"+reqURL).toPath());
             // TODO 사용자 요청에 대한 처리는 이 곳까지 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            //byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
