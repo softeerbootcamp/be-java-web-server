8a8ece075dd4c161d24689b709381356c45a69d9
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-09 12:04:41 +0900

    RequestParser에 header 전체를 출력하는 메서드 추가

--- src/main/java/webserver/RequestParser.java
+++ src/main/java/webserver/RequestParser.java
@@ -10,19 +10,19 @@ public class RequestParser {
         String line = br.readLine();
         if(line == null) return null; // 헤더가 null일 경우 null을 응답
         String[] headLine = line.split(" "); // 헤더의 첫 번째 줄을 공백을 기준으로 자른다
-        //GET
-        ///index.html
-        //HTTP/1.1
         String url = headLine[1];// index.html
 
         return url;
     }
 
-    public static void printRequestHeader(){
-        // header 전체를 출력하기
-//            while(!line.equals(" ")){ // http 요청을 끝나는 지점은 null이 아니라 빈 공백 문자열임
-//                System.out.println("request: "+line);
-//                line = br.readLine();
-//            }
+    public static void printRequestHeader(InputStream in) throws IOException {
+         //header 전체를 출력하기
+        BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+        String line = br.readLine();
+        if(line == null) return ;
+        while(!line.equals(" ")){ // http 요청을 끝나는 지점은 null이 아니라 빈 공백 문자열임
+            System.out.println("request: "+line);
+            line = br.readLine();
+        }
     }
 }
