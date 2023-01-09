b73c4f59d423d7aa56d89515beaf44f76e3c0059
jieonkim23 <kwldjs1023@gmail.com>
2023-01-09 22:14:34 +0900

    HttpRequestParser의 오류 수정:  path.spilt(".")는 이스케이프 문자를 사용하지 않으면 오류가 나기때문에 이스케이프 추가.

--- src/main/java/util/HttpRequestParser.java
+++ src/main/java/util/HttpRequestParser.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 public class HttpRequestParser {
     private static final String dir = "./src/main/resources/";
     public static Path mappingPath(String path) {
-        String[] temp = path.split(".");
+        String[] temp = path.split("//.");
         String extension = temp[temp.length - 1];
 
         String targetDir = extension.equals("html") ? "templates" : "static";
