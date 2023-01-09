7c7b1903fc8a47f9589a4dbc2289b503d0af4bd7
i5046821854 <o5046821854@gmail.com>
2023-01-09 18:28:03 +0900

    feat : 파일의 형식을 파싱하여 해당 파일의 절대 경로를 찾아내는 로직을 utils의 메소드로 구현

--- src/main/java/webserver/utils/FilePathParser.java
+++ src/main/java/webserver/utils/FilePathParser.java
@@ -1,8 +1,13 @@
 package webserver.utils;
 
+import webserver.exception.NoFileException;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Map;
+
+import static webserver.utils.RequestUtil.parseRequestedLine;
 
 public class FilePathParser {
 
@@ -12,6 +17,9 @@ public class FilePathParser {
     public static byte[] getStaticFilesPath(String fileName) throws IOException {
         String fileUrl = FilePathParser.class.getResource(pathStatic)  + fileName;
         fileUrl = fileUrl.substring(5);
+        File file = new File(fileUrl);
+        if(!file.isFile())
+            throw new NoFileException();
         byte[] body = Files.readAllBytes(new File(fileUrl).toPath() );
         return body;
     }
@@ -19,7 +27,24 @@ public class FilePathParser {
     public static byte[] getTemplateFilesPath(String fileName) throws IOException {
         String fileUrl = FilePathParser.class.getResource(pathTemplates)  + fileName;
         fileUrl = fileUrl.substring(5);
+        File file = new File(fileUrl);
+        if(!file.isFile())
+            throw new NoFileException();
         byte[] body = Files.readAllBytes(new File(fileUrl).toPath() );
         return body;
+    }
+
+    public static byte[] staticFileResolver(String fileName) throws IOException, NoFileException {
+        String[] type = fileName.split("\\.");
+        String typeName = type[type.length - 1];
+        if(typeName.equals("html") || typeName.equals("ico"))
+            return getTemplateFilesPath(fileName);
+        else
+            return getStaticFilesPath(fileName);
+    }
+
+    public static String findFilePath(Map<String, String> map){
+        String requestLine = map.get("Header: ");
+        return parseRequestedLine(requestLine)[1];
     }
 }
