6efca0179181438caf770278aee20f0188d5bfd4
rohsik2 <rohsik2@gmail.com>
2023-01-09 14:18:20 +0900

    feat(RequestService): request Mapping 시 함수를 객체 형태로 쓰기위한 인터페이스

--- src/main/java/webserver/RequestService.java
+++ src/main/java/webserver/RequestService.java
@@ -1,2 +1,9 @@
-package webserver;public interface RequestService {
+package webserver;
+
+import webserver.httpMock.CustomHttpRequest;
+import webserver.httpMock.CustomHttpResponse;
+
+public interface RequestService {
+    public void doSomething(CustomHttpRequest req, CustomHttpResponse res);
+
 }
