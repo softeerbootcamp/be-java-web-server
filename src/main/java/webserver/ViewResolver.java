dff37504feaadb89fcc7d1771516c05c2630d22c
Soomin-Lim <augustth511@gmail.com>
2023-01-09 18:32:49 +0900

    refactor: 상수명 변경
    - 주석으로 TODO 추가

--- src/main/java/webserver/ViewResolver.java
+++ src/main/java/webserver/ViewResolver.java
@@ -3,13 +3,14 @@ package webserver;
 public class ViewResolver {
 
     private static final String STATIC_RESOURCE_PATH = "src/main/resources/static";
-    private static final String TEMPLATE_RESOUCE_PATH = "src/main/resources/templates";
+    private static final String TEMPLATE_RESOURCE_PATH = "src/main/resources/templates";
 
     public static String process(String viewName) {
+        // TODO
         if (viewName.contains("css") || viewName.contains("js")) {
             return STATIC_RESOURCE_PATH + viewName;
         }
 
-        return TEMPLATE_RESOUCE_PATH + viewName;
+        return TEMPLATE_RESOURCE_PATH + viewName;
     }
 }
