f72026fb51b8e3b06623007e3ca41e9e97c611c9
rohsik2 <rohsik2@gmail.com>
2023-01-09 14:06:01 +0900

    feat(CustomHttpResponse): setter, getter for making response

--- src/main/java/webserver/httpMock/CustomHttpResponse.java
+++ src/main/java/webserver/httpMock/CustomHttpResponse.java
@@ -1,6 +1,7 @@
-package webserver;
+package webserver.httpMock;
 
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomHttpResponse {
@@ -11,8 +12,26 @@ public class CustomHttpResponse {
 
     public CustomHttpResponse(OutputStream out){
         this.out = out;
+        this.body = new ArrayList<>();
     }
 
+    public OutputStream getOutputStream(){
+        return this.out;
+    }
 
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
 
+    public Integer getStatusCode(){
+        return statusCode;
+    }
+
+    public void addToBody(Byte[] datas){
+        body.addAll(List.of(datas));
+    }
+
+    public List<Byte> getBody(){
+        return body;
+    }
 }
