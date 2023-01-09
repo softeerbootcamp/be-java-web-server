cf61aaed123d4c008c2d9f095d0e4a739c80ee70
rentalhub-mac78 <kksy2423@naver.com>
2023-01-09 17:03:00 +0900

    Feat: RequestReader 인터페이스 생성 및 Get 메서드에 따른 RequestGetReader 생성
    HttpMethod를 활용하여 어떠한 HttpRequestReader를 사용할지 선택하기 위해 interface 추가
    추후 Get전용 RequestReader, Post전용 RequestReader등을 생성 예정

---
+++ src/test/java/util/reader/RequestReaderTest.java
@@ -0,0 +1,35 @@
+package util.reader;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import request.HttpMethod;
+import reader.RequestGetReader;
+import reader.RequestReader;
+import request.HttpRequest;
+import util.error.HttpsErrorMessage;
+
+import java.net.ProtocolException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RequestReaderTest {
+
+
+
+    @Test
+    @DisplayName("HttpMethod enum으로 method에 맞는 Reader가져오기(GET)")
+    void selectRequestReaderByMethod() throws ProtocolException {
+        RequestReader requestReader = RequestReader.selectRequestReaderByMethod(HttpMethod.GET);
+        assertThat(requestReader).isInstanceOf(RequestGetReader.class);
+    }
+
+    @Test
+    @DisplayName("HttpMethod enum으로 method에 맞는 Reader가져오기(NOT_MATCH)")
+    void selectRequestReaderByMethodError() throws ProtocolException {
+        ProtocolException protocolException = assertThrows(ProtocolException.class, () -> RequestReader.selectRequestReaderByMethod(HttpMethod.NOT_MATCH));
+        assertThat(protocolException.getMessage()).isEqualTo(HttpsErrorMessage.NOT_VALID_HTTP_FORMAT);
+    }
+}
