f76a3e67eb7e80e4526f853b1f4bb16dac710e35
rentalhub-mac78 <kksy2423@naver.com>
2023-01-09 17:08:55 +0900

    Rename: 테스트파일 소스코드의 구조와 맞게 디렉터리 변경

--- src/test/java/reader/RequestReaderTest.java
+++ src/test/java/reader/RequestReaderTest.java
@@ -1,4 +1,4 @@
-package util.reader;
+package reader;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
--- src/test/java/request/HttpMethodTest.java
+++ src/test/java/request/HttpMethodTest.java
@@ -1,4 +1,4 @@
-package util.reader.request;
+package request;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
