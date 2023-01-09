785a0fff12eb51e801d82d9b615e12736d97bf7b
rohsik2 <rohsik2@gmail.com>
2023-01-09 17:18:22 +0900

    feat(ContentType): 콘텐트 타입 enum 추가 및 파일 확장자에 따른 enum 선택 지원

--- src/main/java/webserver/httpMock/constants/ContentType.java
+++ src/main/java/webserver/httpMock/constants/ContentType.java
@@ -1,12 +1,30 @@
 package webserver.httpMock.constants;
 
+import java.util.List;
+
 public enum ContentType {
-    TEXT_HTML("text/html");
+    IMAGE_ICON("image/x-icon", List.of("ico")),
+    TEXT_HTML("text/html", List.of("html")),
+    TEXT_CSS("text/css", List.of("css")),
+    TEXT_JAVASCRIPT("text/javascript", List.of("js")),
+    TEXT_PLAIN("text/plain", List.of("txt")),
+    TEXT_XML("text/xml", List.of("xml"));
 
     private final String contentType;
+    private final List<String> fileTypes;
 
-    private ContentType(String contentType) {
+    public static ContentType getContentTypeByFileType(String fileType){
+        System.out.println(fileType);
+        for(ContentType content : ContentType.values()){
+            if(content.fileTypes.contains(fileType))
+                return content;
+        }
+        return TEXT_PLAIN;
+    }
+
+    ContentType(String contentType, List<String> fileType) {
         this.contentType = contentType;
+        this.fileTypes = fileType;
     }
 
     public String getContentType() {
