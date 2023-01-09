9875113c90abf01bd2c289cc9d913b3b679c96a6
pjs0418 <tony180322@gmail.com>
2023-01-09 11:23:54 +0900

    URL parsing하는 부분 따로 Util 객체로 분리하여 구현

--- src/main/java/util/HttpRequestUtil.java
+++ src/main/java/util/HttpRequestUtil.java
@@ -1,2 +1,8 @@
-package util;public class HttpRequestUtil {
+package util;
+
+public class HttpRequestUtil {
+    public static String getUrl(String line) {
+        String[] lineSplit = line.split(" ");
+        return lineSplit[1];
+    }
 }
