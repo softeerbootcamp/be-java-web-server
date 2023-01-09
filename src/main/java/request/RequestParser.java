522f3a2e5c9202a756cdb8edac18b8c1e10a1b0f
201724554 <pay7845@naver.com>
2023-01-09 18:23:12 +0900

    Interface에서 static으로 변경

--- src/main/java/request/RequestParser.java
+++ src/main/java/request/RequestParser.java
@@ -7,14 +7,9 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RequestParserImpl implements RequestParser {
-    private final Map<String, String> parsedHttpHeader;
-
-    public RequestParserImpl() {
-        parsedHttpHeader = new HashMap<>();
-    }
-
-    private Map<String, String> parse(InputStream in) {
+public class RequestParser{
+    public static String REQUEST_LINE = "Request-line";
+    private static Map<String, String> parse(InputStream in) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
         Map<String, String> requestMap = new HashMap<>();
         try {
@@ -28,7 +23,7 @@ public class RequestParserImpl implements RequestParser {
         return requestMap;
     }
 
-    private void split(String line, Map<String, String> map) {
+    private static void split(String line, Map<String, String> map) {
         int firstIndexOfDelim = line.indexOf(":");
         if(firstIndexOfDelim == -1 && line.length() >= 6) {
             map.put(REQUEST_LINE, line);
@@ -38,18 +33,22 @@ public class RequestParserImpl implements RequestParser {
         }
     }
 
-    public String getMethod() {
-        String[] token = parsedHttpHeader.get(REQUEST_LINE).split(" ");
+    public static String getMethod(InputStream in) {
+        String[] token = parse(in).get(REQUEST_LINE).split(" ");
         return token[0];
     }
 
-    public String getResource() {
-        String[] token = parsedHttpHeader.get(REQUEST_LINE).split(" ");
+    public static String getResource(InputStream in) {
+        String[] token = parse(in).get(REQUEST_LINE).split(" ");
         return token[1];
     }
 
-    public String getHTTPVersion() {
-        String[] token = parsedHttpHeader.get(REQUEST_LINE).split(" ");
+    public static String getHTTPVersion(InputStream in) {
+        String[] token = parse(in).get(REQUEST_LINE).split(" ");
         return token[2];
+    }
+
+    public static Map<String, String> getAllHeaders(InputStream in) {
+        return parse(in);
     }
 }
