06b3060b9988531dd351aa92c8d4d90718333bbe
Toa <ld980105@snu.ac.kr>
2023-01-10 01:40:22 +0900

    feat : 들어온 문자열과 일치하는 HttpMethod를 반환해주는 static method 추가

--- src/main/java/utils/HttpMethod.java
+++ src/main/java/utils/HttpMethod.java
@@ -17,6 +17,13 @@ public enum HttpMethod {
         this.hasBody = hasBody;
     }
 
+    public static HttpMethod getHttpMethod(String method) {
+        for (HttpMethod mt : HttpMethod.values()){
+            if (mt.name().equals(method))
+                return mt;
+        }
+        throw new IllegalArgumentException("존재하지 않는 메서드입니다.");
+    }
     public boolean hasBody(){
         return hasBody;
     }
