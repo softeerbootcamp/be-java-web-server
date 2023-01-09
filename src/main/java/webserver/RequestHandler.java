537626f3ba6d7bb5bacde6a7404ca89ac7d69f70
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-09 12:01:38 +0900

    RequestHandler 리팩토링
    RequestParser와 ResponseWriter 클래스로 분리하여 리팩토링함

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -22,46 +22,10 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            //브라우저에서 서버로 들어오는 모든 요청은 Inputstream 안에 담겨져 있음
-            //inputstream은 bufferedreader를 통해 line by line으로 읽을 수 있음
-            BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
-            String line = br.readLine();
-            if(line == null) return; // 헤더가 null일 경우 응답하지 않음
-            String[] headLine = line.split(" "); // 헤더의 첫 번째 줄을 공백을 기준으로 자른다
-            //GET
-            ///index.html
-            //HTTP/1.1
-            String url = headLine[1];// index.html
-            // header 전체를 출력하기
-//            while(!line.equals(" ")){ // http 요청을 끝나는 지점은 null이 아니라 빈 공백 문자열임
-//                System.out.println("request: "+line);
-//                line = br.readLine();
-//            }
-            //outputstream은 서버에서 브라우저로 보내는 응답
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("src/main/resources/templates/" + url).toPath());// 현재 사용자에게 넘겨지는 응답
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            //브라우저에서 서버로 들어오는 모든 요청은 Inputstream 안에 담겨져 있음, outputstream은 서버에서 브라우저로 보내는 응답
+            String url = RequestParser.parseRequestHeaderTarget(in);
+            ResponseWriter rw = new ResponseWriter(out);
+            rw.write(url);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
