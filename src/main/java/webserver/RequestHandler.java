48e0ade50171db2014ef22c2282978ea5830364d
rentalhub-mac78 <kksy2423@naver.com>
2023-01-09 17:07:46 +0900

    Feat: 현재 Request의 url을 파싱해 해당 정적 html파일 response로 보냄

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,14 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import request.HttpMethod;
+import reader.RequestReader;
+import request.HttpRequest;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,15 +24,22 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            HttpRequest httpRequest = HttpRequest.getHttpRequest(in);
+            HttpMethod httpMethod = HttpMethod.findMethod(httpRequest);
+
+            RequestReader requestReader = RequestReader.selectRequestReaderByMethod(httpMethod);
+            String url = requestReader.findPathInRequest(httpRequest);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./src/main/resources/templates" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
+
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
