9dda0ea992a4ae6a98dd07da91c2c322025cbdd9
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-09 12:52:52 +0900

    step1 요구사항 구현 인풋스트림으로 받은 메시지 중 경로명을 절대경로명으로 바꿔주는 getUrl 메서드 작성 변환한 절대경로명을 바탕으로 원하는 파일을 브라우저에 띄우도록 run 메서드 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,10 +23,13 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
+
+            String url = getUrl(in);
+
+            byte[] bytes = Files.readAllBytes(new File("./src/main/resources/templates" + url).toPath());
+            response200Header(dos, bytes.length);
+            responseBody(dos, bytes);
+        }catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
@@ -48,8 +49,15 @@ public class RequestHandler implements Runnable {
         try {
             dos.write(body, 0, body.length);
             dos.flush();
-        } catch (IOException e) {
+        }catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String getUrl(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        String firstLine = br.readLine();
+        String[] splitedFirstLine = firstLine.split(" ");
+        return splitedFirstLine[1];
     }
 }
