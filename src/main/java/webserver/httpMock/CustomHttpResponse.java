8640186bbfa7b817db1fa2e777c0fb58f4332576
rohsik2 <rohsik2@gmail.com>
2023-01-09 16:38:36 +0900

    fix(CustomHttpResponse): string, int 형태로 가지고있던 contentType, statusCode를 enum을 가지도록 변경

--- src/main/java/webserver/httpMock/CustomHttpResponse.java
+++ src/main/java/webserver/httpMock/CustomHttpResponse.java
@@ -1,29 +1,40 @@
 package webserver.httpMock;
 
+import webserver.httpMock.constants.ContentType;
+import webserver.httpMock.constants.StatusCode;
+
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CustomHttpResponse {
 
-    OutputStream out;
-    Integer statusCode;
-    List<Byte> body;
+    private OutputStream out;
+    private StatusCode statusCode;
+    private ContentType contentType;
+    private List<Byte> body;
 
     public CustomHttpResponse(OutputStream out){
         this.out = out;
         this.body = new ArrayList<>();
     }
 
+    public void setString(String contentType){
+
+    }
+
+    public ContentType getContentType(){
+        return contentType;
+    }
     public OutputStream getOutputStream(){
         return this.out;
     }
 
-    public void setStatusCode(Integer statusCode) {
+    public void setStatusCode(StatusCode statusCode) {
         this.statusCode = statusCode;
     }
 
-    public Integer getStatusCode(){
+    public StatusCode getStatusCode(){
         return statusCode;
     }
 
