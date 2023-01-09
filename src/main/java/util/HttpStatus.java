493485e9ba76885236d46647c50ffc99225f6601
jhchoi57 <enough1125@gmail.com>
2023-01-09 18:04:30 +0900

    day6 마무리

---
+++ src/main/java/util/HttpStatus.java
@@ -0,0 +1,12 @@
+package util;
+
+public enum HttpStatus {
+    OK(200, "OK");
+
+    private int statusCode;
+    private String status;
+    HttpStatus(int statusCode, String status){
+        this.statusCode = statusCode;
+        this.status = status;
+    }
+}
