4ee28b7e0f49da544ac9eb98d2c2002593269fc9
i5046821854 <o5046821854@gmail.com>
2023-01-09 15:05:48 +0900

    faat : http response를 만들어주는 전용 클래스를 생성

---
+++ src/main/java/webserver/utils/ResponseMaker.java
@@ -0,0 +1,19 @@
+package webserver.utils;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class ResponseMaker {
+
+    public static void response200Header(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+    }
+
+    public static void responseBody(DataOutputStream dos, byte[] body) throws IOException {
+        dos.write(body, 0, body.length);
+        dos.flush();
+    }
+}
