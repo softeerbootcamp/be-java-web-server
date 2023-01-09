50283ca0022de854a357b283cd3fb23b2ba35523
pjs0418 <tony180322@gmail.com>
2023-01-09 15:35:53 +0900

    불필요한 util class 삭제

--- src/main/java/util/HttpRequestUtil.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class HttpRequestUtil {
-    public static String getUrl(String line) {
-        String[] lineSplit = line.split(" ");
-        return lineSplit[1];
-    }
-}
