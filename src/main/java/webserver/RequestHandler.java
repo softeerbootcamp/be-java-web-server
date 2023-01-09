ebfda814b4eb6e46d65fd30b7c5f1b9a7ea6bd20
ajongs <wonjong1030@naver.com>
2023-01-09 11:32:29 +0900

    fix(RequestHandler): body가 index.html file을 읽어와 byte[]로 변환한 값을 가지도록 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +23,7 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./src/main/resources/templates/index.html" ).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
