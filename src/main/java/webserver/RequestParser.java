7b52b4f3ce7c1234741693e526a458af37a92457
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-09 11:47:21 +0900

    request header를 파싱하여 target file만 받아오는 parseRequestHeaderTarget 구현
    BufferedReader를 통해 inputstream을 읽어오고, split을 통해 파싱함

---
+++ src/main/java/webserver/RequestParser.java
@@ -0,0 +1,28 @@
+package webserver;
+
+import java.io.*;
+
+public class RequestParser {
+
+    public static String parseRequestHeaderTarget(InputStream in) throws IOException {
+        //inputstream은 bufferedreader를 통해 line by line으로 읽을 수 있음
+        BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+        String line = br.readLine();
+        if(line == null) return null; // 헤더가 null일 경우 null을 응답
+        String[] headLine = line.split(" "); // 헤더의 첫 번째 줄을 공백을 기준으로 자른다
+        //GET
+        ///index.html
+        //HTTP/1.1
+        String url = headLine[1];// index.html
+
+        return url;
+    }
+
+    public static void printRequestHeader(){
+        // header 전체를 출력하기
+//            while(!line.equals(" ")){ // http 요청을 끝나는 지점은 null이 아니라 빈 공백 문자열임
+//                System.out.println("request: "+line);
+//                line = br.readLine();
+//            }
+    }
+}
