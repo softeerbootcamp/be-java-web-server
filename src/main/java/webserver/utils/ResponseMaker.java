f6405de693105fc648bd1c152bc9ce3e12fbf3cb
i5046821854 <o5046821854@gmail.com>
2023-01-09 15:14:55 +0900

    faat : RequestMaker에 404에러 메시지를 만들어주는 메소드 생성

--- src/main/java/webserver/utils/ResponseMaker.java
+++ src/main/java/webserver/utils/ResponseMaker.java
@@ -12,6 +12,13 @@ public class ResponseMaker {
             dos.writeBytes("\r\n");
     }
 
+    public static void response404Header(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
+        dos.writeBytes("HTTP/1.1 404 NOT FOUND \r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
+    }
+
     public static void responseBody(DataOutputStream dos, byte[] body) throws IOException {
         dos.write(body, 0, body.length);
         dos.flush();
