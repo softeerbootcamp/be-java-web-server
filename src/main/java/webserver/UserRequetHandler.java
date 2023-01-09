a393af0962b56fa81725dc86e2355973907b8780
Soomin-Lim <augustth511@gmail.com>
2023-01-09 12:39:35 +0900

    feat: 요청 URL에서 사용자의 가입 정보를 담은 쿼리스트링을 추출하는 기능 구현

--- src/main/java/webserver/UserRequetHandler.java
+++ src/main/java/webserver/UserRequetHandler.java
@@ -7,7 +7,12 @@ public class UserRequetHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(UserRequetHandler.class);
 
-    public void createUser(String line) {
+    public void createUser(String url) {
+        String[] splited = url.split("\\?");
+        String userInfo = splited[1];
+
+        logger.debug("userInfo : {}" , userInfo);
+
 
     }
 }
