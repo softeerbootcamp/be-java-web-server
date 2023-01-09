6bb8804e1af04c3245761af7cd7aa0cd0c11aef2
tjdwns4537 <tjdwns4537@naver.com>
2023-01-09 11:16:15 +0900

    feat : Request Header 출력 영상 실습 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,34 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+/**
+ * 해야할 것
+ * 브라우저에서 서버로부터 데이터를 받아들이는 부분을 구현해야 함
+ * ex.
+ * InputStream: 브라우저에서 서버쪽으로 들어오는 모든 데이터가 담겨있음
+ * OutStream : 서버에서 클라이언트쪽으로 응답을 보낼 , 응답을 보내는 데이터를 싣어줌
+ * InputStream -> BufferedReader 로 변환하는 api 필요
+ * BufferedReader : 클라이언트에서 요청하는 데이터를 Line by Line으로 읽어들임
+ * ex. BufferedReader br = new BufferedReader(new InputStream(inputStream객체,"UTF-8"));
+ *
+ * request 를 읽어들이는 방법
+ * = null 로 마지막인지 테스트하는 것이 아닌,
+ * = !line.equals("") 로 비교한다.
+ *
+ * sout 보단 로거 활용해서 출력 확인
+ * = logger 를 활용하면 어느 쓰레드에서 로그를 찍는지도 볼 수 있다.
+ *
+ * [ 문제 해결 방법 ]
+ * 첫 번째 라인에 해당하는 line 객체 부분에서
+ * index.html 를 출력한 다음에
+ * 서버에서 이 디렉토리 밑에 있는 index.html 파일을 읽어서
+ * hello World 대신에 응답으로 쏴주면 첫번째 요구사항을 만족한다.
+ * **/
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,6 +45,14 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            logger.debug("request Line : {}",line);
+            while (!line.equals("")) {
+                line = br.readLine();
+                logger.debug("header : {}",line);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
