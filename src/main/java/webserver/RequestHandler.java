45b79311ddd9eefecb3e2357970c2e6a1e8a44ed
psm9718 <psm9718@ajou.ac.kr>
2023-01-09 12:01:00 +0900

    feat: 모든 Request Header 출력하기 + 브라우저에 index.html 내려주기
    요구사항을 잘못 파악하여 Client 클래스는 삭제하였고, 실습 예제에 따라 Request Header를 로그에 출력하는 메서드를 구현함.
    브라우저 상에 index.html 파일을 내려주는 것까지 구현.
    현재는 어떤 요청에도 index.html을 내려주도록 되어 있다. 수정 예정.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,10 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,12 +24,27 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            printHeaders(in);
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            byte[] body = Files.readAllBytes(new File("src/main/resources/templates/index.html").toPath());
+            logger.debug("request : {}",body);
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private static void printHeaders(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        String line = br.readLine();
+        logger.debug("request line : {}", line);
+        while (!line.equals("")) {
+            line = br.readLine();
+            logger.debug("header : {}", line);
         }
     }
 
