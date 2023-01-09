ee02b4b460bfc88326bbadcd9b52871d783ad4ab
solchan98 <dev.sol0427@gmail.com>
2023-01-09 17:20:19 +0900

    feat: RequestHeader 객체 상세 구현 HttpHeaderParser - parse 메서드 상세 구현 (테스트 케이스 성공)

--- src/main/java/http/util/HttpHeaderParser.java
+++ src/main/java/http/util/HttpHeaderParser.java
@@ -6,6 +6,19 @@ public class HttpHeaderParser {
     private HttpHeaderParser() {}
 
     public static HttpHeaders parse(String stringOfHeaders) {
-        return new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
+
+        if (stringOfHeaders.equals("")) {
+            return headers;
+        }
+
+        for (String strOfHeader: stringOfHeaders.split("\n")) {
+            int idxOfColon = strOfHeader.indexOf(":");
+            headers.setHeader(
+                    strOfHeader.substring(0, idxOfColon).trim(),
+                    strOfHeader.substring(idxOfColon + 1).trim());
+        }
+
+        return headers;
     }
 }
