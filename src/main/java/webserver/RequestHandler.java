939d89e949155fdcdfe9089f18e25e1f1c4d3b6a
ajongs <wonjong1030@naver.com>
2023-01-09 12:01:17 +0900

    feat(RequestHandler): HttpMessage 전체를 출력하는 printHttpMessage 메소드 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String DOMAIN = "http://localhost:8080/";
 
     private Socket connection;
 
@@ -23,11 +24,23 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./src/main/resources/templates/index.html" ).toPath());
+            printHttpMessage(in);
+            byte[] body = Files.readAllBytes(new File("./src/main/resources/templates/index.html").toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private void printHttpMessage(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String nextLine = br.readLine();
+        logger.debug("request line : {}", nextLine);
+        while (!nextLine.equals("")) {
+            nextLine = br.readLine();
+            logger.debug("header : {}", nextLine);
+            System.out.println(nextLine);
         }
     }
 
