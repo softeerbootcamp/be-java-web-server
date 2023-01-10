dc860468ed07a025acefc1d0fa5570dad162aee8
Toa <ld980105@snu.ac.kr>
2023-01-10 18:21:12 +0900

    feat : HttpResponse를 생성해주는 util 클래스 구현

---
+++ src/main/java/utils/HttpResponseGenerator.java
@@ -0,0 +1,31 @@
+package utils;
+
+import http.repsonse.HttpResponse;
+import http.request.HttpRequest;
+import model.User;
+import service.UserService;
+
+import java.util.Map;
+
+public class HttpResponseGenerator {
+    public static HttpResponse generateResponse(HttpRequest httpRequest, UserService userService){
+        if (checkParams(httpRequest, userService)) {
+            return new HttpResponse(httpRequest.getVersion(), StatusCode.SEEOTHER, httpRequest.getContentType(), null);
+        }
+        return new HttpResponse(httpRequest.getVersion(), StatusCode.OK, httpRequest.getContentType(),
+                FileIoUtils.loadFile(httpRequest.getRequestTarget()));
+    }
+
+    private static boolean checkParams(HttpRequest httpRequest, UserService userService){
+        if (httpRequest.hasParams()){
+            String target = httpRequest.getRequestTarget();
+            String[] params = target.split("\\?");
+            Map<String, String> map = HttpUtils.parseQuerystring(params[1]);
+            User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+            System.out.println("user = " + user);
+            userService.join(user);
+            return true;
+        }
+        return false;
+    }
+}
