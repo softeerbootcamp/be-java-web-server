99d723788d151e42437558c7e34fc770b2b15ca5
River-Mt <rivermt1998@gmail.com>
2023-01-10 16:06:55 +0900

    :white_check_mark: FileIoUtilsTest 기존 정상작동 테스트 로직 수정, 실패했을시 테스트 추가 - 기존 정상작동 테스트에서 assert 를 사용하지 않았기 때문에 수정

--- src/test/java/utils/FileIoUtilsTest.java
+++ src/test/java/utils/FileIoUtilsTest.java
@@ -2,19 +2,23 @@ package utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class FileIoUtilsTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    @Test
+    @DisplayName("존재하는 파일 요청 시 제대로 작동하는지 테스트")
+    void loadFileSuccessTest()  {
+        assertThatCode(() -> FileIoUtils.loadFile("/index.html"))
+                .doesNotThrowAnyException();
+    }
 
     @Test
-    @DisplayName("/index.html 제대로 반환하는지 테스트")
-    void loadFile() throws Exception {
-        byte[] body = FileIoUtils.loadFile("/index.html");
-        logger.debug("file : {}", new String(body));
+    @DisplayName("존재하지 않는 파일 요청시 실패하는지 테스트")
+    void loadFileFailTest() {
+        assertThatThrownBy(() -> FileIoUtils.loadFile("/test.html"));
     }
 
 }
