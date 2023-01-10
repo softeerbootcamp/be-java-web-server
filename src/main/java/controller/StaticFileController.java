028dd547bc5289aa21f90c05ef18a1c9adbbbe47
rohsik2 <rohsik2@gmail.com>
2023-01-10 15:12:31 +0900

    refactor: staticFile service와 controller 분리 및 로직 정리

--- src/main/java/controller/StaticFileController.java
+++ src/main/java/controller/StaticFileController.java
@@ -1,6 +1,6 @@
 package controller;
 
-import com.github.jknack.handlebars.internal.lang3.ArrayUtils;
+import db.StaticFileService;
 import httpMock.CustomHttpRequest;
 import httpMock.CustomHttpResponse;
 import httpMock.constants.ContentType;
@@ -8,15 +8,18 @@ import httpMock.constants.StatusCode;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
+
 import java.nio.file.Files;
 import java.util.Arrays;
+
+import static db.StaticFileService.getFileTypeFromUrl;
 
 public class StaticFileController implements RequestController {
     private static final StaticFileController fileController = new StaticFileController();
     public static final String[] supportedFileTypes = {
             "html", "ico", "css", "js", "ttf", "woff", "svg", "eot", "woff2", "png", "jpeg", "gif"
     };
+
     public static StaticFileController get() {
         return fileController;
     }
@@ -24,42 +27,23 @@ public class StaticFileController implements RequestController {
     @Override
     public void handleRequest(CustomHttpRequest req, CustomHttpResponse res) {
         getFile(req, res);
-        RequestController.NOT_FOUND(res);
     }
 
     public void getFile(CustomHttpRequest req, CustomHttpResponse res) {
-        String filePath = null;
-        URL resourcePath = getClass().getClassLoader().getResource("./static");
-
+        File file = StaticFileService.getFile(req.getUrl());
+        String fileType = StaticFileService.getFileTypeFromUrl(req.getUrl());
         try {
-            String fileType = getFileTypeFromUrl(req.getUrl());
-            res.setContentType(ContentType.getContentTypeByFileType(fileType));
-
-            if (req.getUrl().endsWith("ico") || req.getUrl().endsWith("html")) {
-                resourcePath = getClass().getClassLoader().getResource("./templates");
-            }
-
-            filePath = resourcePath.getPath();
-            byte[] file = Files.readAllBytes(new File(filePath + req.getUrl()).toPath());
-            res.addToBody(ArrayUtils.toObject(file));
+            byte[] data = Files.readAllBytes(file.toPath());
             res.setStatusCode(StatusCode.OK);
-
+            res.setContentType(ContentType.getContentTypeByFileType(fileType));
+            res.addToBody(data);
         } catch (IOException e) {
-            logger.error("file not found at " + filePath + req.getUrl());
             RequestController.NOT_FOUND(res);
-        } catch (NullPointerException e) {
-            logger.error("Cannot get resourcePath");
-            throw new RuntimeException(e);
         }
     }
 
-    public static String getFileTypeFromUrl(String url) {
-        int index = url.lastIndexOf(".");
-        return url.substring(index + 1);
-    }
-
-    public static boolean isFileTypeSupported(String url){
+    public static boolean isFileTypeSupported(String url) {
         String type = getFileTypeFromUrl(url);
-        return Arrays.stream(supportedFileTypes).filter(item->item.equals(type)).count() == 1L;
+        return Arrays.stream(supportedFileTypes).filter(item -> item.equals(type)).count() == 1L;
     }
 }
