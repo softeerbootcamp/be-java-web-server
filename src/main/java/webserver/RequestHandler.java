79ba7089bcaa67da9591b629f0ed4dd54f9e8a5d
roopre <benny1020@naver.com>
2023-01-10 14:21:24 +0900

    refactor: RequestHandler에서 reponse 생성하는 메서드 분리하여 ResponseWriter 클래스 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,27 +8,20 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import controller.UserController;
-
 public class RequestHandler implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-	private final HttpRequestUtils parser;
-	private final ResponseWriter responseWriter;
+	private RequestUtils parser;
+	private ResponseWriter responseWriter;
 	private Socket connection;
 
-	private UserController userController;
-
 	public RequestHandler(Socket connectionSocket) {
-		this.userController = new UserController();
-		this.parser = new HttpRequestUtils();
-		this.responseWriter = new ResponseWriter();
+		this.parser = new RequestUtils();
 		this.connection = connectionSocket;
 	}
 
@@ -36,29 +29,30 @@ public class RequestHandler implements Runnable {
 		logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
 			connection.getPort());
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String url = parser.pathParse(br.readLine());
+			String line = br.readLine();
+			while (!line.equals("")) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+			logger.debug("threadID : {} request path : {}", Thread.currentThread().getId(), url);
+			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 			DataOutputStream dos = new DataOutputStream(out);
 
-			// HttpRequest 파싱
-			HttpRequest httpRequest = HttpRequestUtils.httpRequestParse(in);
-			logger.info(httpRequest.toString());
-			// TODO: 아래 부분을 인터페이스를 활용하여 확장성 높이고 코드 중복 없애야함, response 또한 새로 클래스 만들어서 적용할 예정
-			// 정적 데이터 요청일 경우의 처리
-			File file = new File("./webapp" + httpRequest.getUrl().getPath());
-			logger.debug(httpRequest.getUrl().getPath());
-
-			if (file.isFile()) {
-				byte[] body = Files.readAllBytes(file.toPath());
-				responseWriter.responseWrite(dos, body);
-			}
-
-			if (httpRequest.urlStartsWith("/create")) {
-				responseWriter.responseWrite(dos, userController.doPost(httpRequest).getBytes("UTF-8"));
-			}
-
+			responseWriter.response200Header(dos, body.length);
+			responseWriter.responseBody(dos, body);
 		} catch (IOException e) {
 			logger.error(e.getMessage());
 		}
-
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 	}
+
+
+
 
 }
