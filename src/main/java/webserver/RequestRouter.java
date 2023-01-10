62e2eac19eca2b0f4762f98592f185c0bab4fe7e
rohsik2 <rohsik2@gmail.com>
2023-01-10 11:53:08 +0900

    feat: UserAccountService를 위한 라우팅테이블 추가

--- src/main/java/webserver/RequestRouter.java
+++ src/main/java/webserver/RequestRouter.java
@@ -1,49 +1,42 @@
 package webserver;
 
-
-import controller.RequestController;
+import com.github.jknack.handlebars.internal.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import controller.StaticFileController;
-import controller.UserAccountController;
-import httpMock.CustomHttpRequest;
-import httpMock.CustomHttpResponse;
+import service.StaticFileService;
+import service.UserAccountService;
+import webserver.httpMock.CustomHttpRequest;
+import webserver.httpMock.CustomHttpResponse;
+import webserver.httpMock.constants.ContentType;
+import webserver.httpMock.constants.StatusCode;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RequestRouter {
-    private static final Logger logger = LoggerFactory.getLogger(RequestRouter.class);
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private static RequestRouter requestRouter;
-    private final Map<String, RequestController> requestMap = new HashMap<>() {{
-        put("/user", UserAccountController.get());
+    private final Map<String, RequestService> requestMap = new HashMap<>() {{
+        put("[^\\s]+\\.(html|js|css|ico|ttf|woff|svg|eot|woff2|png)$", StaticFileService.get());
+        put("(/user).*", UserAccountService.get());
     }};
 
-    private RequestRouter() {
-    }
+    private RequestRouter(){}
 
-    public static RequestRouter getRequestRouter() {
-        if (requestRouter == null)
+    public static RequestRouter getRequestRouter(){
+        if(requestRouter == null)
             requestRouter = new RequestRouter();
         return requestRouter;
     }
 
-    public void doRoute(CustomHttpRequest req, CustomHttpResponse res) {
-        res.setProtocolVersion(req.getProtocolVersion());
-
-        if(StaticFileController.ifFileTypeRequested(req.getUrl())) {
-            StaticFileController.get().handleRequest(req, res);
-            return;
-        }
-
-        for (String keys : requestMap.keySet()) {
-            if (req.getUrl().startsWith(keys)) {
-                requestMap.get(keys).handleRequest(req, res);
-                logger.info(keys + " matches " + req.getUrl());
+    public void doRoute(CustomHttpRequest req, CustomHttpResponse res){
+        for(String regex : requestMap.keySet()){
+            if(req.getUrl().matches(regex)){
+                requestMap.get(regex).handleRequest(req, res);
+                logger.info(regex + " matches "  + req.getUrl());
                 return;
             }
         }
-
-        RequestController.NOT_FOUND(res);
+        RequestService.NOT_FOUND(res);
     }
 }
