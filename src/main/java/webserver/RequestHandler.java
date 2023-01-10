652444ce07d2b4fc6b688c89c52997bd26f455d2
roopre <benny1020@naver.com>
2023-01-10 18:24:12 +0900

    refactor: 정적 요청 처리와 usercontroller 연결

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,20 +8,27 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import controller.UserController;
+
 public class RequestHandler implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-	private RequestUtils parser;
-	private ResponseWriter responseWriter;
+	private final HttpRequestUtils parser;
+	private final ResponseWriter responseWriter;
 	private Socket connection;
 
+	private UserController userController;
+
 	public RequestHandler(Socket connectionSocket) {
-		this.parser = new RequestUtils();
+		this.userController = new UserController();
+		this.parser = new HttpRequestUtils();
+		this.responseWriter = new ResponseWriter();
 		this.connection = connectionSocket;
 	}
 
@@ -29,30 +36,29 @@ public class RequestHandler implements Runnable {
 		logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
 			connection.getPort());
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-			String url = parser.pathParse(br.readLine());
-			String line = br.readLine();
-			while (!line.equals("")) {
-				System.out.println(line);
-				line = br.readLine();
-			}
-			logger.debug("threadID : {} request path : {}", Thread.currentThread().getId(), url);
-			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 			DataOutputStream dos = new DataOutputStream(out);
 
-			responseWriter.response200Header(dos, body.length);
-			responseWriter.responseBody(dos, body);
+			// HttpRequest 파싱
+			HttpRequest httpRequest = HttpRequestUtils.httpRequestParse(in);
+			logger.info(httpRequest.toString());
+
+			// 정적 데이터 요청일 경우의 처리
+			File file = new File("./webapp" + httpRequest.getUrl().getPath());
+			logger.debug(httpRequest.getUrl().getPath());
+
+			if (file.isFile()) {
+				byte[] body = Files.readAllBytes(file.toPath());
+				responseWriter.responseWrite(dos, body);
+			}
+
+			if (httpRequest.urlStartsWith("/create")) {
+				responseWriter.responseWrite(dos, userController.doPost(httpRequest).getBytes("UTF-8"));
+			}
+
 		} catch (IOException e) {
 			logger.error(e.getMessage());
 		}
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
+
 	}
-
-
-
 
 }
