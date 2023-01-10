265b365dddfc8f3a1e1dab8196406d7bb6226166
rohsik2 <rohsik2@gmail.com>
2023-01-10 15:12:31 +0900

    refactor: staticFile service와 controller 분리 및 로직 정리

--- src/main/java/db/StaticFileService.java
+++ src/main/java/db/StaticFileService.java
@@ -1,4 +1,30 @@
 package db;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.File;
+import java.util.List;
+
 public class StaticFileService {
+    private static final String STATIC_PATH = StaticFileService.class.getClassLoader().getResource("./static").getPath();
+    private static final String TEMPLATE_PATH = StaticFileService.class.getClassLoader().getResource("./templates").getPath();
+
+    private static final List<String> TEMPLATE_FILES = List.of("html", "ico");
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+    public static String getFileTypeFromUrl(String url) {
+        int index = url.lastIndexOf(".");
+        return url.substring(index + 1);
+    }
+
+    public static File getFile(String url){
+        String filePath = STATIC_PATH + url;
+        String fileType = getFileTypeFromUrl(url);
+        if (TEMPLATE_FILES.contains(fileType)) {
+            filePath = TEMPLATE_PATH + url;
+        }
+        return new File(filePath);
+    }
 }
