90505f30b6a10c69786ae5cf254c819058596f26
pjs0418 <tony180322@gmail.com>
2023-01-10 16:08:07 +0900

    Response class 구현

--- src/main/java/model/response/Response.java
+++ src/main/java/model/response/Response.java
@@ -1,2 +1,21 @@
-package model.response;public class Response {
+package model.response;
+
+import model.general.Header;
+
+import java.util.Map;
+
+public class Response {
+    private final StatusLine statusLine;
+    private final Map<Header, String> headers;
+    private final byte[] body;
+
+    private Response(StatusLine statusLine, Map<Header, String> headers, byte[] body) {
+        this.statusLine = statusLine;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public static Response of(StatusLine statusLine, Map<Header, String> headers, byte[] body) {
+        return new Response(statusLine, headers, body);
+    }
 }
