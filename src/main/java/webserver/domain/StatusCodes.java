4a916227bd16d8416a782d6ad20da690c4e51f31
i5046821854 <o5046821854@gmail.com>
2023-01-10 18:25:08 +0900

    feat : Http의 각종 상태코드를 enum 클래스에 저장.  해당하는 상태 메시지와 상태값을 포함하도록 구현

---
+++ src/main/java/webserver/domain/StatusCodes.java
@@ -0,0 +1,37 @@
+package webserver.domain;
+
+import java.util.Arrays;
+
+public enum StatusCodes {
+
+    OK("200", "OK"),
+    SEE_OTHER("302", "SEE_OTHER"),
+    NOT_FOUND("404", "NOT_FOUND"),
+    FORBIDDEN("403", "FORBIDDEN"),
+    INTERNAL_SERVER_ERROR("500", "INTERNAL_SERVER_ERROR"),
+    BAD_GATEWAY("502", "BAD_GATEWAY"),
+    SERVICE_UNAVAILABLE("503", "SERVICE_UNAVAILABLE");
+
+    private String statusCode;
+    private String statusMsg;
+    private StatusCodes(String statusCode, String statusMsg){
+        this.statusCode = statusCode;
+        this.statusMsg = statusMsg;
+    }
+
+    public String getStatusCode(){
+        return statusCode;
+    }
+
+    public String getStatusMsg(){
+        return statusMsg;
+    }
+
+    public static StatusCodes findStatus(String msg){
+        return Arrays.stream(StatusCodes.values())
+                .filter(status -> status.getStatusCode().equals(msg))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 에러가 존재하지 않습니다"));
+    }
+
+}
