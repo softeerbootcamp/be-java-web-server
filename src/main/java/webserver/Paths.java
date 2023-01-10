ed5b4197691a9e64a7fc3d6e48e880989020f637
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-10 16:26:14 +0900

    범용 목적을 위해 html 파일들 재수정
    src 컴포넌트 다시 원래대로

--- src/main/java/webserver/Paths.java
+++ src/main/java/webserver/Paths.java
@@ -2,7 +2,7 @@ package webserver;
 
 public enum Paths {
     TEMPLATE_PATH("./src/main/resources/templates"),
-    STATIC_PATH("./src/main/resources");
+    STATIC_PATH("./src/main/resources/static");
 
     private String path;
 
