9d2c4251cc5f340e2a1625e0645e4a09cf47a95d
tjdwns4537 <tjdwns4537@naver.com>
2023-01-10 12:33:06 +0900

    feat : README 수정 구조 변경, 언제 싱글 스레드를 사용하는지

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -67,7 +67,6 @@ public class RequestHandler implements Runnable{
             String extensionString = stringParser.extensionSplit(urlString);
 
             while (!brRead.equals("") && brRead != null) {
-                logger.debug("header : {}", brRead);
                 brRead = br.readLine();
             }
 
