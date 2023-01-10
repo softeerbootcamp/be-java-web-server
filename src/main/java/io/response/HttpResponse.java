c225535d92af5d9e5e840da7f9958ba16e755849
wonju-dev <wonjulee.dev@gmail.com>
2023-01-11 02:35:44 +0900

    feat(HttpResponse): response 객체 구성 로직 구현

--- src/main/java/io/response/HttpResponse.java
+++ src/main/java/io/response/HttpResponse.java
@@ -1,18 +1,28 @@
 package io.response;
 
-import enums.ContentType;
+import enums.ResponseAttribute;
+import enums.ResponseStartLine;
 import enums.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class HttpResponse implements Response {
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
+public class HttpResponse {
 
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
-    private Status status = Status.NOT_FOUND;
-    private ContentType contentType = ContentType.HTML;
+    private static final String PROTOCOL = "HTTP/1.1";
+
+    private Map<ResponseStartLine, String> startLine = new HashMap<>();
+    private Map<ResponseAttribute, String> attributes = new HashMap<>();
     private byte[] body = "NOT FOUND".getBytes();
     private DataOutputStream out;
 
@@ -20,7 +30,6 @@ public class HttpResponse implements Response {
         this.out = out;
     }
 
-    @Override
     public void send() {
         try {
             assembleMessage();
@@ -30,34 +39,68 @@ public class HttpResponse implements Response {
         }
     }
 
-    @Override
     public void update(FindResult findResult) {
-        this.status = findResult.getStatus();
-        this.contentType = findResult.getContentType();
+        startLine.put(ResponseStartLine.PROTOCOL, PROTOCOL);
+        startLine.put(ResponseStartLine.STATUS, findResult.getStatus().getCode());
+        startLine.put(ResponseStartLine.MESSAGE, findResult.getStatus().getMessage());
+        attributes.put(ResponseAttribute.CONTENT_TYPE, findResult.getContentType().getType());
         this.body = findResult.getResource();
     }
 
     private void assembleMessage() {
-        response200Header();
-        responseBody();
-    }
-
-    private void response200Header() {
         try {
-            out.writeBytes("HTTP/1.1 200 OK \r\n");
-            out.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            out.writeBytes("Content-Length: " + body.length + "\r\n");
-            out.writeBytes("\r\n");
+            assembleStatusLine();
+            assembleHeader();
+            assembleBody();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void responseBody() {
-        try {
-            out.write(body, 0, body.length);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+    public void setStartLine(ResponseStartLine key, String value) {
+        startLine.put(key, value);
+    }
+
+    public void setAttribute(ResponseAttribute key, String value) {
+        attributes.put(key, value);
+    }
+
+    private void assembleStatusLine() throws IOException {
+        List<String> startLines = startLine.keySet().stream()
+                .sorted(comparing(ResponseStartLine::getOrder))
+                .map(key -> startLine.get(key))
+                .collect(toList());
+        out.writeBytes(String.join(" ", startLines) + "\n\r");
+    }
+
+    private void assembleHeader() throws IOException {
+        out.writeBytes(getHeaderString());
+        System.out.println(getHeaderString());
+    }
+
+    private String getHeaderString() {
+        String string = "";
+        Set<Map.Entry<ResponseAttribute, String>> entries = attributes.entrySet();
+        for (Map.Entry<ResponseAttribute, String> entry : entries) {
+            string += entry.getKey().getAttribute() + ": " + entry.getValue() + "\n\r";
         }
+        return string;
+    }
+
+    private void assembleBody() throws IOException {
+        out.write(body, 0, body.length);
+    }
+
+    public void setBody(byte[] resource) {
+        this.body = resource;
+    }
+
+    public void redirect(String redirectUrl) {
+        setStartLine(ResponseStartLine.PROTOCOL, PROTOCOL);
+        setStartLine(ResponseStartLine.STATUS, Status.REDIRECT.getCode());
+        setStartLine(ResponseStartLine.MESSAGE, Status.REDIRECT.getMessage());
+        setAttribute(ResponseAttribute.LOCATION, redirectUrl);
+        body = new byte[0];
+        send();
     }
 }
