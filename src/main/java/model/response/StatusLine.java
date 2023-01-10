46f0cae36d60c7141f4d41afb79b06fb3ebb233e
pjs0418 <tony180322@gmail.com>
2023-01-10 15:52:31 +0900

    HTTP 응답의 first line에 해당하는 StatusLine class 구현

---
+++ src/main/java/model/response/StatusLine.java
@@ -0,0 +1,29 @@
+package model.response;
+
+import model.general.Status;
+
+public class StatusLine {
+    private final String httpVersion;
+    private final Status status;
+
+    private StatusLine(String httpVersion, Status status) {
+        this.httpVersion = httpVersion;
+        this.status = status;
+    }
+
+    public static StatusLine of(Status status) {
+        return new StatusLine("HTTP/1.1", status);
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
+    }
+
+    public String getStatusCode() {
+        return String.valueOf(status.getStatusCode());
+    }
+
+    public String getReasonPhrase() {
+        return status.getReasonPhrase();
+    }
+}
