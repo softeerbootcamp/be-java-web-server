4ea8da9e46a2aaedc11f95adcef129b72c9deed5
River-Mt <rivermt1998@gmail.com>
2023-01-10 23:20:50 +0900

    :sparkles: HttpStatusCode 구현 - HttpStatusCode를 다루는 enum

---
+++ src/main/java/http/HttpStatusCode.java
@@ -0,0 +1,23 @@
+package http;
+
+public enum HttpStatusCode {
+    OK("200", "OK"),
+    FOUND("302", "Found");
+
+    private final String code;
+    private final String message;
+
+    HttpStatusCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
