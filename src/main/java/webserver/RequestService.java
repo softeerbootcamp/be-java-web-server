cd0b27c5c114458745e1ab49beb547b8e2322963
rohsik2 <rohsik2@gmail.com>
2023-01-10 11:53:37 +0900

    feat: 로거와 not_found 로직을 쉽게 사용하도록 interface에 추가

--- src/main/java/webserver/RequestService.java
+++ src/main/java/webserver/RequestService.java
@@ -1,9 +1,21 @@
 package webserver;
 
+import com.github.jknack.handlebars.internal.lang3.ArrayUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.httpMock.CustomHttpRequest;
 import webserver.httpMock.CustomHttpResponse;
+import webserver.httpMock.constants.ContentType;
+import webserver.httpMock.constants.StatusCode;
 
 public interface RequestService {
+    static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
     public void handleRequest(CustomHttpRequest req, CustomHttpResponse res);
 
+    public static void NOT_FOUND(CustomHttpResponse res){
+        res.setStatusCode(StatusCode.NOT_FOUND);
+        res.setContentType(ContentType.TEXT_PLAIN);
+        res.addToBody(ArrayUtils.toObject("404 not found".getBytes()));
+    }
 }
