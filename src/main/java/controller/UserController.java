27aa0cd7ae32c2f7580c855ef5220c23729beb40
wonju-dev <wonjulee.dev@gmail.com>
2023-01-11 02:34:23 +0900

    feat(UserController): 자원이 아닌 일반 요청 처리 기능 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,37 +1,39 @@
 package controller;
 
+import dto.UserInfoDTO;
 import filesystem.FileSystem;
 import io.request.HttpRequest;
+import io.request.PathParser;
 import io.response.FindResult;
-import io.response.Response;
+import io.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
+import service.UserService;
 
 public class UserController implements Controller {
 
-    private final Logger logger = LoggerFactory.getLogger(FacadeController.class);
-
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final FileSystem fileSystem = new FileSystem();
+    private final UserService userService = new UserService();
+    private final PathParser pathParser = new PathParser();
 
     @Override
-    public void handle(HttpRequest request, Response response) {
+    public void handle(HttpRequest request, HttpResponse response) {
         logger.info("user controller called");
         if (request.isStaticResourceRequest()) {
             handleStaticResourceRequest(request, response);
+            return;
         }
         handleRequest(request, response);
     }
 
-    private void handleRequest(HttpRequest request, Response response) {
-        Map<String, String> allQuery = request.getAllQuery();
-        for (String s : allQuery.keySet()) {
-            System.out.println(allQuery.get(s));
-        }
+    private void handleRequest(HttpRequest request, HttpResponse response) {
+        UserInfoDTO userInfo = new UserInfoDTO(request.getAllQuery());
+        userService.signIn(userInfo);
+        response.redirect(pathParser.getIndexPageUrl());
     }
 
-    private void handleStaticResourceRequest(HttpRequest request, Response response) {
+    private void handleStaticResourceRequest(HttpRequest request, HttpResponse response) {
         FindResult findResult = fileSystem.findResource(request.getUrl());
         response.update(findResult);
         response.send();
