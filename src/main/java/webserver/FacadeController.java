671ffe4cf772d81819481bff4482e0a5f496c0f6
wonju-dev <wonjulee.dev@gmail.com>
2023-01-10 14:22:26 +0900

    refactor(FacadeController): 미사용 코드 제거

--- src/main/java/webserver/FacadeController.java
+++ src/main/java/webserver/FacadeController.java
@@ -28,44 +28,6 @@ public class FacadeController implements Runnable {
             Request request = requestFactory.create(in);
             Response response = responseFactory.create(request, new DataOutputStream(out));
             response.send();
-            // byte[] file = Files.readAllBytes(new File(String.format(ABSOLUTE_PATH, resourcePath)).toPath());
-            // DataOutputStream dos = new DataOutputStream(out);
-            // response200Header(dos, file.length);
-            // responseBody(dos, file);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private String getRequestMessage(BufferedReader bufferedReader) throws IOException {
-        String msg = "";
-        String line;
-        while (!(line = bufferedReader.readLine()).isEmpty()) {
-            msg += line + "\n\r";
-        }
-
-        return msg;
-    }
-
-    private String getRequestUrl(BufferedReader br) throws IOException {
-        return br.readLine().split(" ")[1];
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
