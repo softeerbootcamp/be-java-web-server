101c3b77a1a60fc63ec72e8ac717c5a5d8e865a2
jieonkim23 <kwldjs1023@gmail.com>
2023-01-10 10:42:31 +0900

    HttpRequestParser 메소드 분리 리팩토링:  mappingPath메소드에 활용되는 코드들을 findExtension, checkExtension등으로 추출하여 분리.

--- src/main/java/util/HttpRequestParser.java
+++ src/main/java/util/HttpRequestParser.java
@@ -4,12 +4,18 @@ import java.io.File;
 import java.nio.file.Path;
 
 public class HttpRequestParser {
-    private static final String dir = "./src/main/resources/";
     public static Path mappingPath(String path) {
+        String dir = "./src/main/resources/";
+        String ex = findExtension(path);
+        String targetDir = checkExtension(ex) ? "templates" : "static";
+        return new File(dir+targetDir+path).toPath();
+    }
+    public static String findExtension(String path){
         String[] temp = path.split("\\.");
         String extension = temp[temp.length - 1];
-
-        String targetDir = extension.equals("html") ? "templates" : "static";
-        return new File(dir+targetDir+path).toPath();
+        return extension;
+    }
+    public static boolean checkExtension(String ex){
+        return ex.equals("html") || ex.equals("ico");
     }
 }
