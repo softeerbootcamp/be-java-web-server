136a5aa9165329148272bfc1ae70ed75381b5232
tjdwns4537 <tjdwns4537@naver.com>
2023-01-10 12:13:11 +0900

    feat : intellij 파일 끝 자동 개행 기능 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -14,31 +14,6 @@ import org.slf4j.LoggerFactory;
 import parser.StringParser;
 import utils.Utility;
 
-
-/**
- * 해야할 것
- * 브라우저에서 서버로부터 데이터를 받아들이는 부분을 구현해야 함
- * ex.
- * InputStream: 브라우저에서 서버쪽으로 들어오는 모든 데이터가 담겨있음
- * OutStream : 서버에서 클라이언트쪽으로 응답을 보낼 , 응답을 보내는 데이터를 싣어줌
- * InputStream -> BufferedReader 로 변환하는 api 필요
- * BufferedReader : 클라이언트에서 요청하는 데이터를 Line by Line으로 읽어들임
- * ex. BufferedReader br = new BufferedReader(new InputStream(inputStream객체,"UTF-8"));
- *
- * request 를 읽어들이는 방법
- * = null 로 마지막인지 테스트하는 것이 아닌,
- * = !line.equals("") 로 비교한다.
- *
- * sout 보단 로거 활용해서 출력 확인
- * = logger 를 활용하면 어느 쓰레드에서 로그를 찍는지도 볼 수 있다.
- *
- * [ 문제 해결 방법 ]
- * 첫 번째 라인에 해당하는 line 객체 부분에서
- * index.html 를 출력한 다음에
- * 서버에서 이 디렉토리 밑에 있는 index.html 파일을 읽어서
- * hello World 대신에 응답으로 쏴주면 첫번째 요구사항을 만족한다.
- * **/
-
 public class RequestHandler implements Runnable{
 
 
@@ -54,9 +29,29 @@ public class RequestHandler implements Runnable{
 
     public void run() {
 
-        //TODO
-
-
+        /*TODO
+        *  해야할 것
+         * 브라우저에서 서버로부터 데이터를 받아들이는 부분을 구현해야 함
+         * ex.
+         * InputStream: 브라우저에서 서버쪽으로 들어오는 모든 데이터가 담겨있음
+         * OutStream : 서버에서 클라이언트쪽으로 응답을 보낼 , 응답을 보내는 데이터를 싣어줌
+         * InputStream -> BufferedReader 로 변환하는 api 필요
+         * BufferedReader : 클라이언트에서 요청하는 데이터를 Line by Line으로 읽어들임
+         * ex. BufferedReader br = new BufferedReader(new InputStream(inputStream객체,"UTF-8"));
+         * #################################################
+         * request 를 읽어들이는 방법
+         * = null 로 마지막인지 테스트하는 것이 아닌,
+         * = !line.equals("") 로 비교한다.
+         * ############################################################
+         * sout 보단 로거 활용해서 출력 확인
+         * = logger 를 활용하면 어느 쓰레드에서 로그를 찍는지도 볼 수 있다.
+         * #######################################
+         * [ 문제 해결 방법 ]
+         * 첫 번째 라인에 해당하는 line 객체 부분에서
+         * index.html 를 출력한 다음에
+         * 서버에서 이 디렉토리 밑에 있는 index.html 파일을 읽어서
+         * hello World 대신에 응답으로 쏴주면 첫번째 요구사항을 만족한다.
+         */
 
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
