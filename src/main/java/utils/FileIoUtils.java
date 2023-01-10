2999a27648f9d02485759c602a340e38fb40a9ab
River-Mt <rivermt1998@gmail.com>
2023-01-10 16:05:22 +0900

    :adhesive_bandage: FileIoUtils 존재하지 않는 파일 요청 시 예외처리 로직 변경

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -5,22 +5,26 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
 
 public final class FileIoUtils {
     public static final String TEMPLATES_PREFIX = "./templates";
     public static final String STATIC_PREFIX = "./static";
 
-    private FileIoUtils() {}
+    private FileIoUtils() {
+    }
 
     public static byte[] loadFile(String filename) throws IOException, URISyntaxException {
         return Files.readAllBytes(getPath(String.valueOf(filename)));
     }
 
     private static Path getPath(String path) throws URISyntaxException {
-        if(path.endsWith("html")){
-            return Paths.get(Objects.requireNonNull(FileIoUtils.class.getClassLoader().getResource(TEMPLATES_PREFIX + path)).toURI());
+        try {
+            if (path.endsWith("html")) {
+                return Paths.get(FileIoUtils.class.getClassLoader().getResource(TEMPLATES_PREFIX + path).toURI());
+            }
+            return Paths.get(FileIoUtils.class.getClassLoader().getResource(STATIC_PREFIX + path).toURI());
+        } catch (NullPointerException e) {
+            throw new NullPointerException("파일이 존재하지 않습니다.");
         }
-        return Paths.get(Objects.requireNonNull(FileIoUtils.class.getClassLoader().getResource(STATIC_PREFIX + path)).toURI());
     }
 }
