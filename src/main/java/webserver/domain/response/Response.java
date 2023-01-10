b52a67f5319dea19a12dd22a841a69fb5968d13f
i5046821854 <o5046821854@gmail.com>
2023-01-11 01:49:49 +0900

    feat : 기존의 util의 함수를 통하여 작성하였던 Response를 별도의 클래스를 생성하여 처리하도록 변경

--- src/main/java/webserver/domain/response/Response.java
+++ src/main/java/webserver/domain/response/Response.java
@@ -1,23 +1,46 @@
 package webserver.domain.response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+import webserver.domain.StatusCodes;
 import webserver.domain.request.RequestLine;
+import webserver.exception.HttpRequestException;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Response {
 
-    private ResponseHeader responseHeader;
-    private ResponseBody responseBody;
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private OutputStream out;
 
-
-    public Response(ResponseHeader responseHeader, ResponseBody ResponseBody){
-        this.responseHeader = responseHeader;
-        this.responseBody = ResponseBody;
-    }
-    public static Response of(String reqLine, String header, String body) {
-        return new Response(ResponseHeader.from(header),ResponseBody.from(body));
+    public Response(OutputStream out){
+        this.out = out;
     }
 
-    public void readRequest(){
-        responseHeader.readHeader();
-        responseBody.readBody();
+    //Make Http response header for every error
+    public void responseHeader(DataOutputStream dos, String errorCode, String errorMsg, int lengthOfBodyContent) throws IOException {
+        String responseLine = "HTTP/1.1 " + errorCode + " " + errorMsg + " \r\n";
+        dos.writeBytes(responseLine);
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
+    }
+
+    public void responseBody(DataOutputStream dos,byte[] body) throws IOException {
+        dos.write(body, 0, body.length);
+        dos.flush();
+    }
+
+    public void makeResponse(byte[] body, String statusCode, String msg)  {
+        DataOutputStream dos = new DataOutputStream(out);
+        try {
+            responseHeader(dos, statusCode, msg, body.length);
+            responseBody(dos, body);
+        } catch (IOException e) {
+            logger.debug(e.getMessage());
+        }
     }
 }
