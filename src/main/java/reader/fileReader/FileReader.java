58caadceeba2a17bcd34e9a3267ca38f5d49da0b
rentalhub-mac78 <kksy2423@naver.com>
2023-01-10 16:00:41 +0900

    Feat: StaticFileReader 객체 생성
    css,js 등 정적타입을 read하는 객체 생성

--- src/main/java/reader/fileReader/FileReader.java
+++ src/main/java/reader/fileReader/FileReader.java
@@ -1,11 +1,17 @@
 package reader.fileReader;
 
-public interface FileReader {
-    byte[] readFile(String url);
+import util.Url;
+import util.UrlType;
 
-    static FileReader selectFileReader(String url) {
-        if (url.matches(TemplatesFileReader.TEMPLATE_REGEX)) {
+public interface FileReader {
+    byte[] readFile(Url url);
+
+    static FileReader selectFileReader(Url url) {
+        UrlType urlType = url.getUrlType();
+        if (urlType.equals(UrlType.TEMPLATES_FILE)) {
             return new TemplatesFileReader();
+        } else if (urlType.equals(UrlType.STATIC_FILE)) {
+            return new StaticFileReader();
         } else {
             return null;
         }
