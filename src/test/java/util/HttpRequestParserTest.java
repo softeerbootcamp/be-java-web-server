0c24c18eacf2ee76734244dce99f5cebc0862289
jieonkim23 <kwldjs1023@gmail.com>
2023-01-10 11:30:59 +0900

    HttpRequestParserTest에 parseQueryTest 추가:  쿼리스트링을 파싱하는 메소드 테스트를 추가함.

--- src/test/java/util/HttpRequestParserTest.java
+++ src/test/java/util/HttpRequestParserTest.java
@@ -3,6 +3,7 @@ package util;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
+import java.util.Map;
 
 
 class HttpRequestParserTest {
@@ -11,5 +12,11 @@ class HttpRequestParserTest {
         HttpRequestParser httpRequestParser = new HttpRequestParser();
         Path result = httpRequestParser.mappingPath("/css/styles.css");
         System.out.println(result);
+   }
+   @Test
+   public void parseQueryTest(){
+        String query = "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
+        Map<String, String> result = HttpRequestParser.parseQuery(query);
+        result.forEach((s1,s2)->System.out.println(s1+':'+s2));
    }
 }
