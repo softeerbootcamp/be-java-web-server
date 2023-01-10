6b7f11c6684c7c0f735276090317395f2d982311
solchan98 <dev.sol0427@gmail.com>
2023-01-10 13:49:10 +0900

    feat: ResourceUtils 중복 로직 공통화 templates를 탐색 후 static을 탐색하는데 탐색하는 로직이 공통이 된다. 이 부분을 하나의 메서드로 추출하여 처리하도록 수정.

--- src/main/java/util/ResourceUtils.java
+++ src/main/java/util/ResourceUtils.java
@@ -8,17 +8,26 @@ import java.nio.file.Paths;
 import java.util.Objects;
 
 public class ResourceUtils {
+
+    private final static String PATH_OF_TEMPLATE = "./templates";
+    private final static String PATH_OF_STATIC = "./static";
+
     public static byte[] loadFileFromClasspath(String filePath) throws IOException {
-        Path path;
-        try {
-            path = Paths.get(Objects.requireNonNull(ResourceUtils.class.getClassLoader().getResource("./templates" + filePath)).toURI());
-        } catch (URISyntaxException | NullPointerException e) {
-            try {
-                path = Paths.get(Objects.requireNonNull(ResourceUtils.class.getClassLoader().getResource("./static" + filePath)).toURI());
-            } catch (URISyntaxException | NullPointerException ex) {
+        Path path = search(PATH_OF_TEMPLATE + filePath);
+        if (path == null) {
+            path = search(PATH_OF_STATIC + filePath);
+            if (path == null) {
                 return new byte[0];
             }
         }
         return Files.readAllBytes(path);
+    }
+
+    public static Path search(String filePath) {
+        try {
+            return Paths.get(Objects.requireNonNull(ResourceUtils.class.getClassLoader().getResource(filePath)).toURI());
+        } catch (URISyntaxException | NullPointerException e) {
+            return null;
+        }
     }
 }
