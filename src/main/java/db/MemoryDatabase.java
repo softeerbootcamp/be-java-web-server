ca7772afe8de0c47c6173a6f0692cd02adf0b592
Toa <ld980105@snu.ac.kr>
2023-01-10 14:47:55 +0900

    refactor : 멀티 스레드를 고려하여 ConcurrentHashMap으로 변경

--- src/main/java/db/MemoryDatabase.java
+++ src/main/java/db/MemoryDatabase.java
@@ -1,14 +1,13 @@
 package db;
 
-import com.google.common.collect.Maps;
-
 import model.User;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MemoryDatabase implements Database {
-    private final static Map<String, User> users = Maps.newHashMap();
+    private final static Map<String, User> users = new ConcurrentHashMap<>();
 
     public void addUser(User user) {
         users.put(user.getUserId(), user);
