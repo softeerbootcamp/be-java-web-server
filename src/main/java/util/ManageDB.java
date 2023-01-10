b624471471a9491671b4694ee43198c8645ab41e
jieonkim23 <kwldjs1023@gmail.com>
2023-01-10 23:14:23 +0900

    :loud_sound: ManageDb의 saveUser메소드에 로그 추가

--- src/main/java/util/ManageDB.java
+++ src/main/java/util/ManageDB.java
@@ -2,13 +2,17 @@ package util;
 
 import db.Database;
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class ManageDB {
+    private static final Logger logger = LoggerFactory.getLogger(ManageDB.class);
     public static void saveUser(Map<String,String> querys){
         User user = new User(querys.get("userId"), querys.get("password"), querys.get("name"), querys.get("email"));
         Database.addUser(user);
+        logger.debug("save user: "+user.toString());
     }
     public static User findUserById(String userId){
         return Database.findUserById(userId);
