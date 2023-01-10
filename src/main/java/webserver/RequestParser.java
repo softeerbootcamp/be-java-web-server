6d04ce4667145234d8133a6741f4735078e6b6eb
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-10 16:06:07 +0900

    parseInputStreamToHttpRequest , parseHeader 메서드 구현
    InputStream을 통해 HttpRequest 객체를 생성하는 parseInputStreamToHttpRequest 메서드 구현 완료,
    BufferReader를 통해 header 내용을 파싱하여 map으로 리턴하는 parseHeader 메서드 구현

--- src/main/java/webserver/RequestParser.java
+++ src/main/java/webserver/RequestParser.java
@@ -1,8 +1,35 @@
 package webserver;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RequestParser {
+
+    private static final String HEADER_DELIMITER = ": ";
+    private static final int HEADER_SPLIT_LIMIT = 2; //split된 배열의 크기
+
+    public static HttpRequest parseInputStreamToHttpRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+        RequestStartLine startLine = RequestStartLine.from(br.readLine());
+        Map<String,String> headers = parseHeader(br);
+        return new HttpRequest(startLine.getMethod(),startLine.getUrl(),startLine.getQueries(),headers);
+
+    }
+
+    private static Map<String, String> parseHeader(BufferedReader br) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        String line;
+        line = br.readLine();
+        while (!line.startsWith("")) {
+            String[] headerTokens = line.split(HEADER_DELIMITER, HEADER_SPLIT_LIMIT);
+            String val= " ";
+            if(headerTokens.length == 2) val = headerTokens[1];
+            headers.put(headerTokens[0],val);
+            line = br.readLine();
+        }
+        return headers;
+    }
 
     public static String parseRequestStartLineTarget(InputStream in) throws IOException {
         //inputstream은 bufferedreader를 통해 line by line으로 읽을 수 있음
