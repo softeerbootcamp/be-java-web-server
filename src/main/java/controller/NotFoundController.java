685a0b7470707f72d13297497988ad28345ba610
pjs0418 <tony180322@gmail.com>
2023-01-10 16:50:56 +0900

    404 Not Found 상태 일 때 응답 리턴하는 NotFoundController class 구현

--- src/main/java/controller/NotFoundController.java
+++ src/main/java/controller/NotFoundController.java
@@ -1,2 +1,12 @@
-package controller;public class NotFoundController {
+package controller;
+
+import model.general.Status;
+import model.request.RequestLine;
+import model.response.Response;
+
+public class NotFoundController implements Controller {
+    @Override
+    public Response getResponse(RequestLine requestLine) {
+        return Response.of(Status.NOT_FOUND);
+    }
 }
