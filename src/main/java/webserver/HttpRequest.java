8e63f5453677921244346c8bd50ff1e7489e3a52
roopre <benny1020@naver.com>
2023-01-10 17:36:31 +0900

    refactor: String path와 queries Map을 멤버변수로 가지는 Url 클래스 구현

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -3,23 +3,32 @@ package webserver;
 import java.util.Map;
 
 public class HttpRequest {
-	private String url;
+	private Url url;
 	private String method;
 
 	private Map<String, String> headers;
 
-	private HttpRequest(String url, String method, Map<String, String> headers) {
+	private HttpRequest(Url url, String method, Map<String, String> headers) {
 		this.url = url;
 		this.method = method;
 		this.headers = headers;
 	}
 
-	public static HttpRequest of(String url, String method, Map<String, String> headers) {
+	public Url getUrl() {
+		return url;
+	}
+
+
+	public static HttpRequest of(Url url, String method, Map<String, String> headers) {
 		return new HttpRequest(url, method, headers);
 	}
 
-	public String getUrl() {
-		return url;
+	public boolean urlStartsWith(String path) {
+		return url.startsWith(path);
+	}
+
+	public Map<String, String> getHeaders() {
+		return headers;
 	}
 
 	@Override
