ba6528371a2bff79db88c5686ae67188a9cfd2dc
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-10 15:01:49 +0900

    신규유저 정보 받아오는 부분 List말고 Map 사용하도록 수정
    list에서 인덱스로 받아오는 것보다 map을 써서 value를 받아오는 게 가독성이 높아서 수정함.
    key값에 대한 필드 생성
    각 메서드들이 HashMap을 반환하도록 수정
    각 메서드의 내부 구조 수정

--- src/main/java/webserver/SignUpController.java
+++ src/main/java/webserver/SignUpController.java
@@ -4,32 +4,40 @@ import db.Database;
 import model.User;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SignUpController {
+    private static final String USER_ID = "userId";
+    private static final String USER_PassWord = "password";
+    private static final String USER_Name = "name";
+    private static final String USER_Email = "email";
+
     public static void enrollNewUser(String unparsedInfo)
     {
-        List<String> userInfo = parseString(unparsedInfo);
+        Map<String, String> UserInfo = parseString(unparsedInfo);
 
-        User newUser = new User(userInfo.get(0), userInfo.get(1), userInfo.get(2), userInfo.get(3));
+        User newUser = new User(UserInfo.get(USER_ID), UserInfo.get(USER_PassWord), UserInfo.get(USER_Name), UserInfo.get(USER_Email));
         Database.addUser(newUser);
         System.out.println(Database.findAll());
     }
 
-    private static List<String> parseString(String unparsedInfo)
+    private static HashMap<String, String> parseString(String unparsedInfo)
     {
+        HashMap<String, String> returnMap = new HashMap<String, String>();
         String onlyUseIndex1[] = unparsedInfo.split("\\?");
-        List<String> userInfo = getUserInfoFromString(onlyUseIndex1[1]);
-        return userInfo;
+        return getUserInfoFromString(onlyUseIndex1[1]);
     }
 
-    private static List<String> getUserInfoFromString(String str)
+    private static HashMap<String, String> getUserInfoFromString(String str)
     {
         String token[] = str.split("&");
-        List<String> userInfo = new ArrayList<String>();
+        HashMap<String, String> userInfo = new HashMap<String, String>();
         for(int i = 0; i < token.length; i++)
         {
-            userInfo.add(token[i].split("=")[1]);
+            String splitToken[] = token[i].split("=");
+            userInfo.put(splitToken[0], splitToken[1]);
         }
         return userInfo;
     }
