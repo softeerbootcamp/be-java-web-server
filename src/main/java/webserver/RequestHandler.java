1e06e6cb13a21a23b2f8e7383459157894a9e88e
i5046821854 <o5046821854@gmail.com>
2023-01-10 18:26:42 +0900

    feat : RequestHandler에 HandlerMapping에 대한 의존관계를 추가하고 이에 따라 로직을 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,48 +2,47 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 
+import com.sun.net.httpserver.HttpPrincipal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.exception.HttpRequestError;
-import webserver.exception.NoFileException;
-import webserver.utils.RequestUtil;
+import webserver.Controller.AuthController;
+import webserver.Controller.Controller;
+import webserver.domain.request.Request;
+import webserver.exception.HttpRequestException;
+import webserver.utils.HttpRequestUtils;
+import webserver.utils.HttpResponseUtil;
 
-import static webserver.utils.FilePathParser.*;
-import static webserver.utils.RequestUtil.*;
-import static webserver.utils.ResponseUtil.*;
+import static webserver.utils.HttpRequestUtils.parseHttpRequest;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
+    private AuthController authController;
     private Socket connection;
-    private RequestUtil requestUtil;
+    private HandlerMapping handlerMapping;
 
     public RequestHandler(Socket connectionSocket) {
+
         this.connection = connectionSocket;
+        this.authController = new AuthController();
+        this.handlerMapping = new HandlerMapping();
+
     }
 
-    public byte[] staticRequestResolver(String fileName) throws IOException, NoFileException {
-        String[] type = fileName.split("\\.");
-        String typeName = type[type.length - 1];
-        if(typeName.equals("html") || typeName.equals("ico"))
-            return getTemplateFilesPath(fileName);
-        else
-            return getStaticFilesPath(fileName);
-    }
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
-            Map<String, String> reqMap = parseHttpRequest(in);
-            readHttpRequest(reqMap);  //Read Http Request Message
-            String fileName = findFilePath(reqMap); //Get an absolute file path for the requested static file
-            makeResponse(fileName, out);  //Create Http Response Message
-
+            Request req = parseHttpRequest(in);
+            req.readRequest();  //Print out Http Request Message
+            Controller controller = handlerMapping.getHandler(req.getRequestLine().getResource());
+            try{
+                controller.handle(req.getRequestLine().getResource(), out);
+            }catch(HttpRequestException e){
+                HttpResponseUtil.makeResponse(e.getMessage().getBytes(), out, e.getErrorCode(), e.getErrorMsg());
+            }
         } catch (IOException e) {
             logger.debug(e.getMessage());
         }
