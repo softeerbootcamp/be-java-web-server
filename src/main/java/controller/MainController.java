43ae3715aa45b651d42df90e8cb9cd79e0b8a5de
wonju-dev <wonjulee.dev@gmail.com>
2023-01-10 18:49:51 +0900

    feat(MainController): MainController 구현

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import filesystem.FileSystem;
+import io.request.HttpRequest;
+import io.response.FindResult;
+import io.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainController implements Controller {
+
+    private final Logger logger = LoggerFactory.getLogger(MainController.class);
+    private final FileSystem fileSystem = new FileSystem();
+
+    @Override
+    public void handle(HttpRequest request, Response response) {
+        logger.info("main controller called");
+        FindResult findResult = fileSystem.findResource(request.getUrl());
+        response.update(findResult);
+        response.send();
+    }
+}
