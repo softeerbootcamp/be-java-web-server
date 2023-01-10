afaa67f30cfbc0db7b3d4a985d5b43cd7379c1a9
i5046821854 <o5046821854@gmail.com>
2023-01-10 18:20:27 +0900

    feat : 여러 컨트롤러의 행동을 정의하는 Controller 인터페이스 생성

---
+++ src/main/java/webserver/Controller/Controller.java
@@ -0,0 +1,12 @@
+package webserver.Controller;
+
+import webserver.domain.response.Response;
+import webserver.exception.HttpRequestException;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public interface Controller {
+
+    void handle(String request, OutputStream out) throws HttpRequestException, IOException;
+}
