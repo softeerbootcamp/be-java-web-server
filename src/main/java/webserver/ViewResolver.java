05779575e2271350d42801862c7513ce63714849
Soomin-Lim <augustth511@gmail.com>
2023-01-10 15:12:34 +0900

    chore: favicon.ico 파일을 resources/static 디렉터리로 이동
    - ViewResolver의 process() 메서드는 파일 확장자가 ".html"인 경우에만 뷰 파일의 위치 경로를 templates 디렉토리로 지정
    - ".html" 문자열을 상수로 정의하여 관리

--- src/main/java/webserver/ViewResolver.java
+++ src/main/java/webserver/ViewResolver.java
@@ -4,9 +4,10 @@ public class ViewResolver {
 
     private static final String STATIC_RESOURCE_PATH = "src/main/resources/static";
     private static final String TEMPLATE_RESOURCE_PATH = "src/main/resources/templates";
+    private static final String HTML_FILE_EXTENSION = ".html";
 
     public static String process(String viewName) {
-        if (viewName.endsWith(".html") || viewName.endsWith("/favicon.ico")) {
+        if (viewName.endsWith(HTML_FILE_EXTENSION)) {
             return TEMPLATE_RESOURCE_PATH + viewName;
         }
 
