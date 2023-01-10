dda95f121e066d755386bfba23cfa9cfb4de4437
Toa <ld980105@snu.ac.kr>
2023-01-10 11:22:18 +0900

    refactor : 존재하지 않는 resource 요청 시 문구를 출력해주는 것으로 변경

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -7,14 +7,14 @@ import java.nio.file.Files;
 public class FileIoUtils {
     private static final String STATIC_RESOURCE_PATH = "src/main/resources/static";
     private static final String TEMPLATE_RESOURCE_PATH = "src/main/resources/templates";
-    public static byte[] loadFile(String file) throws IOException {
+    public static byte[] loadFile(String file) {
         try {
             if (file.contains("html") || file.contains("ico"))
                 return Files.readAllBytes(new File(TEMPLATE_RESOURCE_PATH + file).toPath());
             return Files.readAllBytes(new File(STATIC_RESOURCE_PATH + file).toPath());
         }
         catch (Exception e) {
-            return Files.readAllBytes(new File(TEMPLATE_RESOURCE_PATH + "/index.html").toPath());
+            return "존재하지 않는 리소스입니다.".getBytes();
         }
     }
 }
