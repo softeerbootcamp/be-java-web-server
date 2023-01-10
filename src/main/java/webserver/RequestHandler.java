964eea781a92f5d45b872d1edc71371ac0c04bd6
roopre <benny1020@naver.com>
2023-01-10 14:21:24 +0900

    refactor: RequestHandler에서 reponse 생성하는 메서드 분리하여 ResponseWriter 클래스 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,6 +1,5 @@
 package webserver;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -10,8 +9,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,18 +16,18 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-	private StringParser parser;
+	private RequestUtils parser;
+	private ResponseWriter responseWriter;
 	private Socket connection;
 
 	public RequestHandler(Socket connectionSocket) {
-		this.parser = new StringParser();
+		this.parser = new RequestUtils();
 		this.connection = connectionSocket;
 	}
 
 	public void run() {
 		logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
 			connection.getPort());
-
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String url = parser.pathParse(br.readLine());
@@ -42,8 +39,9 @@ public class RequestHandler implements Runnable {
 			logger.debug("threadID : {} request path : {}", Thread.currentThread().getId(), url);
 			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
 			DataOutputStream dos = new DataOutputStream(out);
-			response200Header(dos, body.length);
-			responseBody(dos, body);
+
+			responseWriter.response200Header(dos, body.length);
+			responseWriter.responseBody(dos, body);
 		} catch (IOException e) {
 			logger.error(e.getMessage());
 		}
@@ -54,23 +52,7 @@ public class RequestHandler implements Runnable {
 		}
 	}
 
-	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-		try {
-			dos.writeBytes("HTTP/1.1 200 OK \r\n");
-			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-			dos.writeBytes("\r\n");
-		} catch (IOException e) {
-			logger.error(e.getMessage());
-		}
-	}
 
-	private void responseBody(DataOutputStream dos, byte[] body) {
-		try {
-			dos.write(body, 0, body.length);
-			dos.flush();
-		} catch (IOException e) {
-			logger.error(e.getMessage());
-		}
-	}
+
+
 }
