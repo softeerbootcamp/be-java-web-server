15f7ebbc639db1ecc846ef0eaa9cc73f45c0c75c
jieonkim23 <kwldjs1023@gmail.com>
2023-01-10 23:14:58 +0900

    :white_check_mark: HttpRequestUtilTest의 parseRequestTest 테스트 코드 추가: HttpRequest 객체를 생성하여 비교하는 것이 다소 번잡하여 params 중 하나와 비교하여 assertThat으로 확인함.

--- src/test/java/util/HttpRequestUtilTest.java
+++ src/test/java/util/HttpRequestUtilTest.java
@@ -1,12 +1,18 @@
 package util;
 
+import Request.HttpRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
-
 class HttpRequestUtilTest {
-
+    private final Logger logger = LoggerFactory.getLogger(HttpRequestUtilTest.class);
    @Test
    public void parseQueryTest(){
         String query = "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
@@ -15,6 +21,12 @@ class HttpRequestUtilTest {
    }
    @Test
     public void parseRequestTest(){
-       //InputStream inputStream = new In
+       InputStream in = new ByteArrayInputStream("GET /user/create?userId=jieon&password=dfs&name=kim&email=kim%40naver.com HTTP/1.1".getBytes());
+       try {
+           HttpRequest httpRequest = HttpRequestUtil.parseRequest(in);
+           Assertions.assertThat(httpRequest.getParams().get("userId").equals("jieon"));
+       } catch (IOException e) {
+           logger.error(e.getMessage());
+       }
    }
 }
