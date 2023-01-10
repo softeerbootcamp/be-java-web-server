fd7993baa9f7a4db491636a3a418f5329b084420
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-10 12:30:45 +0900

    String을 파싱하는 메서드를 따로 구현, static으로 선언

--- src/main/java/utils/Utilites.java
+++ src/main/java/utils/Utilites.java
@@ -1,7 +1,6 @@
 package utils;
 
 public class Utilites {
-
     public static String[] stringParser(String str, String seperator){
         String[] parsedStr = str.split(seperator);
         return parsedStr;
