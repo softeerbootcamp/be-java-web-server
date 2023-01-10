77cabe202a46d337cd4af29e62e8e69f5b95b8eb
wonju-dev <wonjulee.dev@gmail.com>
2023-01-10 18:47:47 +0900

    feat(FacadeController): 요청 위임 메서드 구현

--- src/main/java/controller/FacadeController.java
+++ src/main/java/controller/FacadeController.java
@@ -1,19 +1,27 @@
 package controller;
 
-import io.request.Request;
+import io.request.HttpRequest;
 import io.request.RequestFactory;
 import io.response.Response;
 import io.response.ResponseFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Map;
 
 public class FacadeController implements Runnable {
     private final Logger logger = LoggerFactory.getLogger(FacadeController.class);
     private final RequestFactory requestFactory = new RequestFactory();
     private final ResponseFactory responseFactory = new ResponseFactory();
+
+    private final Map<Domain, Controller> controllers = Map.of(
+            Domain.USER, new UserController(),
+            Domain.MAIN, new MainController()
+    );
 
     private Socket connection;
 
@@ -25,11 +33,18 @@ public class FacadeController implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            Request request = requestFactory.create(in);
-            Response response = responseFactory.create(request, new DataOutputStream(out));
-            response.send();
+            HttpRequest request = requestFactory.create(in);
+            Response response = responseFactory.create(out);
+
+            delegateRequest(request, response);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void delegateRequest(HttpRequest request, Response response) {
+        Controller controller = controllers.get(Domain.find(request.getUrl()));
+
+        controller.handle(request, response);
     }
 }
