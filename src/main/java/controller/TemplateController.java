3e6d0c10a2f58adab229b6107327800cd26ae05c
jun-min1623 <junmin1623@naver.com>
2023-01-10 11:53:47 +0900

    feat:Request에서 requestline 추출해서 set

--- src/main/java/controller/TemplateController.java
+++ src/main/java/controller/TemplateController.java
@@ -4,8 +4,12 @@ import request.Request;
 import response.Response;
 
 public class TemplateController implements Controller{
+    private static final int File_Line_Index = 1;
+
     @Override
     public void selectedController(Request request, Response response) {
-
+        System.out.println("firstLine : "+request);
+        String[] filePath = request.split(" ");
+        return filePath[File_Line_Index];
     }
 }
--- src/main/java/request/Request.java
+++ src/main/java/request/Request.java
@@ -1,4 +1,17 @@
 package request;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Request {
+
+    private static RequestLine requestLine;
+
+    public Request(BufferedReader bufferedReader) throws IOException {
+        setRequestLine(bufferedReader.readLine());
+    }
+    public static void setRequestLine(String line) {
+        Request.requestLine = new RequestLine(line);
+    }
+
 }
