2cd7368bb54b352834f313079868ae3f921b942e
i5046821854 <o5046821854@gmail.com>
2023-01-11 01:51:30 +0900

    fix : Response 객체를 별도로 생성하였으므로 Response를 작성하기 위한 유틸 클래스는 삭제

--- src/main/java/webserver/utils/HttpResponseUtil.java
+++ src/main/java/webserver/utils/HttpResponseUtil.java
@@ -8,25 +8,5 @@ import java.io.OutputStream;
 
 public class HttpResponseUtil {
 
-    //FIXME : RESPONSE 객체에 해당 내용들을 옮기기
 
-    //Make Http response header for every error
-    public static void responseHeader(DataOutputStream dos, String errorCode, String errorMsg, int lengthOfBodyContent) throws IOException {
-        String responseLine = "HTTP/1.1 " + errorCode + " " + errorMsg + " \r\n";
-        dos.writeBytes(responseLine);
-        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-        dos.writeBytes("\r\n");
-    }
-
-    public static void responseBody(DataOutputStream dos,byte[] body) throws IOException {
-        dos.write(body, 0, body.length);
-        dos.flush();
-    }
-
-    public static void makeResponse(byte[] body, OutputStream out, String statusCode, String msg) throws IOException {
-        DataOutputStream dos = new DataOutputStream(out);
-        responseHeader(dos, statusCode, msg, body.length);
-        responseBody(dos, body);
-    }
 }
