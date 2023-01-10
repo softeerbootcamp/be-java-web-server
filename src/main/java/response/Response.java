8ac2c963180937001329a3836d5632e375cc12a0
jun-min1623 <junmin1623@naver.com>
2023-01-10 10:40:40 +0900

    feat: Response 분리

---
+++ src/main/java/response/Response.java
@@ -0,0 +1,34 @@
+package response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Response {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    public Response(){
+
+    }
+    public static void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public static void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import response.Response;
 import utils.RequestUtils;
 
 public class RequestHandler implements Runnable {
@@ -22,37 +23,22 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            Response response = new Response();
             BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
             String line = br.readLine();
             String path = RequestUtils.getFilePathByRequest(line);
             RequestUtils.printRequestLines(line,br);
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./src/main/resources/templates"+path).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            response.response200Header(dos, body.length);
+            response.responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
 }
