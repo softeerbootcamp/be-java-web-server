224711bb7cfd9ec9bccd0b730afdd5bab68372ec
solchan98 <dev.sol0427@gmail.com>
2023-01-10 16:26:25 +0900

    feat: HttpResponseWriter 예외 처리 수정 Request의 각 부분 마다 예외를 체크하지 않고 상위 메서드에서 통합 처리하도록 수정

--- src/main/java/http/util/HttpResponseWriter.java
+++ src/main/java/http/util/HttpResponseWriter.java
@@ -4,6 +4,8 @@ import http.common.HttpBody;
 import http.common.HttpHeaders;
 import http.common.HttpStatus;
 import http.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -11,46 +13,36 @@ import java.io.OutputStream;
 
 public class HttpResponseWriter {
     private HttpResponseWriter() {}
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponseWriter.class);
 
     private static final String HTTP_VERSION = "HTTP/1.1";
 
     public static void write(OutputStream out, HttpResponse response) {
-        DataOutputStream dos = new DataOutputStream(out);
-        writeStatus(dos, response.getStatus());
-        writeHeaders(dos, response.getHeaders());
-        writeBody(dos, response.getBody());
-    }
-
-    private static void writeStatus(DataOutputStream dos, HttpStatus status) {
         try {
-            String statusLine = HTTP_VERSION + " " + status.code() + " " + status.name() + "\r\n";
-            dos.writeBytes(statusLine);
+            DataOutputStream dos = new DataOutputStream(out);
+            writeStatus(dos, response.getStatus());
+            writeHeaders(dos, response.getHeaders());
+            writeBody(dos, response.getBody());
         } catch (IOException e) {
-            // TODO: INTERNAL_SERVER_ERROR 처리하기
-            throw new RuntimeException("응답 상태 정보가 잘못되었습니다.");
+            logger.error(e.getMessage());
         }
     }
 
-    private static void writeHeaders(DataOutputStream dos, HttpHeaders headers) {
-        try {
-            for (String key: headers.keys()) {
-                String header = key + " : " + headers.getValue(key) + "\r\n";
-                dos.writeBytes(header);
-            }
-        } catch (IOException e) {
-            // TODO: INTERNAL_SERVER_ERROR 처리하기
-            throw new RuntimeException("응답 헤더 정보가 잘못되었습니다.");
+    private static void writeStatus(DataOutputStream dos, HttpStatus status) throws IOException {
+        String statusLine = HTTP_VERSION + " " + status.code() + " " + status.name() + "\r\n";
+        dos.writeBytes(statusLine);
+    }
+
+    private static void writeHeaders(DataOutputStream dos, HttpHeaders headers) throws IOException {
+        for (String key: headers.keys()) {
+            String header = key + " : " + headers.getValue(key) + "\r\n";
+            dos.writeBytes(header);
         }
     }
 
-    private static void writeBody(DataOutputStream dos, HttpBody body) {
-        try {
-            dos.writeBytes("\n");
-            dos.write(body.data());
-            dos.flush();
-        } catch (IOException e) {
-            // TODO: INTERNAL_SERVER_ERROR 처리하기
-            throw new RuntimeException("응답 바디 정보가 잘못되었습니다.");
-        }
+    private static void writeBody(DataOutputStream dos, HttpBody body) throws IOException {
+        dos.writeBytes("\n");
+        dos.write(body.data());
+        dos.flush();
     }
 }
