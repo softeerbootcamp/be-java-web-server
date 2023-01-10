0ca9702bfe0971b39aaca8a0d4db94d3f18f2068
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-10 16:02:21 +0900

    일반화된 HttpResponse객체 사용

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -1,2 +1,60 @@
-package webserver;public class HttpResponse {
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpResponse {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+    private HttpStatus status;
+    private ContentType contentType;
+    private final Map<String, String> headers;
+    private final Map<String, String> cookies;
+    private byte[] body;
+
+
+    public HttpResponse() {
+        this.status = null;
+        this.contentType = null;
+        this.headers = new HashMap<>();
+        this.cookies = new HashMap<>();
+    }
+
+    public void addToHeader(String key, String val){
+        headers.put(key,val);
+    }
+
+    public void setStatus(HttpStatus status){
+        this.status = status;
+    }
+
+    public void setContentType(ContentType contentType) {
+        this.contentType = contentType;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+
+    public String getHeaderByKey(String key) {
+        return headers.get(key);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public ContentType getContentType() {
+        return contentType;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+
 }
