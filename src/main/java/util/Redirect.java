4f0c51b618301e842d279a8839cbb81f6a4ed59e
rentalhub <rentalhub@rentalhubui-MacBookPro.local>
2023-01-10 16:55:14 +0900

    getRedirectLink 메소드 생성 String으로 넘겨준 reqURL에 해당하는 redirect 링크 값이 있으면 반환 해당 값 통해 RequestHandler에서 redirect 할 계획

--- src/main/java/util/Redirect.java
+++ src/main/java/util/Redirect.java
@@ -2,10 +2,18 @@ package util;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Redirect {
-    private Map<String, String> redirectLink = new HashMap<>(){{
+    private static Map<String, String> redirectLink = new HashMap<>(){{
         put("create","/index.html");
     }};
 
+    public static String getRedirectLink(String reqURL){
+        for (String key: redirectLink.keySet()){
+            if (reqURL.contains(key))
+                return redirectLink.get(key);
+        }
+        return "";
+    }
 }
