407339fc752a7130090ba909b78295692424667c
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-10 12:29:17 +0900

    쿼리 스트링 파싱하는 메서드 구현
    & 와 = 기호를 기준으로 파싱하여 Map을 통해 각 쿼리명과 쿼리값을 저장하여 리턴

--- src/main/java/utils/HttpRequestUtils.java
+++ src/main/java/utils/HttpRequestUtils.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequestUtils {
-    //  userId=user&password=66a41ad2-bda9-4c70-807c-e0e13ff04475&name=qet&email=wf%40ags.c
 
     public static Map<String,String> parseQueryString(String str){
         String[] queryInfos = Utilites.stringParser(str,"&"); //userId=user , password=66a41ad2-bda9-4c70-807c-e0e13ff04475
