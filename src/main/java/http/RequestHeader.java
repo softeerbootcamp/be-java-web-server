868a2f510c1c2a91ef9baa7c51d961e42f5a95d3
ktykty0722 <ktykty0722@naver.com>
2023-01-10 11:35:22 +0900

    Feat : RequestHeader 클래스 추가 - 요청에서, 첫 번째 줄을 제외한 나머지 requestHeader를 저장해줄 클래스 추가

---
+++ src/main/java/http/RequestHeader.java
@@ -0,0 +1,21 @@
+package http;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toMap;
+
+public class RequestHeader {
+    private final Map<String, String> headers;
+
+    private RequestHeader(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public RequestHeader from(List<String> requestHeader) {
+        return requestHeader.stream()
+                .map(header -> header.split(" "))
+                .collect(collectingAndThen(toMap(header -> header[0], header -> header[1]), RequestHeader::new));
+    }
+}
