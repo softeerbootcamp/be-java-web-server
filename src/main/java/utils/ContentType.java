bb18a6ad6ed28263997cadf0a3a0723ea52e9c06
Toa <ld980105@snu.ac.kr>
2023-01-12 07:42:01 +0900

    :sparkles: contentType Enum 클래스로 구현

--- src/main/java/utils/ContentType.java
+++ src/main/java/utils/ContentType.java
@@ -1,4 +1,30 @@
 package utils;
 
 public enum ContentType {
+    HTML("html", "text/html"),
+    CSS("css", "text/css"),
+    JS("js", "text/javascript"),
+    JPEG("jpeg", "image/jpeg"),
+    PNG("png", "image/png"),
+    FAVICON("ico", "image/webp");
+
+    private final String extension;
+    private final String type;
+
+    ContentType(String extension, String type) {
+        this.extension = extension;
+        this.type = type;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public static ContentType getContentType(String type) {
+        try {
+            return valueOf(type.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("존재하지 않는 타입입니다.");
+        }
+    }
 }
