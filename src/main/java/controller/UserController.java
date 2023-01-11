b3df84959c47cbc9f30225f3b2ef304ccabae745
pjs0418 <tony180322@gmail.com>
2023-01-11 17:39:08 +0900

    UserController 수정 Logger 추가 부모 경로 추가 회원 가입 HTML 받아 오는 함수 추가 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -8,11 +8,20 @@ import model.response.Response;
 import model.response.StatusLine;
 import service.UserService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserController implements Controller {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
+
+    private static final String fileParentPath = "src/main/resources/templates/user";
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -21,7 +30,9 @@ public class UserController implements Controller {
     @Override
     public Response getResponse(RequestLine requestLine) {
         if(requestLine.getUri().startsWith("/user/create")) return createUser(requestLine);
-        else return Response.from(Status.NOT_FOUND);
+        else if(requestLine.getUri().equals("/user/form.html")) return getUserFormHtml(requestLine);
+
+        return Response.from(Status.NOT_FOUND);
     }
 
     private Response createUser(RequestLine requestLine) {
@@ -34,5 +45,21 @@ public class UserController implements Controller {
         headers.put(Header.of("Location"), "/index.html");
 
         return Response.of(StatusLine.from(Status.FOUND), headers);
+    }
+
+    private Response getUserFormHtml(RequestLine requestLine) {
+        Map<Header, String> headers = new HashMap<>();
+        headers.put(Header.of("Content-Type"), "text/html;charset=utf-8");
+
+        byte[] body = {};
+        try {
+            body = Files.readAllBytes(new File(fileParentPath + requestLine.getUri() + "form.html").toPath());
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+
+        headers.put(Header.of("Content-Length"), Integer.toString(body.length));
+
+        return Response.of(StatusLine.from(Status.OK), headers, body);
     }
 }
