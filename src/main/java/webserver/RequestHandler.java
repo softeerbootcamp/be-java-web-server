3ec589bd0537bfae7fea59e2ea5bfd5f2c7a1d9e
jieonkim23 <kwldjs1023@gmail.com>
2023-01-11 11:23:54 +0900

    :sparkles: ReqeustHandler를 HttpResponse와 HttpResponseUtil의 makeResponse를 활용하도록 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.util.Objects;
 
 import Request.HttpRequest;
+import Response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.*;
@@ -27,11 +28,21 @@ public class RequestHandler implements Runnable {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             HttpRequest httpRequest = HttpRequestUtil.parseRequest(in);
             Path path = FileIoUtil.mappingPath(httpRequest.getPath());
+
             if(Objects.nonNull(httpRequest.getParams()))
                 ManageDB.saveUser(httpRequest.getParams());
-            if(Objects.nonNull(path))
-                HttpResponseUtil.response(out, path);
+
+            HttpResponse httpResponse = HttpResponseUtil.makeResponse(httpRequest, path);
+            DataOutputStream dos = new DataOutputStream(out);
+            dos.writeBytes(httpResponse.toString());
+            logger.debug("response: \n"+httpResponse.toString());
+            responseBody(dos, httpResponse.getBody());
+
+//            if(Objects.nonNull(path))
+//                HttpResponseUtil.response(out, path);
         } catch (IOException e) {
+            logger.error(e.getMessage());
+        } catch (NullPointerException e){
             logger.error(e.getMessage());
         }
     }
@@ -48,6 +59,8 @@ public class RequestHandler implements Runnable {
     }
 
     private void responseBody(DataOutputStream dos, byte[] body) {
+        if(Objects.isNull(body))
+            throw new NullPointerException();
         try {
             dos.write(body, 0, body.length);
             dos.flush();
