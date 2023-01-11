a5af9bf02d33f8300cb175544e3b04500a510258
rentalhub <rentalhub@rentalhubui-MacBookPro.local>
2023-01-11 18:09:51 +0900

    MemoryUserRepositoryTest 작성 addUserTest: user가 리포지토리에 정상적으로 등록되는지 테스트 findUserByIdTest: 리포지토리의 user들 중 id에 부합하는 user가 find되는지 테스트 findAll: 등록된 유저가 모두 조회되는지 테스트

---
+++ src/test/java/model/repository/MemoryUserRepositoryTest.java
@@ -0,0 +1,45 @@
+package model.repository;
+
+import model.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryUserRepositoryTest {
+    MemoryUserRepository memoryUserRepository = new MemoryUserRepository();
+
+    @AfterEach
+    public void afterEach(){
+        memoryUserRepository.clearUsers();
+    }
+
+    @Test
+    void addUserTest() {
+        User user = new User("testId","testPW","testName","test@mail.com");
+        memoryUserRepository.addUser(user);
+        assertThat(memoryUserRepository.findUserById(user.getUserId()).get()).isEqualTo(user);
+    }
+
+    @Test
+    void findUserByIdTest() {
+        User user1 = new User("testId1","testPW","testName","test@mail.com");
+        User user2 = new User("testId2","testPW","testName","test@mail.com");
+        memoryUserRepository.addUser(user1);
+        assertThat(memoryUserRepository.findUserById("testId1").get()).isEqualTo(user1);
+    }
+
+    @Test
+    void findAll() {
+        User user1 = new User("testId1","testPW","testName","test@mail.com");
+        User user2 = new User("testId2","testPW","testName","test@mail.com");
+        memoryUserRepository.addUser(user1);
+        memoryUserRepository.addUser(user2);
+
+        Collection<User> result = memoryUserRepository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
