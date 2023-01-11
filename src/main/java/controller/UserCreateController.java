9f978599aff6de6bba4c00b41a22727d39c41feb
River-Mt <rivermt1998@gmail.com>
2023-01-11 11:41:48 +0900

    :sparkles: 회원가입과 관련된 UserCreateController 구현

---
+++ src/main/java/controller/UserCreateController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import http.HttpRequest;
+import http.HttpResponse;
+import http.Uri;
+import service.UserService;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class UserCreateController implements Controller {
+    private final List<String> paths;
+
+    public UserCreateController() {
+        this.paths = Collections.singletonList("/user/create");
+    }
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+        String httpMethod = httpRequest.getMethod().toString();
+        if(httpMethod.equals("GET")) {
+            doGet(httpRequest, httpResponse);
+        }
+    }
+
+    @Override
+    public boolean isUri(HttpRequest httpRequest) {
+        Uri uri = httpRequest.getUri();
+        return paths.stream().anyMatch(uri::isEndsWith);
+    }
+
+    public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+        Uri uri = httpRequest.getUri();
+        Map<String, String> queryParams = uri.getParameters();
+        UserService.create(queryParams);
+
+        httpResponse.setResponse("/index.html");
+    }
+}
