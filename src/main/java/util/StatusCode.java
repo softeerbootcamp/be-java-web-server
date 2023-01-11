f3a9a0d508898f8434f5978c999795beeace4ce8
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 02:18:58 +0900

    :fire: StatusCode enum class의 getStatusCode가 불필요하여 삭제

--- src/main/java/util/StatusCode.java
+++ src/main/java/util/StatusCode.java
@@ -1,12 +1,16 @@
 package util;
 
+import Request.HttpRequest;
+
+import java.util.Objects;
+
 public enum StatusCode {
     CONTINUE(100, "Continue"),
     OK(200, "OK"),
     CREATED(201, "Created"),
     ACCEPTED(202, "Accepted"),
     NO_CONTENT(204, "No Content"),
-    FOUNT(302, "Found"),
+    FOUND(302, "Found"),
     SEE_OTHER(303, "See Other"),
     TEMPORARY_REDIRECT(307, "Temporary Redirect"),
     NOT_FOUND(404, "Not Found");
@@ -17,9 +21,6 @@ public enum StatusCode {
     StatusCode(int code, String statusText) {
         this.code = code;
         this.statusText = statusText;
-    }
-    public static StatusCode getStatusCode(String code){
-        return valueOf(code);
     }
 
     @Override
