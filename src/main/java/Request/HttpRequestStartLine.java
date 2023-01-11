05410e9796783436fb056ec28961c8f4c6a03695
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 02:25:01 +0900

    :recycle: HttpRequestStartLine에 from 메소드 추가: HttpRequestUtil에 있던 기능을 옮겨옴.

--- src/main/java/Request/HttpRequestStartLine.java
+++ src/main/java/Request/HttpRequestStartLine.java
@@ -1,16 +1,34 @@
 package Request;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class HttpRequestStartLine {
     private String path;
     private String protocol;
     private String method;
+    private static final String BLANK = " ";
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestStartLine.class);
 
     public HttpRequestStartLine(String method, String path, String protocol) {
         this.path = path;
         this.protocol = protocol;
         this.method = method;
     }
-
+    public static HttpRequestStartLine from(BufferedReader br) {
+        String line = null;
+        try {
+            line = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        logger.debug("request : " + line);
+        String[] temp = line.split(BLANK);
+        return new HttpRequestStartLine(temp[0], temp[1], temp[2]);
+    }
     public String getPath() {
         return path;
     }
