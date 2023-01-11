cb48085b872879aa171797fb067e57538119077d
jieonkim23 <kwldjs1023@gmail.com>
2023-01-11 11:45:26 +0900

    :sparkles: HttpResponse에 ContentType 선택할 수 있도록 변경

--- src/main/java/Response/HttpResponse.java
+++ src/main/java/Response/HttpResponse.java
@@ -8,7 +8,7 @@ public class HttpResponse {
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private byte[] body;
     private StatusCode status;
-    private String contentType = "text/html;charset=UTF-8";
+    private String contentType;
     private String protocol;
 
     public HttpResponse(byte[] body, StatusCode status, String contentType, String protocol) {
@@ -26,7 +26,7 @@ public class HttpResponse {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("%s %s \r\n", this.protocol, this.status.toString()));
-        sb.append(String.format("Content-Type: %s \r\n", this.contentType));
+        sb.append(String.format("Content-Type: %s \r\n", this.contentType + ";charset=UTF-8"));
         sb.append(String.format("Content-Length: %d \r\n", this.body==null? 0 :this.body.length));
         sb.append("\r\n");
         return sb.toString();
