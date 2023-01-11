83f6d3bd0c921b756c6ef58225c006659782d598
Toa <ld980105@snu.ac.kr>
2023-01-12 00:00:57 +0900

    :art: URI 클래스명 변경

--- src/main/java/http/request/URI.java
+++ src/main/java/http/request/URI.java
@@ -2,11 +2,11 @@ package http.request;
 
 import java.util.Map;
 
-public class Uri {
+public class URI {
     private final String path;
     private final Map<String, String> querys;
 
-    public Uri(String path, Map<String, String> querys) {
+    public URI(String path, Map<String, String> querys) {
         this.path = path;
         this.querys = querys;
     }
