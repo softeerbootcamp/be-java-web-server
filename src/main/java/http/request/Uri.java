266b8ff1136125a83c256e0fd290fa70fd88710b
Toa <ld980105@snu.ac.kr>
2023-01-11 23:24:58 +0900

    :sparkles: URI 클래스 구현

--- src/main/java/http/request/Uri.java
+++ src/main/java/http/request/Uri.java
@@ -1,4 +1,21 @@
 package http.request;
 
+import java.util.Map;
+
 public class Uri {
+    private final String path;
+    private final Map<String, String> querys;
+
+    public Uri(String path, Map<String, String> querys) {
+        this.path = path;
+        this.querys = querys;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Map<String, String> getQuerys() {
+        return querys;
+    }
 }
