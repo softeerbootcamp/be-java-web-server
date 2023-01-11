ad2a5c096a7318593133bed52b30fbd7bba236a8
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 02:27:02 +0900

    :recycle: HttpRequestHeaders에 from 메소드 추가: HttpRequestUtil에 있던 기능을 옮겨옴.

--- src/main/java/Request/HttpRequestHeaders.java
+++ src/main/java/Request/HttpRequestHeaders.java
@@ -1,13 +1,29 @@
 package Request;
 
+import util.HttpRequestUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequestHeaders {
     private Map<String, String> headers = new HashMap<>();
-
     public HttpRequestHeaders(Map<String, String> headers) {
         this.headers = headers;
     }
 
+    public static HttpRequestHeaders from(BufferedReader br) {
+        Map<String, String> headers = null;
+        try {
+            headers = HttpRequestUtil.extractHeaders(br);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new HttpRequestHeaders(headers);
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
 }
