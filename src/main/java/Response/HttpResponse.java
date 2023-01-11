e67d95a85b758a3dce6fe137b5df3da2946d8650
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 02:31:20 +0900

    :sparkles: HttpResponse에 header 추가, createHttpResponse 구현: content type, content length 등이 각각의 필드로 구현했었는데 map을 이용하여 header로 묶어서 처리.

--- src/main/java/Response/HttpResponse.java
+++ src/main/java/Response/HttpResponse.java
@@ -1,34 +1,64 @@
 package Response;
 
+import Request.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.ContentType;
+import util.FileIoUtil;
+import util.HttpResponseUtil;
 import util.StatusCode;
+
+import java.nio.file.Path;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class HttpResponse {
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private byte[] body;
     private StatusCode status;
-    private String contentType;
     private String protocol;
+    private Map<String, String> headers;
 
-    public HttpResponse(byte[] body, StatusCode status, String contentType, String protocol) {
+    public HttpResponse(byte[] body, StatusCode status, String protocol, Map<String, String> headers) {
         this.body = body;
         this.status = status;
-        this.contentType = contentType;
         this.protocol = protocol;
+        this.headers = headers;
+    }
+
+    public static HttpResponse createHttpResponse(HttpRequest httpRequest, StatusCode statusCode) {
+        Path path = FileIoUtil.mappingPath(httpRequest.getPath());
+        byte[] body = null;
+        if (Objects.nonNull(path))
+            body = HttpResponseUtil.generateBody(path);
+
+        String protocol = httpRequest.getProtocol();
+
+        ContentType contentType = ContentType.PLAIN;
+        if (statusCode.equals(StatusCode.OK)) {
+            String ex = FileIoUtil.findExtension(httpRequest.getPath());
+            contentType = ContentType.valueOf(ex.toUpperCase());
+        }
+
+        Map<String, String> headers = new LinkedHashMap<>();                //headers
+        headers.put("Content-Type", contentType.getContentText());
+        headers.put("Content-Length", String.valueOf(body == null ? 0 : body.length));
+
+        return new HttpResponse(body, statusCode, protocol, headers);
+    }
+
+    public void putHeaders(String type, String value) {
+        this.headers.put(type, value);
     }
 
     public byte[] getBody() {
         return body;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format("%s %s \r\n", this.protocol, this.status.toString()));
-        sb.append(String.format("Content-Type: %s \r\n", this.contentType + ";charset=UTF-8"));
-        sb.append(String.format("Content-Length: %d \r\n", this.body==null? 0 :this.body.length));
-        sb.append("\r\n");
-        return sb.toString();
+    public String getHeaders() {
+        return this.protocol + " " + this.status.toString() + " \r\n" + this.headers.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue())
+                .collect(Collectors.joining(" \r\n")) + "\r\n";
     }
 }
