10436c0e96d6889605e73eb2dc372bac544f22e8
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 02:13:50 +0900

    :sparkles: ContentType을 enum class로 관리하기 위해 생성

---
+++ src/main/java/util/ContentType.java
@@ -0,0 +1,24 @@
+package util;
+
+public enum ContentType {
+    HTML("text/html"),
+    CSS("text/css"),
+    JS("text/javascript"),
+    WOFF("application/font-woff"),
+    TTF("application/font-ttf"),
+    EOT("application/vnd.ms-fontobject"),
+    OTF("application/font-otf"),
+    SVG("image/svg+xml"),
+    PNG("image/jpeg"),
+    ICO("image/x-icon"),
+    PLAIN("text/plain");
+    private String contentText;
+
+    ContentType(String contentText) {
+        this.contentText = contentText;
+    }
+
+    public String getContentText() {
+        return contentText + ";charset=UTF-8";
+    }
+}
