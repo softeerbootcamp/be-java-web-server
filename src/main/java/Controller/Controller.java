07b2d272926dabf4b6c0a7b5e5fbfc92623de007
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 02:32:23 +0900

    :sparkles: Controller 라는 abstract Class 를 구현

---
+++ src/main/java/Controller/Controller.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import Request.HttpRequest;
+
+import java.io.DataOutputStream;
+
+public abstract class Controller {
+    HttpRequest httpRequest;
+    DataOutputStream dos;
+
+    public Controller(HttpRequest httpRequest, DataOutputStream dos) {
+        this.httpRequest = httpRequest;
+        this.dos = dos;
+    }
+
+    public void response(){
+
+    }
+}
