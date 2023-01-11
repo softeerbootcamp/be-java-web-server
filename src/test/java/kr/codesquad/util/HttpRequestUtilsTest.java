50f66f20d554cfe762b25e70c8c97970377b2c00
Soomin-Lim <augustth511@gmail.com>
2023-01-11 17:27:20 +0900

    test: HttpRequestUtils 클래스의 parseRequestHeader() 메서드를 검증하는 테스트 코드 작성
    - parseRequestHeader(): 요청 헤더에서 이름/값을 추출하여 반환하는지 검증

--- src/test/java/kr/codesquad/util/HttpRequestUtilsTest.java
+++ src/test/java/kr/codesquad/util/HttpRequestUtilsTest.java
@@ -4,11 +4,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import util.HttpRequestUtils;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestUtilsTest {
+
+    @Test
+    @DisplayName("요청 헤더에서 이름/값을 추출하여 반환하는지 검증한다.")
+    void parseRequestHeader() {
+        List<String> headers = List.of("Host: localhost:8080", "Connection: keep-alive","Accept: text/html");
+
+        Map<String, String> result = HttpRequestUtils.parseRequestHeader(headers);
+
+        assertThat(result.get("Host")).isEqualTo("localhost:8080");
+        assertThat(result.get("Connection")).isEqualTo("keep-alive");
+        assertThat(result.get("Accept")).isEqualTo("text/html");
+    }
 
     @Test
     @DisplayName("URL에서 쿼리 스트링을 추출하여 반환하는지 검증한다.")
