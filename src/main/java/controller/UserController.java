43d3c65fdfe4470479bb3ebead323a7ce0db83dd
wonju-dev <wonjulee.dev@gmail.com>
2023-01-11 15:43:03 +0900

    refactor(UserController): 미사용 코드 제거

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,10 +1,8 @@
 package controller;
 
 import dto.UserInfoDTO;
-import filesystem.FileSystem;
 import io.request.HttpRequest;
 import io.request.PathParser;
-import io.response.FindResult;
 import io.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,17 +11,12 @@ import service.UserService;
 public class UserController implements Controller {
 
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
-    private final FileSystem fileSystem = new FileSystem();
     private final UserService userService = new UserService();
     private final PathParser pathParser = new PathParser();
 
     @Override
     public void handle(HttpRequest request, HttpResponse response) {
         logger.info("user controller called");
-        if (request.isStaticResourceRequest()) {
-            handleStaticResourceRequest(request, response);
-            return;
-        }
         handleRequest(request, response);
     }
 
@@ -31,11 +24,5 @@ public class UserController implements Controller {
         UserInfoDTO userInfo = new UserInfoDTO(request.getAllQuery());
         userService.signIn(userInfo);
         response.redirect(pathParser.getIndexPageUrl());
-    }
-
-    private void handleStaticResourceRequest(HttpRequest request, HttpResponse response) {
-        FindResult findResult = fileSystem.findResource(request.getUrl());
-        response.update(findResult);
-        response.send();
     }
 }
