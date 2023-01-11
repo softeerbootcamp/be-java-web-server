a0ef8cf324a3bd67b9581c32a76a713a459f310f
Toa <ld980105@snu.ac.kr>
2023-01-11 12:00:21 +0900

    :art: Extension을 구하는 메서드를 분리함

--- src/main/java/utils/FileIoUtils.java
+++ src/main/java/utils/FileIoUtils.java
@@ -1,19 +1,30 @@
 package utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Set;
 
 public class FileIoUtils {
     private static final String STATIC_RESOURCE_PATH = FileIoUtils.class.getClassLoader().getResource("./static").getPath();
-    private static final String TEMPLATE_RESOURCE_PATH = FileIoUtils.class.getClassLoader().getResource("./template").getPath();
+    private static final String TEMPLATE_RESOURCE_PATH = FileIoUtils.class.getClassLoader().getResource("./templates").getPath();
+    private static final Set<String> TEMPLATE_EXTENSIONS = Set.of("html", "ico");
+    private static final Logger logger = LoggerFactory.getLogger(FileIoUtils.class);
 
     public static byte[] loadFile(String file) {
         try {
-            if (file.contains("html") || file.contains("ico"))
+            String fileExtension = getExtension(file);
+            if (TEMPLATE_EXTENSIONS.contains(fileExtension))
                 return Files.readAllBytes(new File(TEMPLATE_RESOURCE_PATH + file).toPath());
             return Files.readAllBytes(new File(STATIC_RESOURCE_PATH + file).toPath());
         } catch (Exception e) {
             return StatusCode.NOTFOUND.toString().getBytes();
         }
+    }
+
+    private static String getExtension(String file){
+        return file.substring(file.lastIndexOf(".") + 1);
     }
 }
