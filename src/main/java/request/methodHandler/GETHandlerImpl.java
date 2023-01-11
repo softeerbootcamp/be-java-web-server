1d43a5713cea4a4b8db52622701ee879560d815e
201724554 <pay7845@naver.com>
2023-01-11 18:53:05 +0900

    request에서 요청한 url에 따라 requestUrlEnum에서 정의한 메소드로 분기하는 기능 추가

--- src/main/java/request/methodHandler/GETHandlerImpl.java
+++ src/main/java/request/methodHandler/GETHandlerImpl.java
@@ -1,38 +1,35 @@
 package request.methodHandler;
 
-import model.User;
 import request.Request;
-import request.RequestParser;
+import request.methodHandler.RequestUrlEnum.GETRequestUrlEnum;
 
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Map;
+import java.util.List;
 
 public class GETHandlerImpl implements HttpMethodHandler {
     private final DataOutputStream dos;
-    private final String USER_REGISTER_URL = "/user/create";
 
     public GETHandlerImpl(DataOutputStream dos) {
         this.dos = dos;
     }
 
     @Override
-    public void handle(Request request) throws IOException {
-        handleGET(request);
+    public void handle(Request request) {
+        for(GETRequestUrlEnum requestUrlEnum : GETRequestUrlEnum.values()) {
+            if(this.contains(requestUrlEnum.getSupportingUrl(requestUrlEnum), request.getResource())) {
+                requestUrlEnum.handle(request, dos);
+                return;
+            }
+        }
+        //404 exception
     }
 
-    private void handleGET(Request request) throws IOException {
-        if(request.getResource().startsWith(USER_REGISTER_URL)) {
-            Map<String, String> parsedGETQueryString = RequestParser.parseGETQueryString(request.getResource());
-            User user = new User(parsedGETQueryString);
-
-            return;
+    boolean contains(List<String> list, String resource) {
+        for(String token : list) {
+            if(resource.contains(token)) {
+                return true;
+            }
         }
-
-        Files.readAllBytes(new File("src/main/resources/"
-                + (request.getResource().contains(".html") || request.getResource().contains(".ico")
-                ? "templates" : "static") + request.getResource()).toPath());
+        return false;
     }
 }
