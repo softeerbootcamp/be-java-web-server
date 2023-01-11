8557de1a001b9ff021737180b9276e64dddcfaf2
rohsik2 <rohsik2@gmail.com>
2023-01-11 17:23:30 +0900

    :recycle: 정적 팩토리 매서드 사용코드로 변경 및 리펙토링

--- src/main/java/controller/RequestController.java
+++ src/main/java/controller/RequestController.java
@@ -7,15 +7,18 @@ import httpMock.constants.StatusCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+
 public interface RequestController {
     Logger logger = LoggerFactory.getLogger(RequestController.class);
 
     static CustomHttpResponse NOT_FOUND() {
-        CustomHttpResponse res = new CustomHttpResponse();
-        res.setStatusCode(StatusCode.NOT_FOUND);
-        res.setContentType(ContentType.TEXT_PLAIN);
-        res.setBody("404 not found".getBytes());
-        return res;
+        return CustomHttpResponse.of(
+                StatusCode.NOT_FOUND,
+                ContentType.TEXT_PLAIN,
+                new HashMap<>(),
+                "404 not found".getBytes()
+        );
     }
 
     CustomHttpResponse handleRequest(CustomHttpRequest req);
