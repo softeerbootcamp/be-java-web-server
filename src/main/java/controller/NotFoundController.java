c2d9770f59ffcf815e35ab6d6a509254c30d458d
pjs0418 <tony180322@gmail.com>
2023-01-11 11:49:58 +0900

    Response에서 변경된 함수명 적용(of -> from)

--- src/main/java/controller/NotFoundController.java
+++ src/main/java/controller/NotFoundController.java
@@ -7,6 +7,6 @@ import model.response.Response;
 public class NotFoundController implements Controller {
     @Override
     public Response getResponse(RequestLine requestLine) {
-        return Response.of(Status.NOT_FOUND);
+        return Response.from(Status.NOT_FOUND);
     }
 }
