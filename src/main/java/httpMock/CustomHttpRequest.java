085f0f2006584ba36f20b3c61ea8ee33d70cfc10
rohsik2 <rohsik2@gmail.com>
2023-01-11 12:11:27 +0900

    :recycle: from, of를 사용한 정적 팩토리 메서드로 교체

--- src/main/java/httpMock/CustomHttpRequest.java
+++ src/main/java/httpMock/CustomHttpRequest.java
@@ -1,38 +1,80 @@
 package httpMock;
 
+import controller.RequestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CustomHttpRequest {
     private static final Logger logger = LoggerFactory.getLogger(CustomHttpRequest.class);
-    private final Map<String, String> requestParams;
-    private final Map<String, List<String>> requestHeaders;
+    private Map<String, String> requestParams;
+    private Map<String, List<String>> requestHeaders;
     private String httpMethod;
     private String protocolVersion;
     private String url;
 
-    public CustomHttpRequest(InputStream inputStream) {
-        requestHeaders = new HashMap<>();
-        setFirstLineHeaders(inputStream);
-        requestParams = setRequestParams();
+    public static CustomHttpRequest from(InputStream inputStream){
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader br = new BufferedReader(inputStreamReader);
+        try {
+            String firstLine = br.readLine();
+            List<String> headers = br.lines().collect(Collectors.toList());
+            return of(firstLine, headers );
+        }
+        catch (IOException e){
+            logger.error(e.getMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    public static CustomHttpRequest of(String firstLine, List<String> headers){
+        return new CustomHttpRequest(firstLine, headers);
+    }
+
+    private CustomHttpRequest(String firstLine, List<String> headers) {
+        setFirstLineHeaders(firstLine);
+        setRequestHeaders(headers);
+        setRequestParams(this.url);
         logger.info("url : " + this.url + ", method : " + this.httpMethod + ", params : " + this.requestParams);
     }
 
-    public String getUrl() {
-        return url;
+    private void setFirstLineHeaders(String firstLine) {
+        String[] firstLineDatas = firstLine.split("\\s");
+
+        if (firstLineDatas.length != 3)
+            throw new IllegalArgumentException("First Line of Http must have 2 space : " + firstLine);
+
+        if (firstLineDatas[1].equals("/") || firstLineDatas[1].equals(""))
+            firstLineDatas[1] = "/index.html";
+
+        this.httpMethod = firstLineDatas[0];
+        this.url = java.net.URLDecoder.decode(firstLineDatas[1], StandardCharsets.UTF_8);
+        this.protocolVersion = firstLineDatas[2];
     }
 
-    public Map<String, String> getRequestParams() {
-        return requestParams;
+    private void setRequestHeaders(List<String> headers){
+        requestHeaders = new HashMap<>();
+        headers.forEach(this::addToRequestHeader);
     }
 
-    public String getProtocolVersion() {
-        return protocolVersion;
+    private void setRequestParams(String url) {
+        requestParams = new HashMap<>();
+        if (url.contains("?")) {
+            String tempParamStr = url.substring(url.indexOf("?") + 1);
+            String[] params = tempParamStr.split("&");
+            for (String param : params) {
+                try {
+                    String[] data = param.split("=");
+                    requestParams.put(data[0], data[1]);
+                } catch (Exception e) {
+                    logger.error("String split failed : " + param);
+                }
+            }
+        }
     }
 
     private void addToRequestHeader(String line) {
@@ -46,47 +88,17 @@ public class CustomHttpRequest {
         this.requestHeaders.put(itemName, itemList);
     }
 
-    private Map<String, String> setRequestParams() {
-        Map<String, String> requestParams = new HashMap<>();
-        if (url.contains("?")) {
-            String tempParamStr = url.substring(url.indexOf("?") + 1);
-            String[] params = tempParamStr.split("&");
-            for (String param : params) {
-                try {
-                    String[] data = param.split("=");
-                    requestParams.put(data[0], data[1]);
-                } catch (Exception e) {
-                    logger.error("String split failed : " + param);
-                }
-            }
-        }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Map<String, String> getRequestParams() {
         return requestParams;
     }
 
-    private void setFirstLineHeaders(InputStream inputStream) {
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader br = new BufferedReader(inputStreamReader);
-        try {
-            String[] firstLine = firstLineSpliter(br.readLine());
-            this.httpMethod = firstLine[0];
-            this.url = java.net.URLDecoder.decode(firstLine[1], StandardCharsets.UTF_8);
-            this.protocolVersion = firstLine[2];
-            while (br.ready()) {
-                addToRequestHeader(br.readLine());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String[] firstLineSpliter(String firstLine) {
-        String[] result = firstLine.split("\\s");
-        if (result.length != 3) {
-            throw new IllegalArgumentException("First Line of Http must have 2 space : " + firstLine);
-        }
-        if (result[1].equals("/") || result[1].equals(""))
-            result[1] = "/index.html";
-        return result;
+    public String getProtocolVersion() {
+        return protocolVersion;
     }
 
 
