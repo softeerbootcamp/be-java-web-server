902d8ceb6e6ac6c670a9b1d88fb9626874936503
solchan98 <dev.sol0427@gmail.com>
2023-01-11 16:33:12 +0900

    feat: HttpExceptionHandler 수정 notFoundExceptionHandler 메서드에서 로깅을 남길 때 요청 정보를 좀 더 남기고자 HttpRequest를 파라미터로 전달 받도록 수정

--- src/main/java/http/exception/HttpExceptionHandler.java
+++ src/main/java/http/exception/HttpExceptionHandler.java
@@ -3,10 +3,14 @@ package http.exception;
 import http.common.HttpBody;
 import http.common.HttpStatus;
 import http.common.MediaType;
+import http.request.HttpRequest;
 import http.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.ResourceUtils;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 public final class HttpExceptionHandler {
     private HttpExceptionHandler() {}
@@ -14,14 +18,17 @@ public final class HttpExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(HttpExceptionHandler.class);
     
-    public static void handle(HttpException e, HttpResponse response) {
+    public static void handle(HttpRequest request, HttpResponse response, HttpException e) {
         if (e.status == HttpStatus.NOT_FOUND) {
-            notFoundExceptionHandler(response, e.getMessage());
+            notFoundExceptionHandler(request, response, e.getMessage());
         }
     }
 
-    private static void notFoundExceptionHandler(HttpResponse response, String message) {
-        logger.debug("{}  {} - {}", EXCEPTION_PREFIX, HttpStatus.NOT_FOUND, message);
+    private static void notFoundExceptionHandler(HttpRequest request, HttpResponse response, String message) {
+        logger.debug("{} {} [{}] - {}",
+                EXCEPTION_PREFIX, response.getStatus(),
+                URLDecoder.decode(request.getPath(), StandardCharsets.UTF_8)
+                , message);
 
         response.setStatus(HttpStatus.NOT_FOUND);
         HttpBody body = new HttpBody(ResourceUtils.loadFileFromClasspath("/not_found.html"));
