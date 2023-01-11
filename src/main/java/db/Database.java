9f92b64c329044fb0941efb4212b1a65f9a1b439
pjs0418 <tony180322@gmail.com>
2023-01-11 16:56:16 +0900

    멤버 함수 static 삭제

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -10,15 +10,15 @@ import java.util.Map;
 public class Database {
     private static final Map<String, User> users = Maps.newHashMap();
 
-    public static void addUser(User user) {
+    public void addUser(User user) {
         users.put(user.getUserId(), user);
     }
 
-    public static User findUserById(String userId) {
+    public User findUserById(String userId) {
         return users.get(userId);
     }
 
-    public static Collection<User> findAll() {
+    public Collection<User> findAll() {
         return users.values();
     }
 }
