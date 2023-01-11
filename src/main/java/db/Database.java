96a25cac1c9691bfec7c176fdb0ad077044e6882
pjs0418 <tony180322@gmail.com>
2023-01-11 16:51:01 +0900

    멤버 변수 final로 변경

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 import java.util.Map;
 
 public class Database {
-    private static Map<String, User> users = Maps.newHashMap();
+    private static final Map<String, User> users = Maps.newHashMap();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
