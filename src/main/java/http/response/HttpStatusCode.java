888442499daeb9b4dd797c3e4a164b1f448cf079
Soomin-Lim <augustth511@gmail.com>
2023-01-11 12:36:41 +0900

    feat: HttpStatusCode 클래스에 상태 코드와 메시지를 저장하는 멤버변수 statusCode, message 추가

--- src/main/java/http/response/HttpStatusCode.java
+++ src/main/java/http/response/HttpStatusCode.java
@@ -1,4 +1,14 @@
 package http.response;
 
 public enum HttpStatusCode {
+    OK(200, "OK");
+
+    private final int statusCode;
+    private final String message;
+
+
+    HttpStatusCode(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
 }
