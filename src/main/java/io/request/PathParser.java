1f50455672aace1ed98f0a3c7235a66101470b93
wonju-dev <wonjulee.dev@gmail.com>
2023-01-11 12:28:43 +0900

    refactor(PathParser): 정적 파일 경로 파싱 기능 리펙토링

--- src/main/java/io/request/PathParser.java
+++ src/main/java/io/request/PathParser.java
@@ -1,54 +1,29 @@
 package io.request;
 
-import java.util.List;
+import java.util.Map;
 
 public class PathParser {
 
-    private static final String TEMPLATE_PATH = "src/main/resources/templates/%s";
-    private static final String STATIC_PATH = "src/main/resources/static/%s";
-    private static final List<String> statics = List.of("css", "fonts", "images", "js");
+    private static final String TEMPLATE_PATH = "src/main/resources/templates%s";
+    private static final String STATIC_PATH = "src/main/resources/static%s";
     private static final String DOMAIN = "/";
     public static final String NOT_FOUND_HTML = "notfound.html";
     public static final String INDEX_HTML = "index.html";
 
-    public String parse(String url) {
-        if (isIndexPage(url)) {
-            return getIndexPagePath();
-        }
-        if (isHtml(url)) {
-            return String.format(TEMPLATE_PATH, url);
-        }
-        if (isStaticResource(url)) {
-            return String.format(STATIC_PATH, url);
-        }
-        return String.format(TEMPLATE_PATH, NOT_FOUND_HTML);
-    }
+    private static final Map<String, String> mappingInfo = Map.of(
+            "/", TEMPLATE_PATH + INDEX_HTML,
+            ".html", TEMPLATE_PATH,
+            ".css", STATIC_PATH,
+            ".js", STATIC_PATH,
+            "*", NOT_FOUND_HTML
+    );
 
-    public String getIndexPagePath() {
-        return String.format(TEMPLATE_PATH, INDEX_HTML);
+    public String parse(String url) {
+        String extension = mappingInfo.keySet().stream().filter(key -> url.endsWith(key)).findAny().orElse("*");
+        return String.format(mappingInfo.get(extension), url);
     }
 
     public String getIndexPageUrl() {
         return DOMAIN;
-    }
-
-    private boolean isStaticResource(String url) {
-        return statics.contains(getFileExtension(url));
-    }
-
-    private boolean isHtml(String url) {
-        return url.contains("html");
-    }
-
-    private boolean isIndexPage(String url) {
-        return url.equals("/") || url.split("/").length == 0;
-    }
-
-    private String getFileExtension(String url) {
-        String[] chunks = url.split("\\.", 2);
-        if (chunks.length >= 2) {
-            return chunks[1];
-        }
-        return "";
     }
 }
