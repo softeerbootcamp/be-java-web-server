ced61fff84baf51fa256c5c6fa4212f5145b2e99
pjs0418 <tony180322@gmail.com>
2023-01-11 18:00:50 +0900

    Logger로 User 회원 가입 잘 됐는지 확인

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -4,14 +4,24 @@ import com.google.common.collect.Maps;
 
 import model.domain.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Collection;
 import java.util.Map;
 
 public class Database {
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
+
     private static final Map<String, User> users = Maps.newHashMap();
 
     public void addUser(User user) {
         users.put(user.getUserId(), user);
+
+        for(User testUser : users.values()) {
+            logger.debug("userId: {}, password: {}, name: {}, email: {}",
+                    testUser.getUserId(), testUser.getPassword(), testUser.getName(), testUser.getEmail());
+        }
     }
 
     public User findUserById(String userId) {
