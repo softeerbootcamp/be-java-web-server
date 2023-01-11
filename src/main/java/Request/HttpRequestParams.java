a5f4738c9d3ed8e7d771089dc8b478ba5b15c47b
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 02:26:14 +0900

    :recycle: HttpRequestParams에 from 메소드 추가: HttpRequestUtil에 있던 기능을 옮겨옴.

--- src/main/java/Request/HttpRequestParams.java
+++ src/main/java/Request/HttpRequestParams.java
@@ -1,13 +1,28 @@
 package Request;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.FileIoUtil;
+import util.HttpRequestUtil;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class HttpRequestParams {
     private Map<String, String> params = new HashMap<>();
 
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestParams.class);
     public HttpRequestParams(Map<String, String> params) {
         this.params = params;
+    }
+    public static HttpRequestParams from(String path) {
+        String query = FileIoUtil.checkCreateUser(path);
+        if(Objects.nonNull(query)){
+            Map<String, String> params = HttpRequestUtil.extractParams(query);
+            return new HttpRequestParams(params);
+        }
+        return null;
     }
 
     public Map<String, String> getParams() {
