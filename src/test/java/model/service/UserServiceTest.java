7a6b299ae33a3df46704e073477504da3532b0f1
rentalhub <rentalhub@rentalhubui-MacBookPro.local>
2023-01-11 17:57:51 +0900

    UserServiceTest 작성 회원 가입 및 중복 아이디 회원 가입 테스트 회원 가입 시 findOneUser(userId)로 성공 테스트 중복 아이디 회원 가입 시 IllegalStateException을 적절한 메시지와 함께 발생시키는지 확인

--- src/test/java/model/service/UserServiceTest.java
+++ src/test/java/model/service/UserServiceTest.java
@@ -1,4 +1,47 @@
+package model.service;
+
+import model.domain.User;
+import model.repository.MemoryUserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserServiceTest {
-  
+
+    UserService userService;
+    MemoryUserRepository memoryUserRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memoryUserRepository = new MemoryUserRepository();
+        userService = new UserService(memoryUserRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memoryUserRepository.clearUsers();
+    }
+    @Test
+    void joinTest() {
+        User user = new User("testId","testPW","testName","test@mail.com");
+        userService.join(user);
+        User findUser = userService.findOneUser("testId").get();
+        assertThat(findUser).isEqualTo(user);
+    }
+
+    @Test
+    void joinDuplicateExceptionTest(){
+        User user1 = new User("testId","testPW","testName","test@mail.com");
+        User user2 = new User("testId","testPW2","testName2","test2@mail.com");
+        userService.join(user1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                ()->userService.join(user2));
+        assertThat(e.getMessage()).isEqualTo("중복된 아이디입니다. 다른 아이디를 사용해주세요.");
+    }
+
 }
