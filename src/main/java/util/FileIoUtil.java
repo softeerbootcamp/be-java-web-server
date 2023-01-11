17fe7afb1a787b64a463beb894288ff5e8297396
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 02:24:37 +0900

    :recycle: FileIoUtil에 checkCreateUser메소드 추가: HttpRequestUtil에 있던 메소드를 FileIoUtil에 있는 것이 더 적합하다 생각하여 옮겨옴.

--- src/main/java/util/FileIoUtil.java
+++ src/main/java/util/FileIoUtil.java
@@ -12,6 +12,15 @@ public class FileIoUtil {
     private static Set<String> templateTypes = new HashSet<>(Arrays.asList("html", "ico"));
     private static Set<String> staticTypes = new HashSet<>(Arrays.asList("css", "js", "eot", "svg", "ttf", "woff", "woff2", "png"));
 
+    private static final String QUESTION = "?";
+
+    public static String checkCreateUser(String path){
+        if (path.startsWith("/user/create")) {
+            int idx = path.indexOf(QUESTION);
+            return path.substring(idx + 1);
+        }
+        return null;
+    }
     public static Path mappingPath(String path) {
         String ex = findExtension(path);
         if(templateTypes.contains(ex)) return new File(TEMPLATE_DIR+path).toPath();
