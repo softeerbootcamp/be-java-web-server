6749fbb568373a5e61d8ba9914fcdfca66766c28
jieonkim23 <kwldjs1023@gmail.com>
2023-01-11 12:17:58 +0900

    :recycle: FileIoUtil의 mappintPath()에서 extention확인하는 부분을 set을 활용하도록 리팩토링

--- src/main/java/util/FileIoUtil.java
+++ src/main/java/util/FileIoUtil.java
@@ -2,28 +2,26 @@ package util;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FileIoUtil {
     private static final String TEMPLATE_DIR = "./src/main/resources/templates";
     private static final String STATIC_DIR = "./src/main/resources/static";
+    private static Set<String> templateTypes = new HashSet<>(Arrays.asList("html", "ico"));
+    private static Set<String> staticTypes = new HashSet<>(Arrays.asList("css", "js", "eot", "svg", "ttf", "woff", "woff2", "png"));
 
     public static Path mappingPath(String path) {
         String ex = findExtension(path);
-        if(checkTemplate(ex)) return new File(TEMPLATE_DIR+path).toPath();
-        if(checkStatic(ex)) return new File(STATIC_DIR+path).toPath();
-
+        if(templateTypes.contains(ex)) return new File(TEMPLATE_DIR+path).toPath();
+        if(staticTypes.contains(ex)) return new File(STATIC_DIR+path).toPath();
         return null;
     }
     public static String findExtension(String path){
         String[] temp = path.split("\\.");
         String extension = temp[temp.length - 1];
         return extension;
-    }
-    public static boolean checkTemplate(String ex){
-        return ex.equals("html") || ex.equals("ico");
-    }
-    public static boolean checkStatic(String ex){
-        return ex.equals("html") || ex.equals("ico");
     }
 
 }
