a606298b0382d67bde37c530987c943675e3ca74
jieonkim23 <kwldjs1023@gmail.com>
2023-01-11 11:16:04 +0900

    :sparkles: HttpResponse의 필드를 추가학고 해당 생성자 추가와 toString오버라이드: 앞으로 HttpResponse를 클래스로 관리하고 이용하기 위해 필드 수정함.

--- src/main/java/Response/HttpResponse.java
+++ src/main/java/Response/HttpResponse.java
@@ -3,15 +3,32 @@ package Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataOutputStream;
-
 public class HttpResponse {
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private byte[] body;
-    DataOutputStream dos;
+    private String statusCode;
+    private String statusText;
+    private String contentType = "text/html;charset=UTF-8";
+    private String protocol;
 
-    public HttpResponse(byte[] body) {
+    public HttpResponse(byte[] body, String statusCode, String statusText, String protocol) {
         this.body = body;
+        this.statusCode = statusCode;
+        this.statusText = statusText;
+        this.protocol = protocol;
     }
 
+    public byte[] getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s %s %s\r\n", this.protocol, this.statusCode, this.statusText));
+        sb.append(String.format("Content-Type: %s \r\n", this.contentType));
+        sb.append(String.format("Content-Length: %d \r\n", this.body==null? 0 :this.body.length));
+        sb.append("\r\n");
+        return sb.toString();
+    }
 }
