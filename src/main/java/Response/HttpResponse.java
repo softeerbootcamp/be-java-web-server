bcb7fdfd0beedd7a360661ed5670e28e0d142496
jieonkim23 <kwldjs1023@gmail.com>
2023-01-11 11:34:30 +0900

    :sparkles: HttpResponse에서 Status enum을 활용하도록 변경

--- src/main/java/Response/HttpResponse.java
+++ src/main/java/Response/HttpResponse.java
@@ -2,19 +2,19 @@ package Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.StatusCode;
 
 public class HttpResponse {
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private byte[] body;
-    private String statusCode;
-    private String statusText;
+    private StatusCode status;
     private String contentType = "text/html;charset=UTF-8";
     private String protocol;
 
-    public HttpResponse(byte[] body, String statusCode, String statusText, String protocol) {
+    public HttpResponse(byte[] body, StatusCode status, String contentType, String protocol) {
         this.body = body;
-        this.statusCode = statusCode;
-        this.statusText = statusText;
+        this.status = status;
+        this.contentType = contentType;
         this.protocol = protocol;
     }
 
@@ -25,7 +25,7 @@ public class HttpResponse {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format("%s %s %s\r\n", this.protocol, this.statusCode, this.statusText));
+        sb.append(String.format("%s %s \r\n", this.protocol, this.status.toString()));
         sb.append(String.format("Content-Type: %s \r\n", this.contentType));
         sb.append(String.format("Content-Length: %d \r\n", this.body==null? 0 :this.body.length));
         sb.append("\r\n");
