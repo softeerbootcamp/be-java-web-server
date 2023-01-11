9af76dadb6dbc4a72da470216c475362009950ff
rohsik2 <rohsik2@gmail.com>
2023-01-11 12:18:22 +0900

    :bug: stream -> lines().collect -> while loop
     collect 사용시 pending 상태로 대기해서 응답이 없는 문제가 발생함

--- src/main/java/httpMock/CustomHttpRequest.java
+++ src/main/java/httpMock/CustomHttpRequest.java
@@ -1,13 +1,14 @@
 package httpMock;
 
-import controller.RequestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class CustomHttpRequest {
     private static final Logger logger = LoggerFactory.getLogger(CustomHttpRequest.class);
@@ -22,8 +23,11 @@ public class CustomHttpRequest {
         BufferedReader br = new BufferedReader(inputStreamReader);
         try {
             String firstLine = br.readLine();
-            List<String> headers = br.lines().collect(Collectors.toList());
-            return of(firstLine, headers );
+            List<String> headers = new ArrayList<>();
+            while (br.ready()) {
+                headers.add(br.readLine());
+            }
+            return of(firstLine, headers);
         }
         catch (IOException e){
             logger.error(e.getMessage());
