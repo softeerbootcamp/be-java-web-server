3810204c580efe4f13f9e061352590a5d8b1ae7e
Soomin-Lim <augustth511@gmail.com>
2023-01-11 17:11:24 +0900

    feat: 응답 메시지를 출력하는 기능을 RequestHandler에서 HttpResonse 클래스로 이동
    - HttpResponse 클래스에 바디 메시지를 저장하는 byte[] 타입 멤버변수 추가
    - HttpResonse의 addHttpHeader() 메서드 구현
    - HttpResonse에 setBody() 메서드 추가
        - 바디 메시지를 저장함과 동시에 헤더에 "Content-Length" 헤더 추가
    - HttpResonse에 response() 메서드 추가
        - 응답 메시지 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -34,30 +34,10 @@ public class RequestHandler implements Runnable {
             String viewPath = ViewResolver.process(viewName);
 
             byte[] body = Files.readAllBytes(new File(viewPath).toPath());
+            httpResponse.setBody(body);
 
             DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            httpResponse.response(dos);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
