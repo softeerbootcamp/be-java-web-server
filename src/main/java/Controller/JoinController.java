14e02545b466fd3910a26b98fdc997364962cb06
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 18:03:20 +0900

    :loud_sound: JoinController에 로거 생성하고 로그 추가, HttpResponse 멤버변수 사용으로 변경

--- src/main/java/Controller/JoinController.java
+++ src/main/java/Controller/JoinController.java
@@ -2,6 +2,8 @@ package Controller;
 
 import Request.HttpRequest;
 import Response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.HttpResponseUtil;
 import util.ManageDB;
 import util.StatusCode;
@@ -9,18 +11,20 @@ import util.StatusCode;
 import java.io.DataOutputStream;
 
 public class JoinController extends Controller {
+    private final Logger logger = LoggerFactory.getLogger(JoinController.class);
 
     public JoinController(HttpRequest httpRequest, DataOutputStream dos) {
         super(httpRequest, dos);
+        logger.debug("select joinController");
     }
 
     @Override
     public void response() {
         super.response();
         ManageDB.saveUser(this.httpRequest);
-        HttpResponse httpResponse = HttpResponse.createHttpResponse(this.httpRequest, StatusCode.FOUND);
-        httpResponse.putHeaders("Location", "/index.html");
-        httpResponse.putHeaders("Content-Length", "0");
-        HttpResponseUtil.outResponse(dos, httpResponse);
+        this.httpResponse = HttpResponse.createHttpResponse(this.httpRequest, StatusCode.FOUND);
+        this.httpResponse.putHeaders("Location", "/index.html");
+        this.httpResponse.putHeaders("Content-Length", "0");
+        HttpResponseUtil.outResponse(this.dos, this.httpResponse);
     }
 }
