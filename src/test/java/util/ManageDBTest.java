2d8ee8c9c62ef2b8603b632f078d2715d9983a45
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 10:35:20 +0900

    :white_check_mark: ManageDBTest에서 saveUser메소드를  BDD 방식으로 테스트함

--- src/test/java/util/ManageDBTest.java
+++ src/test/java/util/ManageDBTest.java
@@ -1,11 +1,13 @@
 package util;
 
+import Request.HttpRequest;
 import model.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,20 +15,26 @@ public class ManageDBTest {
     private final Logger logger = LoggerFactory.getLogger(ManageDBTest.class);
     @Test
     public void saveUserTest(){
-        Map<String,String> querys = new HashMap<>(){
-            {
-                put("userId","jieon");
-                put("password", "kdjfk");
-                put("name","kim");
-                put("email", "kim@naver.com");
-            }
-        };
-        User user = new User(querys.get("userId"),querys.get("password"),querys.get("kim"),querys.get("email"));
+        //given
 
-        ManageDB.saveUser(querys);
-        User result = ManageDB.findUserById("jieon");
-        logger.debug("test: "+result.toString());
+        User user = new User("jieon","1234","kim","kim@naver.com");
+        InputStream in = new ByteArrayInputStream("GET /user/create?userId=jieon&password=1234&name=kim&email=kim%40naver.com HTTP/1.1".getBytes());
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            HttpRequest httpRequest = HttpRequest.createReqeust(br);
+        //when
+            ManageDB.saveUser(httpRequest);
 
-        Assertions.assertThat(result.equals(user));
+        //then
+            User result = ManageDB.findUserById("jieon");
+            logger.debug("test: "+result.toString());
+            Assertions.assertThat(result.getUserId()).isEqualTo(user.getUserId());
+            Assertions.assertThat(result.getName()).isEqualTo(user.getName());
+            Assertions.assertThat(result.getPassword()).isEqualTo(user.getPassword());
+            Assertions.assertThat(result.getEmail()).isEqualTo(user.getEmail());
+
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
