aa2b19d657ea5e5b9bba2252565991223cb38edd
jaehunLee-dev <jaehunLee.dev@gmail.com>
2023-01-12 11:08:01 +0900

    sign up form 수정 javascript 통해 빈칸이 있으면 form 제출하지 못하도록 설정 javascript에서 regex 통해 email 형식 맞지않으면 제출하지 못하도록 설정

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,3 +1,24 @@
+function emptyChk(){
+  var signUpForm = document.question;
+  var userId = signUpForm.userId.value;
+  var pw = signUpForm.password.value;
+  var name = signUpForm.name.value;
+  var email = signUpForm.email.value;
+  if (!userId || !pw || !name || !email){
+    alert("모든 정보를 입력해주세요")
+  } else if (!validEmail(email)){
+    alert("올바른 이메일을 입력하세요.")
+  } else{
+    signUpForm.submit();
+  }
+}
+
+function validEmail(email){
+  let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+  if (regex.test(email)) return true;
+  return false;
+}
+
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
