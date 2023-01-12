d68190306d7aa66c0186795db517f88cd31efe02
pjs0418 <tony180322@gmail.com>
2023-01-12 12:03:07 +0900

    HTML이나 ICO 파일인 경우 templates path 그 외 static path로 가도록 구현

--- src/main/java/controller/ViewController.java
+++ src/main/java/controller/ViewController.java
@@ -4,6 +4,7 @@ import model.general.ContentType;
 import model.general.Header;
 import model.general.Status;
 import model.request.Request;
+import model.request.RequestLine;
 import model.response.Response;
 import model.response.StatusLine;
 
@@ -16,11 +17,14 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
+import static model.general.ContentType.HTML;
+import static model.general.ContentType.ICO;
+
 public class ViewController implements Controller {
     private static final Logger logger = LoggerFactory.getLogger(ViewController.class);
 
     private static final String templatePath = "./src/main/resources/templates";
-    private static final String staticPath = "./src/main/resources/templates";
+    private static final String staticPath = "./src/main/resources/static";
 
     @Override
     public Response getResponse(Request request) {
@@ -31,27 +35,33 @@ public class ViewController implements Controller {
     }
 
     private Response getIndexHtmlWhenInputNothing(Request request) {
+        RequestLine requestLine = request.getRequestLine();
+
         byte[] body = {};
         try {
-            body = Files.readAllBytes(new File(templatePath + request.getRequestLine().getUri() + "index.html").toPath());
+            body = Files.readAllBytes(new File(generatePath(requestLine.getContentType()) +
+                    requestLine.getUri() + "index.html").toPath());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
 
-        Map<Header, String> headers = response200Header(request.getRequestLine().getContentType(), body.length);
+        Map<Header, String> headers = response200Header(requestLine.getContentType(), body.length);
 
         return Response.of(StatusLine.from(Status.OK), headers, body);
     }
 
     private Response getIndexHtml(Request request) {
+        RequestLine requestLine = request.getRequestLine();
+
         byte[] body = {};
         try {
-            body = Files.readAllBytes(new File(templatePath + request.getRequestLine().getUri()).toPath());
+            body = Files.readAllBytes(new File(generatePath(requestLine.getContentType()) +
+                    requestLine.getUri()).toPath());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
 
-        Map<Header, String> headers = response200Header(request.getRequestLine().getContentType(), body.length);
+        Map<Header, String> headers = response200Header(requestLine.getContentType(), body.length);
 
         return Response.of(StatusLine.from(Status.OK), headers, body);
     }
@@ -62,5 +72,10 @@ public class ViewController implements Controller {
         headers.put(Header.from("Content-Length"), Integer.toString(messageBodyLength));
 
         return headers;
+    }
+
+    private String generatePath(ContentType contentType) {
+        if(HTML.equals(contentType) || ICO.equals(contentType)) return templatePath;
+        else return staticPath;
     }
 }
