8594fa23684c38c1518b47d1a47e6a16f9ee556b
pjs0418 <tony180322@gmail.com>
2023-01-12 10:48:54 +0900

    Request HTTP Message Header 부분 생성할 수 있도록 함수 구현

--- src/main/java/model/request/Request.java
+++ src/main/java/model/request/Request.java
@@ -2,9 +2,20 @@ package model.request;
 
 import model.general.Header;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Request {
+    private static final Logger logger = LoggerFactory.getLogger(Request.class);
+
     private final RequestLine requestLine;
     private final Map<Header, String> headers;
     private final byte[] body;
@@ -15,7 +26,27 @@ public class Request {
         this.body = body;
     }
 
-    public static Request of(RequestLine requestLine, Map<Header, String> headers) {
+    public static Request from(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+        RequestLine requestLine = RequestLine.from(br.readLine());
+
+        Map<Header, String> headers = makeHeaders(br);
+
         return new Request(requestLine, headers, null);
+    }
+
+    private static Map<Header, String> makeHeaders(BufferedReader br) throws IOException {
+        Map<Header, String> headers = new HashMap<>();
+        String header = br.readLine();
+
+        while(!System.lineSeparator().equals(header)) {
+            logger.debug("Header: {}", header);
+            String[] headerSplit = header.split(": ");
+            headers.put(Header.from(headerSplit[0]), headerSplit[1]);
+            header = br.readLine();
+        }
+
+        return headers;
     }
 }
