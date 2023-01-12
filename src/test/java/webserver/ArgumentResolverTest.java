a861f31e2f581234d79e540a157729f028965dc3
i5046821854 <o5046821854@gmail.com>
2023-01-12 15:27:42 +0900

    feat : ArgumentResolver에 대한 서비스 코드 작성 - 올바른 argument일 때 / 모든 argument를 주지 않았을 때

---
+++ src/test/java/webserver/ArgumentResolverTest.java
@@ -0,0 +1,58 @@
+package webserver;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.Controller.AuthController;
+import webserver.exception.HttpRequestException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgumentResolverTest {
+
+    ArgumentResolver argumentResolver;
+
+    @BeforeEach
+    void testSetUp(){
+        argumentResolver = new ArgumentResolver();
+    }
+
+    @Test
+    @DisplayName("올바른 Argument가 들어왔을 떄")
+    public void userCreateTest_validInput() throws HttpRequestException {
+
+        //given
+        Map<String, String> queryStrs = new HashMap<>();
+        queryStrs.put("userId", "testUser");
+        queryStrs.put("password", "testPass");
+        queryStrs.put("name", "testName");
+        queryStrs.put("email", "test@email.com");
+        List<String> listStr = List.of("userId", "password", "name", "email");
+
+        //when
+        Map<String, String> resultMap = ArgumentResolver.checkParameters(queryStrs, listStr);
+
+        //then
+        Assertions.assertEquals(resultMap.size(),4);
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 Argument가 들어왔을 떄")
+    public void userCreateTest_invalidInput() throws HttpRequestException {
+
+        //given
+        Map<String, String> queryStrs = new HashMap<>();
+        queryStrs.put("password", "testPass");
+        queryStrs.put("name", "testName");
+        queryStrs.put("email", "test@email.com");
+        List<String> listStr = List.of("userId", "password", "name", "email");
+
+        //then
+        Assertions.assertThrows(HttpRequestException.class,()->ArgumentResolver.checkParameters(queryStrs, listStr));
+    }
+}
