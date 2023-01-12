62bd71c148896e3eb9abddc1ef843613c5e06f6d
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 18:08:45 +0900

    :recycle: HttpResponse의 createHttpResponse에서 header생성 부분을 HttpResponseUtil클래스의 메서드로 추출: setHeaders가 필요해지므로 추가.

--- src/main/java/Response/HttpResponse.java
+++ src/main/java/Response/HttpResponse.java
@@ -3,19 +3,19 @@ package Response;
 import Request.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.ContentType;
 import util.FileIoUtil;
 import util.HttpResponseUtil;
 import util.StatusCode;
 
 import java.nio.file.Path;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class HttpResponse {
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+    private static final String NEWLINE = "\r\n";
+    private static final byte[] EMPTYBYTE = "".getBytes();
     private byte[] body;
     private StatusCode status;
     private String protocol;
@@ -29,23 +29,14 @@ public class HttpResponse {
     }
 
     public static HttpResponse createHttpResponse(HttpRequest httpRequest, StatusCode statusCode) {
-        Path path = FileIoUtil.mappingPath(httpRequest.getPath());
-        byte[] body = null;
-        if (Objects.nonNull(path))
+        Path path = FileIoUtil.mappingDirectoryPath(httpRequest.getPath());
+        byte[] body = EMPTYBYTE;
+        if (Objects.nonNull(path) && path.toFile().exists())
             body = HttpResponseUtil.generateBody(path);
 
         String protocol = httpRequest.getProtocol();
 
-        ContentType contentType = ContentType.PLAIN;
-        if (statusCode.equals(StatusCode.OK)) {
-            String ex = FileIoUtil.findExtension(httpRequest.getPath());
-            contentType = ContentType.valueOf(ex.toUpperCase());
-        }
-
-        Map<String, String> headers = new LinkedHashMap<>();                //headers
-        headers.put("Content-Type", contentType.getContentText());
-        headers.put("Content-Length", String.valueOf(body == null ? 0 : body.length));
-
+        Map<String, String> headers = HttpResponseUtil.generateHeaders(httpRequest, statusCode, body.length);
         return new HttpResponse(body, statusCode, protocol, headers);
     }
 
@@ -61,8 +52,12 @@ public class HttpResponse {
         this.headers = headers;
     }
 
+    public void setBody(Path path) {
+        this.body = HttpResponseUtil.generateBody(path);
+    }
+
     public String getHeaders() {
-        return this.protocol + " " + this.status.toString() + " \r\n" + this.headers.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue())
-                .collect(Collectors.joining(" \r\n")) + "\r\n";
+        return this.protocol + " " + this.status.toString() + NEWLINE + this.headers.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue())
+                .collect(Collectors.joining(NEWLINE)) + NEWLINE;
     }
 }
