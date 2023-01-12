3a61c362fdc85926ffe0b2ab56ce73f91c19132a
pjs0418 <tony180322@gmail.com>
2023-01-12 17:33:52 +0900

    Query String 파싱 잘 되는지 Test

--- src/test/java/request/RequestLineTest.java
+++ src/test/java/request/RequestLineTest.java
@@ -1,8 +1,11 @@
 package request;
 
 import model.request.RequestLine;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,12 +24,26 @@ public class RequestLineTest {
     @DisplayName("Controller 매핑 데이터를 얻기 위한 RequestLine 파싱 Test")
     void getControllerCriteria() {
         String line = "GET /user/create?" +
-                "userId=javajigi&password=password" +
-                "&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net HTTP/1.1";
+                "userId=park1234&password=abc123&name=jun&email=jun@slipp.net HTTP/1.1";
 
         RequestLine requestLine = RequestLine.from(line);
         String criteria = requestLine.getControllerCriteria();
 
         assertThat(criteria).isEqualTo("user");
+    }
+
+    @Test
+    @DisplayName("Query String 파싱 Test")
+    void parseQueryString() {
+        String line = "GET /user/create?" +
+                "userId=park1234&password=abc123&name=jun&email=jun@slipp.net HTTP/1.1";
+
+        RequestLine requestLine = RequestLine.from(line);
+        Map<String, String> queryString = requestLine.parseQueryString();
+
+        assertThat(queryString.get("userId")).isEqualTo("park1234");
+        assertThat(queryString.get("password")).isEqualTo("abc123");
+        assertThat(queryString.get("name")).isEqualTo("jun");
+        assertThat(queryString.get("email")).isEqualTo("jun@slipp.net");
     }
 }
