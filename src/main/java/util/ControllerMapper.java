1e1d475e17064dba6cd860f33430a87bf5372484
pjs0418 <tony180322@gmail.com>
2023-01-12 12:02:09 +0900

    정적 파일 요청 되는 경우 ViewController가 호출되도록 처리

--- src/main/java/util/ControllerMapper.java
+++ src/main/java/util/ControllerMapper.java
@@ -10,17 +10,18 @@ import service.UserService;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class ControllerMapper {
     private static final Map<String, Controller> controllerMap = new HashMap<>();
 
     static {
-        controllerMap.put("/", new ViewController());
-        controllerMap.put("index.html", new ViewController());
         controllerMap.put("user", new UserController(new UserService(new Database())));
     }
 
     public static Controller selectController(Request request) {
+        if(Objects.nonNull(request.getRequestLine().getContentType())) return new ViewController();
+
         for(Map.Entry<String, Controller> controllerEntry : controllerMap.entrySet()) {
             if(controllerEntry.getKey().equals(request.getRequestLine().getControllerCriteria())) {
                 return controllerEntry.getValue();
