5b5643fbfd139a8ee2dba4ee11163c5e15ccd147
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 11:48:09 +0900

    :sparkles: MappingController의 mapController에서 NonController로 맵핑되는 조건 추가: path가 없을때, 해당하는 확장자가 없을때

--- src/main/java/Controller/MappingController.java
+++ src/main/java/Controller/MappingController.java
@@ -4,15 +4,17 @@ import Request.HttpRequest;
 import util.FileIoUtil;
 
 import java.io.DataOutputStream;
+import java.nio.file.Path;
 import java.util.Objects;
 
 public class MappingController {
     public static Controller mapController(DataOutputStream dos, HttpRequest httpRequest){
-        String path = httpRequest.getPath();
-        if(Objects.isNull(path)){
+        String requestPath = httpRequest.getPath();
+        Path path = FileIoUtil.mappingPath(requestPath);
+        if(Objects.isNull(requestPath) || Objects.isNull(path)){
             return new NonController(httpRequest, dos);
         }
-        if(Objects.isNull(FileIoUtil.checkCreateUser(path))){
+        if(Objects.isNull(FileIoUtil.checkCreateUser(requestPath))){
             return new FileController(httpRequest, dos);
         }
         return new JoinController(httpRequest, dos);
