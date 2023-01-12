68e326fc6d72f872e80b3fcbfb3134b4c8b5b4e0
roopre <benny1020@naver.com>
2023-01-12 13:41:27 +0900

    refactor: 공통 메서드 묶기위해서 AbstractController 구현

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -1,25 +1,31 @@
 package controller;
 
-import webserver.HttpMethod;
-import webserver.HttpRequest;
-import webserver.HttpResponse;
+import java.io.IOException;
 
-public class AbstractController implements Controller{
+import response.ContentType;
+import webserver.HttpMethod;
+import request.HttpRequest;
+import response.HttpResponse;
+import webserver.HttpStatus;
+
+public class AbstractController implements Controller {
 
 	@Override
-	public HttpResponse service(HttpRequest httpRequest) {
+	public HttpResponse service(HttpRequest httpRequest) throws IOException {
 		if (httpRequest.getMethod().equals(HttpMethod.GET)) {
 			return doGet(httpRequest);
 		}
 		return doPost(httpRequest);
 	}
 
-	public HttpResponse doGet(HttpRequest httpRequest) {
-		return new HttpResponse() // 500내려주기
+	public HttpResponse doGet(HttpRequest httpRequest) throws IOException {
+
+		//File file = new File("./webapp" + httpRequest.getUrl().getPath());
+		return new HttpResponse(HttpStatus.OK, "500 error", ContentType.HTML); // 500내려주기
 	}
 
 	public HttpResponse doPost(HttpRequest httpRequest) {
-		return new HttpResponse() // 500내려주기
+		return new HttpResponse(HttpStatus.OK, "500 error", ContentType.HTML); // 500내려주기 // 500내려주기
 	}
 
 }
