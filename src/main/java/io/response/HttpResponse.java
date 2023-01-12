a9f66b2b4fdcbfd3d7ab3ff9baaf7e9566f6e776
wonju-dev <wonjulee.dev@gmail.com>
2023-01-12 15:46:20 +0900

    refactor(HttpResponse): composition 적용 + 리펙토링

--- src/main/java/io/response/HttpResponse.java
+++ src/main/java/io/response/HttpResponse.java
@@ -1,29 +1,22 @@
 package io.response;
 
-import enums.ResponseAttribute;
-import enums.ResponseStartLine;
 import enums.Status;
+import io.request.Header;
+import io.response.startline.RequestStartLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
+import static io.response.ResponseAttribute.*;
 
 public class HttpResponse {
 
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
-    private static final String PROTOCOL = "HTTP/1.1";
-
-    private Map<ResponseStartLine, String> startLine = new HashMap<>();
-    private Map<ResponseAttribute, String> attributes = new HashMap<>();
-    private byte[] body = "NOT FOUND".getBytes();
+    private RequestStartLine startLine = new RequestStartLine();
+    private Header header = new Header();
+    private Body body = new Body();
     private DataOutputStream out;
 
     public HttpResponse(DataOutputStream out) {
@@ -40,12 +33,10 @@ public class HttpResponse {
     }
 
     public void update(FindResult findResult) {
-        startLine.put(ResponseStartLine.PROTOCOL, PROTOCOL);
-        startLine.put(ResponseStartLine.STATUS, findResult.getStatus().getCode());
-        startLine.put(ResponseStartLine.MESSAGE, findResult.getStatus().getMessage());
-        attributes.put(ResponseAttribute.CONTENT_TYPE, findResult.getContentType().getType());
-        attributes.put(ResponseAttribute.CONTENT_LENGTH, Integer.toString(findResult.getResource().length));
-        this.body = findResult.getResource();
+        startLine.setStatus(findResult.getStatus());
+        header.setAttribute(CONTENT_TYPE, findResult.getContentType().getType());
+        header.setAttribute(CONTENT_LENGTH, findResult.getResource().length);
+        body.setBody(findResult.getResource());
     }
 
     private void assembleMessage() {
@@ -58,49 +49,22 @@ public class HttpResponse {
         }
     }
 
-    public void setStartLine(ResponseStartLine key, String value) {
-        startLine.put(key, value);
-    }
-
-    public void setAttribute(ResponseAttribute key, String value) {
-        attributes.put(key, value);
-    }
-
     private void assembleStatusLine() throws IOException {
-        List<String> startLines = startLine.keySet().stream()
-                .sorted(comparing(ResponseStartLine::getOrder))
-                .map(key -> startLine.get(key))
-                .collect(toList());
-        out.writeBytes(String.join(" ", startLines) + "\n\r");
+        out.writeBytes(startLine.toString());
     }
 
     private void assembleHeader() throws IOException {
-        out.writeBytes(getHeaderString());
-    }
-
-    private String getHeaderString() {
-        String string = "";
-        Set<Map.Entry<ResponseAttribute, String>> entries = attributes.entrySet();
-        for (Map.Entry<ResponseAttribute, String> entry : entries) {
-            string += entry.getKey().getAttribute() + ": " + entry.getValue() + "\n\r";
-        }
-        return string + "\n\r";
+        out.writeBytes(header.toString());
     }
 
     private void assembleBody() throws IOException {
-        out.write(body, 0, body.length);
-    }
-
-    public void setBody(byte[] resource) {
-        this.body = resource;
+        out.write(body.getBody(), 0, body.getBody().length);
     }
 
     public void redirect(String redirectUrl) {
-        setStartLine(ResponseStartLine.PROTOCOL, PROTOCOL);
-        setStartLine(ResponseStartLine.STATUS, Status.REDIRECT.getCode());
-        setStartLine(ResponseStartLine.MESSAGE, Status.REDIRECT.getMessage());
-        setAttribute(ResponseAttribute.LOCATION, redirectUrl);
-        body = new byte[0];
+        startLine.setStatus(Status.REDIRECT);
+        header.setAttribute(LOCATION, redirectUrl);
+        body.clear();
         send();
     }
 }
