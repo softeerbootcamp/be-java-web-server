85161b8572fca2b7b4410bb3a548210e8b56a59e
River-Mt <rivermt1998@gmail.com>
2023-01-12 15:07:42 +0900

    :sparkles: HttpResponse 리다이렉트 추가, 세분화로 인한 코드 변경 - 기존 HttpResponse를 세분화 - 리다이렉트 구현

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -2,41 +2,68 @@ package http.response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.FileIoUtils;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class HttpResponse {
-    private DataOutputStream dos;
-    private String version;
-    private HttpStatusCode httpStatusCode;
-    private ContentType contentType;
+    private final DataOutputStream dos;
+    private final HttpStatusLine statusLine;
+    private final HttpResponseHeaders headers;
+    private final HttpResponseBody responseBody;
 
     private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
 
     public HttpResponse(DataOutputStream dos) {
         this.dos = dos;
-        this.version = "HTTP/1.1";
+        this.statusLine = HttpStatusLine.createDefaultStatusLine();
+        this.headers = HttpResponseHeaders.createDefaultHeaders();
+        this.responseBody = HttpResponseBody.createDefaultBody();
     }
 
-    public void forward(String path) throws IOException {
-        this.contentType = ContentType.from(path);
-        this.httpStatusCode = HttpStatusCode.OK;
-        String filePath = contentType.getDirectory() + path;
-        logger.info("filePath: {}", filePath);
-        byte[] body = FileIoUtils.loadFile(filePath);
-        logger.info("body: {}", body.length);
-        response(body);
+    public void forward(ContentType contentType, byte[] body) throws IOException {
+       setStatusCode(HttpStatusCode.OK);
+       set200Headers(contentType, body.length);
+       setResponseBody(body);
+       send();
     }
 
-    private void response(byte[] body) throws IOException {
-        dos.writeBytes(version + httpStatusCode + "\r\n");
-        dos.writeBytes("Content-Type: " + contentType.getContentType() + body.length + "\r\n");
-        logger.info("Content-Type: " + contentType.getContentType());
-        dos.writeBytes("\r\n");
+    public void sendRedirect(String location) throws IOException {
+        setStatusCode(HttpStatusCode.FOUND);
+        set300Headers(location);
+        send();
+    }
+
+    public void send() throws IOException {
+        dos.writeBytes(statusLine.toString());
+        dos.writeBytes(headers.toString());
+        dos.writeBytes(System.lineSeparator());
+        byte[] body = responseBody.getBody();
+        if(null == body){
+            dos.flush();
+            return;
+        }
+        logger.debug("status line: " + statusLine);
+        logger.debug("headers: " + headers);
         dos.write(body, 0, body.length);
         dos.flush();
+    }
+
+    public void set200Headers(ContentType contentType, int contentLength) {
+        headers.addHeader("Content-Type", contentType.getContentType());
+        headers.addHeader("Content-Length", String.valueOf(contentLength));
+    }
+
+    public void set300Headers(String location) {
+        headers.addHeader("Location", location);
+    }
+
+    public void setStatusCode(HttpStatusCode statusCode) {
+        statusLine.setHttpStatusCode(statusCode);
+    }
+
+    public void setResponseBody(byte[] body) {
+        responseBody.setBody(body);
     }
 
 }
