468a24008a3108ca38c3733e0932b5e662e44822
River-Mt <rivermt1998@gmail.com>
2023-01-12 23:55:55 +0900

    :white_check_mark: HttpRequest 구조 변경으로 인한 테스트 재작성

--- src/test/java/http/request/HttpRequestTest.java
+++ src/test/java/http/request/HttpRequestTest.java
@@ -1,25 +1,42 @@
 package http.request;
 
+import http.Uri;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
-class HttpRequestTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HttpRequestTest {
+
     @Test
-    void testHttpRequestFrom() {
-        String request = "GET /path HTTP/1.1\n" +
-                "Host: www.example.com\n" +
-                "User-Agent: MyApp\n" +
-                "Accept: text/html\n";
+    public void testFrom() throws IOException {
+        String input = "GET /test HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "User-Agent: curl/7.68.0\r\n" +
+                "Accept: */*\r\n" +
+                "\r\n";
 
-        HttpRequest httpRequest = HttpRequest.from(request);
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+        HttpRequest request = HttpRequest.from(in);
 
-        assertEquals(HttpMethod.GET, httpRequest.getMethod());
-        assertEquals("/path", httpRequest.getUri().getPath());
-        assertEquals("HTTP/1.1", httpRequest.getStartLine().getHttpVersion());
-        assertEquals("www.example.com", httpRequest.getHeaders().get("Host"));
-        assertEquals("MyApp", httpRequest.getHeaders().get("User-Agent"));
-        assertEquals("text/html", httpRequest.getHeaders().get("Accept"));
+        HttpStartLine startLine = request.getStartLine();
+        assertEquals(HttpMethod.GET, startLine.getMethod());
+
+        Uri uri = Uri.from("/test");
+        assertEquals(uri.getPath(), startLine.getUri().getPath());
+
+        Map<String, String> headers = request.getHttpHeaders();
+        System.out.println(headers.toString());
+        assertTrue(headers.containsKey("Host"));
+        assertEquals("localhost:8080", headers.get("Host"));
+        assertTrue(headers.containsKey("User-Agent"));
+        assertEquals("curl/7.68.0", headers.get("User-Agent"));
+        assertTrue(headers.containsKey("Accept"));
+        assertEquals("*/*", headers.get("Accept"));
     }
-
 }
