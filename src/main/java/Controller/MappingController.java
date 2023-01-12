e72ccc7e155121e4001a3adb326c3c1e8409d30d
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 11:51:09 +0900

    :adhesive_bandage: MappingController의 mapController에서 NonController로 맵핑되는 조건 제거: join오류로 추가했던거 제거

--- src/main/java/Controller/MappingController.java
+++ src/main/java/Controller/MappingController.java
@@ -4,14 +4,12 @@ import Request.HttpRequest;
 import util.FileIoUtil;
 
 import java.io.DataOutputStream;
-import java.nio.file.Path;
 import java.util.Objects;
 
 public class MappingController {
     public static Controller mapController(DataOutputStream dos, HttpRequest httpRequest){
         String requestPath = httpRequest.getPath();
-        Path path = FileIoUtil.mappingPath(requestPath);
-        if(Objects.isNull(requestPath) || Objects.isNull(path)){
+        if(Objects.isNull(requestPath)){
             return new NonController(httpRequest, dos);
         }
         if(Objects.isNull(FileIoUtil.checkCreateUser(requestPath))){
