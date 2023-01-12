19a7cb9a288515163ba5abbd30a118ddf47e092f
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 18:04:42 +0900

    :sparkles: NonController에서 루트 리퀘스트와 존재하지 않는 파일 리퀘스트를 처리하는 로직을 추가

--- src/main/java/Controller/NonController.java
+++ src/main/java/Controller/NonController.java
@@ -2,21 +2,43 @@ package Controller;
 
 import Request.HttpRequest;
 import Response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.FileIoUtil;
 import util.HttpResponseUtil;
 import util.StatusCode;
 
 import java.io.DataOutputStream;
 
 public class NonController extends Controller {
+    private final Logger logger = LoggerFactory.getLogger(NonController.class);
+
     public NonController(HttpRequest httpRequest, DataOutputStream dos) {
         super(httpRequest, dos);
+        logger.debug("select nonController");
     }
+
 
     @Override
     public void response() {
         super.response();
-        HttpResponse httpResponse = HttpResponse.createHttpResponse(httpRequest, StatusCode.NOT_FOUND);
-        httpResponse.setHeaders(null);
+        if (this.httpRequest.getPath().equals("/")) {
+            this.httpResponse = responseRoot();
+        }
+        this.httpResponse = response404();
         HttpResponseUtil.outResponse(dos, httpResponse);
     }
+
+    public HttpResponse response404() {
+        HttpResponse httpResponse = HttpResponse.createHttpResponse(httpRequest, StatusCode.NOT_FOUND);
+        return httpResponse;
+    }
+
+    public HttpResponse responseRoot() {
+        HttpResponse httpResponse = HttpResponse.createHttpResponse(httpRequest, StatusCode.OK);
+        httpResponse.setBody(FileIoUtil.mappingDirectoryPath("/index.html"));
+        httpResponse.setHeaders(HttpResponseUtil.generateHeaders(httpRequest, StatusCode.OK, httpResponse.getBody().length));
+        return httpResponse;
+    }
+
 }
