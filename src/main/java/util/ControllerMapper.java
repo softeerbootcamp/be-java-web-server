c4891e60972a836a65b90fe3dd619fe37e572302
Soomin-Lim <augustth511@gmail.com>
2023-01-12 17:43:59 +0900

    feat: ControllerMapper에 Map<String, Controller> 타입 멤버변수 cotrollerMap 추가
    - cotrollerMap에 Controller를 구현한 구현체 객체를 미리 생성하여 추가해 넣음으로써 getController() 메서드에서 요청 URL에 따라 적절한 Controller를 반환하도록 함

--- src/main/java/util/ControllerMapper.java
+++ src/main/java/util/ControllerMapper.java
@@ -5,13 +5,24 @@ import Controller.UserController;
 import Controller.ResourceController;
 import http.request.HttpRequest;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class ControllerMapper {
+
+    private static final Map<String, Controller> controllerMap = new LinkedHashMap<>();
+
+    static {
+        controllerMap.put("/user", new UserController());
+    }
 
     public static Controller getController(HttpRequest request) {
         String url = request.getUrl();
 
-        if (url.startsWith("/user")) {
-            return new UserController();
+        for (String key : controllerMap.keySet()) {
+            if (url.startsWith(key)) {
+                return controllerMap.get(key);
+            }
         }
 
         return new ResourceController();
