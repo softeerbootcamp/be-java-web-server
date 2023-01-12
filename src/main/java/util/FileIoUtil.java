9509433f64bb74862e51986e6467c4c42ccf24a0
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 18:15:55 +0900

    :recycle: FileIoUtil의 directory path를 절대경로에서 getProperty로 수정

--- src/main/java/util/FileIoUtil.java
+++ src/main/java/util/FileIoUtil.java
@@ -1,18 +1,25 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class FileIoUtil {
-    private static final String TEMPLATE_DIR = "./src/main/resources/templates";
-    private static final String STATIC_DIR = "./src/main/resources/static";
+    private static final Logger logger = LoggerFactory.getLogger(FileIoUtil.class);
+    private static final String PROJECT_DIR = System.getProperty("user.dir");
+    private static final String TEMPLATE_DIR = PROJECT_DIR + "/src/main/resources/templates";
+    private static final String STATIC_DIR = PROJECT_DIR + "/src/main/resources/static";
     private static Set<String> templateTypes = new HashSet<>(Arrays.asList("html", "ico"));
     private static Set<String> staticTypes = new HashSet<>(Arrays.asList("css", "js", "eot", "svg", "ttf", "woff", "woff2", "png"));
-
+    private static final String DOT = "\\.";
     private static final String QUESTION = "?";
+    private static final String SLASH = "/";
 
     public static String checkCreateUser(String path) {
         if (path.startsWith("/user/create")) {
@@ -22,15 +29,15 @@ public class FileIoUtil {
         return null;
     }
 
-    public static Path mappingPath(String path) {
+    public static Path mappingDirectoryPath(String path) throws NullPointerException{
         String ex = findExtension(path);
-        if (templateTypes.contains(ex)) return new File(TEMPLATE_DIR + path).toPath();
+        if (templateTypes.contains(ex) ) return new File(TEMPLATE_DIR + path).toPath();
         if (staticTypes.contains(ex)) return new File(STATIC_DIR + path).toPath();
         return null;
     }
 
     public static String findExtension(String path) {
-        String[] temp = path.split("\\.");
+        String[] temp = path.split(DOT);
         String extension = temp[temp.length - 1];
         return extension;
     }
