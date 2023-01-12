23f1215b9221f096c0afda3b8a779a7b33c6e811
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 18:02:36 +0900

    :loud_sound: FileController에 로거 생성하고 로그 추가, HttpResponse 멤버변수 사용으로 변경

--- src/main/java/Controller/FileController.java
+++ src/main/java/Controller/FileController.java
@@ -2,20 +2,25 @@ package Controller;
 
 import Request.HttpRequest;
 import Response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.HttpResponseUtil;
 import util.StatusCode;
 
 import java.io.DataOutputStream;
 
 public class FileController extends Controller {
+    private final Logger logger = LoggerFactory.getLogger(FileController.class);
+
     public FileController(HttpRequest httpRequest, DataOutputStream dos) {
         super(httpRequest, dos);
+        logger.debug("select fileController");
     }
 
     @Override
     public void response() {
         super.response();
-        HttpResponse httpResponse = HttpResponse.createHttpResponse(this.httpRequest, StatusCode.OK);
-        HttpResponseUtil.outResponse(this.dos, httpResponse);
+        this.httpResponse = HttpResponse.createHttpResponse(this.httpRequest, StatusCode.OK);
+        HttpResponseUtil.outResponse(this.dos, this.httpResponse);
     }
 }
