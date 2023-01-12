bb65998b24266b2f16a3fb5c4fe0ee8034d6cdbc
201724554 <pay7845@naver.com>
2023-01-12 22:11:49 +0900

    요청에 대한 처리와 응답 생성 분리

--- src/main/java/webserver/ClientRequestThread.java
+++ src/main/java/webserver/ClientRequestThread.java
@@ -2,10 +2,14 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import request.Request;
 import request.RequestHandler;
+import response.HttpResponse;
+import response.HttpResponseBody;
 
 public class ClientRequestThread implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(ClientRequestThread.class);
@@ -13,6 +17,8 @@ public class ClientRequestThread implements Runnable {
     private final RequestHandler requestHandler;
 
     private Socket connection;
+
+    private Request request;
 
     public ClientRequestThread(Socket connectionSocket, RequestHandler requestHandler) {
         this.connection = connectionSocket;
@@ -24,11 +30,16 @@ public class ClientRequestThread implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            DataOutputStream dos = new DataOutputStream(out);
             if(in.available() == 0) {
                 return;
             }
-            requestHandler.handleRequest(in, dos, connection.getPort());
+            this.request = Request.of(in);
+            byte[] body = requestHandler.handleRequest(request, connection.getPort());
+            if(body == null) {
+                HttpResponse.Http404Response(out);
+                return;
+            }
+            HttpResponse.Http200Response(request, out, HttpResponseBody.of(body));
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
