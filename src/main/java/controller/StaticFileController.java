3ef28a282f0a60b1ae8111489261f35f770965ae
Toa <ld980105@snu.ac.kr>
2023-01-12 11:02:11 +0900

    :sparkles: StaticFileController 클래스 구현

--- src/main/java/controller/StaticFileController.java
+++ src/main/java/controller/StaticFileController.java
@@ -2,10 +2,26 @@ package controller;
 
 import http.response.HttpResponse;
 import http.request.HttpRequest;
+import utils.ContentType;
+import utils.FileIoUtils;
+import utils.StatusCode;
 
 public class StaticFileController implements RequestController{
+    public final static String STATIC_FILE_CONTROLLER = "StaticFileController";
     @Override
     public void handleRequest(HttpRequest httpRequest, HttpResponse httpResponse) {
-        
+        try {
+            byte[] body = FileIoUtils.loadFile(httpRequest.getUri().getPath());
+            httpResponse.setBody(body);
+            httpResponse.setContentType(ContentType.getContentType(httpRequest.getExtension()));
+            httpResponse.setStatusCode(StatusCode.OK);
+        }
+        catch (IllegalArgumentException e) {
+            if (httpRequest.getExtension().equals("/")) {
+                httpResponse.redirect();
+                return;
+            }
+            httpResponse.setStatusCode(StatusCode.NOTFOUND);
+        }
     }
 }
