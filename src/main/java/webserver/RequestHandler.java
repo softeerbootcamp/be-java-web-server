0fa95b58dacff7c2e4caf9fbebc92a69e2e02fff
i5046821854 <o5046821854@gmail.com>
2023-01-12 12:28:06 +0900

    feat : Requesthandler에서 Response에 관여하지 않고 바로 handler로 체이닝을 돌리도록 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,33 +2,23 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.Controller.AuthController;
 import webserver.Controller.Controller;
-import webserver.domain.ContentType;
 import webserver.domain.StatusCodes;
 import webserver.domain.request.Request;
 import webserver.domain.response.Response;
-import webserver.exception.HttpRequestException;
-import webserver.utils.CommonUtils;
-import webserver.utils.StaticResourceFinder;
 
 import static webserver.utils.HttpRequestUtils.parseHttpRequest;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private AuthController authController;
     private Socket connection;
     private HandlerMapping handlerMapping;
 
     public RequestHandler(Socket connectionSocket) {
-
         this.connection = connectionSocket;
-        this.authController = new AuthController();
         this.handlerMapping = new HandlerMapping();
-
     }
 
 
@@ -42,23 +32,14 @@ public class RequestHandler implements Runnable {
 
             req.readRequest();  //Print out Http Request Message
 
-            String requestedPath = req.getRequestLine().getResource();
-            StaticResourceFinder.staticFileResolver(requestedPath).ifPresentOrElse(fileAsBytes ->{
-                res.setBody(fileAsBytes);
-                res.setContentType(StaticResourceFinder.getExtension(requestedPath));
-                res.setStatusCode(StatusCodes.OK);
-                res.writeResponse();
-            },()->{
-                try{
-                    Controller controller = handlerMapping.getHandler(requestedPath);
-                    String [] parsedPath = CommonUtils.parseRequestLine(requestedPath);
-                    controller.handle(parsedPath[0], parsedPath[1], res);
-                    res.writeResponse();
-                }catch (HttpRequestException e){
-                    res.setStatusCode(e.getErrorCode());
-                    res.writeResponse();
-                }
-            });
+            Controller controller = handlerMapping.getHandler(req);  //get the controller to handle request
+            controller.handle(req, res);
+
+            if(res.getStatusCode() == StatusCodes.NOT_FOUND){  // request is not precessed by all the controllers
+                handlerMapping.getStaticHandler().handle(req, res);  //get static controller to handle this request
+            }
+            res.writeResponse();  //write http response and send it back to client side
+
         } catch (IOException e) {
             logger.debug(e.getMessage());
         }
