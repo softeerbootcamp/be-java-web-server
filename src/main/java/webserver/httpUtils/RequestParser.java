dbc5fb70fad0cae37a8de77806d8ad04ccde45db
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-12 15:29:53 +0900

    request body 수정
    body부분을 넣으면 브라우저에 정상적으로 출력되지 않음
    왜 그런지 알아보는 중

--- src/main/java/webserver/httpUtils/RequestParser.java
+++ src/main/java/webserver/httpUtils/RequestParser.java
@@ -14,16 +14,22 @@ import java.util.Map;
 
 public class RequestParser {
 
+    private static String currentLine = new String();
 
     public static Request parseRequestFromInputStream(InputStream in) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-        String currentLine = br.readLine();
+        currentLine = br.readLine();
 
         Request req = new Request();
-        req.setReqLine(parseRequestLine(currentLine));
+        req.setReqLine(parseRequestLine(currentLine)); currentLine = br.readLine();
         req.setReqHeader(getHeaderKeyValues(currentLine, br));
-        req.setReqBody(getBody(currentLine, br));
+
+        System.out.println(req.getReqLine());
+        System.out.println(req.getReqHeader());
+
+
+        //req.setReqBody(getBody(currentLine, br)); ?? 이거 넣으면 왜 안댐?
 
         return req;
     }
@@ -31,14 +37,16 @@ public class RequestParser {
     private static Map<String, String> parseRequestLine(String currentLine)
     {
         Map<String, String> parsedRequestLine = new HashMap<String, String>();
+
         String tokens[] = currentLine.split(" ");
         parsedRequestLine.put(Request.REQLINE_METHOD, tokens[0]);
         parsedRequestLine.put(Request.REQLINE_QUERY, tokens[1].equals("/") ? Paths.HOME_PATH : tokens[1]);
         parsedRequestLine.put(Request.REQLINE_VERSION, tokens[2]);
+
         return parsedRequestLine;
     }
 
-    private static Map<String, String> getHeaderKeyValues(String currentLine, BufferedReader br) throws IOException {
+    private static Map<String, String> getHeaderKeyValues(String currentLine, BufferedReader br) throws IOException{
         Map<String, String> ret = new HashMap<String, String>();
 
         while(!currentLine.isBlank())
@@ -48,7 +56,6 @@ public class RequestParser {
 
             currentLine = br.readLine();
         }
-
         return ret;
     }
 
