bb4cda1e374dbe8b229e7e9cb971e78db77b4bc6
jieonkim23 <kwldjs1023@gmail.com>
2023-01-12 18:17:44 +0900

    :recycle: MappingController의 컨트롤러 맵핑 로직을 변경

--- src/main/java/Controller/MappingController.java
+++ src/main/java/Controller/MappingController.java
@@ -4,17 +4,20 @@ import Request.HttpRequest;
 import util.FileIoUtil;
 
 import java.io.DataOutputStream;
+import java.nio.file.Path;
 import java.util.Objects;
 
 public class MappingController {
     public static Controller mapController(DataOutputStream dos, HttpRequest httpRequest) {
         String requestPath = httpRequest.getPath();
-        if (Objects.isNull(requestPath)) {
-            return new NonController(httpRequest, dos);
+        Path path = FileIoUtil.mappingDirectoryPath(requestPath);
+
+        if (Objects.nonNull(FileIoUtil.checkCreateUser(requestPath))) {
+            return new JoinController(httpRequest, dos);
         }
-        if (Objects.isNull(FileIoUtil.checkCreateUser(requestPath))) {
+        if (path.toFile().exists()) {
             return new FileController(httpRequest, dos);
         }
-        return new JoinController(httpRequest, dos);
+        return new NonController(httpRequest, dos);
     }
 }
