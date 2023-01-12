67ad6a3026261b14f3666ae67564e443a815d72e
i5046821854 <o5046821854@gmail.com>
2023-01-12 12:26:35 +0900

    feat : AuthController의 handler 메소드를 간소화 시키고, Enum 타입의 이름을 routerPath로 변경

--- src/main/java/webserver/Controller/AuthController.java
+++ src/main/java/webserver/Controller/AuthController.java
@@ -1,63 +1,53 @@
 package webserver.Controller;
 
+import webserver.ArgumentResolver;
 import webserver.Service.AuthService;
 import webserver.domain.ContentType;
 import webserver.domain.StatusCodes;
 import webserver.domain.response.Response;
 import webserver.exception.HttpRequestException;
-import webserver.utils.CommonUtils;
-import webserver.utils.HttpRequestUtils;
-import webserver.utils.HttpResponseUtil;
-
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static webserver.utils.CommonUtils.checkParameters;
-
 public class AuthController implements Controller {
 
-    private Response res;
+    private Response response;
     private AuthService authService;
 
     public AuthController(){
         this.authService = new AuthService();
     }
 
-    private void userCreate(Map<String, String> queryStrs) {
-        if(!checkParameters(List.of("userId", "password", "name", "email"), queryStrs)) {
-            throw new HttpRequestException(StatusCodes.INTERNAL_SERVER_ERROR);
-        }
+    private void userCreate(Map<String, String> queryStrs) throws HttpRequestException{
         byte[] result = authService.join(queryStrs.get("userId"), queryStrs.get("password"), queryStrs.get("name"), queryStrs.get("email"));
-        res.setStatusCode(StatusCodes.SEE_OTHER);
-        res.setBody(result);
-        res.setContentType(ContentType.TEXT_HTML);
-        res.setRedirection("http://localhost:8080/index.html");
+        response.redirect(StatusCodes.SEE_OTHER, result, ContentType.TEXT_HTML, "http://localhost:8080/index.html");
     }
-
 
     @Override
-    public void handle(String path, String queryString, Response res) throws HttpRequestException {
-        this.res = res;
-        switch (Enum.find(path)){
-            case CREATE:
-                Map<String, String> queryStrs = CommonUtils.parseQueryStrings(queryString);
-                userCreate(queryStrs);
+    public void chain(String path, Map<String, String> queryString, Response res) {
+        response = res;
+        try{
+            switch (RouterPath.find(path)){
+                case CREATE:
+                    Map<String, String> queryStrs = ArgumentResolver.checkParameters(queryString, List.of("userId", "password", "name", "email"));
+                    userCreate(queryStrs);
+            }
+        }catch (HttpRequestException e){
+            response.error(e.getErrorCode());
         }
     }
 
-    public enum Enum{
+    public enum RouterPath{
         CREATE("/user/create");
 
         private String path;
 
-        private Enum(String path){
+        private RouterPath(String path){
             this.path = path;
         }
-        public static Enum find(String req){
-            return Arrays.stream(Enum.values())
+        public static RouterPath find(String req){
+            return Arrays.stream(RouterPath.values())
                     .filter(each-> req.startsWith(each.getPath()))
                     .findFirst()
                     .orElseThrow(() -> new HttpRequestException(StatusCodes.NOT_FOUND));
