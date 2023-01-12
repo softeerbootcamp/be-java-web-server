f3776ec08c69e381686d0278eec9c538bef97387
wonju-dev <wonjulee.dev@gmail.com>
2023-01-12 15:38:19 +0900

    feat(RequestParameter): RequestParameter 구현
    RequestParameter는 메서드 바디(Post,) 쿼리 스트링(GET)에 포함된 데이터를
    관리하는 class이다.

---
+++ src/main/java/io/request/RequestParameter.java
@@ -0,0 +1,34 @@
+package io.request;
+
+import io.request.startLine.StartLine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestParameter {
+    private Map<String, String> requestParameters;
+
+    public RequestParameter(StartLine startLine) {
+        this.requestParameters = parseQueryString(startLine.getUrl());
+    }
+
+    private Map<String, String> parseQueryString(String url) {
+        Map<String, String> parameters = new HashMap<>();
+        if (!url.contains("?")) {
+            return parameters;
+        }
+
+        String queryString = url.split("\\?", 2)[1];
+        String[] chunks = queryString.split("&");
+        for (String chunk : chunks) {
+            String[] pair = chunk.split("=");
+            parameters.put(pair[0], pair[1]);
+        }
+
+        return parameters;
+    }
+
+    public String getParameter(String key) {
+        return requestParameters.get(key);
+    }
+}
