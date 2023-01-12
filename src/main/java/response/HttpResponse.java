e91a01778b222f53153537c981923abbdc38dc55
roopre <benny1020@naver.com>
2023-01-12 14:00:35 +0900

    refactor: 위치 변경

---
+++ src/main/java/response/HttpResponse.java
@@ -0,0 +1,54 @@
+package response;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import webserver.HttpStatus;
+
+public class HttpResponse {
+
+	private final HttpResponseLine line;
+	private final Map<String, String> header;
+	private final String responseBody;
+
+	public HttpResponse(final HttpStatus httpStatus, final String responseBody,
+		final ContentType contentType, final String redirectUrl) {
+		this.line = new HttpResponseLine(httpStatus);
+		this.header = new HashMap<>();
+		initHeader(contentType, responseBody, redirectUrl);
+		this.responseBody = responseBody;
+	}
+
+	public HttpResponse(final HttpStatus httpStatus, final String responseBody, final ContentType contentType) {
+		this.line = new HttpResponseLine(httpStatus);
+		this.header = new HashMap<>();
+		initHeader(contentType, responseBody);
+		this.responseBody = responseBody;
+	}
+
+	private void initHeader(final ContentType contentType, final String responseBody) {
+		header.put("Content-Type", contentType.getContentType() + ";charset-utf-8");
+		header.put("Content-Length", String.valueOf(responseBody.getBytes(StandardCharsets.UTF_8).length));
+	}
+
+	private void initHeader(final ContentType contentType, final String responseBody, final String redirectUrl) {
+		initHeader(contentType, responseBody);
+		header.put("Location", redirectUrl);
+	}
+
+	@Override
+	public String toString() {
+		return String.join("\r\n",
+			line + " ",
+			printHeader() + "\n",
+			responseBody);
+	}
+
+	public String printHeader() {
+		return header.entrySet().stream()
+			.map(entry -> String.format("%s: %s ", entry.getKey(), entry.getValue()))
+			.collect(Collectors.joining(System.lineSeparator()));
+	}
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,35 +1,24 @@
 package webserver;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import controller.Controller;
-import controller.UserController;
+import controller.RequestMappingHandler;
+import request.HttpRequest;
+import request.HttpRequestUtils;
+import response.HttpResponse;
 
 public class RequestHandler implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
-	private final HttpRequestUtils parser;
-	private final ResponseWriter responseWriter;
 	private Socket connection;
 
-	private UserController userController;
-
 	public RequestHandler(Socket connectionSocket) {
-		this.userController = new UserController();
-		this.parser = new HttpRequestUtils();
-		this.responseWriter = new ResponseWriter();
 		this.connection = connectionSocket;
 	}
 
@@ -37,30 +26,14 @@ public class RequestHandler implements Runnable {
 		logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
 			connection.getPort());
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			DataOutputStream dos = new DataOutputStream(out);
-
-			// HttpRequest 파싱
+			//DataOutputStream dos = new DataOutputStream(out);
+			// HttpRequest HttpResponse
 			HttpRequest httpRequest = HttpRequestUtils.httpRequestParse(in);
-			logger.info(httpRequest.toString());
-
-			// TODO: 아래 부분을 인터페이스를 활용하여 확장성 높이고 코드 중복 없애야함, response 또한 새로 클래스 만들어서 적용할 예정
-			// 정적 데이터 요청일 경우의 처리
-			File file = new File("./webapp" + httpRequest.getUrl().getPath());
+			HttpResponse httpResponse = RequestMappingHandler.findController(httpRequest).service(httpRequest);
 			logger.debug(httpRequest.getUrl().getPath());
-
-			Controller controller = RequestMappingHandler.findController(httpRequest);
-			HttpResponse httpResponse = controller.service(httpRequest);
-
-			// TODO file controller 만들어야함
-			if (file.isFile()) {
-				byte[] body = Files.readAllBytes(file.toPath());
-				responseWriter.responseWrite(dos, body);
-			}
-
-			if (httpRequest.urlStartsWith("/create")) {
-				responseWriter.responseWrite(dos, httpResponse.toString().getBytes("UTF-8"));
-			}
-
+			// 전송
+			out.write(httpResponse.toString().getBytes(StandardCharsets.UTF_8));
+			out.flush();
 		} catch (IOException e) {
 			logger.error(e.getMessage());
 		}
