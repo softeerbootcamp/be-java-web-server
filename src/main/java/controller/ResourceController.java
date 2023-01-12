edb99d7e84970b78825c8d7cf34966ef07ebb38f
River-Mt <rivermt1998@gmail.com>
2023-01-12 15:09:27 +0900

    :recycle: HttpResponse 구조 변경으로 인한 리팩토링

--- src/main/java/controller/ResourceController.java
+++ src/main/java/controller/ResourceController.java
@@ -1,14 +1,22 @@
 package controller;
 
 import http.request.HttpRequest;
+import http.response.ContentType;
 import http.response.HttpResponse;
 import http.request.Uri;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import static utils.FileIoUtils.loadFile;
+
 public class ResourceController implements Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResourceController.class);
+
     private final List<String> paths;
     public ResourceController() {
         this.paths = Arrays.asList(".html", ".ico", ".css", ".js", "woff", "ttf", "png");
@@ -25,7 +33,13 @@ public class ResourceController implements Controller {
     }
 
     public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
-        httpResponse.forward(httpRequest.getUri().getPath());
+        String path = httpRequest.getUri().getPath();
+        ContentType contentType = ContentType.from(path);
+        String filePath = contentType.getDirectory() + path;
+        logger.debug("filePath: " + filePath);
+        byte[] body = loadFile(filePath);
+
+        httpResponse.forward(contentType, body);
     }
 
 }
