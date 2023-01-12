5267547bf813f5b97fd2e88fcbebf9399f15a877
pjs0418 <tony180322@gmail.com>
2023-01-12 10:17:34 +0900

    Request 객체 생성 및 생성자 구현

---
+++ src/main/java/model/request/Request.java
@@ -0,0 +1,17 @@
+package model.request;
+
+import model.general.Header;
+
+import java.util.Map;
+
+public class Request {
+    private final RequestLine requestLine;
+    private final Map<Header, String> headers;
+    private final byte[] body;
+
+    private Request(RequestLine requestLine, Map<Header, String> headers, byte[] body) {
+        this.requestLine = requestLine;
+        this.headers = headers;
+        this.body = body;
+    }
+}
