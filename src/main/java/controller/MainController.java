7d157e5998e9b3468afef7d30ee38f5c78fd2f3c
wonju-dev <wonjulee.dev@gmail.com>
2023-01-12 16:17:57 +0900

    refactor: Static 메서드 사용

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -10,12 +10,11 @@ import org.slf4j.LoggerFactory;
 public class MainController implements Controller {
 
     private final Logger logger = LoggerFactory.getLogger(MainController.class);
-    private final FileSystem fileSystem = new FileSystem();
 
     @Override
     public void handle(HttpRequest request, HttpResponse response) {
         logger.debug("main controller called");
-        FindResult findResult = fileSystem.findResource(request.getUrl());
+        FindResult findResult = FileSystem.findResource(request.getUrl());
         response.update(findResult);
         response.send();
     }
