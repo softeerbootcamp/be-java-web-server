95a4aaf833a28c8c46da93181c715d40feefec4e
roopre <benny1020@naver.com>
2023-01-12 10:00:14 +0900

    fix: UserController가 HttpResponse 반환하도록 수정

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -10,16 +10,18 @@ import org.slf4j.LoggerFactory;
 import db.Database;
 import model.User;
 import webserver.HttpRequest;
+import webserver.HttpResponse;
 import webserver.Url;
 
-public class UserController {
+public class UserController extends AbstractController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-	public String doPost(HttpRequest request) {
-		Url url = request.getUrl();
+	public HttpResponse doGet(HttpRequest httpRequest) {
+		Url url = httpRequest.getUrl();
 		User user = User.of(url.getQuery("userId"), url.getQuery("password"), url.getQuery("name"), url.getQuery("email"));
 		logger.info(user + " 회원가입했습니다.");
 		Database.addUser(user);
-		return "OK";
+		return new HttpResponse() // TODO
 	}
+
 }
