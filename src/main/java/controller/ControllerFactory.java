2188e51cc894b0588873cb8861704d03de984820
River-Mt <rivermt1998@gmail.com>
2023-01-12 16:50:08 +0900

    :sparkles: 컨트롤러 매칭 실패 시 404 처리 로직 구현

--- src/main/java/controller/ControllerFactory.java
+++ src/main/java/controller/ControllerFactory.java
@@ -2,6 +2,8 @@ package controller;
 
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,9 +12,12 @@ import java.util.List;
 
 public class ControllerFactory {
 
+    private static final Logger logger = LoggerFactory.getLogger(ControllerFactory.class);
+
     private static final List<Controller> controllers;
 
-    private ControllerFactory(){}
+    private ControllerFactory() {
+    }
 
     static {
         controllers = new ArrayList<>();
@@ -26,8 +31,14 @@ public class ControllerFactory {
                 .stream()
                 .filter(c -> c.isUri(httpRequest))
                 .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+                .orElse(null);
 
+        if(controller == null) {
+            httpResponse.do404();
+            return;
+        }
+
+        logger.debug("Controller {}",controller);
         controller.service(httpRequest, httpResponse);
     }
 
