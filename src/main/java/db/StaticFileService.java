676ca80f68d7993eef804eb7417f88b9bfd916e7
rohsik2 <rohsik2@gmail.com>
2023-01-12 14:16:15 +0900

    :recycle: 불필요한 Import 삭제 및 코드 린트 수정

--- src/main/java/db/StaticFileService.java
+++ src/main/java/db/StaticFileService.java
@@ -2,7 +2,6 @@ package db;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
 
 import java.io.File;
 import java.util.List;
@@ -10,7 +9,6 @@ import java.util.List;
 public class StaticFileService {
     private static final String STATIC_PATH = StaticFileService.class.getClassLoader().getResource("./static").getPath();
     private static final String TEMPLATE_PATH = StaticFileService.class.getClassLoader().getResource("./templates").getPath();
-
     private static final List<String> TEMPLATE_FILES = List.of("html", "ico");
     private static final Logger logger = LoggerFactory.getLogger(StaticFileService.class);
 
