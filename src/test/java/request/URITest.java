f8a614c7797c9cd60ad7111244b2f825a8d0ff96
Toa <ld980105@snu.ac.kr>
2023-01-12 17:22:44 +0900

    :sparkles: URITest 구현

---
+++ src/test/java/request/URITest.java
@@ -0,0 +1,25 @@
+package request;
+
+import http.request.URI;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class URITest {
+    @Test
+    @DisplayName("URI 생성 및 path 테스트")
+    void test_URI_PATH() {
+        URI uri = URI.create("/user/create?id=mino&password=1234");
+        assertThat(uri.getPath()).isEqualTo("/user/create");
+    }
+
+    @Test
+    @DisplayName("URI 생성 및 query 테스트")
+    void test_URI_QUERY() {
+        URI uri = URI.create("/user/create?id=mino&password=1234");
+        assertThat(uri.getQuerys().get("id")).isEqualTo("mino");
+        assertThat(uri.getQuerys().get("password")).isEqualTo("1234");
+    }
+}
