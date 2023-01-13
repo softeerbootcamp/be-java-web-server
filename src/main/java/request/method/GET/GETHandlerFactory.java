0adb9c070dbcd13da06a521890b9c54bb14a3ed0
201724554 <pay7845@naver.com>
2023-01-14 00:12:40 +0900

    TODO 추가

--- src/main/java/request/method/GET/GETHandlerFactory.java
+++ src/main/java/request/method/GET/GETHandlerFactory.java
@@ -9,6 +9,7 @@ public class GETHandlerFactory {
     public static GETHandler generateHandler(Request request) {
         GETHandler handler = NoMatchHandler.of();
         for(HandlerEnum handlerEnum : HandlerEnum.values()) {
+            // TODO: resource를 enum의 url과 비교하는 것을 equals 메소드를 사용할 수 있도록 수정
             if(request.getResource().contains(handlerEnum.getUrl())) {
                 return handlerEnum.getHandler();
             }
