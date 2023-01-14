6ac016b93f9d2de4af5b098bde4c45bcb04b8800
i5046821854 <o5046821854@gmail.com>
2023-01-15 02:02:59 +0900

    feat : 데이터베이스의 findUserById의 반환 타입을 Optional로 변경

--- src/main/java/webserver/Service/AuthService.java
+++ src/main/java/webserver/Service/AuthService.java
@@ -8,12 +8,12 @@ import webserver.exception.HttpRequestException;
 public class AuthService {
 
     public byte[] join(String userId, String password, String name, String email) {
-        User user = Database.findUserById(userId);
-        if(user == null){
-            Database.addUser(new User(userId, password, name, email));
-            return userId.getBytes();
-        }
-        throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('사용자가 이미 존재합니다'); history.go(-1);</script>");
+        Database.findUserById(userId).ifPresent(m -> {
+            throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('사용자가 이미 존재합니다'); history.go(-1);</script>");
+        });
+        Database.addUser(new User(userId, password, name, email));
+        return userId.getBytes();
     }
 
 }
+
