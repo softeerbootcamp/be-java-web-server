04578fe7bd1430fcea92f68de830029c3f58b201
i5046821854 <o5046821854@gmail.com>
2023-01-16 18:13:50 +0900

    feat : staticController의 chain 메소드 파라미터 변경

--- src/main/java/webserver/controller/StaticController.java
+++ src/main/java/webserver/controller/StaticController.java
@@ -1,15 +1,19 @@
 package webserver.controller;
 
 import webserver.domain.StatusCodes;
+import webserver.domain.request.Request;
 import webserver.domain.response.Response;
+import webserver.exception.HttpRequestException;
 import webserver.utils.StaticResourceFinder;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class StaticController implements Controller {
+
     @Override
-    public void chain(String path, Map<String, String> queryString, Response res) throws IOException {
+    public void chain(Request req, Response res) throws HttpRequestException, IOException {
+        String path = req.getRequestLine().getResource().getPath();
         StaticResourceFinder.staticFileResolver(path).ifPresentOrElse(
                 (fileAsBytes) -> res.ok(StatusCodes.OK, fileAsBytes, StaticResourceFinder.getExtension(path)),  //if file exists
                 ()-> res.notFoundError(StatusCodes.NOT_FOUND)); //if not
