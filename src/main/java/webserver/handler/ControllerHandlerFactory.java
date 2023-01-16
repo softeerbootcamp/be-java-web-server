a72b9a5387b119ec053baa4432782f24d6987ac3
ajongs <wonjong1030@naver.com>
2023-01-16 17:25:46 +0900

    refactor(ControllerHandlerFactory): get 메소드의 쿼리스트링을 처리하는 핸들러 제거
    get에 쿼리스트링을 사용하여 db 작업이나 동적 자원을 요청할 수 없음
    기존의 학습용 쿼리스트링 처리 로직을 삭제해야함

--- src/main/java/webserver/handler/ControllerHandlerFactory.java
+++ src/main/java/webserver/handler/ControllerHandlerFactory.java
@@ -6,14 +6,11 @@ import webserver.domain.HttpRequest;
 
 public class ControllerHandlerFactory {
     public static ControllerHandler getHandler(HttpRequest httpRequest) {
-        String url = httpRequest.getRequestLine().getUrl();
         HttpMethod httpMethod =  httpRequest.getRequestLine().getHttpMethod();
-        if (url.contains("?")) {
-            return QueryStringHandler.getInstance();
-        }
         if (httpMethod == HttpMethod.POST) {
             return Dispatcher.getInstance();
         }
         return StaticHandler.getInstance();
     }
 }
+종
