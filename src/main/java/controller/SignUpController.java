971a35967909fe64b82da145f0d81a62111337b8
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-16 12:08:37 +0900

    post 요청 분기 타게 되는 doPost() 메서드 구현, redirect 부분 로직 분리해서 구현

--- src/main/java/controller/SignUpController.java
+++ src/main/java/controller/SignUpController.java
@@ -2,25 +2,35 @@ package controller;
 
 import db.Database;
 import model.User;
+import service.SignUpService;
 import webserver.HttpRequest;
 import webserver.HttpResponse;
-import webserver.ResponseWriter;
-
-import java.io.IOException;
 
 public class SignUpController extends BaseController{
     private static final String SIGN_UP_PATH_URL = "/user/create";
     // service는 BaseController에서 더 건들이지 않아도 되므로 오버라이딩 하지 않음
+
+    private static final String REDIRECT_URL ="/index.html";
+
+    private final SignUpService signUpService;
+
+    public SignUpController(SignUpService signUpService) {
+        this.signUpService = signUpService;
+    }
+
     @Override
     public void doGet(HttpRequest request, HttpResponse response) {
-        String userId = request.getQueryByKey("userId");
-        String password = request.getQueryByKey("password");
-        String name = request.getQueryByKey("name");
-        String email = request.getQueryByKey("email");//1234%40khu.ac.kr
-        email.replace("%40","@");
-        User user = User.of(userId,password,name,email);
-        Database.addUser(user);
-        String redirectUrl = "/index.html";
+        signUpService.signUp(request,response);
+        redirect(REDIRECT_URL,response);
+    }
+
+    @Override
+    public void doPost(HttpRequest request, HttpResponse response) {
+        signUpService.singUpByPost(request,response);
+        redirect(REDIRECT_URL,response);
+    }
+
+    public void redirect(String redirectUrl,HttpResponse response ){
         response.redirect(redirectUrl);
     }
 
