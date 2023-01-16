0ba110e22ad777eef338f0c2330dbeba372e7dfb
jieonkim23 <kwldjs1023@gmail.com>
2023-01-16 18:35:42 +0900

    :recycle: MatchController에 controller mapping 기능을 이동시킴

---
+++ src/main/java/Controller/MatchController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import Request.HttpRequest;
+import util.FileIoUtil;
+
+import java.io.DataOutputStream;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class MatchController {
+    public static Controller match(DataOutputStream dos, HttpRequest httpRequest) {
+        String requestPath = httpRequest.getPath();
+        Path path = FileIoUtil.mappingDirectoryPath(requestPath);
+
+        if (requestPath.startsWith("/user/create")) {
+            return new JoinController(httpRequest, dos);
+        }
+        try {
+            if (path.toFile().exists()) {
+                return new FileController(httpRequest, dos);
+            }
+        } catch (NullPointerException e) {
+            return new NonController(httpRequest, dos);
+        }
+        return new NonController(httpRequest, dos);
+    }
+}
