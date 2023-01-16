16e4e3ed73fa63bde987b2c80465553d0809dddb
wonju-dev <wonjulee.dev@gmail.com>
2023-01-16 11:36:38 +0900

    feat(UserController): http request method에 따라 다른 작업 수행하도록 변경

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,11 +1,15 @@
 package controller;
 
 import dto.UserInfoDTO;
+import http.common.Method;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 import static filesystem.PathParser.DOMAIN;
 
@@ -13,16 +17,23 @@ public class UserController implements Controller {
 
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService = new UserService();
+    private final Map<Method, BiConsumer<HttpRequest, HttpResponse>> handlers = Map.of(
+            Method.GET, this::doGet,
+            Method.POST, this::doPost
+    );
 
     @Override
-    public void handle(HttpRequest request, HttpResponse response) {
+    public void service(HttpRequest request, HttpResponse response) {
         logger.debug("user controller called");
-        handleRequest(request, response);
+        handlers.get(request.getMethod()).accept(request, response);
     }
 
-    private void handleRequest(HttpRequest request, HttpResponse response) {
+    private void doGet(HttpRequest request, HttpResponse response) {
         UserInfoDTO userInfo = UserInfoDTO.of(request.getParameters("userId", "password", "name", "email"));
         userService.signIn(userInfo);
         response.redirect(DOMAIN);
+    }
+
+    private void doPost(HttpRequest request, HttpResponse response) {
     }
 }
