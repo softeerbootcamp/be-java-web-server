59a76d5995633e65d4d50adfd4ba1ee756332877
jieonkim23 <kwldjs1023@gmail.com>
2023-01-17 02:33:04 +0900

    :sparkles: ManageDB에 이미 존재하는 유저인지 확인하는 메소드 구현: 이름이 같으면 그냥 같은 유저로 취급함, 추후 조건에 따라 수정예정

--- src/main/java/util/ManageDB.java
+++ src/main/java/util/ManageDB.java
@@ -1,12 +1,14 @@
 package util;
 
 import Request.HttpRequest;
+import Response.HttpResponse;
 import db.Database;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class ManageDB {
     private static final Logger logger = LoggerFactory.getLogger(ManageDB.class);
@@ -17,7 +19,12 @@ public class ManageDB {
         Database.addUser(user);
         logger.debug(String.format("\nsave user: %s\n", user.toString()));
     }
-
+    public static boolean alreadyExist(HttpRequest httpRequest){
+        String userId = httpRequest.getParams().get("userId");
+        if(Objects.nonNull(findUserById(userId)))
+            return true;
+        return false;
+    }
     public static User findUserById(String userId) {
         return Database.findUserById(userId);
     }
