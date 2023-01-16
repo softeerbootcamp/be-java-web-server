2e2dfbb49e7c1a7a06a2860ec71effa113b777be
Soomin-Lim <augustth511@gmail.com>
2023-01-16 11:40:48 +0900

    feat: 예외 처리를 하기 위해 RequestHandler의 run() 메서드에서 HttpRequest, HttpResponse 객체 생성 후 수행하는 로직을 FrontController 클래스 생성 후 FrontController의 process() 메서드로 이동
    - FrontController 클래스 생성
    - WebServer 클래스의 main() 메서드에서 FrontController 객체 생성 후 RequestHandler 생성자의 인자로 frontController를 넘겨줌
    - RequestHandler의 private final 멤버변수로 FrontController 추가
    - RequestHandler의 run() 메서드에서 frontController.process() 메서드 호출
    - frontController.process() 메서드
        - 매핑된 컨트롤러를 찾지 못했을 때 발생하는 ControllerNotFoundException, 잘못된 URL 요청을 보냈을 때 발생하는 UrlNotFoundException, 파일을 찾지 못했을 때 FileNotFoundException을 처리하는 try-catch 문 추가

---
+++ src/main/java/webserver/FrontController.java
@@ -0,0 +1,43 @@
+package webserver;
+
+import Controller.Controller;
+import exception.ControllerNotFoundException;
+import exception.UrlNotFoundException;
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.ControllerMapper;
+import view.ViewResolver;
+
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class FrontController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FrontController.class);
+
+    public void process(HttpRequest httpRequest, HttpResponse httpResponse, OutputStream out) {
+        try {
+            Controller controller = ControllerMapper.getController(httpRequest);
+
+            String viewName = controller.process(httpRequest, httpResponse);
+            String viewPath = ViewResolver.process(viewName);
+
+            sendResponse(out, httpResponse, viewPath);
+        } catch (ControllerNotFoundException | UrlNotFoundException | FileNotFoundException e) {
+            logger.error(e.getMessage());
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void sendResponse(OutputStream out, HttpResponse httpResponse, String viewPath) throws IOException {
+        httpResponse.makeBodyMessage(viewPath);
+
+        DataOutputStream dos = new DataOutputStream(out);
+        httpResponse.send(dos);
+    }
+}
