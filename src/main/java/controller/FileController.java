4c2a2d0ee16bc1ee77af8449465ef836198d43c6
roopre <benny1020@naver.com>
2023-01-16 18:20:08 +0900

    refactor: /webapp static 변수로 선언

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -10,11 +10,12 @@ import response.HttpResponse;
 import webserver.HttpStatus;
 
 public class FileController extends AbstractController {
+	private final static String RESOURCE_DIR = "/webapp";
 	public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
 		String path = httpRequest.getUrl().getPath();
 		String[] splitList = path.split("\\.");
 		final String extension = splitList[splitList.length - 1];
-		File file = new File("./webapp" + httpRequest.getUrl().getPath());
+		File file = new File(RESOURCE_DIR + httpRequest.getUrl().getPath());
 		httpResponse.setHttpResponse(HttpStatus.OK, new String(Files.readAllBytes(file.toPath())),
 			ContentType.findContentType(extension));
 	}
