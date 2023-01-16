f3eab3a4e9bc8eac4d08dff7a87a32d182fe1c0e
wonju-dev <wonjulee.dev@gmail.com>
2023-01-16 16:49:44 +0900

    refactor(Header): refactor attribute parsing method

--- src/main/java/http/request/Header.java
+++ src/main/java/http/request/Header.java
@@ -1,12 +1,21 @@
 package http.request;
 
-import http.response.enums.ResponseAttribute;
+import http.common.HeaderAttribute;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Header {
+
+    private static final String HEADER_SEPARATOR = "\n\r";
+    private static final String ATTRIBUTE_SEPARATOR = ":";
+    private static final String EMPTY_STRING = "";
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
+
     private Map<String, String> attributes = new HashMap<>();
 
     public Header() {
@@ -18,28 +27,42 @@ public class Header {
 
     private Map<String, String> parseMessage(String message) {
         Map<String, String> att = new HashMap<>();
-        String[] lines = message.split("\n\r");
-        for (String line : lines) {
-            String[] chunks = line.split(":");
-            att.put(chunks[0].trim(), chunks[1].trim());
-        }
+        Arrays.stream(message.split(HEADER_SEPARATOR))
+                .forEach(attribute -> {
+                    List<String> splitPair = getSplitPair(attribute);
+                    att.put(splitPair.get(KEY_INDEX), splitPair.get(VALUE_INDEX));
+                });
         return att;
     }
 
-    public void setAttribute(ResponseAttribute key, String value) {
+    private List<String> getSplitPair(String attribute) {
+        List<String> split = Arrays.stream(attribute.split(ATTRIBUTE_SEPARATOR)).map(String::trim).collect(Collectors.toList());
+        if (split.size() > VALUE_INDEX) {
+            return List.of(split.get(KEY_INDEX), split.get(VALUE_INDEX));
+        }
+        return List.of(split.get(KEY_INDEX), EMPTY_STRING);
+    }
+
+    public void setAttribute(HeaderAttribute key, String value) {
         attributes.put(key.getValue(), value);
     }
 
-    public void setAttribute(ResponseAttribute key, Integer value) {
+    public void setAttribute(HeaderAttribute key, Integer value) {
         setAttribute(key, Integer.toString(value));
     }
 
-    public String getAttribute(ResponseAttribute key) {
-        return attributes.get(key.getValue());
+    public String getAttribute(HeaderAttribute key) {
+        String value = attributes.get(key.getValue());
+        if (value == null) {
+            return key.getDefaultValue();
+        }
+        return value;
     }
 
     @Override
     public String toString() {
-        return String.join("\n\r", attributes.keySet().stream().map(k -> k + ":" + attributes.get(k)).collect(Collectors.toList())) + "\n\r" + "\n\r";
+        return String.join(HEADER_SEPARATOR, attributes.keySet().stream()
+                .map(k -> k + ATTRIBUTE_SEPARATOR + attributes.get(k))
+                .collect(Collectors.toList())) + HEADER_SEPARATOR + HEADER_SEPARATOR;
     }
 }
