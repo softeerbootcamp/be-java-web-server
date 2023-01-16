37453ca3b866d653d0e57a7bf82af9e2364e1bab
rentalhub-mac78 <kksy2423@naver.com>
2023-01-16 17:37:53 +0900

    Test: RequestPostReader raedData 기능 추가(form태그의 데이터 전송)

---
+++ src/test/java/reader/RequestPostReaderTest.java
@@ -0,0 +1,41 @@
+package reader;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import request.HttpRequest;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestPostReaderTest {
+
+    private final byte[] BASIC_REQUEST_INCLUDE_BODY = ("POST /user/create HTTP/1.1\n" +
+            "Host: localhost:8080\n" +
+            "Connection: keep-alive\n" +
+            "Content-Length: 59\n" +
+            "Content-Type: application/x-www-form-urlencoded\n" +
+            "Accept: */*\n" +
+            "\n" +
+            "userId=test&password=testPW&name=test&email=test@naver.com").getBytes();
+
+    @Test
+    @DisplayName("Post의 요청의 form데이터 읽기 테스트")
+    void readData() throws IOException {
+        //given
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(BASIC_REQUEST_INCLUDE_BODY);
+        //when
+        HttpRequest httpRequest = HttpRequest.getHttpRequest(inputStream);
+        RequestReader requestReader = new RequestPostReader();
+        HashMap<String, String> usermap = requestReader.readData(httpRequest);
+        //then
+        assertThat(usermap.get("userId")).isEqualTo("test");
+        assertThat(usermap.get("password")).isEqualTo("testPW");
+        assertThat(usermap.get("name")).isEqualTo("test");
+        assertThat(usermap.get("email")).isEqualTo("test@naver.com");
+    }
+}
