92955499dcfeeec116691dbe2cacfecdd9b05f1c
roopre <benny1020@naver.com>
2023-01-16 21:12:25 +0900

    refactor: 불필요한 로깅 제거

--- src/main/java/request/HttpRequestUtils.java
+++ src/main/java/request/HttpRequestUtils.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,21 +19,17 @@ import com.google.common.collect.Maps;
 
 public class HttpRequestUtils {
 
-	private static final Logger logger = LoggerFactory.getLogger(HttpRequestUtils.class);
-
 	public static HttpRequest httpRequestParse(InputStream in) {
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+			Map<String, String> headers = new HashMap<>();
+			Map<String, String> body = new HashMap<>();
 			String line = br.readLine();
 			HttpRequestLine httpRequestLine = HttpRequestLine.parse(line);
 			line = br.readLine();
-			Map<String, String> headers = new HashMap<>();
-			Map<String, String> body = new HashMap<>();
 			while (!line.equals("")) {
 				headers.put(line.split(": ")[0], line.split(": ")[1]);
 				line = br.readLine();
-				logger.debug(line);
-
 			}
 
 			if (headers.containsKey("Content-Length")) {
@@ -40,7 +37,6 @@ public class HttpRequestUtils {
 				char[] buffer = new char[contentLength];
 				br.read(buffer, 0, contentLength);
 				String requestBody = new String(buffer);
-				logger.debug(requestBody);
 				String[] tokens = requestBody.split("&");
 				body.putAll(
 					Arrays.stream(tokens).map(t -> t.split("=")).collect(Collectors.toMap(p -> p[0], p -> p[1])));
@@ -63,20 +59,4 @@ public class HttpRequestUtils {
 
 	}
 
-	public static Pair<String, String> getKeyValue(String keyValue, String regex) {
-		if (Strings.isNullOrEmpty(keyValue)) {
-			return null;
-		}
-
-		String[] tokens = keyValue.split(regex);
-		if (tokens.length != 2) {
-			return null;
-		}
-		return new Pair<String, String>(tokens[0], tokens[1]);
-	}
-
-	public String pathParse(String line) {
-		String[] str = line.split(" ");
-		return str[1];
-	}
 }
