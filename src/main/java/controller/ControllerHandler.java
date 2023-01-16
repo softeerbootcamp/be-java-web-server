34af21c1216dd12a05ab7f15e1d9e2bb501c1800
ktykty0722 <ktykty0722@naver.com>
2023-01-16 11:32:37 +0900

    Feat : HttpResponse 수정 및 HttpResponse 사용 부분 수정 - HttpResponse 내부 response 메소드들 삭제 후 sendResponse 추가 - ControllerHandler handle 메소드 반환 타입 변경 후 findController 내부 코드 이동 후 삭제 - Requesthandler HttpResponse 사용 방식 변경

--- src/main/java/controller/ControllerHandler.java
+++ src/main/java/controller/ControllerHandler.java
@@ -1,6 +1,9 @@
 package controller;
 
-import http.*;
+import http.HttpRequest;
+import http.HttpResponse;
+import http.RequestLine;
+import http.Uri;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.RequestHandler;
@@ -10,7 +13,7 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ControllerHandler{
+public class ControllerHandler {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private static List<Controller> controllers = new ArrayList<>();
@@ -18,27 +21,18 @@ public class ControllerHandler{
     static {
         controllers.add(new UserController());
     }
-    public static Controller findController(HttpRequest httpRequest) {
+
+    public static HttpResponse handle(HttpRequest httpRequest) throws IOException, URISyntaxException {
         RequestLine requestLine = httpRequest.getRequestLine();
         Uri uri = requestLine.getUri();
-        if(!uri.isQueryParameterExist() && uri.isEndWithResourceType()) {
-            return new ViewController();
+        if (!uri.isQueryParameterExist() && uri.isEndWithResourceType()) {
+            return new ViewController().doService(httpRequest);
         }
         return controllers
                 .stream()
                 .filter(controller -> controller.isMatch(httpRequest))
                 .findFirst()
-                .orElseThrow(() -> new ControllerNotFoundException("Not Found Controller"));
-    }
-
-    public static void handle(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
-        try {
-            Controller controller = findController(httpRequest);
-            controller.doService(httpRequest, httpResponse);
-        } catch (ControllerNotFoundException |ResourceTypeNotFoundException e) {
-            logger.error(e.getMessage());
-            httpResponse.response404Header();
-            httpResponse.responseBody(e.getMessage().getBytes());
-        }
+                .orElseThrow(() -> new ControllerNotFoundException("Not Found Controller"))
+                .doService(httpRequest);
     }
 }
--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -16,8 +16,6 @@ public class HttpResponse {
     private final String contentType;
     private final Map<String, String> headers;
     private final byte[] body;
-    private final DataOutputStream dos;
-
 
     private HttpResponse(
             HttpStatus httpStatus,
@@ -43,52 +41,20 @@ public class HttpResponse {
         return new HttpResponse(httpStatus, contentType, headers, body, httpVersion);
     }
 
-
-    public void response200Header(String contentType, int lengthOfBodyContent) {
+    public void sendResponse(DataOutputStream dos) {
         try {
-            dos.writeBytes(httpVersion + " " + HttpStatus.OK + System.lineSeparator());
+            dos.writeBytes(httpVersion + " " + httpStatus.getCode() + System.lineSeparator());
             dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8" + System.lineSeparator());
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + System.lineSeparator());
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    public void response302Header(String location) {
-        try {
-            dos.writeBytes(httpVersion + " " + HttpStatus.FOUND + System.lineSeparator());
-            dos.writeBytes("Location: " + location + System.lineSeparator());
+            for (String key : headers.keySet()) {
+                dos.writeBytes(key + ": " + headers.get(key) + System.lineSeparator());
+            }
+            dos.writeBytes("Content-Length: " + body.length + System.lineSeparator());
             dos.writeBytes(System.lineSeparator());
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
 
-    public void response404Header() {
-        try {
-            dos.writeBytes(httpVersion + " " + HttpStatus.NOT_FOUND + System.lineSeparator());
-            dos.writeBytes(System.lineSeparator());
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-
-    public void responseBody(byte[] body) {
-        try {
             dos.write(body, 0, body.length);
             dos.flush();
         } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    public void responseBody() {
-        try {
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+            logger.error("While writing response " + e.getMessage());
         }
     }
 }
