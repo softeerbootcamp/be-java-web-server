e3fac0c3bb0fb84740d28f2752309f3008747423
psm9718 <psm9718@gmaill.com>
2023-01-16 18:21:27 +0900

    feat: UserController 클래스 생성
    로그인, 회원가입 등 유저 관련 처리를 위해 조건에 맞는 메서드를 처리할 수 있는 컨트롤러 생성. 아직 기능은 미완성이며, 각 조건을 어떻게 분기할 것인지에 대한 구조를 고민중임.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -12,12 +12,12 @@ public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private final Socket connection;
-    private final UserService userService;
+    private final UserController userController;
 
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
-        this.userService = new UserService();
+        this.userController = new UserController();
     }
 
     public void run() {
@@ -27,8 +27,9 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream()) {
             Request request = new Request(in);
 
+            //TODO Controller 고민, 조건에 맞게 처리될 수 있도록
             if (request.getRequestParams().size() != 0) {
-                userService.signUpUser(request);
+                userController.signUp(request);
             }
 
             ResponseHandler responseHandler = new ResponseHandler(connection);
