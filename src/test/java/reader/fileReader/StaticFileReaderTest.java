3c54f3ab6bea1296cb650716f2436f6ab12e6829
rentalhub-mac78 <kksy2423@naver.com>
2023-01-16 16:49:07 +0900

    Test: StaticFileReaderTest resources/static 내의 파일 읽기 테스트

---
+++ src/test/java/reader/fileReader/StaticFileReaderTest.java
@@ -0,0 +1,40 @@
+package reader.fileReader;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.Url;
+import util.UrlType;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StaticFileReaderTest {
+
+    private static final String VALID_CSS_URL = "/css/styles.css";
+    private static final String NOT_VALID_CSS_URL = "/css/notvalid.css";
+    private static final String RELATIVE_PATH = "./src/main/resources/static";
+
+    @Test
+    @DisplayName("resources/static 내의 있는 file 읽기")
+    void readFile() throws IOException {
+        String url = VALID_CSS_URL;
+        FileReader fileReader = new StaticFileReader();
+        assertThat(fileReader.readFile(new Url(url, UrlType.STATIC_FILE))).isEqualTo(
+                Files.readAllBytes(new File(RELATIVE_PATH + url).toPath())
+        );
+    }
+
+    @Test
+    @DisplayName("resources/static 내의 있는 file읽기중 예외 테스트")
+    void readFileThrowException()  {
+        String url = NOT_VALID_CSS_URL;
+        FileReader fileReader = new StaticFileReader();
+        assertThatThrownBy(() -> fileReader.readFile(new Url(url, UrlType.STATIC_FILE)))
+                .isInstanceOf(IOException.class);
+    }
+}
