794dc9dabb713f568d7fd201a74d05fb30e1e943
rentalhub-mac78 <kksy2423@naver.com>
2023-01-16 17:38:00 +0900

    Feat: RequestPostReader raedData 기능 추가(form태그의 데이터 전송)

---
+++ src/main/java/reader/RequestPostReader.java
@@ -0,0 +1,24 @@
+package reader;
+
+import request.HttpRequest;
+
+import java.util.HashMap;
+
+public class RequestPostReader implements RequestReader{
+
+    private final String REGEX_FORM = "&";
+    private final String REGEX_FORM_DATA = "=";
+
+
+    @Override
+    public HashMap<String, String> readData(HttpRequest httpRequest) {
+        StringBuilder data = httpRequest.getRequestBody().getData();
+        String[] keyValuePairs = data.toString().split(REGEX_FORM);
+        HashMap<String, String> map = new HashMap<>();
+        for (String keyValuePair : keyValuePairs) {
+            String[] keyValue = keyValuePair.split(REGEX_FORM_DATA);
+            map.put(keyValue[0], keyValue[1]);
+        }
+        return map;
+    }
+}
