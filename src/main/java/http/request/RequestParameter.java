febab9921564eef1248aa8140d72087cc6a299b6
wonju-dev <wonjulee.dev@gmail.com>
2023-01-16 13:57:30 +0900

    feat(RequestParameter): 바디 파싱 기능 구현

--- src/main/java/http/request/RequestParameter.java
+++ src/main/java/http/request/RequestParameter.java
@@ -1,14 +1,30 @@
 package http.request;
 
+import http.common.Body;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class RequestParameter {
     private Map<String, String> requestParameters;
 
+    public RequestParameter(Body body) {
+        this.requestParameters = parseBody(body);
+    }
+
+    private Map<String, String> parseBody(Body body) {
+        byte[] data = body.getData();
+        String msg = "";
+        for (byte datum : data) {
+            msg += (char) datum;
+        }
+        return parseQueryString(msg);
+    }
+
     public RequestParameter(RequestStartLine startLine) {
         this.requestParameters = parseQueryString(startLine.getUrl());
     }
+
 
     private Map<String, String> parseQueryString(String url) {
         Map<String, String> parameters = new HashMap<>();
