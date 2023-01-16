880f135a3d7504b2ee4b2c00a7caeb8fea6c3447
pjs0418 <tony180322@gmail.com>
2023-01-16 12:14:05 +0900

    body라는 멤버 변수 이름이 애매해서 content라는 이름으로 변경

--- src/main/java/model/request/RequestBody.java
+++ src/main/java/model/request/RequestBody.java
@@ -7,27 +7,27 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RequestBody {
-    private final Map<String, String> body;
+    private final Map<String, String> content;
 
-    private RequestBody(Map<String, String> body) {
-        this.body = body;
+    private RequestBody(Map<String, String> content) {
+        this.content = content;
     }
 
     public static RequestBody of(BufferedReader br, int contentLength) throws IOException {
         char[] body = new char[contentLength];
         int bodyLength = br.read(body, 0, contentLength);
 
-        Map<String, String> messageBody = parseRequestBody(String.valueOf(body));
-        return new RequestBody(messageBody);
+        Map<String, String> content = parseRequestBody(String.valueOf(body));
+        return new RequestBody(content);
     }
 
-    private static Map<String, String> parseRequestBody(String body) {
+    public static Map<String, String> parseRequestBody(String body) {
         return Arrays.stream(body.split("&"))
                 .map(b -> b.split("="))
                 .collect(Collectors.toMap(a -> a[0], a -> a[1]));
     }
 
-    public Map<String, String> getBody() {
-        return body;
+    public Map<String, String> getContent() {
+        return content;
     }
 }
