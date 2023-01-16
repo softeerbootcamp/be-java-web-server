4cbc03900f09d7b29eebfa4811a0d429d3d291f7
rohsik2 <rohsik2@gmail.com>
2023-01-16 10:21:43 +0900

    :white_check_mark: 아이디 중복검사 테스트 작성

---
+++ src/test/java/controller/UserAccountControllerTest.java
@@ -0,0 +1,40 @@
+package controller;
+
+import httpMock.CustomHttpRequest;
+import httpMock.CustomHttpResponse;
+import model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import service.UserService;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserAccountControllerTest {
+    static UserAccountController userAccountController;
+
+    @BeforeAll
+    static void getUserAccountController() {
+        userAccountController = UserAccountController.get();
+    }
+
+    @Test
+    @DisplayName("유저 아이디 중복 검사")
+    void 유저아이디_중복검사() {
+        UserService.addUser(new User("asdf", "asdfasdf123", "asdf", "asdfasdf@asdf.com"));
+
+        CustomHttpRequest req = CustomHttpRequest.of(
+                "GET /user/create?userId=asdf&password=123123&email=asdfasdf@asdf.com HTTP/1.1",
+                Collections.EMPTY_LIST
+        );
+
+        CustomHttpResponse res = userAccountController.makeAccount(req);
+
+
+        //then
+        assertEquals(new String(res.getBody()), "userID duplicated");
+
+    }
+}
