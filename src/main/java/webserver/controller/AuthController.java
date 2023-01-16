6fac2425777171fdd6bdb95dec162668b5e68160
i5046821854 <o5046821854@gmail.com>
2023-01-16 18:10:38 +0900

    refactor : userCreate 메소드의 애노테이션에 해당 메소드의 http method 정보를 기입하도록

--- src/main/java/webserver/controller/AuthController.java
+++ src/main/java/webserver/controller/AuthController.java
@@ -1,9 +1,8 @@
 package webserver.controller;
 
-import com.google.j2objc.annotations.ObjectiveCName;
-import webserver.ControllerExecutioner;
-import webserver.annotation.ControllerInfo;
+import webserver.ControllerInterceptor;
 import webserver.Service.AuthService;
+import webserver.annotation.ControllerInfo;
 import webserver.domain.ContentType;
 import webserver.domain.RequestMethod;
 import webserver.domain.StatusCodes;
@@ -21,8 +20,9 @@ public class AuthController implements Controller {
     public AuthController(){
         this.authService = new AuthService();
     }
-    
-    @ControllerInfo(method = RequestMethod.POST, path = "/user/create", methodName = "userCreate", queryStr = {"userId", "password", "name", "email"})
+
+
+    @ControllerInfo(path = "/user/create", methodName = "userCreate", queryStr = {"userId", "password", "name", "email"}, method = RequestMethod.POST)
     public void userCreate(Map<String, String> queryStrs, Response response) throws HttpRequestException{
         //TODO: authService.join의 리턴값 핸들링
         byte[] result = authService.join(queryStrs.get("userId"), queryStrs.get("password"), queryStrs.get("name"), queryStrs.get("email"));
@@ -33,11 +33,10 @@ public class AuthController implements Controller {
             );
     }
 
-
     @Override
     public void chain(Request req, Response res) throws HttpRequestException, IOException {
         try{
-            ControllerExecutioner.executeController(AuthController.class, req, res);
+            ControllerInterceptor.executeController(AuthController.class, req, res);
         }catch (HttpRequestException e){
             res.error(e.getErrorCode(), e.getMsg().getBytes(), ContentType.TEXT_HTML);
         }
