31dc084b52c3fb98baa91341a085557cae83be10
jieonkim23 <kwldjs1023@gmail.com>
2023-01-16 18:54:27 +0900

    :recycle: JoinController를 인터페이스를 상속받도록 수정하고 response를 빌더 패턴을 사용하도록 수정

--- src/main/java/Controller/JoinController.java
+++ src/main/java/Controller/JoinController.java
@@ -2,27 +2,32 @@ package Controller;
 
 import Request.HttpRequest;
 import Response.HttpResponse;
+import Response.HttpResponseStartLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpResponseUtil;
 import util.ManageDB;
-import util.StatusCode;
+import Request.StatusCode;
 
 import java.io.DataOutputStream;
 
-public class JoinController extends Controller {
+public class JoinController implements Controller {
     private final Logger logger = LoggerFactory.getLogger(JoinController.class);
-
+    HttpRequest httpRequest;
+    DataOutputStream dos;
     public JoinController(HttpRequest httpRequest, DataOutputStream dos) {
-        super(httpRequest, dos);
         logger.debug("select joinController");
+        this.httpRequest  = httpRequest;
+        this.dos = dos;
     }
 
     @Override
     public HttpResponse createResponse() {
         ManageDB.saveUser(this.httpRequest);
 
-        HttpResponse response = HttpResponse.createHttpResponse("", StatusCode.FOUND, httpRequest.getProtocol());
-        response.putHeaders("Location", "/index.html");
+        HttpResponse response = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.FOUND, httpRequest.getProtocol()))
+                .headers(HttpResponseUtil.generateHeaders("", StatusCode.NOT_FOUND, 0));
+        response.putHeader("Location", "/index.html");
         return response;
     }
 }
