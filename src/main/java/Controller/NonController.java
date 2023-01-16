43d690235a8db5ba0567531e2b53b259dd507379
jieonkim23 <kwldjs1023@gmail.com>
2023-01-16 18:54:36 +0900

    :recycle: NonController를 인터페이스를 상속받도록 수정하고 response를 빌더 패턴을 사용하도록 수정

--- src/main/java/Controller/NonController.java
+++ src/main/java/Controller/NonController.java
@@ -1,22 +1,28 @@
 package Controller;
 
 import Request.HttpRequest;
+import Response.ContentType;
 import Response.HttpResponse;
+import Response.HttpResponseBody;
+import Response.HttpResponseStartLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import Request.StatusCode;
 import util.HttpResponseUtil;
-import util.StatusCode;
 
 import java.io.DataOutputStream;
+import java.util.Map;
 
-public class NonController extends Controller {
+public class NonController implements Controller {
     private final Logger logger = LoggerFactory.getLogger(NonController.class);
+    HttpRequest httpRequest;
+    DataOutputStream dos;
 
     public NonController(HttpRequest httpRequest, DataOutputStream dos) {
-        super(httpRequest, dos);
-        logger.debug("select nonController");
+        logger.debug("select NonController");
+        this.httpRequest = httpRequest;
+        this.dos = dos;
     }
-
 
     @Override
     public HttpResponse createResponse() {
@@ -28,16 +34,20 @@ public class NonController extends Controller {
 
     public HttpResponse response404() {
         logger.debug("[response404]");
-        HttpResponse httpResponse = HttpResponse.createHttpResponse(httpRequest.getPath(), StatusCode.NOT_FOUND, httpRequest.getProtocol());
-        byte[] body = "존재하지않는 파일입니다.".getBytes();
-        httpResponse.setBody(body);
-        httpResponse.putHeaders("Content-Length", String.valueOf(body.length));
+        byte[] body = "404: 존재하지않는 파일입니다.".getBytes();
+        HttpResponse httpResponse = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.NOT_FOUND, httpRequest.getProtocol()))
+                .headers(HttpResponseUtil.generateHeaders("", StatusCode.NOT_FOUND, body.length))
+                .body(new HttpResponseBody(body));
         return httpResponse;
     }
 
     public HttpResponse responseRoot() {
         logger.debug("[responseRoot]");
-        HttpResponse httpResponse = HttpResponse.createHttpResponse("/index.html", StatusCode.OK, httpRequest.getProtocol());
+        byte[] body = HttpResponseUtil.generateBody("/index.html");
+        HttpResponse httpResponse = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.OK, httpRequest.getProtocol()))
+                .headers(HttpResponseUtil.generateHeaders("/index.html", StatusCode.OK, body.length))
+                .body(new HttpResponseBody(body));
+
         return httpResponse;
     }
 }
