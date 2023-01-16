c32c7a87ebd365b30c78bb853f19b0db0cab7e96
Toa <ld980105@snu.ac.kr>
2023-01-16 18:24:25 +0900

    :sparkles: login을 관리하는 LoginController 추가

--- src/main/java/webserver/Dispatcher.java
+++ src/main/java/webserver/Dispatcher.java
@@ -1,21 +1,26 @@
 package webserver;
 
 import controller.Controller;
+import controller.LoginController;
 import controller.StaticFileController;
 import controller.UserController;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class Dispatcher {
     private final static Map<String, Controller> controllers;
+    private final static Logger logger = LoggerFactory.getLogger(Dispatcher.class);
 
     static {
         AppConfig appConfig = new AppConfig();
         controllers = Map.of(
                 UserController.PATH, new UserController(appConfig.userService()),
-                StaticFileController.PATH, new StaticFileController()
+                StaticFileController.PATH, new StaticFileController(),
+                LoginController.PATH, new LoginController(appConfig.userService())
         );
     }
 
