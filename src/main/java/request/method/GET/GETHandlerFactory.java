b31e30c4c8688b252fd7f35711906d6497ed76c5
201724554 <pay7845@naver.com>
2023-01-16 18:16:30 +0900

    GETNoMatchHandler의 of 메소드에서 getInstance 메소드로 변경

--- src/main/java/request/method/GET/GETHandlerFactory.java
+++ src/main/java/request/method/GET/GETHandlerFactory.java
@@ -7,7 +7,7 @@ import request.method.GET.handlers.GETNoMatchHandler;
 
 public class GETHandlerFactory {
     public static GETHandler generateHandler(Request request) {
-        GETHandler handler = GETNoMatchHandler.of();
+        GETHandler handler = GETNoMatchHandler.getInstance();
         for(GETHandlerEnum GETHandlerEnum : GETHandlerEnum.values()) {
             // TODO: resource를 enum의 url과 비교하는 것을 equals 메소드를 사용할 수 있도록 수정
             if(request.getResource().contains(GETHandlerEnum.getUrl())) {
