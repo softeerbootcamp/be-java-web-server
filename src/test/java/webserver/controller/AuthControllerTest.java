d19420ea18f8087f3643779991055b36d889cebb
i5046821854 <o5046821854@gmail.com>
2023-01-16 18:12:07 +0900

    feat : AuthControllerTest에서의 Mock 객체 생성

--- src/test/java/webserver/controller/AuthControllerTest.java
+++ src/test/java/webserver/controller/AuthControllerTest.java
@@ -1,15 +1,16 @@
 package webserver.controller;
-import net.bytebuddy.matcher.CollectionOneToOneMatcher;
 import org.junit.jupiter.api.*;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import webserver.ControllerExecutioner;
+import webserver.ControllerInterceptor;
 import webserver.Service.AuthService;
 import webserver.domain.ContentType;
 import webserver.domain.StatusCodes;
+import webserver.domain.request.Request;
 import webserver.domain.response.Response;
 import webserver.exception.HttpRequestException;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,10 +20,25 @@ import static org.mockito.Mockito.*;
 class AuthControllerTest {
 
     AuthController authController;
+    AuthService authService;
+    Request request;
+    Response response;
+    MockedStatic<ControllerInterceptor> ceMock;
 
     @BeforeEach
     void testSetUp(){
         authController = new AuthController();
+        String requestLine = "GET /index.html /1.1";
+        String header = "password: testPass\n"+
+                        "name: testName\n"+
+                        "email : test@email.com";
+
+        String body = "";
+        request = Request.of(requestLine, header, body);
+        response = mock(Response.class);
+        authService = mock(AuthService.class);
+        ceMock = mockStatic(ControllerInterceptor.class);
+
     }
 
     @Test
@@ -30,94 +46,67 @@ class AuthControllerTest {
     public void userCreateTest() throws HttpRequestException {
 
         AuthService authService = mock(AuthService.class);
-        Response response = mock(Response.class);
 
         // given
-        Map<String, String> queryStrs = new HashMap<>();
-        queryStrs.put("userId", "testUser");
-        queryStrs.put("password", "testPass");
-        queryStrs.put("name", "testName");
-        queryStrs.put("email", "test@email.com");
         byte[] joinResult = "testResult".getBytes();
 
         // when
-        when(authService.join(anyString(), anyString(), anyString(), anyString())).thenReturn(joinResult);
+        when(authService.join(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString())).thenReturn(joinResult);
 
         //then
-        authController.userCreate(queryStrs, response);
+        authController.userCreate(request.getRequestHeader(), response);
         verify(response).redirect(StatusCodes.SEE_OTHER, joinResult, ContentType.TEXT_HTML, "http://localhost:8080/index.html");
     }
 
     @Test
     @DisplayName("체이닝 메소드 검사-올바른 경로로 보냈을 떄")
-    void chainingMethodTest_ValidPath(){
+    void chainingMethodTest_ValidPath() throws IOException {
 
         //given
         String path = "/user/create";
-        Response res = mock(Response.class);
         AuthService authService = mock(AuthService.class);
+        MockedStatic<ControllerInterceptor> ceMock = mockStatic(ControllerInterceptor.class);
 
-        Map<String, String> queryStrs = new HashMap<>();
-        MockedStatic<ControllerExecutioner> ceMock = mockStatic(ControllerExecutioner.class);
-
-        queryStrs.put("userId", "testUser");
-        queryStrs.put("password", "testPass");
-        queryStrs.put("name", "testName");
-        queryStrs.put("email", "test@email.com");
-
-        ceMock.when(()-> ControllerExecutioner.executeController(Mockito.any(Class.class), Mockito.any(Map.class), Mockito.any(Response.class), Mockito.any(String.class))).thenAnswer(invocation->null);
-        authController.chain(path, queryStrs, res);
+        ceMock.when(()-> ControllerInterceptor.executeController(Mockito.any(Class.class),  Mockito.any(Request.class), Mockito.any(Response.class))).thenThrow(new HttpRequestException(StatusCodes.INTERNAL_SERVER_ERROR));
+        authController.chain(request, response);
 
         //then
-        verify(authService, times(1)).join(anyString(), anyString(), anyString(), anyString());
+        verify(authController, times(1)).userCreate(Mockito.any(Map.class), Mockito.any(Response.class));
     }
 
 
     @Test
     @DisplayName("체이닝 메소드 검사-옳지 않은 경로로 보냈을 때")
-    void chainingMethodTest_inValidPath(){
+    void chainingMethodTest_inValidPath() throws IOException {
 
         //given
         String path = "/user/***";
-        Response res = mock(Response.class);
-        MockedStatic<ControllerExecutioner> ceMock = mockStatic(ControllerExecutioner.class);
-
-        Map<String, String> queryStrs = new HashMap<>();
-        queryStrs.put("userId", "testUser");
-        queryStrs.put("password", "testPass");
-        queryStrs.put("name", "testName");
-        queryStrs.put("email", "test@email.com");
+        MockedStatic<ControllerInterceptor> ceMock = mockStatic(ControllerInterceptor.class);
 
         //when
-        ceMock.when(()-> ControllerExecutioner.executeController(Mockito.any(Class.class), Mockito.any(Map.class), Mockito.any(Response.class), Mockito.any(String.class))).thenAnswer(invocation->null);
-        authController.chain(path, queryStrs, res);
+        ceMock.when(()-> ControllerInterceptor.executeController(Mockito.any(Class.class),  Mockito.any(Request.class), Mockito.any(Response.class))).thenThrow(new HttpRequestException(StatusCodes.INTERNAL_SERVER_ERROR));
+        authController.chain(request, response);
 
         //then
-        Assertions.assertThrows(HttpRequestException.class, () -> authController.chain(path, queryStrs, res));
+        Assertions.assertThrows(HttpRequestException.class, () -> authController.chain(request, response));
 
     }
 
 
     @Test
     @DisplayName("체이닝 메소드 검사_올바르지 않은 파라미터를 보넀을 때")
-    void chainingMethodTest_inValidParameter(){
+    void chainingMethodTest_inValidParameter() throws IOException {
 
         //given
         String path = "/user/***";
-        Response res = mock(Response.class);
-        MockedStatic<ControllerExecutioner> ceMock = mockStatic(ControllerExecutioner.class);
-
-        Map<String, String> queryStrs = new HashMap<>();
-        queryStrs.put("password", "testPass");
-        queryStrs.put("name", "testName");
-        queryStrs.put("email", "test@email.com");
+        MockedStatic<ControllerInterceptor> ceMock = mockStatic(ControllerInterceptor.class);
 
         //when
-        ceMock.when(()-> ControllerExecutioner.executeController(Mockito.any(Class.class), Mockito.any(Map.class), Mockito.any(Response.class), Mockito.any(String.class))).thenThrow(new HttpRequestException(StatusCodes.INTERNAL_SERVER_ERROR));
-        authController.chain(path, queryStrs, res);
+        ceMock.when(()-> ControllerInterceptor.executeController(Mockito.any(Class.class),  Mockito.any(Request.class), Mockito.any(Response.class))).thenThrow(new HttpRequestException(StatusCodes.INTERNAL_SERVER_ERROR));
+        authController.chain(request, response);
 
         //then
-        assertEquals(StatusCodes.INTERNAL_SERVER_ERROR, res.getStatusCode());
+        assertEquals(StatusCodes.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
 
 
