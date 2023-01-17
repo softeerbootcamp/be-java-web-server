5afbb270b92c01a6d2267d38eef547ef46dc33d5
rentalhub-mac78 <kksy2423@naver.com>
2023-01-17 17:44:44 +0900

    Feat: Cookie 객체 생성

---
+++ src/main/java/util/Cookie.java
@@ -0,0 +1,19 @@
+package util;
+
+public class Cookie {
+    private String key;
+    private String value;
+
+    public Cookie(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
