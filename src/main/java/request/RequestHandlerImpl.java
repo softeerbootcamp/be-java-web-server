26f3eb63f1a4326e178142fcc03f63ccecbb8927
201724554 <pay7845@naver.com>
2023-01-17 23:11:51 +0900

    handle 매개변수에서 port 제거

--- src/main/java/request/RequestHandlerImpl.java
+++ src/main/java/request/RequestHandlerImpl.java
@@ -6,15 +6,10 @@ import request.method.HttpMethod;
 import response.Response;
 
 public class RequestHandlerImpl implements RequestHandler {
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandlerImpl.class);
     @Override
-    public Response handle(Request request, int port) {
+    public Response handle(Request request) {
         for (HttpMethod method : HttpMethod.values()) {
             if (request.getMethod().equals(method.getMethod())) {
-                logger.debug("handle request {} of port {}, request header {}", request.getMethod(), port, request);
-                logger.debug("{} {} {}", request.getMethod(), request.getResource(), request.getVersion());
-                logger.debug("headers: {}", request.getRequestHeader());
-                logger.debug("body: {}", request.getRequestBody());
                 return method.handleRequest(request);
             }
         }
