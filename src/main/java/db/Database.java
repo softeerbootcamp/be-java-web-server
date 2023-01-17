b28e851089310eb0c36dfb8a1352a09b0dc45877
wonju-dev <wonjulee.dev@gmail.com>
2023-01-17 12:46:38 +0900

    feat(Database): implement session DB

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -2,12 +2,14 @@ package db;
 
 import com.google.common.collect.Maps;
 import model.User;
+import service.Session;
 
 import java.util.Collection;
 import java.util.Map;
 
 public class Database {
     private static Map<String, User> users = Maps.newHashMap();
+    private static Map<String, Session> sessions = Maps.newHashMap();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
@@ -23,5 +25,9 @@ public class Database {
 
     public static Collection<User> findAll() {
         return users.values();
+    }
+
+    public static void addSession(Session session) {
+        sessions.put(session.getId(), session);
     }
 }
