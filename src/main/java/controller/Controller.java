b9454347fb9016ee38c2ceb2ee89bdc970a1ed85
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-17 12:02:12 +0900

    인터페이스의 default 메서드는 구현부를 가질 수 있다, 오버라이딩도 가능

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,8 +1,22 @@
 package controller;
 
-import webserver.HttpRequest;
-import webserver.HttpResponse;
+import webserver.*;
 
 public interface Controller {
-    void service(HttpRequest request,HttpResponse response);
+    default void service(HttpRequest request, HttpResponse response) {
+
+        if(request.getMethod().equals(HttpMethod.GET)) doGet(request,response);
+        if(request.getMethod().equals(HttpMethod.POST)) doPost(request,response);
+    }
+
+    default void doGet(HttpRequest request,HttpResponse response){
+        response.setStatus(HttpStatus.ERROR);
+        response.setContentType(ContentType.HTML);
+    }
+
+    default void doPost(HttpRequest request,HttpResponse response){
+        response.setStatus(HttpStatus.ERROR);
+        response.setContentType(ContentType.HTML);
+    }
+
 }
