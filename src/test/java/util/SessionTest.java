b635d1a543862cb4a1b6209347263d7a7232a89c
ajongs <wonjong1030@naver.com>
2023-01-17 22:03:53 +0900

    test(SessionTest): 세션 유효성 검증 성공하는 테스트 코드 추가

--- src/test/java/util/SessionTest.java
+++ src/test/java/util/SessionTest.java
@@ -1,4 +1,24 @@
+package util;
+
+import db.SessionStorage;
+import model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SessionTest {
-  
+    @Test
+    void 세션_유효성_검증_성공() {
+        Session session = Session.createSessionWith(new User("ajongs", "password"));
+        SessionStorage.addSession(session);
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertThat(session.isValid()).isEqualTo(true);
+    }
 }
