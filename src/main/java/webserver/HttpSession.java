aaf91515aa6c5098a6fd912037eb5cda154591f0
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-17 15:04:26 +0900

    각 객체당 한 명의 사용자 로그인 정보를 다루는 세션 객체 생성

--- src/main/java/webserver/HttpSession.java
+++ src/main/java/webserver/HttpSession.java
@@ -1,2 +1,32 @@
-package webserver;public class HttpSession {
+package webserver;
+
+import java.util.Map;
+
+public class HttpSession { // 각 사용자마다 해당되는 고유한 세션이 있음. 이 세션 안에 사용자의 정보가 담기는 거임
+
+    private final String sessionId;
+    private final Map<String, String> userInfo;
+    private boolean valid;
+
+    public HttpSession(String sessionId, Map<String, String> userInfo) {
+        this.sessionId = sessionId;
+        this.userInfo = userInfo;
+        this.valid = true;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public Map<String, String> getUserInfo() {
+        return userInfo;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void inValidate() {
+        this.valid = false;
+    }
 }
