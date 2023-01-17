5b5354dd07000947c3d328c6302da6e747f57418
River-Mt <rivermt1998@gmail.com>
2023-01-17 12:47:09 +0900

    :sparkles: HTTP 상태코드 추가 (303 See Other)

--- src/main/java/http/response/HttpStatusCode.java
+++ src/main/java/http/response/HttpStatusCode.java
@@ -4,6 +4,7 @@ public enum HttpStatusCode {
     OK("200", "OK"),
     CREATED("201", "Created"),
     FOUND("302", "Found"),
+    SEE_OTHER("303", "See other"),
     UNAUTHORIZED("401", "Unauthorized"),
     NOT_FOUND("404", "Not Found");
 
