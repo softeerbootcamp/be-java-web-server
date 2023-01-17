c07481717e99c73e0dddf1e4f65cfb850ec1c71c
ajongs <wonjong1030@naver.com>
2023-01-17 17:24:09 +0900

    test(SessionStorageTest): 세션 추가가 잘되는 테스트 하는 코드 추가

--- src/test/java/db/SessionStorageTest.java
+++ src/test/java/db/SessionStorageTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package db;
+
+import model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import util.Session;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+
 class SessionStorageTest {
-  
+    @Test
+    void 세션추가테스트() {
+        Session session = Session.createSessionWith(new User("ajongs", "password"));
+        SessionStorage.addSession(session);
+
+        Session find = SessionStorage.findSessionBy(session.getSid());
+
+        Assertions.assertThat(session).isEqualTo(find);
+    }
 }
