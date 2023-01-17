a533a60200129aef19e5cfbaf0f233dbb96d5020
rentalhub-mac78 <kksy2423@naver.com>
2023-01-17 16:06:19 +0900

    Fix: UserController RequestDataType 변경에 따른 type 설정 변경

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,37 +2,33 @@ package controller;
 
 
 import controller.annotation.ControllerInfo;
+import controller.annotation.ControllerMethodInfo;
 import db.Database;
 import db.UserDatabase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reader.RequestGetReader;
 import reader.RequestPostReader;
 import reader.RequestReader;
 import request.HttpRequest;
-import response.Data;
 import response.HttpResponse;
 import service.Service;
 import service.UserService;
-import util.FileType;
-import util.HttpMethod;
-import util.HttpStatus;
-import util.UrlType;
+import util.*;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
+@ControllerInfo(regex = "^/user/[^\\.]*$")
 public class UserController implements Controller {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-    protected static String REGEX = ".*\\/user.*";
     private Database userDatabase = new UserDatabase();
     private Service userService = new UserService();
     private RequestReader requestReader;
 
 
-    @ControllerInfo(path = "/user/create", u = UrlType.NOTHING, method = HttpMethod.POST)
+    @ControllerMethodInfo(path = "/user/create", type = RequestDataType.IN_BODY, method = HttpMethod.POST)
     public HttpResponse UserQueryString(DataOutputStream dataOutputStream, HttpRequest httpRequest) throws IOException {
         requestReader = new RequestPostReader();
 
@@ -40,7 +36,7 @@ public class UserController implements Controller {
         User user = (User) userService.createModel(userMap);
         userDatabase.addData(user);
 
-        HttpResponse httpResponse = new HttpResponse(new Data(dataOutputStream), FileType.HTML, HttpStatus.RE_DIRECT);
+        HttpResponse httpResponse = new HttpResponse(new response.Data(dataOutputStream), FileType.HTML, HttpStatus.RE_DIRECT);
 
         logger.debug("저장된 user:{}", userDatabase.findAll());
 
