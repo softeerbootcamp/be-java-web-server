b228c8ded34184a127338f62cbbb9ef303ddd5f7
roopre <benny1020@naver.com>
2023-01-17 22:36:05 +0900

    feat: login시에 session 구현

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -3,10 +3,13 @@ package controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.User;
 import request.HttpRequest;
 import response.ContentType;
 import response.HttpResponse;
 import service.UserService;
+import session.Session;
+import session.SessionManager;
 import webserver.HttpStatus;
 
 public class LoginController extends AbstractController {
@@ -34,6 +37,10 @@ public class LoginController extends AbstractController {
 
 	public void doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
 		if (userService.matchIdPassword(httpRequest.getRequestBody("userId"), httpRequest.getRequestBody("password"))) {
+			User user = userService.findByUserId(httpRequest.getRequestBody("userId"));
+			Session session = new Session();
+			session.setAttribuite("user", user);
+			// TODO 세션 적용해야함
 			httpResponse.setHttpResponse(HttpStatus.FOUND, "", ContentType.HTML, "/index.html");
 		} else {
 			httpResponse.setHttpResponse(HttpStatus.FOUND, "", ContentType.HTML, "/user/login_failed.html");
