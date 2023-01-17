2231e0321003f1c582712e7befb3926385c3c759
ajongs <wonjong1030@naver.com>
2023-01-17 17:25:03 +0900

    test(SessionStorageTest): UUID 객체가 가르키고 있는 변수가 달라도 동일여부를 판단하는지 체크하는 테스트 코드 추가

--- src/test/java/db/SessionStorageTest.java
+++ src/test/java/db/SessionStorageTest.java
@@ -19,4 +19,13 @@ class SessionStorageTest {
 
         Assertions.assertThat(session).isEqualTo(find);
     }
+    @Test
+    void UUID() {
+        UUID uid1 = UUID.randomUUID();
+        UUID uid2 = UUID.fromString(uid1.toString());
+        HashMap<UUID, String> map = new HashMap<>();
+        map.put(uid1, "1");
+
+        Assertions.assertThat(map.get(uid2)).isEqualTo("1");
+    }
 }
