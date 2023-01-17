2cc8d6c60492a248d04b209667eb7dc7c3eaf97a
ajongs <wonjong1030@naver.com>
2023-01-17 21:18:19 +0900

    feat(Session): 유휴시간을 구할 수 있는 기능 추가

--- src/main/java/util/Session.java
+++ src/main/java/util/Session.java
@@ -8,6 +8,10 @@ public class Session {
     private UUID sid;
     private String id;
 
+    private int maxInactiveInterval = 5;
+    private long creationTime = 0L;
+    private long lastAcceptedTime = creationTime;
+
     private Session(String id) {
         this.sid = UUID.randomUUID();
         this.id = id;
@@ -22,5 +26,19 @@ public class Session {
 
     public String getId() {
         return id;
+    }
+
+    public void setMaxInactiveInterval(int maxInactiveInterval) {
+        this.maxInactiveInterval = maxInactiveInterval;
+    }
+
+    public void setCreationTime(long creationTime) {
+        this.creationTime = creationTime;
+        this.lastAcceptedTime = creationTime;
+    }
+    public long getIdleTime() {
+        long timeNow = System.currentTimeMillis();
+        long timeIdle = lastAcceptedTime - timeNow;
+        return timeIdle;
     }
 }
