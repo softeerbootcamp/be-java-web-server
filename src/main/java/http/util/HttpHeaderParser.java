46bfe1a6724c9737cfdcacbe5c151df5e3692bc9
solchan98 <dev.sol0427@gmail.com>
2023-01-17 16:02:58 +0900

    feat: HttpHeaderParser 수정 헤더 파싱 중 쿠키인 경우 헤더의 쿠키 필드에 추가하도록 수정

--- src/main/java/http/util/HttpHeaderParser.java
+++ src/main/java/http/util/HttpHeaderParser.java
@@ -14,11 +14,23 @@ public class HttpHeaderParser {
 
         for (String strOfHeader: stringOfHeaders.split("\n")) {
             int idxOfColon = strOfHeader.indexOf(":");
-            headers.addHeader(
-                    strOfHeader.substring(0, idxOfColon).trim(),
-                    strOfHeader.substring(idxOfColon + 1).trim());
+            String key = strOfHeader.substring(0, idxOfColon).trim();
+            String value = strOfHeader.substring(idxOfColon + 1).trim();
+
+            if (key.equals("Cookie")) {
+                parseCookie(headers, value);
+            }
+            headers.addHeader(key, value);
         }
 
         return headers;
+    }
+
+    private static void parseCookie(HttpHeaders headers, String cookies) {
+        String[] cookieArr = cookies.split(";");
+        for (String cookie: cookieArr) {
+            String[] strings = cookie.trim().split("=");
+            headers.addCookie(strings[0], strings[1]);
+        }
     }
 }
