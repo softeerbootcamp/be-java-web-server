04248db6c22901da647a57335710ccc92f2f455e
i5046821854 <o5046821854@gmail.com>
2023-01-17 18:24:26 +0900

    feat : 서비스 단에서 IfpresentOrElse 구문을 OrElseNull문으로 변경

--- src/main/java/webserver/Service/AuthService.java
+++ src/main/java/webserver/Service/AuthService.java
@@ -1,6 +1,6 @@
 package webserver.Service;
 
-import db.Database;
+import db.UserDatabase;
 import model.User;
 import webserver.domain.StatusCodes;
 import webserver.exception.HttpRequestException;
@@ -8,27 +8,19 @@ import webserver.exception.HttpRequestException;
 public class AuthService {
 
     public byte[] join(String userId, String password, String name, String email) {
-        Database.findUserById(userId).ifPresent(m -> {
+        UserDatabase.findUserById(userId).ifPresent(m -> {
             throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('사용자가 이미 존재합니다'); history.go(-1);</script>");
         });
-        Database.addUser(new User(userId, password, name, email));
+        UserDatabase.addUser(new User(userId, password, name, email));
         return userId.getBytes();
     }
 
-    public byte[] login(String userId, String password) {
-
-        StringBuilder cookie = new StringBuilder();
-
-        Database.findUserById(userId).ifPresentOrElse(user -> {  //if there's an id that is equivalent to given parameter
-            if(user.getPassword().equals(password))  //with valid password
-                cookie.append(user);
-            else   // with invalid password
-                throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('패스워드가 올바르지 않습니다'); history.go(-1);</script>");
-        }, ()->{
-            throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('id를 다시 입력해주세요'); history.go(-1);</script>");
-        });
-
-        return cookie.toString().getBytes();
+    public String login(String userId, String password) {
+        User user = UserDatabase.findUserById(userId)
+                .orElseThrow(() -> new HttpRequestException(StatusCodes.SEE_OTHER, "<script>alert('존재하지 않는 계정입니다');</script>", "/user/login_failed.html")); //wrong ID
+        if(!user.getPassword().equals(password))  //with wrong password
+            throw new HttpRequestException(StatusCodes.SEE_OTHER, "<script>alert('패스워드가 올바르지 않습니다');</script>", "/user/login_failed.html");
+        return user.getUserId(); // with valid password
     }
 }
 
