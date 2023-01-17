dc0cee602bbc292435d8f437447aedfc6904061d
ajongs <wonjong1030@naver.com>
2023-01-17 12:07:46 +0900

    feat(UserController): user/login으로 넘어오는 요청을 매핑하고 서비스를 수행한 후 HttpResponseMessage를 반환하는 기능 구현

--- src/main/java/was/controller/UserController.java
+++ src/main/java/was/controller/UserController.java
@@ -1,10 +1,13 @@
 package was.controller;
 
+import model.User;
 import service.UserService;
 import was.annotation.PostMapping;
 import webserver.domain.HttpRequest;
 import webserver.domain.HttpResponse;
 import webserver.domain.HttpResponseMessage;
+
+import java.util.Map;
 
 public class UserController implements Controller{
     private static UserController userController;
@@ -23,5 +26,17 @@ public class UserController implements Controller{
 
         HttpResponse httpResponse = new HttpResponse();
         return new HttpResponseMessage(httpResponse.sendRedirect("/index.html"), httpResponse.getBody());
+    }
+
+    @PostMapping("/user/login")
+    public HttpResponseMessage login(HttpRequest httpRequest) {
+        Map<String, String> body = httpRequest.getBody();
+        User user = new User(body.get("userId"), body.get("password"));
+
+        HttpResponse httpResponse = new HttpResponse();
+        if (userService.login(user)) {
+            return new HttpResponseMessage(httpResponse.sendRedirect("/index.html"), httpResponse.getBody());
+        }
+        return new HttpResponseMessage(httpResponse.unauthorized(), httpResponse.getBody());
     }
 }
