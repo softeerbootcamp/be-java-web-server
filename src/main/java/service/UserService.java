a6c8891e6a0977e84bb0b36c9ac02aaa9a146125
wonju-dev <wonjulee.dev@gmail.com>
2023-01-17 12:43:20 +0900

    feat(UserService): login 구현

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,19 +1,31 @@
 package service;
 
 import db.Database;
-import dto.UserInfoDTO;
+import dto.LogInDTO;
+import dto.SignUpDTO;
 import model.User;
 
 public class UserService {
-    public String signIn(UserInfoDTO userInfo) {
+    public String signUp(SignUpDTO userInfo) {
         User findUser = Database.findUserById(userInfo.getUserId());
 
         if (findUser != null) {
             return null;
         }
-        User user = User.of(userInfo);
+
+        User user = new User(userInfo.getUserId(), userInfo.getPassword(), userInfo.getName(), userInfo.getEmail());
         Database.addUser(user);
 
         return user.getUserId();
+    }
+
+    public Session logIn(LogInDTO userInfo) {
+        User findUser = Database.findUserById(userInfo.getUserId());
+
+        if (findUser != null) {
+            return new Session(findUser);
+        }
+
+        return new Session(null); // todo: throw 적용하기
     }
 }
