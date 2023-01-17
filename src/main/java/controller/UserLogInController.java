57d1c88c65b7781c38610e24876fe03d4cf8125d
River-Mt <rivermt1998@gmail.com>
2023-01-17 17:01:50 +0900

    :sparkles: 로그인 시 세션을 저장하고 쿠키르 response 하도록 구현현

--- src/main/java/controller/UserLogInController.java
+++ src/main/java/controller/UserLogInController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import http.Cookie;
+import http.Session;
+import http.SessionManager;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import http.response.HttpStatusCode;
@@ -11,8 +14,11 @@ import java.util.Map;
 
 import static db.Database.findUserById;
 
-public class UserLogInController extends AbstractController{
+public class UserLogInController extends AbstractController {
 
+    public static final String USER_ID = "userId";
+    public static final String PASSWORD = "password";
+    public static final String COOKIE_SUFFIX = "; path=/";
     public static final String INDEX_PATH = "/index.html";
     public static final String LOGIN_FAILED_PATH = "/user/login_failed.html";
 
@@ -22,21 +28,30 @@ public class UserLogInController extends AbstractController{
 
     @Override
     public void doPost(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
-        Map<String, String> userInfo = httpRequest.getParameters();
-        String requestUserId = userInfo.get("userId");
-        String requestPassword = userInfo.get("password");
+        try {
+            Map<String, String> userInfo = httpRequest.getParameters();
+            String requestUserId = userInfo.get(USER_ID);
+            String requestPassword = userInfo.get(PASSWORD);
 
-        if(isExistUser(requestUserId, requestPassword)) {
-            httpResponse.sendRedirect(HttpStatusCode.SEE_OTHER, INDEX_PATH);
-            return;
+            validateUser(requestUserId, requestPassword);
+
+            Session session = SessionManager.create();
+            session.setAttribute(USER_ID, requestUserId);
+
+            String id = session.getId();
+            Cookie cookie = Cookie.of(Session.DEFAULT_SESSION_ID, id + COOKIE_SUFFIX);
+
+            httpResponse.sendRedirect(HttpStatusCode.FOUND, INDEX_PATH, cookie);
+        } catch (IllegalArgumentException e) {
+            httpResponse.sendRedirect(HttpStatusCode.FOUND, LOGIN_FAILED_PATH);
         }
-
-        httpResponse.sendRedirect(HttpStatusCode.UNAUTHORIZED, LOGIN_FAILED_PATH);
     }
 
-    private boolean isExistUser(String requestUserId, String requestPassword) {
+    private void validateUser(String requestUserId, String requestPassword) {
         User user = findUserById(requestUserId);
-        return user != null && requestPassword.equals(user.getPassword());
+        if (user == null || !requestPassword.equals(user.getPassword())) {
+            throw new IllegalArgumentException("로그인 실패");
+        }
     }
 
 }
