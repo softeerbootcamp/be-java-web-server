5e63c21586352206141770c5aff7776fdfc154f8
roopre <benny1020@naver.com>
2023-01-17 15:21:58 +0900

    refactor: UserController에서 UserService를 composition하도록 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import request.HttpRequest;
 import response.ContentType;
 import response.HttpResponse;
+import service.UserService;
 import webserver.HttpStatus;
 import webserver.Url;
 
@@ -17,6 +18,7 @@ public class UserController extends AbstractController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	public static final String PATH = "/user/create";
 	private static UserController instance;
+	private final UserService userService;
 
 	public static UserController getInstance() {
 		if (instance == null) {
@@ -27,13 +29,14 @@ public class UserController extends AbstractController {
 		return instance;
 	}
 
+	public UserController() {
+		this.userService = UserService.getInstance();
+	}
+
 	public void doPost(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
-		User user = User.of(httpRequest.getRequestBody("userId"), httpRequest.getRequestBody("password"), httpRequest.getRequestBody("name"),
+		userService.addUser(httpRequest.getRequestBody("userId"), httpRequest.getRequestBody("password"), httpRequest.getRequestBody("name"),
 			httpRequest.getRequestBody("email"));
-		Database.addUser(user);
-		logger.info(user + " 회원가입했습니다.");
-		// refactoring
-		// redirect
+
 		httpResponse.setHttpResponse(HttpStatus.FOUND,
 			"", ContentType.HTML,
 			"/index.html");
