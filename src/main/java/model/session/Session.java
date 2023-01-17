dfee09f77c2fe55052b57daf98b4e5c60ee7ff8e
pjs0418 <tony180322@gmail.com>
2023-01-17 11:33:48 +0900

    Session class 생성 및 생성자 구현 멤버 변수로 sessionId와 HashMap 형태의 sessionData를 갖는다.

---
+++ src/main/java/model/session/Session.java
@@ -0,0 +1,14 @@
+package model.session;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Session {
+    private final String sessionId;
+    private final Map<String, Object> sessionData;
+
+    public Session(String sessionId) {
+        this.sessionId = sessionId;
+        this.sessionData = new HashMap<>();
+    }
+}
