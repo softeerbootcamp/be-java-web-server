630e1be9c65299f50c9d2bcaa04f50e5c6af993d
rentalhub-mac78 <kksy2423@naver.com>
2023-01-17 16:08:41 +0900

    Refacotr: URLType을 RequestDataType으로 변경

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -1,18 +1,17 @@
 package controller;
 
-import controller.annotation.ControllerInfo;
+import controller.annotation.ControllerMethodInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reader.fileReader.FileReader;
 import reader.fileReader.StaticFileReader;
 import reader.fileReader.TemplatesFileReader;
 import request.HttpRequest;
-import response.Data;
 import response.HttpResponse;
 import util.FileType;
 import util.HttpMethod;
 import util.HttpStatus;
-import util.UrlType;
+import util.RequestDataType;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -23,20 +22,20 @@ public class FileController  implements  Controller{
 
     FileReader fileReader;
 
-    @ControllerInfo(path = "", u = UrlType.TEMPLATES_FILE, method = HttpMethod.GET)
+    @ControllerMethodInfo(path = "", type = RequestDataType.TEMPLATES_FILE, method = HttpMethod.GET)
     public HttpResponse TemplateController(DataOutputStream dataOutputStream, HttpRequest httpRequest) throws IOException {
         fileReader = new TemplatesFileReader();
         byte[] data = new byte[0];
         data = fileReader.readFile(httpRequest.getUrl());
-        return new HttpResponse(new Data(dataOutputStream, data), FileType.getFileType(httpRequest.getUrl()), HttpStatus.OK);
+        return new HttpResponse(new response.Data(dataOutputStream, data), FileType.getFileType(httpRequest.getUrl()), HttpStatus.OK);
     }
 
-    @ControllerInfo(path = "", u = UrlType.STATIC_FILE, method = HttpMethod.GET)
+    @ControllerMethodInfo(path = "", type = RequestDataType.STATIC_FILE, method = HttpMethod.GET)
     public HttpResponse StaticController(DataOutputStream dataOutputStream, HttpRequest httpRequest) throws IOException {
         fileReader = new StaticFileReader();
         byte[] data = new byte[0];
         data = fileReader.readFile(httpRequest.getUrl());
-        return new HttpResponse(new Data(dataOutputStream, data), FileType.getFileType(httpRequest.getUrl()), HttpStatus.OK);
+        return new HttpResponse(new response.Data(dataOutputStream, data), FileType.getFileType(httpRequest.getUrl()), HttpStatus.OK);
     }
 
 
