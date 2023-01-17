5026c46773f0084cfbbb11ef3f9d3f9355e49870
jaehunLee-dev <jaehunLee.dev@gmail.com>
2023-01-17 10:40:34 +0900

    refactor: Session 클래스 수정 sessionId와 User를 매칭해주는 static Map loginSession 생성 패키지를 view->util로 변경

--- src/main/java/util/Session.java
+++ src/main/java/util/Session.java
@@ -1,18 +1,22 @@
-package view;
+package util;
 
+import model.domain.User;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Session {
     private static final int leftLimit = 48;
     private static final int rightLimit = 122;
     private static final int sessionIdLength = 10;
-
-    String sessionId;
-    Session(){
-        sessionId = new Random().ints(leftLimit,rightLimit+1)
+    public static Map<String, User> loginSession = new HashMap<>();
+    Session(User user){
+        String sessionId = new Random().ints(leftLimit,rightLimit+1)
                 .filter(i->(i<=57 || i>=65) && (i<=90 || i>=97))
                 .limit(sessionIdLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
+        loginSession.put(sessionId, user);
     }
 }
