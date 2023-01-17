d33ad13685efec4551d73af09523df4b046242d9
roopre <benny1020@naver.com>
2023-01-17 14:09:35 +0900

    refactor: 불필요한 주석 제거

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,7 @@ import java.nio.charset.StandardCharsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import controller.Controller;
 import controller.FrontController;
-import controller.RequestMappingHandler;
 import request.HttpRequest;
 import request.HttpRequestUtils;
 import response.HttpResponse;
@@ -30,8 +28,6 @@ public class RequestHandler implements Runnable {
 		logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
 			connection.getPort());
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			//DataOutputStream dos = new DataOutputStream(out);
-			// HttpRequest HttpResponse
 
 			HttpRequest httpRequest = HttpRequestUtils.httpRequestParse(in);
 			HttpResponse httpResponse = new HttpResponse();
