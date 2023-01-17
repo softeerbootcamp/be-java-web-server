b624fc0cf3feebc9dd5efa77bdcce31c2d66bdcb
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-17 16:42:39 +0900

    요청에 담긴 cookie 헤더 값을 통해 세션 조회하기 확인
    만약 쿠키에 sid 값이 있으면 그 값으로 세션을 조회함
    현재 쿠키에는 세션 아이디 값이 담겨져 있으므로

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -2,29 +2,38 @@ package webserver;
 
 import utils.HttpRequestUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
 
+    public static final String SID_ATTR_KEY = "sid";
+
     private final HttpMethod method;
     private final String url;
     //private final String host;
     private final Map<String, String> queries;
+
+    private final Map<String, String> cookies;
     private final Map<String, String> headers;
     private final String body;
 
+    private HttpSession httpSession;
 
-    public HttpRequest(HttpMethod method, String url, Map<String, String> queries,Map<String, String> headers,String body){
+    public HttpRequest(HttpMethod method, String url, Map<String, String> queries, Map<String, String> cookies, Map<String, String> headers, String body){
         this.method = method;
         this.url = url;
-        //this.host = host;
         this.queries = queries;
+        this.cookies = cookies;
         this.headers = headers;
         this.body = body;
+
+        if(cookies.containsKey(SID_ATTR_KEY)){
+            System.out.println("request에서 쿠키 파싱을 통해 얻은 sid를 통해 얻은 유저 정보");
+            httpSession = HttpSessionManager.getSession(cookies.get(SID_ATTR_KEY));
+            System.out.println(httpSession.getUserInfo());
+        }
     }
 
 
@@ -47,6 +56,19 @@ public class HttpRequest {
     public HttpMethod getMethod() {
         return method;
     }
+
+    public Map<String, String>  getCookies(){ return cookies;}
+    public String getCookie(String key) {
+        return cookies.get(key);
+    }
+
+    public HttpSession getSession() {
+        if (this.httpSession == null) {
+            this.httpSession = HttpSessionManager.getSession(cookies.get(SID_ATTR_KEY));
+        }
+        return this.httpSession;
+    }
+
 
     public String getUrl() {
         return url;
