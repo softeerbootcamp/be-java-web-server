981e54b00c46334013b95ffb3345d56ad88e59bb
wonju-dev <wonjulee.dev@gmail.com>
2023-01-17 15:05:51 +0900

    feat(Cookie): add path field

--- src/main/java/http/common/Cookie.java
+++ src/main/java/http/common/Cookie.java
@@ -1,12 +1,16 @@
 package http.common;
 
 public class Cookie {
+
+    private static final String ROOT_PATH = "/";
     private String key;
     private String value;
+    private String path;
 
     public Cookie(String key, String value) {
         this.key = key;
         this.value = value;
+        this.path = ROOT_PATH;
     }
 
     public String getKey() {
@@ -19,6 +23,6 @@ public class Cookie {
 
     @Override
     public String toString() {
-        return key + "=" + value;
+        return key + "=" + value + "; " + "path=" + path;
     }
 }
