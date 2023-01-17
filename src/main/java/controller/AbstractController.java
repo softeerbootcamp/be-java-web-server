dc0449318f0d79f37dd8514ae2e569ef37c36884
River-Mt <rivermt1998@gmail.com>
2023-01-17 12:16:28 +0900

    :adhesive_bandage: String으로 굳이 형변환 하지 않아도 되기 때문에 수정

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import http.Uri;
+import http.request.HttpMethod;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 
@@ -14,12 +15,12 @@ public class AbstractController implements Controller {
 
     @Override
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException, URISyntaxException {
-        String httpMethod = httpRequest.getMethod().toString();
-        if (httpMethod.equals("GET")) {
+        HttpMethod httpMethod = httpRequest.getMethod();
+        if (httpMethod.equals(HttpMethod.GET)) {
             doGet(httpRequest, httpResponse);
         }
 
-        if (httpMethod.equals("POST")) {
+        if (httpMethod.equals(HttpMethod.POST)) {
             doPost(httpRequest, httpResponse);
         }
     }
