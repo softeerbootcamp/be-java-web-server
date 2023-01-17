10a237c0d3205b09ca4e75ed1256eb8b82720b4c
River-Mt <rivermt1998@gmail.com>
2023-01-17 17:13:56 +0900

    :fire: 사용하지 않는 메서드 삭제

--- src/main/java/http/Session.java
+++ src/main/java/http/Session.java
@@ -8,23 +8,19 @@ public class Session {
 
     public static final String DEFAULT_SESSION_ID = "JSESSIONID";
 
-    private final Map<String, Object> attributes = new HashMap<>();
+    private final Map<String, String> attributes = new HashMap<>();
     private final UUID id = UUID.randomUUID();
 
     public String getId() {
         return this.id.toString();
     }
 
-    public void setAttribute(String name, Object value) {
+    public void setAttribute(String name, String value) {
         this.attributes.put(name, value);
     }
 
     public Object getAttribute(String name) {
         return this.attributes.get(name);
-    }
-
-    public void invalidate() {
-        this.attributes.clear();
     }
 
 }
