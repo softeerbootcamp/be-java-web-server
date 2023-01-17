912fa669618fb3f03ab966ed99a78957d3670a4d
wonju-dev <wonjulee.dev@gmail.com>
2023-01-17 12:38:31 +0900

    refactor(User): User 객체 생성 방식 변경

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,38 +1,24 @@
 package model;
 
-import dto.UserInfoDTO;
-
 public class User {
     private String userId;
     private String password;
     private String name;
     private String email;
 
-    private User(String userId, String password, String name, String email) {
+    public User(String userId, String password, String name, String email) {
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
     }
 
-    public static User of(UserInfoDTO userInfo) {
-        return new User(userInfo.getUserId(), userInfo.getPassword(), userInfo.getName(), userInfo.getEmail());
-    }
-
     public String getUserId() {
         return userId;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     @Override
