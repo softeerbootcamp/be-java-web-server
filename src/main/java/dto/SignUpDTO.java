7545aade8fb5f6d30d69b6dbb3eb16a9dcd9b1fc
wonju-dev <wonjulee.dev@gmail.com>
2023-01-17 12:33:02 +0900

    refactor(SignUpDTO): SignUpDTO 분리

--- src/main/java/dto/SignUpDTO.java
+++ src/main/java/dto/SignUpDTO.java
@@ -2,35 +2,22 @@ package dto;
 
 import java.util.Map;
 
-public class UserInfoDTO {
+public class SignUpDTO extends LogInDTO {
 
-    public static final String USER_ID = "userId";
-    public static final String PASSWORD = "password";
     public static final String NAME = "name";
     public static final String EMAIL = "email";
-
-    private String userId;
-    private String password;
     private String name;
     private String email;
 
-    private UserInfoDTO(Map<String, String> args) {
-        this.userId = args.get(USER_ID);
-        this.password = args.get(PASSWORD);
-        this.name = args.get(NAME);
-        this.email = args.get(EMAIL);
+
+    private SignUpDTO(String userId, String password, String name, String email) {
+        super(userId, password);
+        this.name = name;
+        this.email = email;
     }
 
-    public static UserInfoDTO of(Map<String, String> parameters) {
-        return new UserInfoDTO(parameters);
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getPassword() {
-        return password;
+    public static SignUpDTO of(Map<String, String> parameters) {
+        return new SignUpDTO(parameters.get(USER_ID), parameters.get(PASSWORD), parameters.get(NAME), parameters.get(EMAIL));
     }
 
     public String getName() {
