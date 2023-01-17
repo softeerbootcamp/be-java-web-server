1e8335c82b1f0c24581e151566093a3128a166fb
i5046821854 <o5046821854@gmail.com>
2023-01-17 11:47:49 +0900

    feat : userLogin에 대한 비즈니스 로직을 AuthService에 구현

--- src/main/java/webserver/Service/AuthService.java
+++ src/main/java/webserver/Service/AuthService.java
@@ -15,5 +15,20 @@ public class AuthService {
         return userId.getBytes();
     }
 
+    public byte[] login(String userId, String password) {
+
+        StringBuilder cookie = new StringBuilder();
+
+        Database.findUserById(userId).ifPresentOrElse(user -> {  //if there's an id that is equivalent to given parameter
+            if(user.getPassword().equals(password))  //with valid password
+                cookie.append(user);
+            else   // with invalid password
+                throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('패스워드가 올바르지 않습니다'); history.go(-1);</script>");
+        }, ()->{
+            throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('id를 다시 입력해주세요'); history.go(-1);</script>");
+        });
+
+        return cookie.toString().getBytes();
+    }
 }
 
