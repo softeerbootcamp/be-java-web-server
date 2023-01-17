7d1f34e1ba8dce0eb2757d2ba1f42e53c125c6f2
roopre <benny1020@naver.com>
2023-01-17 14:41:40 +0900

    feat: UserRepository의 구현체 구현 및 싱글톤 방식으로 구현

---
+++ src/main/java/db/UserReposioryImpl.java
@@ -0,0 +1,37 @@
+package db;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import model.User;
+
+public class UserReposioryImpl implements UserRepository {
+	private static UserRepository instance;
+
+	public static UserRepository getInstance() {
+		if (instance == null) {
+			synchronized (UserReposioryImpl.class) {
+				instance = new UserReposioryImpl();
+			}
+		}
+		return instance;
+	}
+
+	private Map<String, User> users = Maps.newHashMap();
+	@Override
+	public void addUser(User user) {
+		users.put(user.getUserId(), user);
+	}
+
+	@Override
+	public User findUserById(String userId) {
+		return users.get(userId);
+	}
+
+	@Override
+	public Collection<User> findAll() {
+		return users.values();
+	}
+}
