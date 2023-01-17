68710e81fe264820287ded2451af7bdf73f77db4
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-17 16:48:23 +0900

    로그인 후에 로그인 정보(유저 정보)를 담은 세션 객체 만들기
    HttpSessionManager를 통해 세션 객체를 생성하고, 생성한 세션 객체의 식별 번호를 응답 객체의 쿠키 맵(set-cookie 헤더와는 별개)에 넣어줌

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,25 +1,30 @@
 package controller;
 
 import service.LoginService;
-import service.SignUpService;
 import webserver.HttpRequest;
 import webserver.HttpResponse;
+import webserver.HttpSession;
+import webserver.HttpSessionManager;
+
+import java.util.UUID;
 
 public class LoginController implements Controller{
 
     private static final String REDIRECT_URL ="/index.html";
     private static final String FAILED_REDIRECT_URL ="/user/login_failed.html";
-
+    private static final String LOGINED_ATTR_KEY = "sid";
     private LoginService service= new LoginService();
 
     @Override
     public void doPost(HttpRequest request, HttpResponse response) {
-        System.out.println(request.parseBody().entrySet());
+        //System.out.println(request.parseBody().entrySet());
         String userId = request.parseBody().get("userId");
         String password = request.parseBody().get("password");
         boolean alreadyUser = service.checkRightUser(userId,password);
         if(alreadyUser){
-            response.addCookie("sid","logined");
+            HttpSession session = HttpSessionManager.createSession(request.parseBody());
+            HttpSessionManager.addSession(session);
+            response.addCookie(LOGINED_ATTR_KEY, session.getId());
             response.redirect(REDIRECT_URL);
         }
         else response.redirect(FAILED_REDIRECT_URL);
