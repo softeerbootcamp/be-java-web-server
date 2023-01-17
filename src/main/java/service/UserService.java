4e52012e6baba6d91e1f63dcfe3c43b91614d1c4
pjs0418 <tony180322@gmail.com>
2023-01-17 14:04:32 +0900

    로그인 함수에서 인자값으로 User 객체 받을 수 있도록 수정

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -3,7 +3,6 @@ package service;
 import db.Database;
 import model.domain.User;
 
-import java.util.Map;
 import java.util.Objects;
 
 public class UserService {
@@ -17,8 +16,8 @@ public class UserService {
         database.addUser(user);
     }
 
-    public boolean logIn(Map<String, String> userLoginInfo) {
-        User user = database.findUserById(userLoginInfo.get("userId"));
-        return Objects.nonNull(user) && user.getPassword().equals(userLoginInfo.get("password"));
+    public boolean logIn(User user) {
+        User findUser = database.findUserById(user.getUserId());
+        return Objects.nonNull(findUser) && findUser.getPassword().equals(user.getPassword());
     }
 }
