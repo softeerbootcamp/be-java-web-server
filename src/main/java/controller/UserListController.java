f6b971cece8e38273d3e7c8951cf19451493dc4c
solchan98 <dev.sol0427@gmail.com>
2023-01-17 17:47:47 +0900

    feat: UserListController 수정 데이터베이스의 유저 값을 가져와 페이지를 렌더링하는 로직 추가

--- src/main/java/controller/UserListController.java
+++ src/main/java/controller/UserListController.java
@@ -1,13 +1,45 @@
 package controller;
 
+import db.Database;
+import http.common.ContentType;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import model.User;
+import util.ResourceUtils;
+
+import java.util.Collection;
 
 public class UserListController implements AuthController {
     public static final String PATH = "/user/list";
 
     @Override
     public void doGet(HttpRequest request, HttpResponse response) {
-        response.redirect("/user/list.html");
+        response.setBody(getTable(Database.findAll()).getBytes());
+        response.addHeader("Content-Type", ContentType.TEXT_HTML.getType());
+
     }
+
+    private String getTable(Collection<User> users) {
+        int idx = 1;
+
+        StringBuilder content = new StringBuilder();
+
+        for (User user: users) {
+            content.append("<tr><th scope=\"row\">")
+                    .append(idx)
+                    .append("</th> <td>")
+                    .append(user.getUserId())
+                    .append("</td> <td>")
+                    .append(user.getName())
+                    .append("</td> <td>")
+                    .append(user.getEmail())
+                    .append("</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td></tr>");
+        }
+
+        byte[] bytes = ResourceUtils.loadFileFromClasspath("/user/list.html");
+        String html = new String(bytes);
+        String[] split = html.split("<tbody>", 2);
+        return split[0] + "<tbody>" + content + split[1];
+    }
+
 }
