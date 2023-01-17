a9a81c20103123d6c55a4551a81fb10677b509ae
rohsik2 <rohsik2@gmail.com>
2023-01-17 16:10:16 +0900

    :sparkles: session 만드는 파트에서 session을 리턴하도록 변경

--- src/main/java/repository/SessionRepo.java
+++ src/main/java/repository/SessionRepo.java
@@ -8,16 +8,17 @@ import java.util.Map;
 import java.util.UUID;
 
 public class SessionRepo {
-    private static Map<String, Session> sessionMap;
+    private static final Map<String, Session> sessionMap;
 
     static{
         sessionMap = new HashMap<>();
     }
 
-    public static String createSession(String userId){
+    public static Session createSession(String userId) {
         String SSID = UUID.randomUUID().toString();
-        sessionMap.put(SSID, new Session(SSID, userId));
-        return SSID;
+        Session sess = new Session(SSID, userId);
+        sessionMap.put(SSID, sess);
+        return sess;
     }
 
     public static Session findBySSID(String ssid){
