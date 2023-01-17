1aef1afc91b4b01676e0b3ae06f24af53e2579ae
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-17 15:26:28 +0900

    현재 사용자 별 있어야 하는 세션 객체들에 대한 관리를 해주는 객체 - 세션CRUD

--- src/main/java/webserver/HttpSessionManager.java
+++ src/main/java/webserver/HttpSessionManager.java
@@ -1,2 +1,29 @@
-package webserver;public class HttpSessionManager {
+package webserver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class HttpSessionManager {
+
+    private static final Map<String, HttpSession> sessions = new HashMap<>(); //해당 맵에서의 키는 각 value인 HttpSession의 키와 동일함
+
+    public static HttpSession getSession(String id) {
+        HttpSession session = sessions.get(id);
+        return session;
+    }
+
+    private static void removeSession(HttpSession session) {
+        if (session != null) {
+            sessions.remove(session.getId());
+        }
+    }
+
+    public static HttpSession createSession() {
+        return new HttpSession(UUID.randomUUID().toString(), new HashMap<>());
+    }
+
+    private static void addSession(HttpSession session) {
+        sessions.put(session.getId(), session);
+    }
 }
