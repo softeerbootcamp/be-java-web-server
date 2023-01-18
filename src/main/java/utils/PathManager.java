99e6b32630f8273efca0afb763f3dfade1d9dbd0
Toa <ld980105@snu.ac.kr>
2023-01-18 12:31:36 +0900

    :sparkles: PathManager util class 구현

---
+++ src/main/java/utils/PathManager.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class PathManager {
+    public static final String HOME_PATH = "/index.html";
+    public static final String LOGIN_PATH = "/user/login.html";
+    public static final String LOGIN_FAILED_PATH = "/user/login_failed.html";
+
+}
