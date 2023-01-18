c3000733ca00fe5284a2e77c2f5b24357f9efbf6
River-Mt <rivermt1998@gmail.com>
2023-01-19 00:02:15 +0900

    :sparkles: /user/list.form 에 기본으로 있는 두명 + user들의 리스트를 동적으로 출력하도록 하는 메서드 구현

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,7 +2,10 @@ package service;
 
 import db.UserRepository;
 import model.User;
+import utils.FileIoUtils;
 
+import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
 
 public class UserService {
@@ -26,6 +29,35 @@ public class UserService {
         if (user == null || !requestPassword.equals(user.getPassword())) {
             throw new IllegalArgumentException("로그인 실패");
         }
+    }
+
+    public byte[] makeUserListBody(String filepath) throws IOException {
+
+        String fileData = new String(FileIoUtils.loadFile(filepath));
+
+        fileData = fileData.replace("%user_list", userListToString());
+
+        return fileData.getBytes();
+    }
+
+    private String userListToString() {
+        Collection<User> userList = UserRepository.findAll();
+        StringBuilder sb = new StringBuilder();
+
+        int idx = 3;
+
+        for (User user : userList) {
+            sb.append("<tr>");
+            sb.append("<th scope=\"row\">").append(idx).append("</th>");
+            sb.append("<td>").append(user.getUserId()).append("</td>");
+            sb.append("<td>").append(user.getName()).append("</td>");
+            sb.append("<td>").append(user.getEmail()).append("</td>");
+            sb.append("<td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>");
+            sb.append("<tr>");
+            idx++;
+        }
+
+        return sb.toString();
     }
 
 }
