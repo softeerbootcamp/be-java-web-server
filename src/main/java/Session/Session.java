85b83595e9efcdbbdf409ec96c9bfd9b678a2ada
jieonkim23 <kwldjs1023@gmail.com>
2023-01-18 12:11:16 +0900

    :sparkles: Session 클래스 생성 : 유저 객체와 세션 아이디를 필드로 가지는 클래스

---
+++ src/main/java/Session/Session.java
@@ -0,0 +1,28 @@
+package Session;
+
+import model.User;
+
+import java.util.UUID;
+
+public class Session {
+    private final User user;
+    private final String sessionId;
+
+    public Session(User user) {
+        this.user = user;
+        this.sessionId = generateSessionId();
+    }
+
+    public String getUserId() {
+        return this.user.getUserId();
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    private String generateSessionId(){
+        return UUID.randomUUID().toString();
+    }
+
+}
