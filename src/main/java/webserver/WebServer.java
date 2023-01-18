a6c433bfeb2e0d60c7466789f7a080b930324960
201724554 <pay7845@naver.com>
2023-01-18 23:03:00 +0900

    구조 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -5,7 +5,6 @@ import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import request.RequestHandlerImpl;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
@@ -26,7 +25,7 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new ClientRequestThread(connection, new RequestHandlerImpl()));
+                Thread thread = new Thread(new ClientRequestThread(connection));
                 thread.start();
             }
         }
