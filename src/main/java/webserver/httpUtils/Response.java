b217b398a1ccb4fe5f3102ce1d5287f10313601c
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-18 17:48:05 +0900

    response builder pattern

--- src/main/java/webserver/httpUtils/Response.java
+++ src/main/java/webserver/httpUtils/Response.java
@@ -51,4 +51,38 @@ public class Response {
     public void setResBody(byte[] resBody) {
         body.setContext(resBody);
     }
+
+    // region builder pattern
+    public Response withVersion(String version)
+    {
+        resLine.setVersion(version);
+        return this;
+    }
+
+    public Response withStatCodeAndText(int code, String text)
+    {
+        resLine.setStatCode(code);
+        resLine.setStatText(text);
+        return this;
+    }
+
+    public Response withHeaderKeyVal(String key, String val)
+    {
+        header.insertKeyVal(key, val);
+        return this;
+    }
+
+    public Response withBodyBytes(byte[] body)
+    {
+        this.body.setContext(body);
+        return this;
+    }
+
+    public Response withBodyString(String body)
+    {
+        this.body.setContext(body);
+        return this;
+    }
+
+    // endregion
 }
