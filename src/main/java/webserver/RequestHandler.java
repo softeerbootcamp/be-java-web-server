0249d0077179a74d596243b0e4d086b152aec984
roopre <benny1020@naver.com>
2023-01-18 16:53:44 +0900

    docs: 불필요한 로깅 제거

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,14 +5,17 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import controller.FrontController;
+import model.User;
 import request.HttpRequest;
 import request.HttpRequestUtils;
 import response.HttpResponse;
+import session.SessionManager;
 
 public class RequestHandler implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -25,12 +28,13 @@ public class RequestHandler implements Runnable {
 	}
 
 	public void run() {
-		logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-			connection.getPort());
+		//logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+		//	connection.getPort());
 		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
 			HttpRequest httpRequest = HttpRequestUtils.httpRequestParse(in);
 			HttpResponse httpResponse = new HttpResponse();
+
 			frontController.service(httpRequest, httpResponse);
 
 			// 전송
