3c365063b9f97a4a75a8e57d7037fa85d99a16cb
rohsik2 <rohsik2@gmail.com>
2023-01-18 11:11:38 +0900

    :bug: assert문 삭제

--- src/main/java/service/SessionService.java
+++ src/main/java/service/SessionService.java
@@ -2,25 +2,34 @@ package service;
 
 import model.Session;
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import repository.SessionRepo;
+import repository.UserRepo;
 
 public class SessionService {
+    private static final Logger logger = LoggerFactory.getLogger(SessionService.class);
 
     public static Session addUserToSession(User user) {
         return SessionRepo.createSession(user.getUserId());
     }
 
-    public User getUserBySessionId(String ssid) {
+    public static User getUserBySessionId(String ssid) {
         Session sess = SessionRepo.findBySSID(ssid);
         if (!SessionRepo.isExpired(ssid)) {
-            assert sess != null;
-            return UserService.findUserById(sess.getUserId());
+            return UserRepo.findUserById(sess.getUserId());
         }
         return null;
     }
 
-    public static Session expireSession(String ssid){
-        if(ssid != null)
+    public static boolean isValidSSID(String ssid) {
+        logger.info("SID {} is {} {}", ssid, ssid != null && !SessionRepo.isExpired(ssid) && SessionRepo.findBySSID(ssid) != null, SessionRepo.findBySSID(ssid));
+        return ssid != null
+                && (SessionRepo.findBySSID(ssid) != null);
+    }
+
+    public static Session expireSession(String ssid) {
+        if (ssid != null)
             SessionRepo.deleteSession(ssid);
         return Session.EXPIRED;
     }
