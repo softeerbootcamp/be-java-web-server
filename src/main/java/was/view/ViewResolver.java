133be0449304f7980cde03e7e6d200d43d238fd0
ajongs <wonjong1030@naver.com>
2023-01-18 20:32:35 +0900

    feat(ViewResolver): html로 들어온 파일을 한줄씩 읽고 로그인과 회원가입 버튼 문장을 삭제하고 다시 byte[] 반환하는 기능 추가

--- src/main/java/was/view/ViewResolver.java
+++ src/main/java/was/view/ViewResolver.java
@@ -1,2 +1,27 @@
-package was.view;public class ViewResolver {
+package was.view;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ViewResolver {
+    public static byte[] makeDynamicHtml(String userId, String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                if (line.contains("로그인") || line.contains("회원가입")) {
+                    continue;
+                }
+                sb.append(line);
+            }
+            return sb.toString().getBytes();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
