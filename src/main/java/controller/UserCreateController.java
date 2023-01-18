0ab266c1fb83f264de3bb5ce7d3e8fea3d1e268c
River-Mt <rivermt1998@gmail.com>
2023-01-18 22:26:10 +0900

    :fire: 쓸모 없는 부분 삭제

--- src/main/java/controller/UserCreateController.java
+++ src/main/java/controller/UserCreateController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import http.Uri;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import org.slf4j.Logger;
@@ -21,8 +20,7 @@ public class UserCreateController extends AbstractController {
 
     @Override
     public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
-        Uri uri = httpRequest.getUri();
-        Map<String, String> queryParams = uri.getParameters();
+        Map<String, String> queryParams = httpRequest.getParameters();
         userService.create(queryParams);
 
         httpResponse.sendRedirect(REDIRECT_PATH);
