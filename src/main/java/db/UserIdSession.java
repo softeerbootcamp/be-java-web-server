8010b3261cc01641b3b4cf8adfd25654d3d23ac6
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-18 12:26:10 +0900

    static

--- src/main/java/db/UserIdSession.java
+++ src/main/java/db/UserIdSession.java
@@ -7,12 +7,12 @@ import java.util.Map;
 public class UserIdSession {
     private static Map<String, String> sessions;
 
-    public String getUserId(String sessionId)
+    public static String getUserId(String sessionId)
     {
         return sessions.get(sessionId);
     }
 
-    public String addUserId(String userId)
+    public static String addUserId(String userId)
     {
         String randSessionId = SessionIdGenerator.generateSessionId();
         sessions.put(randSessionId, userId);
