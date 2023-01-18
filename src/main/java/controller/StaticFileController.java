91a8d6d15272f9eab00d32399b33f9f7efbe6bc6
Toa <ld980105@snu.ac.kr>
2023-01-18 18:13:40 +0900

    :recycle: StaticFileController의 구조 변경

--- src/main/java/controller/StaticFileController.java
+++ src/main/java/controller/StaticFileController.java
@@ -4,15 +4,23 @@ import http.response.HttpResponse;
 import http.request.HttpRequest;
 import utils.ContentType;
 import utils.FileIoUtils;
+import utils.HttpMethod;
 import utils.StatusCode;
 
 public class StaticFileController implements Controller {
     public final static String PATH = "/";
     @Override
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) {
+        if (httpRequest.getHttpMethod().equals(HttpMethod.GET)) {
+            doGet(httpRequest, httpResponse);
+            return;
+        }
+        throw new IllegalArgumentException("유효하지 않은 HTTP 메서드입니다");
+    }
+
+    public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) {
         try {
-            byte[] body = FileIoUtils.loadFile(httpRequest.getUri().getPath());
-            httpResponse.setBody(body);
+            httpResponse.setBody(FileIoUtils.loadFile(httpRequest.getUri().getPath()));
             httpResponse.setContentType(ContentType.getContentType(FileIoUtils.getExtension(httpRequest.getUri().getPath())));
             httpResponse.setStatusCode(StatusCode.OK);
         }
