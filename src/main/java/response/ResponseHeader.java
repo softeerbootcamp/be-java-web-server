0f11672e4f0f2533393465a84aaec05d745d918d
201724554 <pay7845@naver.com>
2023-01-18 23:02:38 +0900

    정적 팩토리 생성자 이름 변경

--- src/main/java/response/ResponseHeader.java
+++ src/main/java/response/ResponseHeader.java
@@ -1,29 +1,29 @@
 package response;
 
-import request.Request;
-
 public class ResponseHeader {
     public static String DEFAULT_HTTP_VERSION = "HTTP/1.1";
+
+    public static String CHARSET_UTF_ENCODING = "charset=utf-8";
 
     private String header;
 
     private ResponseHeader(byte[] body, HttpResponseStatus httpResponseStatus) {
         header = DEFAULT_HTTP_VERSION + " " + httpResponseStatus.getCode() + "\r\n";
-        header += "Content-Type: text/plain;charset=utf-8\r\n";
+        header += "Content-Type: text/plain;" + CHARSET_UTF_ENCODING + "\r\n";
         header += "Content-Length: " + body.length + "\r\n";
         header += "\r\n";
     }
 
     private ResponseHeader(byte[] body, String contentType, HttpResponseStatus httpResponseStatus) {
         header = DEFAULT_HTTP_VERSION  + " " +  httpResponseStatus.getCode() + "\r\n";
-        header += "Content-Type: " + contentType + ";charset=utf-8\r\n";
+        header += "Content-Type: " + contentType + ";" + CHARSET_UTF_ENCODING + "\r\n";
         header += "Content-Length: " + body.length + "\r\n";
         header += "\r\n";
     }
 
     private ResponseHeader(byte[] body, String contentType, HttpResponseStatus httpResponseStatus, String optional) {
         header = DEFAULT_HTTP_VERSION + " " +  httpResponseStatus.getCode() + "\r\n";
-        header += "Content-Type: " + contentType + ";charset=utf-8\r\n";
+        header += "Content-Type: " + contentType + ";" + CHARSET_UTF_ENCODING + "\r\n";
         header += "Content-Length: " + body.length + "\r\n";
         header += optional;
         header += "\r\n";
@@ -33,11 +33,11 @@ public class ResponseHeader {
         return new ResponseHeader(body, httpResponseStatus);
     }
 
-    public static ResponseHeader of(byte[] body, String contentType, HttpResponseStatus httpResponseStatus) {
+    public static ResponseHeader createSimpleRespHeader(byte[] body, String contentType, HttpResponseStatus httpResponseStatus) {
         return new ResponseHeader(body, contentType, httpResponseStatus);
     }
 
-    public static ResponseHeader of(byte[] body, String contentType, HttpResponseStatus httpResponseStatus, String optional) {
+    public static ResponseHeader createFullRespHeader(byte[] body, String contentType, HttpResponseStatus httpResponseStatus, String optional) {
         return new ResponseHeader(body, contentType, httpResponseStatus, optional);
     }
 
