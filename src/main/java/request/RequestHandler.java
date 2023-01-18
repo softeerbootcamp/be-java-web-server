2a2d77f4a7b9e5f637133a8e3041069c659fe037
201724554 <pay7845@naver.com>
2023-01-18 22:53:56 +0900

    메소드 별로 Handler interface를 만들지 않고, 메소드에 상관없이 모든 handler가 구현하는 interface 생성

--- src/main/java/request/RequestHandler.java
+++ src/main/java/request/RequestHandler.java
@@ -1,7 +1,28 @@
 package request;
 
+import response.HttpResponseStatus;
 import response.Response;
+import service.SessionService;
+import service.UserService;
 
 public interface RequestHandler {
-    Response handle(Request request);
+    UserService userService = UserService.getInstance();
+
+    SessionService sessionService = SessionService.getInstance();
+
+    default Response doGet(Request request) {
+        return Response.from(HttpResponseStatus.METHOD_NOT_ALLOWED);
+    }
+
+    default Response doPost(Request request) {
+        return Response.from(HttpResponseStatus.METHOD_NOT_ALLOWED);
+    }
+
+    default Response doPut(Request request) {
+        return Response.from(HttpResponseStatus.METHOD_NOT_ALLOWED);
+    }
+
+    default Response doDelete(Request request) {
+        return Response.from(HttpResponseStatus.METHOD_NOT_ALLOWED);
+    }
 }
