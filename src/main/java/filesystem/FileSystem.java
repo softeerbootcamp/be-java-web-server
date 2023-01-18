c0a43bc85578cc13652afc7d364b7e2b3e982f4c
wonju-dev <wonjulee.dev@gmail.com>
2023-01-18 18:03:25 +0900

    feat(FileSystem): template만들기, 정적 리소스로 만들기 메서드 구현

--- src/main/java/filesystem/FileSystem.java
+++ src/main/java/filesystem/FileSystem.java
@@ -1,25 +1,81 @@
 package filesystem;
 
-import java.io.File;
+import db.Database;
+import exception.FileSystemException;
+import http.common.Session;
+import http.request.HttpRequest;
+import model.User;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileSystem {
 
     private FileSystem() {
     }
 
-    public static FindResource findResource(String url) {
-        String resourcePath = PathResolver.parse(url);
-        byte[] resource = readFile(new File(resourcePath));
-
-        FindResource findResult = new FindResource(resourcePath, resource);
-        return findResult;
+    public static FindResource findDynamicResource(HttpRequest request) {
+        String resourcePath = PathResolver.parse(request.getUrl());
+        byte[] resource = readFile(resourcePath);
+        resource = insertData(resource, Database.getSession(request.getCookie(Session.SESSION_FIELD_NAME).getValue()).getUser());
+        return new FindResource(resourcePath, resource);
     }
 
-    private static byte[] readFile(File file) {
+    private static byte[] insertData(byte[] resource, User user) {
         try {
-            return Files.readAllBytes(file.toPath());
+            BufferedReader bf = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(resource)));
+            String html = "";
+            String line = bf.readLine();
+            do {
+                if (line.trim().equals("<%")) {
+                    bf.readLine();
+                    bf.readLine();
+                    html += "<li>" + "<a href='#' role='button'>" + user.getUserId() + "</a>" + "</li>" + System.lineSeparator();
+                    html += bf.readLine() + System.lineSeparator();
+                    html += bf.readLine() + System.lineSeparator();
+                    bf.readLine();
+                } else {
+                    html += line + System.lineSeparator();
+                }
+                line = bf.readLine();
+            } while (line != null);
+            return html.getBytes();
+        } catch (IOException e) {
+            throw new FileSystemException(e);
+        }
+    }
+
+    public static FindResource findStaticResource(HttpRequest request) {
+        String resourcePath = PathResolver.parse(request.getUrl());
+        byte[] resource = readFile(resourcePath);
+        resource = trimSpecialSymbol(resource);
+        return new FindResource(resourcePath, resource);
+    }
+
+    private static byte[] trimSpecialSymbol(byte[] resource) {
+        try {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(resource)));
+            String html = "";
+            String line = bf.readLine();
+            do {
+                if (!line.trim().equals("<%") && !line.trim().equals("%>")) {
+                    html += line + System.lineSeparator();
+                }
+                line = bf.readLine();
+            } while (line != null);
+            return html.getBytes();
+        } catch (IOException e) {
+            throw new FileSystemException(e);
+        }
+    }
+
+    private static byte[] readFile(String resourcePath) {
+        try {
+            return Files.readAllBytes(Paths.get(resourcePath));
         } catch (IOException e) {
             return new byte[0];
         }
