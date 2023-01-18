5717054763e6c0a0a1e64616005f4d1593bf5de3
roopre <benny1020@naver.com>
2023-01-18 21:19:32 +0900

    feat: 요청하는 파일 없을때 예외 처리 구현

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -1,9 +1,11 @@
 package controller;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import exception.BadRequestException;
 import response.ContentType;
 import request.HttpRequest;
 import response.HttpResponse;
@@ -27,6 +29,10 @@ public class FileController extends AbstractController {
 		String[] splitList = path.split("\\.");
 		final String extension = splitList[splitList.length - 1];
 		File file = new File("./webapp" + httpRequest.getUrl().getPath());
+
+		if (!file.isFile()) {
+			throw new BadRequestException("요청하는 파일이 존재하지 않습니다.");
+		}
 		httpResponse.setHttpResponse(HttpStatus.OK, new String(Files.readAllBytes(file.toPath())),
 			ContentType.findContentType(extension));
 	}
