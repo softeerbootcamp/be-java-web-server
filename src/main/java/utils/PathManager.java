8e75d351985e4d2987a049209dd92a523e2df530
Toa <ld980105@snu.ac.kr>
2023-01-19 08:02:20 +0900

    :sparkles: user list path 추가 추가

--- src/main/java/utils/PathManager.java
+++ src/main/java/utils/PathManager.java
@@ -4,5 +4,5 @@ public class PathManager {
     public static final String HOME_PATH = "/index.html";
     public static final String LOGIN_PATH = "/user/login.html";
     public static final String LOGIN_FAILED_PATH = "/user/login_failed.html";
-
+    public static final String USER_LIST_PATH = "/user/list.html";
 }
