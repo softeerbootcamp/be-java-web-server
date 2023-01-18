f20687e7cc1e90e7485f4555b3c34f802ba3e9c5
solchan98 <dev.sol0427@gmail.com>
2023-01-18 17:27:19 +0900

    feat: index.html 로그인 여부에 따라 동적 렌더링 기존에는 로그인 여부 상관없이 로그인 버튼이 카테고리에 존재. 로그인 여부에 따라 로그인 버튼 노출과 사용자 닉네임 노출

--- src/main/java/view/IndexView.java
+++ src/main/java/view/IndexView.java
@@ -2,6 +2,7 @@ package view;
 
 import http.common.ContentType;
 import http.request.HttpRequest;
+import http.request.HttpSession;
 import http.response.HttpResponse;
 import util.ResourceUtils;
 
@@ -9,8 +10,21 @@ public class IndexView implements View {
 
     @Override
     public void render(HttpRequest request, HttpResponse response, Model data) {
-        byte[] bytes = ResourceUtils.loadFileFromClasspath("/index.html");
+        byte[] bytes = request.getSession() == null ? guest() : auth(request.getSession());
         response.setBody(bytes);
         response.addHeader("Content-Type", ContentType.TEXT_HTML.name());
+    }
+
+    private byte[] guest() {
+        return ResourceUtils.loadFileFromClasspath("/index.html");
+    }
+
+    private byte[] auth(HttpSession session) {
+        String target = "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>";
+        String name = "<!--<li>name</li>-->";
+        String replaceName = "<li>" + session.user().getName() + "</li>";
+        String page = new String(ResourceUtils.loadFileFromClasspath("/index.html"));
+
+        return page.replace(target, "").replace(name, replaceName).getBytes();
     }
 }
