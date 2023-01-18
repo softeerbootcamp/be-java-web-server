b399cd9cd12221d43a5017705634f8420f9af72c
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-18 14:45:39 +0900

    프론트 컨트롤러 내에서 정적,동적 파일 요청을 구분하는 분기문 추가
    좀 더 리팩토링 필요,, 일단 구현만 되게 해놓음

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import service.SignUpService;
+import webserver.HttpMethod;
 import webserver.HttpRequest;
 import webserver.HttpResponse;
 
@@ -14,6 +15,7 @@ public class FrontController implements Controller {
     private static final Map<String,Controller> controllers;
     private static final String SIGN_UP_PATH_URL = "/user/create";
     private static final String LOGIN_PATH_URL ="/user/login";
+    private static final String USER_LIST_PATH_URL ="/user/list";
 
     //객체 캐싱
     static{
@@ -21,11 +23,14 @@ public class FrontController implements Controller {
         SignUpService signUpService = new SignUpService();
         SignUpController signUpController = new SignUpController(signUpService);
         LoginController loginInController = new LoginController();
+        UserListController userListController = new UserListController();
         controllers.put(SIGN_UP_PATH_URL,signUpController);
         controllers.put(LOGIN_PATH_URL,loginInController);
+        controllers.put(USER_LIST_PATH_URL,userListController);
     }
 
     public Controller getControllerByUrl(String pathUrl){
+        System.out.println("getControllerByUrl: "+pathUrl);
         return controllers.get(pathUrl);
     }
 
@@ -38,19 +43,41 @@ public class FrontController implements Controller {
     public void service(HttpRequest request, HttpResponse response) {
         try{
             String url = request.getUrl();
-            //Controller controller = new ReturnFileController();
+
+            String dynamicUrl ="";
+            //get이면 파일 요청
+            if(request.getMethod().equals(HttpMethod.GET)){
+                if(!(url.contains("."))){
+                    dynamicUrl = url+".html";
+                    request.setUrl(dynamicUrl);
+                }
+            }
+
+            System.out.println("dynamicUrl:"+dynamicUrl);
+
             Controller controller = new NotFoundExceptionHandler();
-            //일단 디폴트 컨트롤러  - 일반 파일 요청시
-            if(url.contains(".")) controller = new ReturnFileController();
-            //만약 파일 요청이 아니라면
-            if(!url.contains(".")) controller = getControllerByUrl(url);
+            //일반 정적 파일 요청시
+            if(url.contains(".")&&(dynamicUrl.isEmpty())) {
+                System.out.println("static controller");
+                controller = new ReturnFileController();
+            }
+            //동적 파일 요청이라면
+            if(!(dynamicUrl.isEmpty())) {
+                System.out.println("dynamic controller");
+                controller = getControllerByUrl(url);
+            }
+            //만약 파일 요청이 아니라 그 외 요청이라면 (post등)
+            if(!url.contains(".") && (dynamicUrl.isEmpty())) {
+                System.out.println("etc controller");
+                controller = getControllerByUrl(url);
+            }
             controller.service(request,response);
         }catch(NullPointerException e){
             NotFoundExceptionHandler.showErrorPage(response);
+             e.printStackTrace();
             System.out.println("해당 url에 대한 응답이 없습니다");
         } catch (FileNotFoundException e) {
             NotFoundExceptionHandler.showErrorPage(response);
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (URISyntaxException e) {
