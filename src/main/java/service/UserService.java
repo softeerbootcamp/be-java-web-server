4e7c758a14f678d189d64074e071c57dc2935733
rentalhub-mac78 <kksy2423@naver.com>
2023-01-18 17:54:23 +0900

    Feat: 유저 로그인정보를 받아 로그인 로직 추가

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,18 +1,42 @@
 package service;
 
+import db.UserDatabase;
 import model.User;
+import util.error.HttpsErrorMessage;
+import util.error.erroclass.NotLoggedException;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class UserService implements Service<User> {
 
     private final String[] userKey = {"userId", "password", "name", "email"};
 
+    private UserDatabase userDatabase = new UserDatabase();
+
 
     @Override
     public User createModel(Map<String, String> userMap) {
         return new User(userMap.get(userKey[0]), userMap.get(userKey[1]), userMap.get(userKey[2]), userMap.get(userKey[3]));
+    }
+
+    public User validLogin(Map<String, String> loginInfo) throws NotLoggedException {
+        String loginUserId = loginInfo.get(userKey[0]);
+        String loginPassword = loginInfo.get(userKey[1]);
+
+        Optional<User> optionalUser = userDatabase.findObjectById(loginUserId);
+        if (optionalUser.isEmpty()) {
+            throw new NotLoggedException(HttpsErrorMessage.NOT_LOGGED);
+        }
+        User user = optionalUser.get();
+        if (!isMatchLogin(loginUserId, loginPassword, user)) {
+            throw new NotLoggedException(HttpsErrorMessage.NOT_LOGGED);
+        }
+        return user;
+    }
+
+    private static boolean isMatchLogin(String loginUserId, String loginPassword, User user) {
+        return user.getUserId().equals(loginUserId) && user.getPassword().equals(loginPassword);
     }
 
 }
