1a13a0198e5fe9ec9f11b03aa2c0e21f9b532aa0
jieonkim23 <kwldjs1023@gmail.com>
2023-01-19 00:30:10 +0900

    :sparkles: FileController에서 로그인 중이면 로그인 버튼을 없애고 유저 이름을 표시하도록 body생성 로직 변경

--- src/main/java/Controller/FileController.java
+++ src/main/java/Controller/FileController.java
@@ -5,10 +5,13 @@ import Response.HttpResponse;
 import Response.HttpResponseBody;
 import Response.HttpResponseHeaders;
 import Response.HttpResponseStartLine;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import Request.StatusCode;
+import util.HtmlBuildUtil;
 import util.HttpResponseUtil;
+import util.LoginUtil;
 
 public class FileController implements Controller {
     private final Logger logger = LoggerFactory.getLogger(FileController.class);
@@ -19,10 +22,18 @@ public class FileController implements Controller {
 
     @Override
     public HttpResponse createResponse(HttpRequest httpRequest) {
-        byte[] body = HttpResponseUtil.generateBody(httpRequest.getPath());
+        byte[] body = createBody(httpRequest);
         HttpResponse httpResponse = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.OK, httpRequest.getProtocol()))
                 .headers(new HttpResponseHeaders(httpRequest.getPath(), body.length))
                 .body(new HttpResponseBody(body));
         return httpResponse;
+    }
+    private byte[] createBody(HttpRequest httpRequest) {
+        try {
+            User user = LoginUtil.checkSession(httpRequest);
+            return HtmlBuildUtil.withoutLogoutWithUserName(httpRequest.getPath(), user).getBytes();
+        } catch (NullPointerException e) {
+            return HttpResponseUtil.generateBody(httpRequest.getPath());
+        }
     }
 }
