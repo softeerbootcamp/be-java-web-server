e59cdfec6e22dcecc5cd63f06ab039abfc322dab
jieonkim23 <kwldjs1023@gmail.com>
2023-01-19 00:26:59 +0900

    :recycle: redirect하는 메소드를 Controller에 옮김

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -1,7 +1,11 @@
 package Controller;
 
 import Request.HttpRequest;
+import Request.StatusCode;
 import Response.HttpResponse;
+import Response.HttpResponseBody;
+import Response.HttpResponseHeaders;
+import Response.HttpResponseStartLine;
 import util.HttpResponseUtil;
 
 import java.io.DataOutputStream;
@@ -15,4 +19,11 @@ public interface Controller {
         HttpResponseUtil.sendResponse(dos, httpResponse);
     }
 
+    default HttpResponse redirect(String loc, HttpRequest httpRequest) {
+        HttpResponse response = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.FOUND, httpRequest.getProtocol()))
+                .headers(new HttpResponseHeaders("", 0))
+                .body(new HttpResponseBody("".getBytes()));
+        response.putHeader("Location", loc);
+        return response;
+    }
 }
