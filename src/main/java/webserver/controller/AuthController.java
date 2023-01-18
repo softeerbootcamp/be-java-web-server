2d96fa21fe59359155048fd632a5c6741fae6572
i5046821854 <o5046821854@gmail.com>
2023-01-18 18:38:39 +0900

    feat : 유저 리스트를 보여주는 페이지에 대한 핸들링 메소드를 추가

--- src/main/java/webserver/controller/AuthController.java
+++ src/main/java/webserver/controller/AuthController.java
@@ -11,6 +11,7 @@ import webserver.domain.request.Request;
 import webserver.domain.response.Response;
 import webserver.exception.HttpRequestException;
 import webserver.utils.HttpCookieUtils;
+import webserver.utils.StaticResourceFinder;
 
 import java.io.IOException;
 import java.util.Map;
@@ -35,7 +36,7 @@ public class AuthController implements Controller {
     public void userLogin(Map<String, String> queryStrs, Response response){
         try{
             String userId = authService.login(queryStrs.get("userId"), queryStrs.get("password"));
-            if(queryStrs.get("Cookie") != null)     //when client got no session for now, generate a new cookie and add it on response header
+            if(queryStrs.get("Cookie") == null)     //when client got no session for now, generate a new cookie and add it on response header
                 response.addCookieOnHeader(HttpCookieUtils.generateCookie(userId).toString());
             response.redirect(StatusCodes.SEE_OTHER, ("<script>alert('로그인이 완료되었습니다.');</script>").getBytes(), ContentType.TEXT_HTML,"/index.html");
         }catch(HttpRequestException e){
@@ -43,11 +44,19 @@ public class AuthController implements Controller {
         }
     }
 
-    @ControllerInfo(path = "/user/logout", methodName = "userLogout", method = RequestMethod.POST)
+    @ControllerInfo(path = "/user/logout", methodName = "userLogout", method = RequestMethod.GET) //TODO : logout 요청 POST로 처리하기
     public void userLogout(Map<String, String> queryStrs, Response response){
         String sessionId = queryStrs.get("Cookie");
-        HttpCookieUtils.cookieInvalidation(sessionId);
         response.ok(StatusCodes.OK, ("<script>alert('로그아웃 완료되었습니다.'); window.location.href = 'http://localhost:8080/index.html';</script>").getBytes(), ContentType.TEXT_HTML);
+        String cookieStr= HttpCookieUtils.cookieInvalidation(sessionId);
+        response.addCookieOnHeader(HttpCookieUtils.cookieInvalidation(sessionId));
+    }
+
+    @ControllerInfo(path = "/user/list", methodName = "userList", method = RequestMethod.GET)
+    public void userList(Map<String, String> queryStrs, Response response) throws IOException {
+        String filePath = "/user/list.html";
+        StaticResourceFinder.staticFileResolver(filePath)
+                .ifPresent( fileAsBytes -> response.ok(StatusCodes.OK, fileAsBytes, StaticResourceFinder.getExtension(filePath))); //if not
     }
 
     @Override
