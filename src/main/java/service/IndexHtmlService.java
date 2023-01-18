94e935dd91c4f6b64f2124c28bdf5cc2c48f4390
jhchoi57 <enough1125@gmail.com>
2023-01-18 16:58:42 +0900

    REFACTOR : 로그인 상태일 경우 로그인버튼과 회원가입 버튼 없애는 로직 함수로 분리

--- src/main/java/service/IndexHtmlService.java
+++ src/main/java/service/IndexHtmlService.java
@@ -7,19 +7,22 @@ import model.User;
 import util.HttpResponseUtils;
 
 public class IndexHtmlService {
-    public static HttpResponse service(String filePath, User loginUser, String httpVersion, String contentType) {
+    public static HttpResponse service(String filePath, User logInUser, String httpVersion, String contentType) {
 
         // 파일 경로를 넘겨서 http response string 생성
         String responseString = new String(HttpResponseUtils.makeBody(filePath));
-        byte[] responseBody = responseString.replace(
-                "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>\n" +
-                        "                <li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>",
-                "<li><a role=\"button\">" + loginUser.getName() + "</a></li>").getBytes();
-                //"로그인", loginUser.getName()).getBytes();
+        byte[] responseBody = banLogInAndSignUpWhenUserLogInState(responseString, logInUser);
 
         return new HttpResponse.HttpResponseBuilder()
                 .setHttpStatusLine(new HttpStatusLine(HttpStatus.OK, httpVersion))
                 .set200Header(contentType, responseBody)
                 .build();
+    }
+
+    public static byte[] banLogInAndSignUpWhenUserLogInState(String responseString, User logInUser){
+        return responseString.replace(
+                "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>\n" +
+                        "                <li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>",
+                "<li><a role=\"button\">" + logInUser.getName() + "</a></li>").getBytes();
     }
 }
