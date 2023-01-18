06d64d191db52e28e0b9290d5b648541933918a5
jhchoi57 <enough1125@gmail.com>
2023-01-18 16:51:30 +0900

    FEAT : login 이후 index.html에서 유저 이름 눌렀을 때 하이퍼링크 삭제, 회원가입 버튼 삭제

--- src/main/java/service/IndexHtmlService.java
+++ src/main/java/service/IndexHtmlService.java
@@ -11,7 +11,11 @@ public class IndexHtmlService {
 
         // 파일 경로를 넘겨서 http response string 생성
         String responseString = new String(HttpResponseUtils.makeBody(filePath));
-        byte[] responseBody = responseString.replace("로그인", loginUser.getName()).getBytes();
+        byte[] responseBody = responseString.replace(
+                "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>\n" +
+                        "                <li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>",
+                "<li><a role=\"button\">" + loginUser.getName() + "</a></li>").getBytes();
+                //"로그인", loginUser.getName()).getBytes();
 
         return new HttpResponse.HttpResponseBuilder()
                 .setHttpStatusLine(new HttpStatusLine(HttpStatus.OK, httpVersion))
