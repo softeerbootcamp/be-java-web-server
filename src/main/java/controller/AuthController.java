d0a144f5893605c496928c3274cf70bef461fb27
solchan98 <dev.sol0427@gmail.com>
2023-01-18 12:47:14 +0900

    feat: AuthController 수정 로그인 유저라면 HttpRequest의 HttpSession이 담겨오기 때문에 HttpSession의 존재 여부만 체크하도록 수정

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -1,22 +1,13 @@
 package controller;
 
 import http.request.HttpRequest;
-import http.request.HttpSession;
 import http.response.HttpResponse;
-
-import java.util.Optional;
-
-import static http.common.HttpSessionStorage.getSession;
 
 public interface AuthController extends Controller {
 
     @Override
     default void execute(HttpRequest request, HttpResponse response) {
-        String sessionId = request.getHeaders().getCookie("session");
-        Optional<HttpSession> sessionOptional = getSession(sessionId);
-
-        if (sessionOptional.isPresent()) {
-            request.setSession(sessionOptional.get());
+        if (request.getSession() != null) {
             Controller.super.execute(request, response);
             return;
         }
