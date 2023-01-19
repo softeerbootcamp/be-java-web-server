aeafb5ebb56bfac39c01fc76690347ed14cfe4fd
rentalhub-mac78 <kksy2423@naver.com>
2023-01-19 14:26:51 +0900

    Feat: 홈 화면에서 사용자 이름 렌더링 기능 추

--- src/main/java/view/HomeRender.java
+++ src/main/java/view/HomeRender.java
@@ -12,11 +12,20 @@ public class HomeRender {
         return homeRender;
     }
 
-    public byte[] addSignInAndUpTag(String homeData) {
-        homeData = homeData.replace("<!--                <li><a href=\"user/login.html\" role=\"button\">로그인</a></li>-->",
+
+    public byte[] addSignInAndUpTag(byte[] homeData) {
+        String homeStr = new String(homeData);
+        homeStr = homeStr.replace("<!--                <li><a href=\"user/login.html\" role=\"button\">로그인</a></li>-->",
                 "                <li><a href=\"user/login.html\" role=\"button\">로그인</a></li>");
-        homeData = homeData.replace("<!--                <li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>-->",
+        homeStr = homeStr.replace("<!--                <li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>-->",
                 "                <li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>");
-        return homeData.getBytes();
+        return homeStr.getBytes();
+    }
+
+    public byte[] addUserName(byte[] homeData,String userName) {
+        String homeStr = new String(homeData);
+        homeStr = homeStr.replace("<!--                <li><a href=\"#\">이름</i></a></li>-->",
+                "                <li><a href=\"#\">"+userName+"</i></a></li>");
+        return homeStr.getBytes();
     }
 }
