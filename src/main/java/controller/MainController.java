0912bf09106d5d6e79c76d63faf018b3b2f9e067
wonju-dev <wonjulee.dev@gmail.com>
2023-01-19 14:17:00 +0900

    feat(AuthService)

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,15 +1,13 @@
 package controller;
 
-import db.Database;
 import filesystem.FileSystem;
 import filesystem.FindResource;
 import filesystem.PathResolver;
-import http.common.Cookie;
-import http.common.Session;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import service.AuthService;
 
 import static filesystem.PathResolver.authenticatedUrl;
 import static filesystem.PathResolver.indexRequestUrl;
@@ -17,6 +15,7 @@ import static filesystem.PathResolver.indexRequestUrl;
 public class MainController implements Controller {
 
     private final Logger logger = LoggerFactory.getLogger(MainController.class);
+    private final AuthService authService = new AuthService();
 
 
     @Override
@@ -30,29 +29,16 @@ public class MainController implements Controller {
     private FindResource getResource(HttpRequest request) {
         // todo: 리팩토링,,,,
         if (indexRequestUrl.contains(request.getUrl())) {
-            if (isAuthenticated(request)) {
+            if (authService.isAuthenticated(request)) {
                 return FileSystem.getIndexPage(request);
             }
-            return FileSystem.findStaticResource(request);
         }
         if (authenticatedUrl.contains(request.getUrl())) {
-            if (isAuthenticated(request)) {
+            if (authService.isAuthenticated(request)) {
                 return FileSystem.getUserListPage(request);
             }
             return new FindResource(PathResolver.LOGIN_HTML, FileSystem.readFile(PathResolver.parse(PathResolver.LOGIN_HTML)));
         }
         return FileSystem.findStaticResource(request);
-    }
-
-    private boolean isAuthenticated(HttpRequest request) {
-        Cookie sessionCookie = request.getCookie(Session.SESSION_FIELD_NAME);
-        if (sessionCookie == null) {
-            return false;
-        }
-        Session session = Database.getSession(sessionCookie.getValue());
-        if (session == null) {
-            return false;
-        }
-        return session.isValid();
     }
 }
