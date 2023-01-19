0f7aad60649c2f277eb0551d74335445e51562d2
RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>
2023-01-19 15:29:04 +0900

    새 경로 상수 생성
    로그인 실패 시 새로 생성한 상수 경로로 redirect

--- src/main/java/webserver/Paths.java
+++ src/main/java/webserver/Paths.java
@@ -9,4 +9,6 @@ public class Paths {
     public static final String STATIC_PATH = "./src/main/resources/static";
 
     public static final String ENROLL_FAIL_PATH = "/user/form_failed.html";
+
+    public static final String LOGIN_FAIL_PATH = "/user/login_failed.html";
 }
