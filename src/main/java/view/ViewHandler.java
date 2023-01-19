d1cc07f87f1a59e17f469bdaca3fa819f9d85701
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-19 12:37:19 +0900

    뷰 단에서의 프론트 컨트롤러 역할
    컨트롤러의 리턴 값으로 나온 뷰의 파일 경로를 바탕으로 해당되는 뷰 객체를 찾아서 그 뷰의 render 함수 호출

--- src/main/java/view/ViewHandler.java
+++ src/main/java/view/ViewHandler.java
@@ -1,2 +1,23 @@
-package view;public class ViewHandler {
+package view;
+
+import webserver.HttpRequest;
+import webserver.HttpResponse;
+
+import java.util.Map;
+
+public class ViewHandler {
+    private static Map<String, View> views ;
+
+    static {
+        views = Map.of(
+                "/user/list.html", new UserListView()
+        );
+    }
+
+    public static void handle(String path, HttpRequest request, HttpResponse response, Model model) {
+        if (views.containsKey(path)) {
+            views.get(path).render(request, response, model);
+        }
+    }
+
 }
