bdb1fc1a5568050a290d425cedb20664c936c539
rohsik2 <rohsik2@gmail.com>
2023-01-19 14:20:09 +0900

    :sparkels: user list 동적 생성을 하는 코드를 controller에 추가

--- src/main/java/controller/UserAccountController.java
+++ src/main/java/controller/UserAccountController.java
@@ -1,20 +1,25 @@
 package controller;
 
+import Utility.HtmlMakerUtility;
 import Utility.UserValidation;
 import httpMock.CustomHttpFactory;
 import httpMock.CustomHttpRequest;
 import httpMock.CustomHttpResponse;
+import httpMock.constants.ContentType;
 import httpMock.constants.HttpMethod;
+import httpMock.constants.StatusCode;
 import model.Session;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import repository.UserRepo;
 import service.SessionService;
+import service.StaticFileService;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class UserAccountController implements RequestController {
     private static final Logger logger = LoggerFactory.getLogger(UserAccountController.class);
@@ -25,6 +30,7 @@ public class UserAccountController implements RequestController {
         put("/user/create", (req) -> makeAccount(req));
         put("/user/login", (req) -> loginAccount(req));
         put("/user/logout", (req) -> logoutAccount(req));
+        put("/user/list", (req)-> userList(req));
     }};
 
 
@@ -65,9 +71,12 @@ public class UserAccountController implements RequestController {
         if(!UserValidation.isUserCreationFormValid(userId, password, name, email))
             return CustomHttpFactory.BAD_REQUEST("email, password, name을 올바르게 입력해 주세요.");
 
-        UserRepo.addUser(new User(userId, password, name, email));
-
-        return CustomHttpFactory.REDIRECT("/index.html");
+        User user = new User(userId, password, name, email);
+        UserRepo.addUser(user);
+        CustomHttpResponse res = CustomHttpFactory.REDIRECT("/index.html");
+        Session sess = SessionService.addUserToSession(user);
+        res.addToCookie(sess.toString());
+        return res;
     }
 
     public CustomHttpResponse loginAccount(CustomHttpRequest req) {
@@ -102,5 +111,34 @@ public class UserAccountController implements RequestController {
         CustomHttpResponse res = CustomHttpFactory.REDIRECT("/user/login.html");
         res.addToCookie(expired.toString());
         return res;
+    }
+
+
+    public CustomHttpResponse userList(CustomHttpRequest req){
+        Set<HttpMethod> allowedMethods = Set.of(HttpMethod.GET);
+        if (!allowedMethods.contains(req.getHttpMethod()))
+            return CustomHttpFactory.METHOD_NOT_ALLOWED();
+
+        if(!SessionService.isValidSSID(req.getSSID())){
+            return CustomHttpFactory.REDIRECT("/user/login.html");
+        }
+
+        File file = StaticFileService.getFile("/user/list.html");
+        try{
+            List<User> userList = new ArrayList<>(UserRepo.findAll());
+            String listPageStr = StaticFileService.renderFile(file, new HashMap<>() {{
+                put("userTable", HtmlMakerUtility.userListRows(userList));
+            }});
+            return CustomHttpResponse.of(
+                    StatusCode.OK,
+                    ContentType.TEXT_HTML,
+                    CustomHttpResponse.EMPTY_HEADER,
+                    listPageStr.getBytes()
+                    );
+
+        } catch (IOException e) {
+            logger.error("error while reading {}", file.getPath());
+            return CustomHttpFactory.INTERNAL_ERROR("error while readling " + file.getPath());
+        }
     }
 }
