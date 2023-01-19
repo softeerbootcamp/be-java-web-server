56825483b3115d7a032c9d94932d72cd4f72bf07
Soomin-Lim <augustth511@gmail.com>
2023-01-19 10:45:37 +0900

    feat: http://localhost:8080 으로 들어오면 index.html 파일을 보여주도록 수정
    - HomeLoginView.render()를 호출할 때 URL도 인자로 전달

--- src/main/java/Controller/DynamicResourceController.java
+++ src/main/java/Controller/DynamicResourceController.java
@@ -4,12 +4,15 @@ import db.Database;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import view.HomeLoginView;
 import webserver.session.Session;
 import webserver.session.SessionConst;
 
 public class DynamicResourceController implements Controller {
 
+    private static final Logger logger = LoggerFactory.getLogger(DynamicResourceController.class);
     private static final DynamicResourceController instance = new DynamicResourceController();
 
     public static DynamicResourceController getInstance() {
@@ -27,12 +30,17 @@ public class DynamicResourceController implements Controller {
             return;
         }
 
+        String url = request.getUrl();
+        if (url.equals("/")) {
+            url = "/index.html";
+        }
+
         Session session = request.getSession();
         String userId = session.getAttribute(SessionConst.USER_ID);
 
         User loginUser = Database.findUserById(userId);
 
-        String body = HomeLoginView.render(loginUser.getName(), request.getUrl());
+        String body = HomeLoginView.render(loginUser.getName(), url);
 
         response.ok(request);
         response.setBodyMessage(body);
