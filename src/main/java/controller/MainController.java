10eba9e6a43d4d582d6f92e5d6658f26a1024ac3
wonju-dev <wonjulee.dev@gmail.com>
2023-01-19 15:44:07 +0900

    refactor: create dynamic html

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -2,20 +2,27 @@ package controller;
 
 import filesystem.FileSystem;
 import filesystem.FindResource;
-import filesystem.PathResolver;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.AuthService;
+import service.UserService;
 
-import static filesystem.PathResolver.authenticatedUrl;
-import static filesystem.PathResolver.indexRequestUrl;
+import java.util.List;
+
+import static filesystem.PathResolver.DOMAIN;
+import static filesystem.PathResolver.INDEX_HTML;
 
 public class MainController implements Controller {
 
+    public static final List<String> optionalAuthUrls = List.of("/", DOMAIN + INDEX_HTML);
+    public static final List<String> mustNotAuthUrls = List.of("/user/login.html", "/user/form.html");
+    public static final List<String> mustAuthUrls = List.of("/user/list.html");
+
     private final Logger logger = LoggerFactory.getLogger(MainController.class);
     private final AuthService authService = new AuthService();
+    private final UserService userService = new UserService();
 
 
     @Override
@@ -28,17 +35,25 @@ public class MainController implements Controller {
 
     private FindResource getResource(HttpRequest request) {
         // todo: 리팩토링,,,,
-        if (indexRequestUrl.contains(request.getUrl())) {
+        if (mustNotAuthUrls.contains(request.getUrl())) {
             if (authService.isAuthenticated(request)) {
-                return FileSystem.getIndexPage(request);
+                return FileSystem.getStaticResource(INDEX_HTML);
+            }
+            return FileSystem.getStaticResource(request.getUrl());
+        }
+
+        if (mustAuthUrls.contains(request.getUrl())) {
+            if (authService.isAuthenticated(request)) {
+                return FileSystem.getPersonalizedResource(request.getUrl(), HtmlGenerator.getUserListLi(userService.getAllUser()));
+            }
+            return null;
+        }
+
+        if (optionalAuthUrls.contains(request.getUrl())) {
+            if (authService.isAuthenticated(request)) {
+                return FileSystem.getPersonalizedResource(request.getUrl(), HtmlGenerator.getUserAnchor(authService.getSession(request).getUser()));
             }
         }
-        if (authenticatedUrl.contains(request.getUrl())) {
-            if (authService.isAuthenticated(request)) {
-                return FileSystem.getUserListPage(request);
-            }
-            return new FindResource(PathResolver.LOGIN_HTML, FileSystem.readFile(PathResolver.parse(PathResolver.LOGIN_HTML)));
-        }
-        return FileSystem.findStaticResource(request);
+        return FileSystem.getStaticResource(request.getUrl());
     }
 }
