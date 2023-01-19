08e9526ac053e7808f68e323fb29f8e7058c6a4f
pjs0418 <tony180322@gmail.com>
2023-01-19 11:59:42 +0900

    NotFoundController에서 Response 객체 리턴하는 로직 수정

--- src/main/java/controller/NotFoundController.java
+++ src/main/java/controller/NotFoundController.java
@@ -3,10 +3,11 @@ package controller;
 import model.general.Status;
 import model.request.Request;
 import model.response.Response;
+import model.response.StatusLine;
 
 public class NotFoundController implements Controller {
     @Override
     public Response getResponse(Request request) {
-        return Response.of(request, Status.NOT_FOUND);
+        return Response.from(StatusLine.of(request.getRequestLine().getHttpVersion(), Status.NOT_FOUND));
     }
 }
