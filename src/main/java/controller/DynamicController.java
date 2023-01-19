08f03bc347ce62c85a247c0245315eedcc9ced67
jun-min1623 <junmin1623@naver.com>
2023-01-19 10:48:27 +0900

    feat : 동적 html 관리를 위한 Dynamic Controller 기능 구현

--- src/main/java/controller/DynamicController.java
+++ src/main/java/controller/DynamicController.java
@@ -4,7 +4,27 @@ public class DynamicController {
     // byte 를 받아와서 byte 반환하는 형태가 괜찮은가?
     public static byte[] runDynamicFactory(byte[] body){
         byte[] manualBody;
-        manualBody = new byte[0];
+        String stringBody;
+        // 1. body를
+        stringBody = byteArrayToHex(body);
+        stringBody = stringBody.replace("<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>","");
+        manualBody = decodeWithHex(stringBody);
         return manualBody;
+    }
+    public static String byteArrayToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (final byte b : bytes) {
+            sb.append(String.format("%02X", b & 0xff));
+        }
+        return sb.toString();
+    }
+    public static byte[] decodeWithHex(String hexStr) {
+        int len = hexStr.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hexStr.charAt(i), 16) << 4)
+                    + Character.digit(hexStr.charAt(i + 1), 16));
+        }
+        return data;
     }
 }
