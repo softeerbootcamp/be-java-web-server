426462ca2b9cdef452da5d54765c0ec6ca8e8703
rentalhub-mac78 <kksy2423@naver.com>
2023-01-19 14:27:12 +0900

    Feat: 로그인시에는 사용자 이름, 비로그인시에는 로그인/회원가입 태그 렌더링

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -2,6 +2,8 @@ package controller;
 
 import controller.annotation.ControllerInfo;
 import controller.annotation.ControllerMethodInfo;
+import db.SessionDatabase;
+import model.Session;
 import reader.fileReader.FileReader;
 import reader.fileReader.TemplatesFileReader;
 import request.HttpRequest;
@@ -9,6 +11,7 @@ import request.RequestDataType;
 import response.Data;
 import response.HttpResponse;
 import service.SessionService;
+import util.Cookie;
 import util.FileType;
 import util.HttpMethod;
 import util.HttpStatus;
@@ -22,26 +25,31 @@ public class HomeController implements Controller {
     private FileReader fileReader;
 
     private SessionService sessionService = new SessionService();
+    private SessionDatabase sessionDatabase = new SessionDatabase();
 
     private HomeRender homeRender = HomeRender.getInstance();
 
     @ControllerMethodInfo(path = "/index.html", type = RequestDataType.TEMPLATES_FILE, method = HttpMethod.GET)
     public HttpResponse home(DataOutputStream dataOutputStream, HttpRequest httpRequest) throws IOException {
-
-
         fileReader = new TemplatesFileReader();
-        byte[] data = new byte[0];
+        byte[] data ;
         data = fileReader.readFile(httpRequest.getUrl());
-
-        //로그인 확인
-        if (!sessionService.cookieValidInSession(httpRequest.getRequestHeader())) {
-            data = homeRender.addSignInAndUpTag(new String(data));
-        }
-
+        data = updateHtmlUsingSession(httpRequest, data);
         return new HttpResponse.Builder()
                 .setData(new Data(dataOutputStream,data))
                 .setFileType(FileType.HTML)
                 .setHttpStatus(HttpStatus.OK)
                 .build();
+    }
+
+    private byte[] updateHtmlUsingSession(HttpRequest httpRequest, byte[] htmlData) {
+        if (sessionService.cookieValidInSession(httpRequest.getRequestHeader())) {
+            Cookie cookie = Cookie.extractCookie(httpRequest.getRequestHeader()).get();
+            Session session = sessionDatabase.findObjectById(cookie.getValue()).get();
+            htmlData = homeRender.addUserName(htmlData,session.getUser().getName());
+        }else{
+            htmlData = homeRender.addSignInAndUpTag(htmlData);
+        }
+        return htmlData;
     }
 }
