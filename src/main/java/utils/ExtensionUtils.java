2d8f74e8b51e1a64da89f924e83439797cb90ca0
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-19 15:53:41 +0900

    확장자만 뽑아내는 함수 Util로 분리

--- src/main/java/utils/ExtensionUtils.java
+++ src/main/java/utils/ExtensionUtils.java
@@ -1,2 +1,16 @@
-package utils;public class ExtensionUtils {
+package utils;
+
+import webserver.ContentType;
+
+public class ExtensionUtils {
+
+    private static final String MESSAGE_UNSUPPORTED_EXTENSION = "지원되지 않는 확장자 입니다.";
+    private static final String EXTENSION_DELIMITER = "\\.";
+
+    public static ContentType extractExtension(String url) {
+        String[] tokens = url.split(EXTENSION_DELIMITER);
+        return ContentType.fromExtension(tokens[tokens.length - 1])
+                .orElseThrow(() -> new IllegalArgumentException(MESSAGE_UNSUPPORTED_EXTENSION));
+    }
 }
+
