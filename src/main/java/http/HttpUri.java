ec6ad38af135ae22a2ec1679a624d28a1636f871
jhchoi57 <enough1125@gmail.com>
2023-01-19 16:34:13 +0900

    REFACTOR : HttpUri 쓰지 않는 함수 삭제

--- src/main/java/http/HttpUri.java
+++ src/main/java/http/HttpUri.java
@@ -15,12 +15,6 @@ public class HttpUri {
         return uri;
     }
 
-    // uri에 . 이 있으면 정적 파일을 주면 되지 않을까?
-    // 다른 방법으로 판단 해야 할듯 > 고민중
-    public boolean isStaticUri() {
-        return uri.contains(".");
-    }
-
     public String getFileNameExtension() {
         return uri.substring(uri.lastIndexOf(".") + 1);
     }
