9be7539353f6f842303b67ec72897640b35fda35
River-Mt <rivermt1998@gmail.com>
2023-01-19 15:41:46 +0900

    :sparkles: 로그인 했을 시 로그인 버튼을 없애고 유저이름이 보이도록 하는 기능 추가

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -3,6 +3,10 @@ package controller;
 import http.ContentType;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.SessionService;
+import service.UserService;
 
 import java.io.IOException;
 
@@ -11,19 +15,38 @@ import static utils.FileIoUtils.loadFile;
 public class IndexController extends AbstractController {
 
     private static final String INDEX_PATH = "/index.html";
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-    public IndexController() {
+
+    private final SessionService sessionService;
+    private final UserService userService;
+
+    public IndexController(SessionService sessionService, UserService userService) {
+        this.sessionService = sessionService;
+        this.userService = userService;
     }
 
     @Override
     public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
-        String path = INDEX_PATH;
-        ContentType contentType = ContentType.from(path);
-        String filePath = contentType.getDirectory() + path;
+        ContentType contentType = ContentType.from(INDEX_PATH);
+        String filePath = contentType.getDirectory() + INDEX_PATH;
 
-        byte[] body = loadFile(filePath);
+        try {
+            String sessionId = httpRequest.getSessionId();
+            logger.info(sessionId);
 
-        httpResponse.forward(contentType, body);
+            sessionService.validateHasSession(sessionId);
+            logger.info("check");
+
+            logger.info(sessionService.getUserId(sessionId));
+            byte[] body = userService.makeUserNameIndexBody(sessionService.getUserId(sessionId), filePath);
+            httpResponse.forward(contentType, body);
+
+        } catch (NullPointerException e) {
+            byte[] body = loadFile(filePath);
+
+            httpResponse.forward(contentType, body);
+        }
     }
 
 }
