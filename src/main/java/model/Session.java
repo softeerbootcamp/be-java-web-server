6742c9ba14cff86a8eebb1b1463c6e9b4c32bbd8
River-Mt <rivermt1998@gmail.com>
2023-01-19 15:06:45 +0900

    :recycle: Session 리팩토링

--- src/main/java/model/Session.java
+++ src/main/java/model/Session.java
@@ -1,40 +1,20 @@
 package model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Session {
 
-    private final String id;
-    private final Map<String, Object> attributes;
+    private final String sessionId;
+    private final String userId;
 
-    public Session(String id) {
-        this.id = id;
-        this.attributes = new HashMap<>();
+    public Session(String sessionId, String userId) {
+        this.sessionId = sessionId;
+        this.userId = userId;
     }
 
-    public void setAttribute(String name, Object value) {
-        attributes.put(name, value);
+    public String getSessionId() {
+        return sessionId;
     }
 
-    public Object getAttribute(String name) {
-        return attributes.get(name);
+    public String getUserId() {
+        return userId;
     }
-
-    public void removeAttribute(String name) {
-        attributes.remove(name);
-    }
-
-    public void invalidate() {
-        attributes.clear();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public Map<String, Object> getAttributes() {
-        return attributes;
-    }
-
 }
