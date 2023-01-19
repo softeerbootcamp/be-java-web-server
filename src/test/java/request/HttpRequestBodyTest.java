ed0a78e81e7a804a15dfad062636b28cbe049c3b
Toa <ld980105@snu.ac.kr>
2023-01-19 18:37:17 +0900

    :white_check_mark: HttpRequestBody 테스트 수정

--- src/test/java/request/HttpRequestBodyTest.java
+++ src/test/java/request/HttpRequestBodyTest.java
@@ -4,14 +4,15 @@ import http.request.HttpRequestBody;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class HttpRequestBodyTest {
     @Test
     @DisplayName("RequestBody 생성")
     void test_RequestBody_create() {
-        HttpRequestBody httpRequestBody = HttpRequestBody.from("hello");
-
-        assertThat(httpRequestBody.toString()).contains("hello");
+        HttpRequestBody httpRequestBody = HttpRequestBody.from("sid=1234&hello=1234");
+        assertThat(httpRequestBody.getParams()).isEqualTo(Map.of("sid", "1234", "hello", "1234"));
     }
 }
