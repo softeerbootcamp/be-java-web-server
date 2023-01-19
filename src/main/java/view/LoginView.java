8609dda1ba3d87f267d01ba055c5827bfc4a2ca3
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-19 15:55:27 +0900

    q 각 경로에 해당되는 파일에 대해 뷰 응답으로 생성"

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,2 +1,19 @@
-package view;public class LoginView {
+package view;
+
+import utils.FileIoUtils;
+import webserver.ContentType;
+import webserver.HttpRequest;
+import webserver.HttpResponse;
+import webserver.HttpStatus;
+
+public class LoginView implements View{
+    private static final String CONTENT_LENGTH_HEADER_KEY = "Content-Length";
+    @Override
+    public void render(HttpRequest request, HttpResponse response, Model data) {
+        response.setStatus(HttpStatus.OK);
+        response.setContentType(ContentType.HTML);
+        byte[] body = FileIoUtils.loadFileFromClasspath("./templates/user/login.html");
+        response.addToHeader(CONTENT_LENGTH_HEADER_KEY, String.valueOf(body.length));
+        response.setBody(body);//body에는 요청한 파일 내용이 들어감
+    }
 }
--- src/main/java/view/SignUpView.java
+++ src/main/java/view/SignUpView.java
@@ -1,2 +1,17 @@
-package view;public class SignUpView {
+package view;
+
+import model.User;
+import utils.FileIoUtils;
+import webserver.*;
+
+public class SignUpView implements View{
+    private static final String CONTENT_LENGTH_HEADER_KEY = "Content-Length";
+    @Override
+    public void render(HttpRequest request, HttpResponse response, Model data) {
+        response.setStatus(HttpStatus.OK);
+        response.setContentType(ContentType.HTML);
+        byte[] body = FileIoUtils.loadFileFromClasspath("./templates/user/form.html");
+        response.addToHeader(CONTENT_LENGTH_HEADER_KEY, String.valueOf(body.length));
+        response.setBody(body);//body에는 요청한 파일 내용이 들어감
+    }
 }
