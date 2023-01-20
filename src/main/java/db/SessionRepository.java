d2b0ae5a49a6d50a29524057dc6de842dc48f621
River-Mt <rivermt1998@gmail.com>
2023-01-20 10:44:28 +0900

    :adhesive_bandage: new HashMap<>() 을 Maps.newHaspMap()으로 수정

--- src/main/java/db/SessionRepository.java
+++ src/main/java/db/SessionRepository.java
@@ -1,13 +1,13 @@
 package db;
 
+import com.google.common.collect.Maps;
 import model.Session;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class SessionRepository {
 
-    private final Map<String, Session> sessions = new HashMap<>();
+    private final Map<String, Session> sessions = Maps.newHashMap();
 
     public void addSession(String id, Session session) {
         sessions.put(id, session);
