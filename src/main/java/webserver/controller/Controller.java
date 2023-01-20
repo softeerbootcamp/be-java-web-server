0a2f38f842fb532fd8e8bf860974aefe7058b15c
i5046821854 <o5046821854@gmail.com>
2023-01-20 11:28:20 +0900

    refactor : ToDo 삭제

--- src/main/java/webserver/controller/Controller.java
+++ src/main/java/webserver/controller/Controller.java
@@ -1,15 +1,12 @@
 package webserver.controller;
 
-import webserver.domain.request.URI;
+import webserver.domain.ModelAndView;
 import webserver.domain.request.Request;
 import webserver.domain.response.Response;
 import webserver.exception.HttpRequestException;
-
 import java.io.IOException;
-import java.util.Map;
 
 public interface Controller {
 
-    //TODO : modelAndView를 반환하도록
-    void chain(Request req, Response res) throws HttpRequestException, IOException;
+    void chain(Request req, Response res, ModelAndView mv) throws HttpRequestException, IOException;
 }
