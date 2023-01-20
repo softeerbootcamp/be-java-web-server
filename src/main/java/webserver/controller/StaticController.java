82dfafaf1d5aafa0c31820e93df50dde1b63ba27
i5046821854 <o5046821854@gmail.com>
2023-01-20 11:40:46 +0900

    feat : 컨트롤러에서 정적 리소스 파일을 읽지 않고, 이후에 view에서 읽을 수 있도록 수정

--- src/main/java/webserver/controller/StaticController.java
+++ src/main/java/webserver/controller/StaticController.java
@@ -1,10 +1,9 @@
 package webserver.controller;
 
-import webserver.domain.StatusCodes;
+import webserver.domain.ModelAndView;
 import webserver.domain.request.Request;
 import webserver.domain.response.Response;
 import webserver.exception.HttpRequestException;
-import webserver.utils.StaticResourceFinder;
 
 import java.io.IOException;
 
@@ -20,12 +19,9 @@ public class StaticController implements Controller {
         private static final StaticController INSTANCE = new StaticController();
     }
     @Override
-    public void chain(Request req, Response res) throws HttpRequestException, IOException {
-
+    public void chain(Request req, Response res, ModelAndView mv) throws HttpRequestException, IOException {
+        ControllerInterceptor.executeController(getInstance(), req, res, mv);
         String path = req.getRequestLine().getResource().getPath();  //리소스 위치 경로
-
-        StaticResourceFinder.staticFileResolver(path).ifPresentOrElse(
-                (fileAsBytes) -> res.ok(StatusCodes.OK, fileAsBytes, StaticResourceFinder.getExtension(path)),  //if file exists
-                ()-> res.notFoundError(StatusCodes.NOT_FOUND)); //if not
+        mv.setViewPath(path);
     }
 }
