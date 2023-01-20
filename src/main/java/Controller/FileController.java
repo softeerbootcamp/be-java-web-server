72abbcc75303b56c58f5fc61041aa250ef4af2b5
jieonkim23 <kwldjs1023@gmail.com>
2023-01-20 10:13:58 +0900

    :sparkles: FileController를 fileView를 이용하는 것으로 변경

--- src/main/java/Controller/FileController.java
+++ src/main/java/Controller/FileController.java
@@ -9,31 +9,47 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import Request.StatusCode;
+import util.FileIoUtil;
 import util.HtmlBuildUtil;
 import util.HttpResponseUtil;
 import util.LoginUtil;
+import Exception.*;
+import view.FileView;
+
+import java.util.Objects;
 
 public class FileController implements Controller {
+    public static final String PATH = "";
     private final Logger logger = LoggerFactory.getLogger(FileController.class);
+    private static FileController fileController=null;
 
-    public FileController() {
-        logger.debug("select fileController");
+    public static FileController getInstance(){
+        if(Objects.isNull(fileController)){
+            fileController = new FileController();
+        }
+        return fileController;
     }
 
     @Override
     public HttpResponse createResponse(HttpRequest httpRequest) {
-        byte[] body = createBody(httpRequest);
+        try {
+            FileIoUtil.isExistFile(httpRequest.getPath());
+        } catch (NotExistFileException e) {
+            return response404(httpRequest);
+        }
+        byte[] body = FileView.getInstance().render(httpRequest);
         HttpResponse httpResponse = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.OK, httpRequest.getProtocol()))
                 .headers(new HttpResponseHeaders(httpRequest.getPath(), body.length))
                 .body(new HttpResponseBody(body));
         return httpResponse;
     }
-    private byte[] createBody(HttpRequest httpRequest) {
-        try {
-            User user = LoginUtil.checkSession(httpRequest);
-            return HtmlBuildUtil.buildHtml(httpRequest.getPath(), user);
-        } catch (NullPointerException e) {
-            return HttpResponseUtil.generateBody(httpRequest.getPath());
-        }
+
+    public HttpResponse response404(HttpRequest httpRequest) {
+        logger.debug("[response404]");
+        byte[] body = "404: 존재하지않는 파일입니다.".getBytes();
+        HttpResponse httpResponse = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.NOT_FOUND, httpRequest.getProtocol()))
+                .headers(new HttpResponseHeaders("", body.length))
+                .body(new HttpResponseBody(body));
+        return httpResponse;
     }
 }
