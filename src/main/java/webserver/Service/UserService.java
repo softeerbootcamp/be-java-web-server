ebe5eed6e382354c43ac04294557beee48ed9341
i5046821854 <o5046821854@gmail.com>
2023-01-20 11:39:07 +0900

    fix : 이메일을 URLDecoder로 감싸서 던질 수 있도록 수정

--- src/main/java/webserver/Service/UserService.java
+++ src/main/java/webserver/Service/UserService.java
@@ -6,6 +6,8 @@ import webserver.controller.UserController;
 import webserver.domain.StatusCodes;
 import webserver.exception.HttpRequestException;
 
+import java.net.URLDecoder;
+
 public class UserService {
 
     private UserService (){}
@@ -14,7 +16,7 @@ public class UserService {
         return UserService.LazyHolder.INSTANCE;
     }
 
-    private static class LazyHolder{
+    private static class LazyHolder{   //Singleton
         private static final UserService INSTANCE = new UserService();
     }
 
@@ -22,15 +24,15 @@ public class UserService {
         UserDatabase.findUserById(userId).ifPresent(m -> {
             throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('사용자가 이미 존재합니다'); history.go(-1);</script>");
         });
-        UserDatabase.addUser(new User(userId, password, name, email));
+        UserDatabase.addUser(new User(userId, password, name, URLDecoder.decode(email)));
         return userId.getBytes();
     }
 
     public String login(String userId, String password) {
         User user = UserDatabase.findUserById(userId)
-                .orElseThrow(() -> new HttpRequestException(StatusCodes.SEE_OTHER, "<script>alert('존재하지 않는 계정입니다');</script>", "/user/login_failed.html")); //wrong ID
+                .orElseThrow(() -> new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('존재하지 않는 계정입니다'); window.location.href = 'http://localhost:8080/user/login_failed.html';</script>")); //wrong ID
         if(!user.getPassword().equals(password))  //with wrong password
-            throw new HttpRequestException(StatusCodes.SEE_OTHER, "<script>alert('패스워드가 올바르지 않습니다');</script>", "/user/login_failed.html");
+            throw new HttpRequestException(StatusCodes.BAD_REQUEST, "<script>alert('패스워드가 올바르지 않습니다'); window.location.href = 'http://localhost:8080/user/login_failed.html';</script>");
         return user.getUserId(); // with valid password
     }
 }
