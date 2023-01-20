196ff31a3a7072ad6f78c84a70c241e623140a8e
jieonkim23 <kwldjs1023@gmail.com>
2023-01-20 10:21:21 +0900

    :sparkles: getInstance추가 및 파일 컨트롤러 이용하도록 변경

--- src/main/java/Controller/NonController.java
+++ src/main/java/Controller/NonController.java
@@ -7,40 +7,25 @@ import org.slf4j.LoggerFactory;
 import Request.StatusCode;
 import util.HttpResponseUtil;
 
+import java.util.Objects;
+
 
 public class NonController implements Controller {
     private final Logger logger = LoggerFactory.getLogger(NonController.class);
     public static final String INDEX_HTML = "/index.html";
-
-
-    public NonController() {
-        logger.debug("select NonController");
+    private static NonController nonController = null;
+    public static NonController getInstance(){
+        if(Objects.isNull(nonController)){
+            nonController = new NonController();
+        }
+        return nonController;
     }
-
     @Override
     public HttpResponse createResponse(HttpRequest httpRequest) {
         if (httpRequest.getPath().equals("/")) {
-            return responseRoot(httpRequest);
+            httpRequest.setPath(INDEX_HTML);
+            return FileController.getInstance().createResponse(httpRequest);
         }
-        return response404(httpRequest);
-    }
-
-    public HttpResponse response404(HttpRequest httpRequest) {
-        logger.debug("[response404]");
-        byte[] body = "404: 존재하지않는 파일입니다.".getBytes();
-        HttpResponse httpResponse = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.NOT_FOUND, httpRequest.getProtocol()))
-                .headers(new HttpResponseHeaders("", body.length))
-                .body(new HttpResponseBody(body));
-        return httpResponse;
-    }
-
-    public HttpResponse responseRoot(HttpRequest httpRequest) {
-        logger.debug("[responseRoot]");
-        byte[] body = HttpResponseUtil.generateBody(INDEX_HTML);
-        HttpResponse httpResponse = new HttpResponse().startLine(new HttpResponseStartLine(StatusCode.OK, httpRequest.getProtocol()))
-                .headers(new HttpResponseHeaders(INDEX_HTML, body.length))
-                .body(new HttpResponseBody(body));
-
-        return httpResponse;
+        return FileController.getInstance().response404(httpRequest);
     }
 }
