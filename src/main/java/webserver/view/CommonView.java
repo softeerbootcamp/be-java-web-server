526f756d78b0120ed3909bec373355c61e0f9287
i5046821854 <o5046821854@gmail.com>
2023-01-20 11:33:14 +0900

    feat : 공통 부분 이외에 동적인 랜더링이 필요없는 리소스에 대한 view를 생성

---
+++ src/main/java/webserver/view/CommonView.java
@@ -0,0 +1,41 @@
+package webserver.view;
+
+import webserver.domain.ContentType;
+import webserver.domain.ModelAndView;
+import webserver.domain.StatusCodes;
+import webserver.domain.request.Request;
+import webserver.domain.response.Response;
+import webserver.utils.StaticResourceFinder;
+
+import java.io.IOException;
+
+import static webserver.utils.StaticResourceFinder.getExtension;
+
+/**
+ * View for all non-dynamic resources
+ */
+public class CommonView implements View{
+
+    private CommonView (){}
+
+    public static CommonView getInstance(){
+        return CommonView.LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder{  //Singleton
+        private static final CommonView INSTANCE = new CommonView();
+    }
+
+    @Override
+    public void render (Request req, Response res, ModelAndView mv, String menuBar, String nameTag) throws IOException{
+        String resourceUrl = mv.getViewPath();
+        StaticResourceFinder.staticFileResolver(resourceUrl).ifPresentOrElse( fileAsBytes -> {  //if file exists
+                    String fileContent = new String(fileAsBytes);
+                    if(getExtension(resourceUrl) == ContentType.TEXT_HTML){
+                        fileContent = fileContent.replace(menuBar, "")
+                                                 .replace("<li id=\"username\"></li>", nameTag);
+                    }
+                    res.ok(StatusCodes.OK, fileContent.getBytes(), getExtension(resourceUrl));}
+                    ,()-> res.notFoundError(StatusCodes.NOT_FOUND)); //if not
+    }
+}
