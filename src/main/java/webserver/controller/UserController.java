4cb95b98e88a27079315475bac16a27def365a10
i5046821854 <o5046821854@gmail.com>
2023-01-20 11:39:50 +0900

    feat : 컨트롤러에서 실행 결과를 ModelAndView에 넣을 수 있도록 수정

--- src/main/java/webserver/controller/UserController.java
+++ src/main/java/webserver/controller/UserController.java
@@ -2,9 +2,7 @@ package webserver.controller;
 
 import db.UserDatabase;
 import model.User;
-import webserver.ControllerInterceptor;
 import webserver.Service.UserService;
-import webserver.UserListViewResolver;
 import webserver.annotation.ControllerInfo;
 import webserver.domain.ContentType;
 import webserver.domain.ModelAndView;
@@ -13,8 +11,7 @@ import webserver.domain.StatusCodes;
 import webserver.domain.request.Request;
 import webserver.domain.response.Response;
 import webserver.exception.HttpRequestException;
-import webserver.utils.HttpCookieUtils;
-
+import webserver.utils.HttpSessionUtils;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -27,51 +24,47 @@ public class UserController implements Controller {
         return UserController.LazyHolder.INSTANCE;
     }
 
-    private static class LazyHolder{
+    private static class LazyHolder{  //Singleton
         private static final UserController INSTANCE = new UserController();
     }
 
-    private UserService userService = UserService.getInstance();
+    public UserService userService = UserService.getInstance();
 
 
     @ControllerInfo(path = "/user/create", methodName = "userCreate", queryStr = {"userId", "password", "name", "email"}, method = RequestMethod.POST)
-    public void userCreate(Map<String, String> queryStrs, Response response) {
-        //TODO: authService.join의 리턴값 핸들링, javascript 문법 하드 코딩 개선
-        byte[] result = userService.addUser(queryStrs.get("userId"), queryStrs.get("password"), queryStrs.get("name"), queryStrs.get("email"));
-        response.ok(StatusCodes.OK, ("<script>alert('계정 생성이 완료되었습니다.'); window.location.href = 'http://localhost:8080/index.html';</script>").getBytes(), ContentType.TEXT_HTML);
+    public void userCreate(Map<String, String> queryStrs, Response response, ModelAndView mv) {
+        userService.addUser(queryStrs.get("userId"), queryStrs.get("password"), queryStrs.get("name"), queryStrs.get("email"));
+        mv.setViewPath("redirect:/index.html");
+        response.ok(StatusCodes.OK, ("<script>alert('계정 생성이 완료되었습니다.'); </script>").getBytes(), ContentType.TEXT_HTML);
     }
 
-    @ControllerInfo(path = "/user/login", methodName = "userLogout", queryStr = {"userId", "password"}, method = RequestMethod.POST)
-    public void userLogin(Map<String, String> queryStrs, Response response){
-        try{
-            String userId = userService.login(queryStrs.get("userId"), queryStrs.get("password"));
-            if(queryStrs.get("Cookie") == null)     //when client got no session for now, generate a new cookie and add it on response header
-                response.addCookieOnHeader(HttpCookieUtils.generateCookie(userId).toString());
-            response.redirect(StatusCodes.SEE_OTHER, ("<script>alert('로그인이 완료되었습니다.');</script>").getBytes(), ContentType.TEXT_HTML,"/index.html");
-        }catch(HttpRequestException e){
-            response.redirect(e.getErrorCode(), e.getMsg().getBytes(), ContentType.TEXT_HTML, e.getRedirectURL());
-        }
+    @ControllerInfo(path = "/user/login", methodName = "userLogin", queryStr = {"userId", "password"}, method = RequestMethod.POST)
+    public void userLogin(Map<String, String> queryStrs, Response response, ModelAndView mv) throws  HttpRequestException{
+        String userId = userService.login(queryStrs.get("userId"), queryStrs.get("password"));
+        if(queryStrs.get("Cookie") == null)     //when client got no session for now, generate a new cookie and add it on response header
+            response.addCookieOnHeader(HttpSessionUtils.generateSession(userId).toString());
+        mv.setViewPath("redirect:/index.html");
+        response.ok(StatusCodes.OK, ("<script>alert('로그인이 완료되었습니다.');</script>").getBytes(), ContentType.TEXT_HTML);
     }
 
-    @ControllerInfo(path = "/user/logout", methodName = "userLogout", method = RequestMethod.GET) //TODO : logout 요청 POST로 처리하기
-    public void userLogout(Map<String, String> queryStrs, Response response){
+    @ControllerInfo(path = "/user/logout", methodName = "userLogout", method = RequestMethod.GET)
+    public void userLogout(Map<String, String> queryStrs, Response response, ModelAndView mv){
         String sessionId = queryStrs.get("Cookie");
-        response.ok(StatusCodes.OK, ("<script>alert('로그아웃 완료되었습니다.'); window.location.href = 'http://localhost:8080/index.html';</script>").getBytes(), ContentType.TEXT_HTML);
-        HttpCookieUtils.cookieInvalidation(sessionId);
-        response.addCookieOnHeader(HttpCookieUtils.cookieInvalidation(sessionId));
+        HttpSessionUtils.cookieInvalidation(sessionId);
+        mv.setViewPath("redirect:/index.html");
+        response.ok(StatusCodes.OK, ("<script>alert('로그아웃 완료되었습니다.'); </script>").getBytes(), ContentType.TEXT_HTML);
+        response.addCookieOnHeader(HttpSessionUtils.cookieInvalidation(sessionId));
     }
 
     @ControllerInfo(path = "/user/list", methodName = "userList", method = RequestMethod.GET)
-    public void userList(Map<String, String> queryStrs, Response response) throws IOException {
-        ModelAndView mv = new ModelAndView();
+    public void userList(Map<String, String> queryStrs, Response response, ModelAndView mv) throws IOException {
         List<User> allUsers = UserDatabase.findAll();
-        mv.setViewName("/user/list.html");
+        mv.setViewPath("/user/list.html");
         mv.addViewModel("user", allUsers);
-        UserListViewResolver.makeUserListView(mv,response);   //TODO : 컨트롤러에서 뷰리졸버를 호출하는 방향이 아닌 RequestHandler에서 호출할 수 있도록
     }
 
     @Override
-    public void chain(Request req, Response res) throws HttpRequestException, IOException {
-        ControllerInterceptor.executeController(UserController.class, req, res);
+    public void chain(Request req, Response res, ModelAndView mv) throws HttpRequestException {
+        ControllerInterceptor.executeController(getInstance(), req, res, mv);
     }
 }
