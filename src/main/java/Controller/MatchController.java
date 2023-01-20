47dcbf1d739a8d405e9de939c7a4f3bcdbe84855
jieonkim23 <kwldjs1023@gmail.com>
2023-01-20 10:20:46 +0900

    :recycle: 컨트롤러 매칭 시 map을 사용하도록 수정

--- src/main/java/Controller/MatchController.java
+++ src/main/java/Controller/MatchController.java
@@ -1,36 +1,35 @@
 package Controller;
 
 import Request.HttpRequest;
-import db.SessionDb;
-import model.User;
 import util.FileIoUtil;
-import util.LoginUtil;
+import Exception.*;
 
-import java.io.DataOutputStream;
-import java.nio.file.Path;
+import java.util.Map;
 import java.util.Objects;
 
 public class MatchController {
-    public static Controller match(HttpRequest httpRequest) {
-        String requestPath = httpRequest.getPath();
-        Path path = FileIoUtil.mappingDirectoryPath(requestPath);
-        if (requestPath.startsWith("/user/list")) {
-            return new UserListController();
-        }
-        if (requestPath.startsWith("/user/login") && !requestPath.contains(".")) {
-            return new LoginController();
-        }
-        if (requestPath.startsWith("/user/create")) {
-            return new JoinController();
-        }
-        try {
-            if (path.toFile().exists()) {
-                return new FileController();
-            }
-        } catch (NullPointerException e) {
-            return new NonController();
-        }
-        return new NonController();
+    private final static Map<String, Controller> cons;
+
+    static {
+        cons = Map.of(JoinController.PATH, JoinController.getInstance(),
+                LoginController.PATH, LoginController.getInstance(),
+                UserListController.PATH, UserListController.getInstance(),
+                FileController.PATH, FileController.getInstance());
     }
 
+    public static Controller matching(HttpRequest httpRequest) {
+        String path = httpRequest.getPath();
+
+        Controller controller = cons.get(filePath(path));
+        if (Objects.isNull(controller)) {
+            return new NonController();
+        }
+        return controller;
+    }
+    private static String filePath(String path) {
+        if (path.contains(".")) {
+            return "";
+        }
+        return path;
+    }
 }
