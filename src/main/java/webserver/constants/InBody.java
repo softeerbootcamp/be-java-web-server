ee7d06bd0489744e7aa538e837fe7898e944ca04
sangho0n <tkdgns0917@gmail.com>
2023-01-23 19:16:24 +0900

    beforeReplaced_UserList 값 변경
    list.html 파일 수정에 따라 변경
    여러줄 치환 시 정상적으로 반영이 안되던 문제 해결

--- src/main/java/webserver/constants/InBody.java
+++ src/main/java/webserver/constants/InBody.java
@@ -7,21 +7,5 @@ public class InBody {
     public static String beforeReplaced_LogInButton1 = "<li><a href=\"../user/login.html\" role=\"button\">로그인</a></li>";
     public static String beforeReplaced_LogInButton2 = "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>";
 
-    public static String beforeReplaced_UserList =
-                            "<tbody>\n" +
-            "                <tr>\n" +
-            "                    <th scope=\"row\">1</th>\n" +
-            "                    <td>javajigi</td>\n" +
-            "                    <td>자바지기</td>\n" +
-            "                    <td>javajigi@sample.net</td>\n" +
-            "                    <td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n" +
-            "                </tr>\n" +
-            "                <tr>\n" +
-            "                    <th scope=\"row\">2</th>\n" +
-            "                    <td>slipp</td>\n" +
-            "                    <td>슬립</td>\n" +
-            "                    <td>slipp@sample.net</td>\n" +
-            "                    <td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n" +
-            "                </tr>\n" +
-            "                </tbody>";
+    public static String beforeReplaced_UserList = "<!-- emptyLIst -->";
 }
