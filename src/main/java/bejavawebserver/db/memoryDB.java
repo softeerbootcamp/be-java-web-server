20a1680be43abfdae2006aafd84c65c9b4a9ed40
jhchoi57 <enough1125@gmail.com>
2023-01-23 11:17:04 +0900

    FEAT : db, user model 추가 및 구현

---
+++ src/main/java/bejavawebserver/db/memoryDB.java
@@ -0,0 +1,31 @@
+package bejavawebserver.db;
+
+import bejavawebserver.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class memoryDB {
+    private static final Logger logger = LoggerFactory.getLogger(memoryDB.class);
+    private static final Map<String, User> users = new HashMap<>();
+
+    public static void addUser(User user) {
+        logger.debug("Database User input : {}", user);
+        users.put(user.getUserId(), user);
+    }
+
+    public static User findUserById(String userId) {
+        return users.get(userId);
+    }
+
+    public static Collection<User> findAll() {
+        return users.values();
+    }
+
+    public static boolean checkDuplicate(User user) {
+        return users.get(user.getUserId()) != null;
+    }
+}
--- src/main/java/bejavawebserver/service/SignUpService.java
+++ src/main/java/bejavawebserver/service/SignUpService.java
@@ -1,5 +1,11 @@
 package bejavawebserver.service;
 
-public class SignUpService {
+import bejavawebserver.db.memoryDB;
+import bejavawebserver.model.User;
 
+public class SignUpService {
+    public static void addDatabase(User user) {
+        //if(memoryDB.checkDuplicate(user)) throw new DuplicateSignUpUserException("중복된 사용자가 있습니다.");
+        memoryDB.addUser(user);
+    }
 }
