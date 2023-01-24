b4789cea288c07975d7ad70b664f880ad77027d6
i5046821854 <o5046821854@gmail.com>
2023-01-24 23:46:10 +0900

    feat : Request의 바디를 스트링으로 지정 향후 ControllerInterceptor에서 메소드의 파라미터와 Request METHOD에 맞춰어서 파싱하도록 수정

--- src/main/java/webserver/controller/ControllerInterceptor.java
+++ src/main/java/webserver/controller/ControllerInterceptor.java
@@ -1,7 +1,8 @@
 package webserver.controller;
 
-import webserver.ArgumentResolver;
+import webserver.argument.argumentResolvers.ArgumentResolver;
 import webserver.annotation.ControllerInfo;
+import webserver.argument.ArgumentResolverMapping;
 import webserver.domain.ContentType;
 import webserver.view.ModelAndView;
 import webserver.domain.RequestMethod;
@@ -12,12 +13,13 @@ import webserver.exception.HttpRequestException;
 import webserver.security.SecurityContext;
 import webserver.security.SecurityFilter;
 import webserver.utils.HttpSessionUtils;
+
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ControllerInterceptor {
@@ -28,35 +30,37 @@ public class ControllerInterceptor {
                 .collect(Collectors.toList());
     }
 
-    private static Map<String, String> checkMethodThenReturnBody(Request req, ControllerInfo controllerInfo, RequestMethod requestMethod) throws HttpRequestException {
+    private static void checkMethodThenReturnBody(ControllerInfo controllerInfo, RequestMethod requestMethod) throws HttpRequestException {
         if (requestMethod != controllerInfo.method())
             throw new HttpRequestException(StatusCodes.METHOD_NOT_ALLOWED, "<script>alert('올바르지 않은 요청입니다'); history.go(-1);</script>");
-        if (requestMethod == RequestMethod.GET)
-            return req.getRequestLine().getResource().getQueryString();
-        return req.getBody();
     }
 
-    private static void loginSessionCheck(Request req, Map<String, String> contentMap, ModelAndView mv){
+    private static void loginSessionCheck(Request req, ModelAndView mv){
         String sessionId = HttpSessionUtils.getSessionIdFromRequest(req).orElse(null);
         if (HttpSessionUtils.isSessionValid(sessionId)) {
-            contentMap.put("Cookie", sessionId);
             SecurityContext.addUser(sessionId);
+            mv.addViewModel("session-id", sessionId);
         }
-        mv.addViewModel("session-id", sessionId);
     }
 
+    private static Object[] addParamToList(Object[] paramList, Response res, ModelAndView mv) {
+        int len = paramList.length;
+        paramList = Arrays.copyOf(paramList, len + 2);
+        paramList[len]  = res;
+        paramList[len+1] = mv;
+        return paramList;
+    }
 
-    public static void executeController(Controller controller, Request req, Response res, ModelAndView mv) throws HttpRequestException{
+    public static void executeController(Controller controller, Request req, Response res, ModelAndView mv) {
         //extract all the methods in the class which include specific annotation
         Class<? extends Controller> clazz = controller.getClass();
         List<Method> methodList = extractAllMethods(clazz);
-        Map<String, String> contentMap = new HashMap<>();
 
         String reqPath = req.getRequestLine().getResource().getPath();
         mv.setViewPath(reqPath);
 
         //hand over the session to controller only if it is valid
-        loginSessionCheck(req, contentMap, mv);
+        loginSessionCheck(req, mv);
         SecurityFilter.checkAuthorization(reqPath);
 
         methodList.forEach(method -> {
@@ -64,16 +68,23 @@ public class ControllerInterceptor {
             if(controllerInfo.path().equals(reqPath)) {
                 try {
                     //check the validity of http method & parameters before executing the method
-                    contentMap.putAll(checkMethodThenReturnBody(req, controllerInfo, req.getRequestLine().getRequestMethod()));
-                    ArgumentResolver.checkParameters(contentMap, Arrays.asList(controllerInfo.queryStr()));
+                    checkMethodThenReturnBody(controllerInfo, req.getRequestLine().getRequestMethod());
+
+                    List<Parameter> collect = Arrays.stream(method.getParameters()).filter(parameter -> parameter.getType() != Response.class && parameter.getType() != ModelAndView.class).collect(Collectors.toList());
+                    ArgumentResolver argumentResolver = ArgumentResolverMapping.findArgumentResolver(req.getRequestHeader().get("Content-Type"));
+                    Object[] paramList = addParamToList(argumentResolver.checkParameters(req, collect), res, mv);
                     //invoke method
-                    method.invoke(controller, contentMap, res, mv);
+                    method.invoke(controller, paramList);
+
                 } catch (HttpRequestException e){
                     res.error(e.getErrorCode(), e.getMsg().getBytes(), ContentType.TEXT_HTML);
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                        throw (HttpRequestException) e.getCause();
+                } catch (IllegalAccessException | InvocationTargetException | IOException | NoSuchMethodException |
+                         InstantiationException ex) {
+                    HttpRequestException e = (HttpRequestException) ex.getCause();
+                    res.error(e.getErrorCode(), e.getMsg().getBytes(), ContentType.TEXT_HTML);
                 }
             }
         });
     }
+
 }
