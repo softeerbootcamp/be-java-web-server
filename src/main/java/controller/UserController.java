2430a1d1708dc584662de0bf67b967dfb9671151
pjs0418 <tony180322@gmail.com>
2023-01-25 11:44:36 +0900

    UserService 리팩토링에 따라 UserController 일부 로직 수정

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -18,10 +18,6 @@ import util.SessionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Collection;
 import java.util.Map;
 
 public class UserController implements Controller {
@@ -86,7 +82,7 @@ public class UserController implements Controller {
         RequestLine requestLine = request.getRequestLine();
 
         if(Sessions.isExistSession(request.getSessionId())) {
-            byte[] body = getUserListHtmlWhenLogin(userService.getUserList(), request);
+            byte[] body = userService.getUserListHtmlWhenLogin(request);
 
             headers = HeaderUtils.response200Header(ContentType.HTML, body.length);
 
@@ -95,36 +91,5 @@ public class UserController implements Controller {
 
         headers = HeaderUtils.responseRedirectLoginHtmlHeader();
         return Response.of(StatusLine.of(requestLine.getHttpVersion(), Status.FOUND), headers);
-    }
-
-    private byte[] getUserListHtmlWhenLogin(Collection<User> users, Request request) {
-        StringBuilder userList = new StringBuilder();
-        int row = 0;
-
-        for(User user : users) {
-            userList.append("<tr><th scope=\"row\">")
-                    .append(++row)
-                    .append("</th><td>")
-                    .append(user.getUserId()).append("</td><td>")
-                    .append(user.getName())
-                    .append("</td><td>")
-                    .append(user.getEmail())
-                    .append("</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td></tr>");
-        }
-
-        byte[] body;
-        try {
-            body = Files.readAllBytes(new File("./src/main/resources/templates/user/list.html").toPath());
-        } catch(IOException e) {
-            return new byte[0];
-        }
-
-        User user = (User) Sessions.getSession(request.getSessionId()).getSessionData().get("user");
-        String originalListHtml = new String(body);
-        String[] splitListHtml = originalListHtml.split("<tbody>");
-        String resultListHtml = splitListHtml[0] + "<tbody>" + userList + splitListHtml[1];
-        resultListHtml = resultListHtml.replace("로그인", user.getName());
-        resultListHtml = resultListHtml.replace("user/login.html", "user/profile.html");
-        return resultListHtml.getBytes();
     }
 }
