ce6125e369540f7799e45b4013a266e8dd25da2c
201724554 <pay7845@naver.com>
2023-01-26 01:06:15 +0900

    QueryExecutor 일부 적용 및 TODO 변경

--- src/main/java/db/UserDatabase.java
+++ src/main/java/db/UserDatabase.java
@@ -27,50 +27,37 @@ public class UserDatabase {
         return instance;
     }
 
-    // TODO: connection 관련 기능 분리하기
     public void addUser(User user) throws IllegalArgumentException, SQLException, NullPointerException {
-        Connection con = DatabaseConnection.getConnection();
-        try {
-            String query = "INSERT INTO Users (id, pwd, userName, email) VALUES(?, ?, ?, ?)";
-            PreparedStatement pstmt = con.prepareStatement(query);
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
-            pstmt.executeUpdate();
+        String query = "INSERT INTO Users (id, pwd, userName, email) VALUES(?, ?, ?, ?)";
+        String[] args = new String[4];
+        args[0] = user.getUserId();
+        args[1] = user.getPassword();
+        args[2] = user.getName();
+        args[3] = user.getEmail();
 
-            con.close();
-            logger.debug("user {} added", user.getUserId());
-        } finally {
-            con.close();
-        }
+        QueryExecutor.executeUpdateQuery(query, args);
     }
 
     public Optional<User> findUserById(String userId) throws SQLException, NullPointerException {
-        Connection con = DatabaseConnection.getConnection();
-        try {
-            String query = "SELECT * FROM Users WHERE id = ?";
-            PreparedStatement pstmt = con.prepareStatement(query);
-            pstmt.setString(1, userId);
-            ResultSet rs = pstmt.executeQuery();
+        String query = "SELECT * FROM Users WHERE id = ?";
+        String[] args = new String[1];
+        args[0] = userId;
 
-            if (!rs.next()) {
-                con.close();
-                return Optional.empty();
-            }
+        List<Map<String, String>> result = QueryExecutor.executeSelectQuery(query, args);
 
-            String id = rs.getString("id");
-            String pwd = rs.getString("pwd");
-            String userName = rs.getString("userName");
-            String email = rs.getString("email");
-
-            con.close();
-            return Optional.of(User.of(id, pwd, userName, email));
-        } finally {
-            con.close();
+        if(result.size() == 0) {
+            return Optional.empty();
         }
+
+        String id = result.get(0).get("id");
+        String pwd = result.get(0).get("pwd");
+        String userName = result.get(0).get("userName");
+        String email = result.get(0).get("email");
+
+        return Optional.of(User.of(id, pwd, userName, email));
     }
 
+    // TODO: 마저 적용하기
     public Optional<User> findUserByIdAndPwd(String userId, String pwd) throws SQLException, NullPointerException {
         Connection con = DatabaseConnection.getConnection();
         try {
