5a4281a4ab0114f412376ea1f55fab0d3efb29c2
pjs0418 <tony180322@gmail.com>
2023-01-25 14:52:22 +0900

    유저 전체 리스트 받아오는 메서드 구현

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,12 +2,8 @@ package service;
 
 import db.Database;
 import model.domain.User;
-import model.request.Request;
-import model.session.Sessions;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
 
@@ -31,34 +27,7 @@ public class UserService {
         return database.findUserById(userId);
     }
 
-    public byte[] getUserListHtmlWhenLogin(Request request) {
-        StringBuilder userList = new StringBuilder();
-        int row = 0;
-
-        for(User user : database.findAll()) {
-            userList.append("<tr><th scope=\"row\">")
-                    .append(++row)
-                    .append("</th><td>")
-                    .append(user.getUserId()).append("</td><td>")
-                    .append(user.getName())
-                    .append("</td><td>")
-                    .append(user.getEmail())
-                    .append("</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td></tr>");
-        }
-
-        byte[] body;
-        try {
-            body = Files.readAllBytes(new File("./src/main/resources/templates/user/list.html").toPath());
-        } catch(IOException e) {
-            return new byte[0];
-        }
-
-        User user = (User) Sessions.getSession(request.getSessionId()).getSessionData().get("user");
-        String originalListHtml = new String(body);
-        String[] splitListHtml = originalListHtml.split("<tbody>");
-        String resultListHtml = splitListHtml[0] + "<tbody>" + userList + splitListHtml[1];
-        resultListHtml = resultListHtml.replace("로그인", user.getName());
-        resultListHtml = resultListHtml.replace("user/login.html", "user/profile.html");
-        return resultListHtml.getBytes();
+    public Collection<User> getUserList() {
+        return database.findAll();
     }
 }
