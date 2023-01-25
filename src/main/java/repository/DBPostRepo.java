d9893fd771801432441d829a0a5fe39e45ba83b7
rohsik2 <rohsik2@gmail.com>
2023-01-25 16:20:01 +0900

    :sparkles: 동적 화면 구성을 위한 utility 및 method 작성

--- src/main/java/repository/DBPostRepo.java
+++ src/main/java/repository/DBPostRepo.java
@@ -1,7 +1,6 @@
 package repository;
 
 import model.Post;
-import model.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -20,7 +19,7 @@ public class DBPostRepo implements PostRepo {
 
     public Post addPost(Post post){
         String[] arguments = {"0", post.getAuthor(), post.getTitle(), post.getContents()};
-        DBConnectionManager.sendSql("insert into Post values (?, ?, ?, ?)", arguments);
+        DBConnectionManager.sendSql("insert into Post(id, author, title, contents) values (?, ?, ?, ?)", arguments);
 
         List<List<String>> result = DBConnectionManager.sendSql("SELECT LAST_INSERT_ID();", null);
         post.setPostId(Long.parseLong(result.get(0).get(0)));
@@ -28,13 +27,13 @@ public class DBPostRepo implements PostRepo {
     }
     public Optional<Post> findPostById(Long postId){
         String[] arguments = {postId.toString()};
-        List<List<String>> result = DBConnectionManager.sendSql("select id, author, title, contents from Post where id=?", arguments);
+        List<List<String>> result = DBConnectionManager.sendSql("select id, author, title, contents, created_at from Post where id=?", arguments);
         if(result.size() == 0)
             return Optional.empty();
         return Optional.of(new Post(result.get(0)));
     }
     public Collection<Post> findAll(){
-        List<List<String>> result = DBConnectionManager.sendSql("select id, author, title, contents from Post", null);
+        List<List<String>> result = DBConnectionManager.sendSql("select id, author, title, contents, created_at from Post", null);
         List<Post> posts = new ArrayList<>();
         for(List<String> row : result){
             posts.add(new Post(row));
