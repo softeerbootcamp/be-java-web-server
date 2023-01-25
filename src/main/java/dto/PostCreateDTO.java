4cc2c61de3297c4b321d2bee5642948427f40fb8
wonju-dev <wonjulee.dev@gmail.com>
2023-01-25 10:57:48 +0900

    feat(PostCraeteDTO): create dto

---
+++ src/main/java/dto/PostCreateDTO.java
@@ -0,0 +1,36 @@
+package dto;
+
+import java.util.Map;
+
+public class PostCreateDTO {
+    public static final String WRITER = "writer";
+    public static final String TITLE = "title";
+    public static final String CONTENT = "contents";
+
+    private String writer;
+    private String title;
+    private String content;
+
+    private PostCreateDTO(String writer, String title, String content) {
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static PostCreateDTO of(Map<String, String> parameters) {
+        return new PostCreateDTO(parameters.get(WRITER), parameters.get(TITLE), parameters.get(CONTENT));
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
