3b7ec1f3c0c72e24b801782d11aa3e78b00c4642
pjs0418 <tony180322@gmail.com>
2023-01-25 18:10:16 +0900

    Database의 addMemo 메서드를 호출하는 writeMemo 메서드 구현

--- src/main/java/service/MemoService.java
+++ src/main/java/service/MemoService.java
@@ -1,11 +1,18 @@
 package service;
 
 import db.Database;
+import model.domain.User;
+
+import java.util.Map;
 
 public class MemoService {
     private final Database database;
 
     public MemoService(Database database) {
         this.database = database;
+    }
+
+    public void writeMemo(User user, Map<String, String> memoInfo) {
+        database.addMemo(user.getUserId(), memoInfo.get("contents"));
     }
 }
