ab3a52db982118798b9e34a4cf1ca7f46876e0d8
pjs0418 <tony180322@gmail.com>
2023-01-25 15:29:40 +0900

    MemoController class 생성 후 작성한 메모 저장하는 로직 제외한 부분 구현

---
+++ src/main/java/controller/MemoController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import model.general.Header;
+import model.general.Method;
+import model.general.Status;
+import model.request.Request;
+import model.request.RequestLine;
+import model.response.Response;
+import model.response.StatusLine;
+import model.session.Sessions;
+import service.MemoService;
+import util.HeaderUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class MemoController implements Controller {
+    private static final Logger logger = LoggerFactory.getLogger(MemoController.class);
+
+    private final MemoService memoService;
+
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
+    }
+
+    @Override
+    public Response getResponse(Request request) {
+        RequestLine requestLine = request.getRequestLine();
+        Method requestMethod = requestLine.getMethod();
+        String requestUri = requestLine.getUri();
+
+        if(requestMethod.equals(Method.POST) &&
+                requestUri.startsWith("/memo")) return createMemoResponse(request);
+
+        return Response.from(StatusLine.of(requestLine.getHttpVersion(), Status.NOT_FOUND));
+    }
+
+    private Response createMemoResponse(Request request) {
+        Map<Header, String> headers;
+        RequestLine requestLine = request.getRequestLine();
+
+        if(Sessions.isExistSession(request.getSessionId())) {
+
+        }
+
+        headers = HeaderUtils.responseRedirectIndexHtmlHeader();
+        return Response.of(StatusLine.of(requestLine.getHttpVersion(), Status.FOUND), headers);
+    }
+}
