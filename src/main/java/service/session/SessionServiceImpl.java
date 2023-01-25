ed029df054ead0cf2484019e6347cf85338345f3
Toa <ld980105@snu.ac.kr>
2023-01-25 23:22:50 +0900

    :sparkles: SessionService 인터페이스와 구현체 구현

---
+++ src/main/java/service/session/SessionServiceImpl.java
@@ -0,0 +1,50 @@
+package service.session;
+
+import db.session.SessionDatabase;
+import model.Session;
+import model.User;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+
+public class SessionServiceImpl implements SessionService {
+    private static final long SESSION_LIFE_TIME_MS = 300000L;
+    private static SessionService sessionService;
+    private final SessionDatabase sessionDatabase;
+
+
+    private SessionServiceImpl(SessionDatabase sessionDatabase) {
+        this.sessionDatabase = sessionDatabase;
+    }
+
+    public static SessionService getInstance(SessionDatabase sessionDatabase) {
+        if (sessionService == null) sessionService = new SessionServiceImpl(sessionDatabase);
+        return sessionService;
+    }
+
+    @Override
+    public String createSession(User data) {
+        String sessionID = UUID.randomUUID().toString();
+        sessionDatabase.addSession(Session.of(sessionID, data.getUserId()));
+        return sessionID;
+    }
+
+    @Override
+    public Optional<Session> getSession(String sessionId) {
+        return Optional.ofNullable(sessionDatabase.findSessionById(sessionId));
+    }
+
+    @Override
+    public void removeSession(String sessionId) {
+        sessionDatabase.deleteSession(sessionId);
+    }
+
+    @Override
+    public void checkExpiredSessions() {
+        long currentTime = System.currentTimeMillis();
+        Collection<Session> sessions = sessionDatabase.findAll();
+        sessions.stream().filter(session -> currentTime - session.getTimeStamp() > SESSION_LIFE_TIME_MS)
+                .forEach(session -> removeSession(session.getSessionId()));
+    }
+}
