a0aecf8c162136646ab3ed7d21327ad05eb8941d
jieonkim23 <kwldjs1023@gmail.com>
2023-01-25 18:28:05 +0900

    :sparkles: IndexController를 생성하여 index로 들어오는 요청을 처리하는 컨트롤러를 구현

---
+++ src/main/java/Controller/IndexController.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import Request.HttpRequest;
+import Request.StatusCode;
+import response.HttpResponse;
+import view.IndexView;
+
+import java.util.Objects;
+
+public class IndexController implements Controller {
+    public static final String PATH = "/index";
+    private static IndexController indexController = null;
+    public static IndexController getInstance() {
+        if (Objects.isNull(indexController)) {
+            synchronized (IndexController.class) {
+                indexController = new IndexController();
+            }
+        }
+        return indexController;
+    }
+    @Override
+    public HttpResponse createResponse(HttpRequest httpRequest) {
+        byte[] body = IndexView.getInstance().render(httpRequest);
+        HttpResponse response = HttpResponse.createResponse("/index.html", StatusCode.OK, httpRequest.getProtocol());
+        response.setHttpResponseBody(body);
+        response.putHeader("Content-Length", String.valueOf(body.length));
+        return response;
+    }
+}
