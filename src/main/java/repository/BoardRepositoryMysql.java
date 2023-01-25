96e7413caace1e3803ee8dd618329d007a51016c
roopre <benny1020@naver.com>
2023-01-25 17:46:33 +0900

    feat: 게시물 데이터베이스에서 읽어오는 findAll() 메서드 구현

--- src/main/java/repository/BoardRepositoryMysql.java
+++ src/main/java/repository/BoardRepositoryMysql.java
@@ -2,11 +2,16 @@ package repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import model.Board;
+import model.User;
 
 public class BoardRepositoryMysql implements BoardRepository {
 
@@ -49,6 +54,22 @@ public class BoardRepositoryMysql implements BoardRepository {
 
 	@Override
 	public Collection<Board> findAll() {
-		return null;
+		try (Connection conn = dbManager.getConnection();
+			 PreparedStatement preparedStatement = conn.prepareStatement("select * from board");
+		) {
+			ResultSet resultSet = preparedStatement.executeQuery();
+			List<Board> boards = new ArrayList<>();
+			while (resultSet.next()) {
+				String boardContents = resultSet.getString("board_contents");
+				String boardWriter = resultSet.getString("board_writer");
+				LocalDateTime boardTime = resultSet.getTimestamp("board_time").toLocalDateTime();
+				String boardTitle = resultSet.getString("board_title");
+				Long id = resultSet.getLong("id");
+				boards.add(Board.of(id, boardContents, boardTitle, boardTime, boardWriter));
+			}
+			return boards;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
