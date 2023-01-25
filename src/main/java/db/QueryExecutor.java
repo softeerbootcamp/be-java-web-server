8140efbd590b5acbefad72ea69e1183a6600070e
201724554 <pay7845@naver.com>
2023-01-26 00:32:11 +0900

    Database 객체와 Connection을 분리하기 위한 객체

---
+++ src/main/java/db/QueryExecutor.java
@@ -0,0 +1,52 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class QueryExecutor {
+    public static ResultSet executeSelectQuery(String query, String[] args) throws SQLException, NullPointerException {
+        Connection con = DatabaseConnection.getConnection();
+        try {
+            PreparedStatement pstmt = con.prepareStatement(query);
+
+            if (args != null) {
+                int index = 1;
+                for (String arg : args) {
+                    pstmt.setString(index, arg);
+                    index++;
+                }
+                if (index != args.length) {
+                    throw new SQLException();
+                }
+            }
+
+            return pstmt.executeQuery();
+        } finally {
+            con.close();
+        }
+    }
+
+    public static void executeUpdateQuery(String query, String[] args) throws SQLException, NullPointerException {
+        Connection con = DatabaseConnection.getConnection();
+        try {
+            PreparedStatement pstmt = con.prepareStatement(query);
+
+            if (args != null) {
+                int index = 1;
+                for (String arg : args) {
+                    pstmt.setString(index, arg);
+                    index++;
+                }
+                if (index != args.length) {
+                    throw new SQLException();
+                }
+            }
+
+            pstmt.executeUpdate();
+        } finally {
+            con.close();
+        }
+    }
+}
