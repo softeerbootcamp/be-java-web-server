769074f289d45363788ca01899a4ea1e8ec9e896
jieonkim23 <kwldjs1023@gmail.com>
2023-01-25 11:37:17 +0900

    :recycle: getContentLength 메서드 추가

--- src/main/java/Request/HttpRequestHeaders.java
+++ src/main/java/Request/HttpRequestHeaders.java
@@ -8,14 +8,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequestHeaders {
-    private Map<String, String> headers = new HashMap<>();
+    private Map<String, String> headers;
 
     public HttpRequestHeaders(Map<String, String> headers) {
         this.headers = headers;
     }
 
     public static HttpRequestHeaders from(BufferedReader br) {
-        Map<String, String> headers = null;
+        Map<String, String> headers = new HashMap<>();
+
         try {
             headers = HttpRequestUtil.extractHeaders(br);
         } catch (IOException e) {
@@ -26,5 +27,12 @@ public class HttpRequestHeaders {
 
     public Map<String, String> getHeaders() {
         return headers;
+    }
+
+    public int getContentLength() {
+        if(headers.get("Content-Length") == null) {
+            return 0;
+        }
+        return Integer.parseInt(headers.get("Content-Length"));
     }
 }
