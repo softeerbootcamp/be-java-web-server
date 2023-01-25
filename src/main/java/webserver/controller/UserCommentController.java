794671ec6e266c470ad3d5c733cc9b10161b63b1
psm9718 <psm9718@gmaill.com>
2023-01-25 16:33:15 +0900

    feat: index.html 한줄 메모장 표시
    로직은 구현되어 있지 않고 한줄 메모장으로 글 작성을 위해 프론트 단에서 필요한 작업들을 수행함. index.html에서 작성하기 누르면 comment/form.html로 이동, 내용 작성하면 보여지도록 하고 로직은 구현 예정.

---
+++ src/main/java/webserver/controller/UserCommentController.java
@@ -0,0 +1,12 @@
+package webserver.controller;
+
+import model.request.Request;
+import model.response.Response;
+
+public class UserCommentController implements UserController {
+
+    @Override
+    public Response service(Request request) {
+        return null;
+    }
+}
