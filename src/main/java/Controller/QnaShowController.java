c86d5702d82a769a477bfe2584a1737a6465918b
jieonkim23 <kwldjs1023@gmail.com>
2023-01-25 18:33:29 +0900

    :sparkles: QnaShowController로 qna/show의 요청을 처리하도록 구현

---
+++ src/main/java/Controller/QnaShowController.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import Request.HttpRequest;
+import Request.StatusCode;
+import repository.QnaRepository;
+import response.HttpResponse;
+import view.QnaShowView;
+
+import java.util.Objects;
+
+public class QnaShowController implements Controller {
+    public static final String PATH = "/qna/show";
+    private static QnaShowController qnaShowController = null;
+
+    public static QnaShowController getInstance() {
+        if (Objects.isNull(qnaShowController)) {
+            synchronized (QnaShowController.class) {
+                qnaShowController = new QnaShowController();
+            }
+        }
+        return qnaShowController;
+    }
+
+    @Override
+    public HttpResponse createResponse(HttpRequest httpRequest) {
+        byte[] body = QnaShowView.getInstance().render(httpRequest);
+        HttpResponse response = HttpResponse.createResponse("/qna/show.html", StatusCode.OK, httpRequest.getProtocol());
+        response.setHttpResponseBody(body);
+        response.putHeader("Content-Length", String.valueOf(body.length));
+        return response;
+    }
+}
