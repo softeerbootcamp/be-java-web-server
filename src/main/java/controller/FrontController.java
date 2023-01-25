e7256ca2d4dc97dc86e7abad5862d45d09faad60
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-25 10:48:22 +0900

    qna 폼에 대한 POST 요청 파싱하여 확인
    post요청의 body 부분 파싱하여 데이터 분리함

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -16,11 +16,13 @@ import java.net.URL;
 import java.util.Map;
 
 public class FrontController {
+
+    //TODO 현재 컨트롤러 별로 doGet빠진 부분 추가하고, user/list 제외하고 나머지 경로들 형식 통일하기
     private static final Map<String,Controller> controllers;
     private static final String SIGN_UP_PATH_URL = "/user/create";
     private static final String LOGIN_PATH_URL ="/user/login";
     private static final String USER_LIST_PATH_URL ="/user/list";
-    private static final String GENERAL_FILE_PATH ="/file";
+    private static final String QNA_FORM_PATH ="/qna/form.html";
 
     //객체 캐싱
     static{
@@ -29,11 +31,12 @@ public class FrontController {
         LoginController loginInController = new LoginController();
         UserListController userListController = new UserListController();
         FileController fileController = new FileController();
+        QnaController qnaController = new QnaController();
         controllers = Map.of(
                 SIGN_UP_PATH_URL,signUpController,
                 LOGIN_PATH_URL,loginInController,
                 USER_LIST_PATH_URL,userListController,
-                GENERAL_FILE_PATH,fileController
+                QNA_FORM_PATH,qnaController
         );
     }
 
@@ -50,7 +53,9 @@ public class FrontController {
         try{
             String url = request.getUrl();
             Model model = new Model();
+            //해당 요청에 대해 찾은 컨트롤러에서 doGet, doPost로 분기되어 처리됨
             Controller controller = controllers.get(url);
+            // 인덱스 파일에서 수많은 파일들 요청에 대해서 처리하기 위해 별도로 만든 것
             if(controller == null){
                 controller = new FileController();
             }
