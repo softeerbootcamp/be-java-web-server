aa9965eabab7383c65decdfcea62693878f39503
wonju-dev <wonjulee.dev@gmail.com>
2023-01-25 11:41:34 +0900

    feat(Post): add field

--- src/main/java/model/Post.java
+++ src/main/java/model/Post.java
@@ -1,13 +1,29 @@
 package model;
 
-public class Post {
-    User user;
-    String title;
-    String Content;
+import dto.PostCreateDTO;
 
-    public Post(User user, String title, String content) {
+public class Post {
+
+    private Long id;
+    private User user;
+    private String title;
+    private String content;
+
+    public Post(User user, PostCreateDTO postCreateDTO) {
         this.user = user;
-        this.title = title;
-        Content = content;
+        this.title = postCreateDTO.getTitle();
+        this.content = postCreateDTO.getContent();
+    }
+
+    public static Post of(User user, PostCreateDTO postCreateDTO) {
+        return new Post(user, postCreateDTO);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
