cee4bf42e19dd11e56c44d5d8d1ec7dc121af5ae
ajongs <wonjong1030@naver.com>
2023-01-25 12:13:29 +0900

    test(BoardControllerTest): 게시판을 등록하는 api 테스트 코드 추가

---
+++ src/main/java/db/BoardDatabase.java
@@ -0,0 +1,14 @@
+package db;
+
+import com.google.common.collect.Maps;
+import model.Board;
+
+import java.util.Map;
+
+public class BoardDatabase {
+    public static Map<String, Board> boards = Maps.newHashMap();
+
+    public static void addBoard(Board board) {boards.put(board.getTitle(), board);}
+
+    public static Board findBoardByTitle(String title) {return boards.get(title);}
+}
