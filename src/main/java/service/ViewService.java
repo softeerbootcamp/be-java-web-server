fdbb00ad653632c3523b8b392a3885c8ade7ce58
pjs0418 <tony180322@gmail.com>
2023-01-25 12:31:53 +0900

    로그인이 되어 있을 경우 모든 HTML 파일 불러올 때 로그인 텍스트 대신 유저 이름이 보일 수 있도록 메서드 따로 추출

--- src/main/java/service/ViewService.java
+++ src/main/java/service/ViewService.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.domain.User;
+import model.request.Request;
+import model.session.Sessions;
+
 public class ViewService {
+    public byte[] makeResponseBodyWhenLoginSuccess(byte[] body, Request request) {
+        User user = (User) Sessions.getSession(request.getSessionId()).getSessionData().get("user");
+        String originalIndexHtml = new String(body);
+        String resultIndexHtml = originalIndexHtml.replace("로그인", user.getName());
+        resultIndexHtml = resultIndexHtml.replace("user/login.html", "user/profile.html");
+        return resultIndexHtml.getBytes();
+    }
 }
