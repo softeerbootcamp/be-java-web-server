adf9669e6b7aa1cc5f96a8f932eabe60fc59fc14
pjs0418 <tony180322@gmail.com>
2023-01-25 16:57:07 +0900

    기존에 Map에 저장하던 User 정보를 MySQL 연동 후 User 정보 저장하는 addUser 함수 로직 변경

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,26 +1,45 @@
 package db;
 
-import com.google.common.collect.Maps;
-
 import model.domain.User;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+
+import static model.general.Database.*;
 
 public class Database {
     private static final Logger logger = LoggerFactory.getLogger(Database.class);
 
-    private static final Map<String, User> users = Maps.newHashMap();
+    private Map<String, User> users = new HashMap<>();
 
     public void addUser(User user) {
-        users.put(user.getUserId(), user);
+        try {
+            Connection connection =
+                    DriverManager.getConnection(DB_URL.getDBInfo(), DB_USER_NAME.getDBInfo(), DB_PASSWORD.getDBInfo());
 
-        for(User testUser : users.values()) {
-            logger.debug("userId: {}, password: {}, name: {}, email: {}",
-                    testUser.getUserId(), testUser.getPassword(), testUser.getName(), testUser.getEmail());
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("insert into user(userId, password, name, email) values(?, ?, ?, ?);");
+
+            preparedStatement.setString(1, user.getUserId());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getName());
+            preparedStatement.setString(4, user.getEmail());
+
+            int changeRow = preparedStatement.executeUpdate();
+            logger.debug("변경된 row 수: {}", changeRow);
+
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
