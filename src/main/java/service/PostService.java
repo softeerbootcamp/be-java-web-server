713c64d9d358938cb0f60fd68021035e55b0dd2d
solchan98 <dev.sol0427@gmail.com>
2023-01-25 15:41:40 +0900

    feat: PostService getPosts 구현 데이터베이스에 존재하는 게시글을 모두 조회하는 로직 구현

--- src/main/java/service/PostService.java
+++ src/main/java/service/PostService.java
@@ -4,7 +4,12 @@ import db.QueryBuilder;
 import db.QueryBuilderFactory;
 import http.exception.InternalServerErrorException;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PostService {
     public void create(String writer, String title, String content) {
@@ -19,5 +24,27 @@ public class PostService {
         if (!result) {
             throw new InternalServerErrorException("서버에서 에러가 발생하였습니다.");
         }
+    }
+
+    public List<Map<String, String>> getPosts() {
+        List<Map<String, String>> posts = new ArrayList<>();
+        try(QueryBuilder qb = QueryBuilderFactory.newQueryBuilder()) {
+            ResultSet resultSet = qb.select("*").from("post").read();
+
+            while(resultSet.next()) {
+                posts.add(
+                        Map.of(
+                                "writer", resultSet.getString("writer"),
+                                "title", resultSet.getString("title"),
+                                "content", resultSet.getString("content"),
+                                "date", resultSet.getString("date")
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return posts;
     }
 }
