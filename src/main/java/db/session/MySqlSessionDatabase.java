813cf4383d1dc06549b5ad945aed5ed453cf0d18
Toa <ld980105@snu.ac.kr>
2023-01-26 08:17:36 +0900

    :sparkles: MySqlSessionDatabase 구현

--- src/main/java/db/session/MySqlSessionDatabase.java
+++ src/main/java/db/session/MySqlSessionDatabase.java
@@ -7,7 +7,9 @@ import webserver.ConnectionPool;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class MySqlSessionDatabase implements SessionDatabase {
@@ -20,16 +22,19 @@ public class MySqlSessionDatabase implements SessionDatabase {
 
     @Override
     public void addSession(Session session) {
-        try (Connection connection = connectionPool.getConnection()) {
+        try {
+            Connection connection = connectionPool.getConnection();
             String insertSQL = "INSERT INTO sessions VALUES (?, ?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertSQL);
-            preparedStatement.setString(1, session.getSessionId());
-            preparedStatement.setString(2, session.getUserId());
-            preparedStatement.setLong(3, session.getTimeStamp());
-            int count = preparedStatement.executeUpdate();
-            if (count == 1)
-                logger.debug("데이터 입력 성공");
-            connectionPool.releaseConnection(connection);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+                preparedStatement.setString(1, session.getSessionId());
+                preparedStatement.setString(2, session.getUserId());
+                preparedStatement.setLong(3, session.getTimeStamp());
+                int count = preparedStatement.executeUpdate();
+                if (count == 1)
+                    logger.debug("데이터 입력 성공");
+            } finally {
+                connectionPool.releaseConnection(connection);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -37,16 +42,67 @@ public class MySqlSessionDatabase implements SessionDatabase {
 
     @Override
     public Session findSessionById(String sessionId) {
+        try {
+            Connection connection = connectionPool.getConnection();
+            String selectSQL = "SELECT * FROM sessions WHERE session_id = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(selectSQL)) {
+                preparedStatement.setString(1, sessionId);
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        String user_id = resultSet.getString("user_id");
+                        long timestamp = resultSet.getLong("timestamp");
+                        return Session.createWithTimeStamp(sessionId, user_id, timestamp);
+                    }
+                }
+            } finally {
+                connectionPool.releaseConnection(connection);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Collection<Session> findAll() {
-        return null;
+        try {
+            Connection connection = connectionPool.getConnection();
+            Collection<Session> sessions = new ArrayList<>();
+            String selectSQL = "SELECT * FROM sessions";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(selectSQL)) {
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        String sessionId = resultSet.getString("session_id");
+                        String userId = resultSet.getString("user_id");
+                        long timestamp = resultSet.getLong("timestamp");
+                        sessions.add(Session.createWithTimeStamp(sessionId, userId, timestamp));
+                    }
+                    return sessions;
+                }
+            } finally {
+                connectionPool.releaseConnection(connection);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 
     @Override
     public void deleteSession(String sessionId) {
-
+        try {
+            Connection connection = connectionPool.getConnection();
+            String selectSQL = "DELETE FROM sessions WHERE session_id = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(selectSQL)) {
+                preparedStatement.setString(1, sessionId);
+                int count = preparedStatement.executeUpdate();
+                if (count == 1)
+                    logger.debug("데이터 삭제 성공");
+            } finally {
+                connectionPool.releaseConnection(connection);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
