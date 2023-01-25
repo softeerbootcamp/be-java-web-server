304b45dec32c6ae9e50817d0dd9af9616ab6e1b2
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-25 17:07:17 +0900

    컨트롤러를 통해 받은 아이디 값에 대한 내용을 담은 동적 페이지 렌더 함수 구현
    글 목록 조회시의 상세 페이지 구현
    db에서부터 pk값을 통해 조회한 제목,작성자, 내용을 담음

--- src/main/java/view/QnaDetailView.java
+++ src/main/java/view/QnaDetailView.java
@@ -1,2 +1,92 @@
-package view;public class QnaDetailView {
+package view;
+
+import db.DBConnection;
+import enums.ContentType;
+import enums.HttpStatus;
+import request.HttpRequest;
+import response.HttpResponse;
+import utils.FileIoUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+public class QnaDetailView implements View{
+    @Override
+    public void render(HttpRequest request, HttpResponse response, Model data) throws IOException, URISyntaxException {
+        response.setStatus(HttpStatus.OK);
+        response.setContentType(ContentType.HTML);
+        byte[] body = FileIoUtils.loadFileFromClasspath("./templates/qna/show.html");
+        String id = (String) data.getAttribute("id");
+        Map<String,String> Details = DBConnection.selectOne(id);
+        body = showDetailById(body,Details).getBytes();
+        response.addToHeader("Content-Length", String.valueOf(body.length));
+        response.setBody(body);//body에는 요청한 파일 내용이 들어감
+    }
+
+    private static String showDetailById(byte[] bytes,Map<String,String> Details){
+        StringBuilder content = new StringBuilder();
+        content.append("<div class=\"container\" id=\"main\">\n" +
+                "    <div class=\"col-md-12 col-sm-12 col-lg-12\">\n" +
+                "        <div class=\"panel panel-default\">\n" +
+                "          <header class=\"qna-header\">\n" +
+                "              <h2 class=\"qna-title\">"+Details.get("title")+"</h2>\n" +
+                "          </header>\n" +
+                "          <div class=\"content-main\">\n" +
+                "              <article class=\"article\">\n" +
+                "                  <div class=\"article-header\">\n" +
+                "                      <div class=\"article-header-thumb\">\n" +
+                "                          <img src=\"https://graph.facebook.com/v2.3/100000059371774/picture\" class=\"article-author-thumb\" alt=\"\">\n" +
+                "                      </div>\n" +
+                "                      <div class=\"article-header-text\">\n" +
+                "                          <a href=\"/users/92/kimmunsu\" class=\"article-author-name\">"+Details.get("writer")+"</a>\n" +
+                "                          <a href=\"/questions/413\" class=\"article-header-time\" title=\"퍼머링크\">\n" +
+                "                              "+Details.get("time")+"\n" +
+                "                              <i class=\"icon-link\"></i>\n" +
+                "                          </a>\n" +
+                "                      </div>\n" +
+                "                  </div>\n" +
+                "                  <div class=\"article-doc\">\n" +
+                "                      <p>"+Details.get("contents")+"</p>\n" +
+                "                  </div>\n" +
+                "                  <div class=\"article-utils\">\n" +
+                "                  </div>\n" +
+                "              </article>\n" +
+                "        </div>\n" +
+                "    </div>\n" +
+                "</div>");
+
+        String html = new String(bytes);
+        html = html.replace("<!--<div class=\"container\" id=\"main\">-->\n" +
+                "<!--    <div class=\"col-md-12 col-sm-12 col-lg-12\">-->\n" +
+                "<!--        <div class=\"panel panel-default\">-->\n" +
+                "<!--          <header class=\"qna-header\">-->\n" +
+                "<!--              <h2 class=\"qna-title\">InitializingBean implements afterPropertiesSet() 호출되지 않는 문제.</h2>-->\n" +
+                "<!--          </header>-->\n" +
+                "<!--          <div class=\"content-main\">-->\n" +
+                "<!--              <article class=\"article\">-->\n" +
+                "<!--                  <div class=\"article-header\">-->\n" +
+                "<!--                      <div class=\"article-header-thumb\">-->\n" +
+                "<!--                          <img src=\"https://graph.facebook.com/v2.3/100000059371774/picture\" class=\"article-author-thumb\" alt=\"\">-->\n" +
+                "<!--                      </div>-->\n" +
+                "<!--                      <div class=\"article-header-text\">-->\n" +
+                "<!--                          <a href=\"/users/92/kimmunsu\" class=\"article-author-name\">kimmunsu</a>-->\n" +
+                "<!--                          <a href=\"/questions/413\" class=\"article-header-time\" title=\"퍼머링크\">-->\n" +
+                "<!--                              2015-12-30 01:47-->\n" +
+                "<!--                              <i class=\"icon-link\"></i>-->\n" +
+                "<!--                          </a>-->\n" +
+                "<!--                      </div>-->\n" +
+                "<!--                  </div>-->\n" +
+                "<!--                  <div class=\"article-doc\">-->\n" +
+                "<!--                      <p>A 에 의존성을 가지는 B라는 클래스가 있습니다.</p><p>B라는 클래스는 InitializingBean 을 상속하고 afterPropertiesSet을 구현하고 있습니다.-->\n" +
+                "<!--                      서버가 가동되면서 bean들이 초기화되는 시점에 B라는 클래스의 afterPropertiesSet 메소드는</p><p>A라는 클래스의 특정 메소드인 afunc()를 호출하고 있습니다.</p>-->\n" +
+                "<!--                  </div>-->\n" +
+                "<!--                  <div class=\"article-utils\">-->\n" +
+                "<!--                  </div>-->\n" +
+                "<!--              </article>-->\n" +
+                "<!--        </div>-->\n" +
+                "<!--    </div>-->\n" +
+                "<!--</div>-->",content);
+        return html;
+    }
 }
