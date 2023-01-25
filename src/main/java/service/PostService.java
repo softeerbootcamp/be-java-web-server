96402636b05af9b71cca9a9c2722c1cd26898a2d
rohsik2 <rohsik2@gmail.com>
2023-01-25 15:50:06 +0900

    :sparkles: Post Service class for making post

--- src/main/java/service/PostService.java
+++ src/main/java/service/PostService.java
@@ -1,19 +1,26 @@
 package service;
 
+import model.Post;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import repository.DBPostRepo;
 
 import java.util.Map;
 
 public class PostService {
-    Logger logger = LoggerFactory.getLogger(PostService.class);
+    private static Logger logger = LoggerFactory.getLogger(PostService.class);
 
-    public Long createPost(Map<String, String> body) {
+    public static Long createPost(Map<String, String> body) {
+        String writer = body.get("writer");
+        String title = body.get("title");
+        String content = body.get("contents");
         logger.debug("writer : {}", body.get("writer"));
         logger.debug("title : {}", body.get("title"));
         logger.debug("contents : {}", body.get("contents"));
 
+        Post post = DBPostRepo.get().addPost(new Post(writer, title, content));
 
-        return null;
+        logger.debug("postID : {}", post.getPostId());
+        return post.getPostId();
     }
 }
