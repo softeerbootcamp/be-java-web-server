a9a0b3be64ba58ed76d20c40584f38a6c3129bb5
JeongA-Shin <jeonga@khu.ac.kr>
2023-01-25 14:03:35 +0900

    로그인 여부에 상관 없이 qna 글 목록을 불러오게 구현
    DBConnection의 selectAll()메서드를 통해 구현함

--- src/main/java/view/IndexView.java
+++ src/main/java/view/IndexView.java
@@ -10,6 +10,8 @@ import utils.FileIoUtils;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
 
 
 public class IndexView implements View{
@@ -27,10 +29,14 @@ public class IndexView implements View{
         if(isLogined(request)){ //로그인 상태인 경우 사용자 이름 표시, 로그인 버튼 없음
             String sessionKey = request.getCookie(COOKIE_SESSION_KEY);
             User user = HttpSessionManager.getSession(sessionKey).getUser();
+            List<Map<String,String>> qnas = (List<Map<String,String>>) data.getAttribute("qna_data");
             body = showUserName(user,body).getBytes();
+            body = showQuestions(body,qnas).getBytes();
         }else{ //로그인 상태가 아닐 경우 그냥 그대로
             body = showLoginButton(body).getBytes();
         }
+        List<Map<String,String>> qnas = (List<Map<String,String>>) data.getAttribute("qna_data");
+        body = showQuestions(body,qnas).getBytes();
 
         response.addToHeader(CONTENT_LENGTH_HEADER_KEY, String.valueOf(body.length));
         response.setBody(body);//body에는 요청한 파일 내용이 들어감
@@ -67,6 +73,34 @@ public class IndexView implements View{
         String html = new String(bytes);
         html = html.replace("<!--<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>-->",content);
         return html;
+    }
 
+    private static String showQuestions(byte[] bytes,List<Map<String,String>> qnas){
+        StringBuilder content = new StringBuilder();
+
+        for(Map<String,String> qna : qnas){
+            content.append("<li>\n" +
+                    "                  <div class=\"wrap\">\n" +
+                    "                      <div class=\"main\">\n" +
+                    "                          <strong class=\"subject\">\n" +
+                    "                              <a href=\"./qna/show.html\">"+qna.get("title")+"</a>\n" +
+                    "                          </strong>\n" +
+                    "                          <div class=\"auth-info\">\n" +
+                    "                              <i class=\"icon-add-comment\"></i>\n" +
+                    "                              <span class=\"time\">"+qna.get("time")+"</span>\n" +
+                    "                              <a href=\"./user/profile.html\" class=\"author\">"+qna.get("writer")+"</a>\n" +
+                    "                          </div>\n" +
+                    "                          <div class=\"reply\" title=\"댓글\">\n" +
+                    "                              <i class=\"icon-reply\"></i>\n" +
+                    "                              <span class=\"point\">"+qna.get("row_id")+"</span>\n" +
+                    "                          </div>\n" +
+                    "                      </div>\n" +
+                    "                  </div>\n" +
+                    "              </li>");
+        }
+
+        String html = new String(bytes);
+        html = html.replace("<!--please add here -->",content);
+        return html;
     }
 }
