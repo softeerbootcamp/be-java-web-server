8e2afc4754cc43bb20e50df32f224353c96c0a02
201724554 <pay7845@naver.com>
2023-01-26 01:04:50 +0900

    executeSelectQuery가 List<Map<String,String>>을 리턴하도록 변경

--- src/main/java/db/QueryExecutor.java
+++ src/main/java/db/QueryExecutor.java
@@ -4,9 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class QueryExecutor {
-    public static ResultSet executeSelectQuery(String query, String[] args) throws SQLException, NullPointerException {
+    public static List<Map<String, String>> executeSelectQuery(String query, String[] args) throws SQLException, NullPointerException {
         Connection con = DatabaseConnection.getConnection();
         try {
             PreparedStatement pstmt = con.prepareStatement(query);
@@ -17,12 +21,23 @@ public class QueryExecutor {
                     pstmt.setString(index, arg);
                     index++;
                 }
-                if (index != args.length) {
+                if (index-1 != args.length) {
                     throw new SQLException();
                 }
             }
 
-            return pstmt.executeQuery();
+            ResultSet rs = pstmt.executeQuery();
+            List<Map<String, String>> resultList = new ArrayList<>();
+            int columnCount = rs.getMetaData().getColumnCount();
+
+            while(rs.next()) {
+                Map<String, String> row = new HashMap<>();
+                for(int i = 1; i <= columnCount; i++) {
+                    row.put(rs.getMetaData().getColumnName(i), rs.getString(i));
+                }
+                resultList.add(row);
+            }
+            return resultList;
         } finally {
             con.close();
         }
