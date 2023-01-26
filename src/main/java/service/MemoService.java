4fb792d6243b57d6edbb1dd7019c85276665d559
Soomin Lim <augustth511@gmail.com>
2023-01-26 13:16:10 +0900

    feat: MemoService에서 write() 메서드 기능 구현
    - Memo 객체를 생성하고, MemoDatabase에 저장
    - Memo 객체를 생성하는 createMemo() 메서드 추가
        - 작성일자는 LocalDateTime.now()를 호출하여 현재 시간을 저장

--- src/main/java/service/MemoService.java
+++ src/main/java/service/MemoService.java
@@ -1,7 +1,12 @@
 package service;
 
+import db.MemoDatabase;
+import model.Memo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 public class MemoService {
 
@@ -15,4 +20,17 @@ public class MemoService {
     private MemoService() {
     }
 
+    public void write(Map<String, String> memoInfo) {
+        Memo memo = createMemo(memoInfo);
+        MemoDatabase.addMemo(memo);
+
+        logger.debug("Memo: {}", memo);
+    }
+
+    private Memo createMemo(Map<String, String> memoInfo) {
+        String writer = memoInfo.get("writer");
+        String content = memoInfo.get("content");
+
+        return new Memo(writer, content, LocalDateTime.now());
+    }
 }
