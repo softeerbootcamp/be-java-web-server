ef5876f2fea07b5b536874371c60e35c397e934a
Soomin Lim <augustth511@gmail.com>
2023-01-26 11:37:07 +0900

    feat: Memo의 멤버변수 수정
    - 메모의 아이디를 저장하는 memoId 추가
        - 추후 MySQL과 연동할 때 Auto increment로 메모의 아이디 값을 저장하도록 할 예정
    - 작성일자를 저장하는 date를 createdAt으로 이름 변경
    - createdAt의 타입을 LocalDateTime으로 변경
    - getter 삭제

--- src/main/java/model/Memo.java
+++ src/main/java/model/Memo.java
@@ -1,25 +1,20 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Memo {
+    private Long memoId;
     private final String userId;
     private final String content;
-    private final String date;
+    private LocalDateTime createdAt;
 
-    public Memo(String userId, String content, String date) {
+    public Memo(String userId, String content) {
         this.userId = userId;
         this.content = content;
-        this.date = date;
     }
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getDate() {
-        return date;
+    @Override
+    public String toString() {
+        return "Memo [memoId = " + memoId + ", writer=" + userId + ", content=" + content + "]";
     }
 }
