321125f68de0f635f9d1585788dfa68008ddf0f6
Soomin Lim <augustth511@gmail.com>
2023-01-26 17:13:03 +0900

    feat: 회원가입한 사용자를 외부 데이터베이스의 user 테이블에 저장하도록 쿼리를 생성하여 보내는 기능 구현

--- src/main/java/db/UserDatabase.java
+++ src/main/java/db/UserDatabase.java
@@ -3,15 +3,40 @@ package db;
 import com.google.common.collect.Maps;
 
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Map;
 
 public class UserDatabase {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserDatabase.class);
     private static Map<String, User> users = Maps.newHashMap();
 
     public static void addUser(User user) {
-        users.put(user.getUserId(), user);
+        try {
+            Connection conn = DbConnectionManager.getConnection();
+            String sql = "insert into User values(?,?,?,?)";
+            PreparedStatement psmt = conn.prepareStatement(sql);
+
+            psmt.setString(1, user.getUserId());
+            psmt.setString(2, user.getPassword());
+            psmt.setString(3, user.getName());
+            psmt.setString(4, user.getEmail());
+            psmt.executeUpdate();
+
+            psmt.close();
+            conn.close();
+
+            users.put(user.getUserId(), user);
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
     }
 
     public static User findUserById(String userId) {
