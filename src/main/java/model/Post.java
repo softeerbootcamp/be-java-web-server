8c1a56a4e70dd713edd4f98fbd92efe7d2505dd4
201724554 <pay7845@naver.com>
2023-01-27 00:26:28 +0900

    게시물 처리를 위한 model, service, db

--- src/main/java/model/Post.java
+++ src/main/java/model/Post.java
@@ -20,4 +20,24 @@ public class Post {
     public static Post of(int id, String title, String content, String userId, LocalDateTime createdTime) {
         return new Post(id, title, content, userId, createdTime);
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
 }
