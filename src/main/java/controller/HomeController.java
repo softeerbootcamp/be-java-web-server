b5d611f9497b21405bde77f39dcca7d4fc2038a5
roopre <benny1020@naver.com>
2023-01-26 17:04:58 +0900

    feat: view 구현 부분을 다른 HomeView 클래스로 책임 위임

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -10,10 +10,10 @@ import org.slf4j.LoggerFactory;
 
 import model.Board;
 import request.HttpRequest;
-import response.ContentType;
 import response.HttpResponse;
 import service.BoardService;
-import webserver.HttpStatus;
+import view.HomeView;
+import view.Model;
 
 public class HomeController extends AbstractController {
 
@@ -24,6 +24,8 @@ public class HomeController extends AbstractController {
 	private static HomeController instance;
 
 	private final BoardService boardService;
+
+	private final HomeView homeView;
 
 	public static HomeController getInstance() {
 		if (instance == null) {
@@ -36,40 +38,13 @@ public class HomeController extends AbstractController {
 
 	public HomeController() {
 		this.boardService = BoardService.getInstance();
+		this.homeView = HomeView.getInstance();
 	}
 
 	public void doGet(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+		Model model = new Model();
 		List<Board> boards = boardService.findBoards();
-		File file = new File("./webapp/index.html");
-		String page = new String(Files.readAllBytes(file.toPath()));
-		StringBuilder boardList = new StringBuilder();
-
-		for (int i = 0; i < boards.size(); i++) {
-			boardList.append("<li>\n"
-				+ "                  <div class=\"wrap\">\n"
-				+ "                      <div class=\"main\">\n"
-				+ "                          <strong class=\"subject\">\n"
-				+ "                              <a href=\"qnahow.html\">");
-			boardList.append(boards.get(i).getTitle());
-			boardList.append("</a>\n"
-				+ "                          </strong>\n"
-				+ "                          <div class=\"auth-info\">\n"
-				+ "                              <i class=\"icon-add-comment\"></i>\n"
-				+ "                              <span class=\"time\">");
-			boardList.append(boards.get(i).getTime());
-			boardList.append("</span>\n"
-				+ "                              <a href=\"userrofile.html\" class=\"author\">");
-			boardList.append(boards.get(i).getWriter());
-			boardList.append("<div class=\"reply\" title=\"댓글\">\n"
-				+ "                              <i class=\"icon-reply\"></i>\n"
-				+ "                              <span class=\"point\">");
-			boardList.append(boards.get(i).getId());
-			boardList.append("</span>\n"
-				+ "                          </div>\n"
-				+ "                      </div>\n"
-				+ "                  </div>");
-		}
-		page = page.replace("<!-- board list -->", boardList);
-		httpResponse.setHttpResponse(HttpStatus.OK, page, ContentType.HTML);
+		model.addModel("boards", boards);
+		homeView.makeView(httpRequest, httpResponse, model);
 	}
 }
