c08a52f7835b7e4d42e1ddf9250331653420163f
wonju-dev <wonjulee.dev@gmail.com>
2023-01-26 17:16:42 +0900

    refactor(AuthService): extract commont method

--- src/main/java/service/AuthService.java
+++ src/main/java/service/AuthService.java
@@ -5,31 +5,24 @@ import http.common.Cookie;
 import http.common.Session;
 import http.request.HttpRequest;
 import model.User;
-
-import java.util.Optional;
+import service.exception.NotFoundException;
 
 public class AuthService {
     public Boolean isAuthenticated(HttpRequest request) {
-        Cookie sessionCookie = request.getCookie(Session.SESSION_FIELD_NAME);
-        if (sessionCookie == null) {
+        try {
+            return getSession(request).isValid();
+        } catch (NotFoundException e) {
             return false;
         }
-        Session session = SessionDB.getSession(sessionCookie.getValue());
-        if (session == null) {
-            return false;
-        }
-        return session.isValid();
     }
 
-    public Optional<User> getUser(HttpRequest request) {
-        Cookie sessionCookie = request.getCookie(Session.SESSION_FIELD_NAME);
-        if (sessionCookie == null) {
-            return Optional.ofNullable(null);
-        }
-        Session session = SessionDB.getSession(sessionCookie.getValue());
-        if (session == null) {
-            return Optional.ofNullable(null);
-        }
-        return Optional.ofNullable(session.getUser());
+    public User getUser(HttpRequest request) {
+        return getSession(request).getUser();
+    }
+
+    private Session getSession(HttpRequest request) {
+        Cookie sessionCookie = request.getCookie(Session.SESSION_FIELD_NAME).orElseThrow(() -> new NotFoundException("sessionCookie not found"));
+        Session session = SessionDB.getSession(sessionCookie.getValue()).orElseThrow(() -> new NotFoundException("session not found"));
+        return session;
     }
 }
