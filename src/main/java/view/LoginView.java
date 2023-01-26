ee783715657772fed51a67c59a615a4ab7cb8456
Soomin Lim <augustth511@gmail.com>
2023-01-26 14:32:47 +0900

    chore: HomeLoginView 클래스를 LoginView 클래스로 이름 변경
    - 주석 변경

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -4,7 +4,7 @@ import util.FileUtils;
 
 import java.io.IOException;
 
-public class HomeLoginView {
+public class LoginView {
 
     public static String render(String name, String url) throws IOException {
         byte[] file = FileUtils.loadFile(url);
@@ -15,7 +15,7 @@ public class HomeLoginView {
         htmlFile = htmlFile.replace("<li><a href=\"../user/login.html\" role=\"button\">로그인</a></li>", userName);
         htmlFile = htmlFile.replace("<li><a href=\"../user/form.html\" role=\"button\">회원가입</a></li>", "");
 
-        htmlFile = htmlFile.replace("<!-- 메모장 글 쓰기 버튼 구현-->", "<a href=\"./memo/form.html\" class=\"btn btn-primary pull-right\" role=\"button\">글 쓰기</a>");
+        htmlFile = htmlFile.replace("<!-- 메모장 글 쓰기 버튼 구현 -->", "<a href=\"./memo/form.html\" class=\"btn btn-primary pull-right\" role=\"button\">글 쓰기</a>");
         return htmlFile;
     }
 }
