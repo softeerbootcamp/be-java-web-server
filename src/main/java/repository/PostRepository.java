ef6d42c1532f3f3188aa0200ae1b360255f15003
wonju-dev <wonjulee.dev@gmail.com>
2023-01-26 15:10:01 +0900

    feat(PostRepository): implement save post method

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -4,84 +4,91 @@ import com.google.common.collect.Lists;
 import dao.PostDAO;
 import db.DBUtil;
 import exception.DBException;
+import model.Post;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public class PostRepository {
+
+    public Post save(Post post) {
+        Connection con = null;
+        PreparedStatement pstm = null;
+        String sql = "INSERT INTO Post(uid, title, content, createdDate) VALUES(?, ?, ?, ?)";
+
+        try {
+            con = DBUtil.getConnection();
+            pstm = con.prepareStatement(sql);
+            pstm.setLong(1, post.getUser().getId());
+            pstm.setString(2, post.getTitle());
+            pstm.setString(3, post.getContent());
+            pstm.setString(4, post.getCreatedDate());
+            pstm.executeUpdate();
+        } catch (SQLException e) {
+            throw new DBException(e);
+        } finally {
+            DBUtil.close(con, pstm, null);
+        }
+
+        return post;
+    }
 
     public List<PostDAO> getAllPosts() {
         Connection con = null;
         PreparedStatement pstm = null;
         ResultSet rs = null;
+        List<PostDAO> posts = Lists.newArrayList();
         String sql = "SELECT * FROM Post";
-
         try {
             con = DBUtil.getConnection();
             pstm = con.prepareStatement(sql);
             rs = pstm.executeQuery();
-
-            List<PostDAO> posts = Lists.newArrayList();
-
             while (rs.next()) {
-                posts.add(getPostDAO(rs));
+                posts.add(PostDAO.of(
+                        rs.getLong("id"),
+                        rs.getLong("uid"),
+                        rs.getString("title"),
+                        rs.getString("content"),
+                        rs.getTimestamp("createdDate").toLocalDateTime()));
             }
-            return posts;
         } catch (SQLException e) {
             throw new DBException(e);
         } finally {
-            close(con, pstm, rs);
+            DBUtil.close(con, pstm, rs);
+            return posts;
         }
     }
 
-    private PostDAO getPostDAO(ResultSet rs) throws SQLException {
-        Long postId = rs.getLong("id");
-        Long userId = rs.getLong("uid");
-        String title = rs.getString("title");
-        String content = rs.getString("content");
-        Date createdDate = rs.getDate("createdDate");
-        return PostDAO.of(postId, userId, title, content, createdDate);
-    }
-
-    public PostDAO findById(long id) {
+    public Optional<PostDAO> findById(long id) {
         Connection con = null;
         PreparedStatement pstm = null;
         ResultSet rs = null;
+        PostDAO postDAO = null;
         String sql = "SELECT * FROM Post WHERE id = (?)";
-
         try {
             con = DBUtil.getConnection();
             pstm = con.prepareStatement(sql);
             pstm.setLong(1, id);
             rs = pstm.executeQuery();
-
             if (rs.next()) {
-                return getPostDAO(rs);
+                postDAO = PostDAO.of(
+                        rs.getLong("id"),
+                        rs.getLong("uid"),
+                        rs.getString("title"),
+                        rs.getString("content"),
+                        rs.getTimestamp("createdDate").toLocalDateTime()
+                );
             }
             throw new IllegalStateException("post not found");
         } catch (SQLException e) {
             throw new DBException(e);
         } finally {
-            close(con, pstm, rs);
-        }
-    }
-
-    private void close(Connection con, PreparedStatement pstm, ResultSet rs) {
-        try {
-            con.close();
-        } catch (SQLException e) {
-        }
-        try {
-            pstm.close();
-        } catch (SQLException e) {
-        }
-        try {
-            rs.close();
-        } catch (SQLException e) {
+            DBUtil.close(con, pstm, rs);
+            return Optional.ofNullable(postDAO);
         }
     }
 }
