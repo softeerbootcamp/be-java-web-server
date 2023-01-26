ba72e5649793f15abee899b2ce3a03e5ee3f73e1
201724554 <pay7845@naver.com>
2023-01-26 14:33:26 +0900

    게시글 모델 추가

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private int id;
+    private String title;
+    private String content;
+    private String userId;
+    private LocalDateTime createdTime;
+
+    private Post(int id, String title, String content, String userId, LocalDateTime createdTime) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.userId = userId;
+        this.createdTime = createdTime;
+    }
+
+    public static Post of(int id, String title, String content, String userId, LocalDateTime createdTime) {
+        return new Post(id, title, content, userId, createdTime);
+    }
+}
