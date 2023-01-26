93b1fa337840624685242ce13690e99cdd5c9578
Toa <ld980105@snu.ac.kr>
2023-01-26 14:21:15 +0900

    :recycle: Memo에서 memo id 삭제

--- src/main/java/model/Memo.java
+++ src/main/java/model/Memo.java
@@ -1,30 +1,27 @@
 package model;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Memo {
-    private int memoId;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     private String content;
     private String author;
     private String dateTime;
 
-    private Memo(int memoId, String content, String author, String dateTime) {
-        this.memoId = memoId;
+
+    private Memo(String content, String author, String dateTime) {
         this.content = content;
         this.author = author;
         this.dateTime = dateTime;
     }
 
-    public static Memo create(int memoId, String content, String author) {
-        return new Memo(memoId, content, author, LocalDateTime.now().toString());
+    public static Memo create(String content, String author) {
+        return new Memo(content, author, LocalDateTime.now().format(formatter));
     }
 
-    public static Memo createWithDT(int memoId, String content, String author, String dateTime) {
-        return new Memo(memoId, content, author, dateTime);
-    }
-
-    public int getMemoId() {
-        return memoId;
+    public static Memo createWithDT(String content, String author, String dateTime) {
+        return new Memo(content, author, dateTime);
     }
 
     public String getContent() {
