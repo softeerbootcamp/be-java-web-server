36a80df3e94e35f23cec03fe2dd5bde803a8f075
jieonkim23 <kwldjs1023@gmail.com>
2023-01-26 20:21:35 +0900

    :sparkles: RequestDataType을 필드로 추가하고 생성자를 그에맞게 변경

--- src/main/java/request/HttpRequestStartLine.java
+++ src/main/java/request/HttpRequestStartLine.java
@@ -10,20 +10,30 @@ public class HttpRequestStartLine {
     private String path;
     private String protocol;
     private HttpMethod method;
+    private RequestDataType type;
     private static final String BLANK = " ";
     private static final Logger logger = LoggerFactory.getLogger(HttpRequestStartLine.class);
 
-    public HttpRequestStartLine(String method, String path, String protocol) {
+    public HttpRequestStartLine(String method, String path, String protocol, RequestDataType type) {
         this.path = path;
         this.protocol = protocol;
         this.method = HttpMethod.valueOf(method);
+        this.type = type;
     }
 
     public static HttpRequestStartLine from(BufferedReader br) {
         String line = FileIoUtil.readLine(br);
         logger.debug("request : " + line);
         String[] temp = line.split(BLANK);
-        return new HttpRequestStartLine(temp[0], temp[1], temp[2]);
+
+        return new HttpRequestStartLine(temp[0], temp[1], temp[2], RequestDataType.getUrlType(temp[1]));
+    }
+    public String getMappingUrl() {
+        return type.getMappingUrl(path);
+    }
+
+    public RequestDataType getType() {
+        return type;
     }
 
     public HttpMethod getMethod() {
