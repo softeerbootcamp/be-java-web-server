87bf179c3e348c51de8a98180f5bf191ba1f224d
jieonkim23 <kwldjs1023@gmail.com>
2023-01-26 20:22:18 +0900

    :sparkles: RequestDataTyp를 이용하여 파라미터를 만들도록 로직 수정

--- src/main/java/request/HttpMethod.java
+++ src/main/java/request/HttpMethod.java
@@ -5,19 +5,25 @@ import org.slf4j.LoggerFactory;
 import util.FileIoUtil;
 
 import java.io.BufferedReader;
+import java.util.HashMap;
 
 public enum HttpMethod {
     GET {
         @Override
-        public HttpRequestParams getParamsByMethod(BufferedReader br, int length, HttpRequestStartLine startLine){
-            String query = FileIoUtil.splitQuery(startLine.getPath());
-            return HttpRequestParams.from(query);
+        public HttpRequestParams getParamsByMethod(BufferedReader br, int length, HttpRequestStartLine startLine) {
+            switch (startLine.getType()) {
+                case QUERY_STRING:
+                    return HttpRequestParams.from(FileIoUtil.getQueryString(startLine.getPath()));
+                case PATH_VARIABLE:
+                    return HttpRequestParams.from(FileIoUtil.getPathVariable(startLine.getPath()));
+            }
+            return new HttpRequestParams(new HashMap<>());
         }
 
     },
     POST {
         @Override
-        public HttpRequestParams getParamsByMethod(BufferedReader br, int length, HttpRequestStartLine startLine){
+        public HttpRequestParams getParamsByMethod(BufferedReader br, int length, HttpRequestStartLine startLine) {
             String data = FileIoUtil.readBuffer(br, length);
             return HttpRequestParams.from(data);
         }
