a31a12a8e3760c1cbda5e2d8a91efa48ac8a3a72
wonju-dev <wonjulee.dev@gmail.com>
2023-01-26 15:02:32 +0900

    feat(DBUtil): extract close method

--- src/main/java/db/DBUtil.java
+++ src/main/java/db/DBUtil.java
@@ -2,21 +2,43 @@ package db;
 
 import exception.DBException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBUtil {
 
     private static final String URL = "jdbc:mysql://localhost:3306/webserver";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "zxcasdqwe";
+    private static final String PASSWORD = "zxcasdqwe123";
 
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             throw new DBException(e);
+        }
+    }
+
+    public static void close(Connection con, PreparedStatement pstm, ResultSet rs) {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new DBException(e);
+            }
+        }
+        if (pstm != null) {
+            try {
+                pstm.close();
+            } catch (SQLException e) {
+                throw new DBException(e);
+            }
+        }
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DBException(e);
+            }
         }
     }
 }
