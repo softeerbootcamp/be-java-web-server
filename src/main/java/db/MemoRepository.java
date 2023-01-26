328d96e1bf291d9514acc08072d8e7acffa09bfb
ktykty0722 <ktykty0722@naver.com>
2023-01-26 15:05:15 +0900

    Feat : try catch로 Exception 관리하도록 변경

--- src/main/java/db/MemoRepository.java
+++ src/main/java/db/MemoRepository.java
@@ -5,60 +5,42 @@ import model.Memo;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class MemoRepository {
     private static final Database DATABASE = Database.getInstance();
 
-    public static void addMemo(Memo memo) throws Exception {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = DATABASE.getConnection();
-            String SQL = "insert into memo(username, createdAt, content) values(?, ?, ?)";
-            pstmt = conn.prepareStatement(SQL);
+    public static void addMemo(Memo memo) {
+        String SQL = "insert into memo(username, createdAt, content) values(?, ?, ?)";
+        try(Connection conn = DATABASE.getConnection(); PreparedStatement pstmt = conn.prepareStatement(SQL)) {
 
             pstmt.setString(1, memo.getUserName());
             pstmt.setString(2, memo.getCreatedAt());
             pstmt.setString(3, memo.getContent());
             pstmt.executeUpdate();
 
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (conn != null) {
-                conn.close();
-            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static Collection<Memo> findAll() throws Exception {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
+    public static Collection<Memo> findAll(){
         Collection<Memo> memos = new ArrayList<>();
-        try {
-            conn = DATABASE.getConnection();
-            String SQL = "select * from memo";
-            pstmt = conn.prepareStatement(SQL);
+        String SQL = "select * from memo";
+        try(Connection conn = DATABASE.getConnection(); PreparedStatement pstmt = conn.prepareStatement(SQL)) {
             ResultSet rs = pstmt.executeQuery();
-
             while (rs.next()) {
                 String username = rs.getString("username");
                 String createdAt = rs.getString("createdAt");
                 String content = rs.getString("content");
                 memos.add(Memo.from(username, createdAt, content));
             }
-
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (conn != null) {
-                conn.close();
-            }
+            return memos;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return memos;
     }
+
 }
