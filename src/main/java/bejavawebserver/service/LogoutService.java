0f203ca66eaf706c2f2241369c90d78e25d9b680
jhchoi57 <enough1125@gmail.com>
2023-01-26 16:47:11 +0900

    REFACTOR : 필요없는 것 삭제

--- src/main/java/bejavawebserver/service/LogoutService.java
+++ src/main/java/bejavawebserver/service/LogoutService.java
@@ -1,16 +1,11 @@
 package bejavawebserver.service;
 
-import bejavawebserver.repository.JdbcRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
 @Service
 public class LogoutService {
-    @Autowired
-    JdbcRepository jdbcRepository;
-
     public void removeSession(HttpSession session) {
         session.removeAttribute(session.getId());
     }
