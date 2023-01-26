098f25c18ebfa9938c26389b7185c549f9b823fd
psm9718 <psm9718@gmaill.com>
2023-01-26 13:26:37 +0900

    feat: 비로그인 상태 한줄 메모 기능 테스트 코드 작성
    비로그인 상태에서 한줄 메모장 작성하기 누르면 로그인 페이지로 redirect되는 것 테스트 코드 작성

--- src/test/java/controller/UserCommentControllerTest.java
+++ src/test/java/controller/UserCommentControllerTest.java
@@ -7,7 +7,6 @@ import model.User;
 import model.request.Request;
 import model.response.HttpStatusCode;
 import model.response.Response;
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -30,7 +29,7 @@ public class UserCommentControllerTest {
     }
 
     @Test
-    @DisplayName("한 줄 메모 작성")
+    @DisplayName("한 줄 메모 작성 (로그인)")
     void commentSave() throws Exception{
         //given
         User user = new User("test", "123", "tester", "test@test");
@@ -40,7 +39,7 @@ public class UserCommentControllerTest {
 
         //when
         String content = "이것은 내용입니다. 하하";
-        String body = "author=" + user.getName() + "&contents=" + content;
+        String body = "contents=" + content;
         String requestMessage = "POST /user/comment HTTP/1.1\n"
                 + "Host: localhost:8080\n"
                 + "Connection: keep-alive\n"
@@ -67,4 +66,38 @@ public class UserCommentControllerTest {
                 }
         );
     }
+
+    @Test
+    @DisplayName("한 줄 메모 작성하기 (비로그인)")
+    void commentSave_login() throws Exception{
+
+        //when
+        String content = "이것은 내용입니다. 하하";
+        String body = "contents=" + content;
+        String requestMessage = "POST /user/comment HTTP/1.1\n"
+                + "Host: localhost:8080\n"
+                + "Connection: keep-alive\n"
+                + "Content-Length: " + body.length() + "\n"
+                + "Content-Type: application/x-www-form-urlencoded\n"
+                + "Accept: */*\n\n"
+                + body;
+        InputStream inputStream = new ByteArrayInputStream(requestMessage.getBytes());
+        Request request = new Request(inputStream);
+
+        UserCommentController userCommentController = new UserCommentController();
+        Response response = userCommentController.service(request);
+
+        //then
+        SoftAssertions.assertSoftly(softAssertions -> {
+                    softAssertions.assertThat(response.getStatusLine().getHttpStatusCode()).isEqualTo(HttpStatusCode.FOUND);
+                    softAssertions.assertThat(response.getHeaders().get("Location")).isEqualTo("/user/login.html");
+                    try {
+                        softAssertions.assertThat(commentDAO.findAll().size()).isEqualTo(0);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+    }
+
 }
