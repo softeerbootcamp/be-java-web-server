0e04ec82db580bd6f5186416497e53b8b0090dd2
jieonkim23 <kwldjs1023@gmail.com>
2023-01-26 17:59:41 +0900

    :sparkles: RequestDataType을 enum으로 생성하여 분류하고 function필드를 이용하여 타입에 따라 url을 분리하는 로직을 람다식으로 처리함

--- src/main/java/request/HttpRequestParams.java
+++ src/main/java/request/HttpRequestParams.java
@@ -2,6 +2,7 @@ package request;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.FileIoUtil;
 import util.HttpRequestUtil;
 
 import java.util.HashMap;
@@ -17,8 +18,10 @@ public class HttpRequestParams {
     }
 
     public static HttpRequestParams from(String query) {
-        Map<String, String> params = HttpRequestUtil.extractParams(query);
-        return new HttpRequestParams(params);
+        if(query.contains("=")) {
+            return new HttpRequestParams(HttpRequestUtil.extractParams(query));
+        }
+        return new HttpRequestParams(Map.of("path_variable", query));
     }
 
     public Map<String, String> getParams() {
