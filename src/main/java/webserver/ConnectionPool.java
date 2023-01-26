76b5e9cbbfcf001c28230de4b3a5c44a21bbc58a
Toa <ld980105@snu.ac.kr>
2023-01-27 00:11:59 +0900

    :recycle: BlockingDeque 사용하여 동시성 문제 해결

--- src/main/java/webserver/ConnectionPool.java
+++ src/main/java/webserver/ConnectionPool.java
@@ -6,7 +6,8 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Stack;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class ConnectionPool implements Runnable {
     private final static Logger logger = LoggerFactory.getLogger(ConnectionPool.class);
@@ -14,42 +15,44 @@ public class ConnectionPool implements Runnable {
     private final String url;
     private final String user;
     private final String password;
-    private Stack<Connection> connections;
+    private BlockingDeque<Connection> connections;
 
     public ConnectionPool(int poolSize, String url, String user, String password) {
         this.poolSize = poolSize;
         this.url = url;
         this.user = user;
         this.password = password;
-        connections = new Stack<>();
+        connections = new LinkedBlockingDeque<>();
     }
 
     public void run() {
         for (int i = 0; i < poolSize; i++) {
             try {
                 Connection connection = DriverManager.getConnection(url, user, password);
-                connections.add(connection);
-            } catch (SQLException e) {
+                connections.put(connection);
+            } catch (SQLException | InterruptedException e) {
                 logger.error(e.getMessage());
             }
         }
         logger.debug("run :" + connections.size());
     }
 
-    public synchronized Connection getConnection() {
-        logger.debug(String.valueOf(connections.size()));
-        while (connections.isEmpty()) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                logger.error("Error waiting for connection" + e.getMessage());
-            }
+    public Connection getConnection() {
+        try {
+            logger.debug(String.valueOf(connections.size()));
+            return connections.take();
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+            return null;
         }
-        return connections.pop();
     }
 
     public void releaseConnection(Connection connection) {
-        connections.add(connection);
-        logger.debug(String.valueOf(connections.size()));
+        try {
+            connections.put(connection);
+            logger.debug(String.valueOf(connections.size()));
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
