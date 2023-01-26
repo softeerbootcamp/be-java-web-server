78d33e2ab75ece3b74f9ffe4e9576f8ceddd829a
rentalhub-mac78 <kksy2423@naver.com>
2023-01-26 14:18:08 +0900

    Fix: DB연결 Connect 객체 생성시 예외처리 (try,catch)

--- src/main/java/util/global/DBConnector.java
+++ src/main/java/util/global/DBConnector.java
@@ -5,25 +5,31 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.*;
 
-public class DBConnector {
+public class DBConnector{
 
     private static final Logger logger = LoggerFactory.getLogger(DBConnector.class);
     private ConfigReader configReader;
 
     private Connection connection;
 
-    public DBConnector() throws SQLException {
+    public DBConnector()  {
         configReader = ConfigReader.getInstance();
 
-       connection = DriverManager.getConnection(
-                configReader.getConfig().get(ConfigReader.KEY_DB_URL),
-                configReader.getConfig().get(ConfigReader.KEY_DB_USERNAME),
-                configReader.getConfig().get(ConfigReader.KEY_DB_PASSWORD));
+        try {
+            connection = DriverManager.getConnection(
+                     configReader.getConfig().get(ConfigReader.KEY_DB_URL),
+                     configReader.getConfig().get(ConfigReader.KEY_DB_USERNAME),
+                     configReader.getConfig().get(ConfigReader.KEY_DB_PASSWORD));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("[ERROR] DB커넥션시에 에러");
+        }
     }
 
     public Connection getConnection() {
         return connection;
     }
+
 
 
 }
